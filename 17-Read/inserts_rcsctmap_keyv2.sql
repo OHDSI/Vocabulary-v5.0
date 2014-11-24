13ba799070692833bd8a975d55cd55ae228c4581
Timur <timur.vakhitov@firstlinesoftware.com>
2014-11-24 16:40:58 +0300

    inserts into rcsctmap and keyv2

---
+++ 17-Read/inserts_rcsctmap_keyv2.sql
@@ -0,0 +1,43 @@
+INSERT INTO CONCEPT_STAGE (concept_id,
+                           concept_name,
+                           domain_id,
+                           vocabulary_id,
+                           concept_class_id,
+                           standard_concept,
+                           concept_code,
+                           valid_start_date,
+                           valid_end_date,
+                           invalid_reason)
+   SELECT NULL,
+          kv2.description,
+          NULL,
+          'Read',
+          NULL,
+          NULL,
+          kv2.readcode || kv2.termcode,
+          TO_DATE ('20141001', 'yyyymmdd'),
+          TO_DATE ('20991231', 'yyyymmdd'),
+          NULL
+     FROM keyv2 kv2;
+
+	 
+INSERT INTO concept_relationship_stage (concept_id_1,
+                                        concept_id_2,
+                                        concept_code_1,
+                                        concept_code_2,
+                                        relationship_id,
+                                        valid_start_date,
+                                        valid_end_date,
+                                        invalid_reason)
+   SELECT NULL,
+          NULL,
+          RSCCT.ReadCode || RSCCT.TermCode,
+          RSCCT.ConceptId,
+          'Maps to',
+          TO_DATE ('20141001', 'yyyymmdd'),
+          TO_DATE ('20991231', 'yyyymmdd'),
+          NULL
+     FROM RCSCTMAP2_UK RSCCT
+     
+
+COMMIT;

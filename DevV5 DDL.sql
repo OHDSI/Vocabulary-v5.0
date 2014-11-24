eba6609ce35da0a97c20cccddfb2a168fb6e7572
Timur <timur.vakhitov@firstlinesoftware.com>
2014-11-24 11:22:02 +0300

    updated DevV5 DDL.sql

--- DevV5 DDL.sql
+++ DevV5 DDL.sql
@@ -1,97 +1,115 @@
 -- Create tables
 
 CREATE TABLE concept (
-  concept_id			INTEGER			NOT NULL,
-  concept_name			VARCHAR(255)	NOT NULL,
-  domain_id				VARCHAR(20)		NOT NULL,
-  vocabulary_id			VARCHAR(20)		NOT NULL,
-  concept_class_id		VARCHAR(20)		NOT NULL,
-  standard_concept		VARCHAR(1)		NULL,
-  concept_code			VARCHAR(50)		NOT NULL,
-  valid_start_date		DATE			NOT NULL,
-  valid_end_date		DATE			NOT NULL,
-  invalid_reason		VARCHAR(1)		NULL
+  concept_id            INTEGER            NOT NULL,
+  concept_name            VARCHAR(255)    NOT NULL,
+  domain_id                VARCHAR(20)        NOT NULL,
+  vocabulary_id            VARCHAR(20)        NOT NULL,
+  concept_class_id        VARCHAR(20)        NOT NULL,
+  standard_concept        VARCHAR(1)        NULL,
+  concept_code            VARCHAR(50)        NOT NULL,
+  valid_start_date        DATE            NOT NULL,
+  valid_end_date        DATE            NOT NULL,
+  invalid_reason        VARCHAR(1)        NULL
 )
 ;
 CREATE TABLE vocabulary (
-  vocabulary_id			VARCHAR(20)		NOT NULL,
-  vocabulary_name		VARCHAR(255)	NOT NULL,
-  vocabulary_reference	VARCHAR(255)	NULL,
-  vocabulary_version	VARCHAR(255)	NULL,
-  vocabulary_concept_id	INTEGER			NOT NULL
+  vocabulary_id            VARCHAR(20)        NOT NULL,
+  vocabulary_name        VARCHAR(255)    NOT NULL,
+  vocabulary_reference    VARCHAR(255)    NULL,
+  vocabulary_version    VARCHAR(255)    NULL,
+  vocabulary_concept_id    INTEGER            NOT NULL
 )
 ;
 
 CREATE TABLE domain (
-  domain_id			VARCHAR(20)		NOT NULL,
-  domain_name		VARCHAR(255)	NOT NULL,
-  domain_concept_id	INTEGER			NOT NULL
+  domain_id            VARCHAR(20)        NOT NULL,
+  domain_name        VARCHAR(255)    NOT NULL,
+  domain_concept_id    INTEGER            NOT NULL
 )
 ;
 
 CREATE TABLE concept_class (
-  concept_class_id			VARCHAR(20)		NOT NULL,
-  concept_class_name		VARCHAR(255)	NOT NULL,
-  concept_class_concept_id	INTEGER			NOT NULL
+  concept_class_id            VARCHAR(20)        NOT NULL,
+  concept_class_name        VARCHAR(255)    NOT NULL,
+  concept_class_concept_id    INTEGER            NOT NULL
 )
 ;
 
 CREATE TABLE concept_relationship (
-  concept_id_1			INTEGER			NOT NULL,
-  concept_id_2			INTEGER			NOT NULL,
-  relationship_id		VARCHAR(20)		NOT NULL,
-  valid_start_date		DATE			NOT NULL,
-  valid_end_date		DATE			NOT NULL,
-  invalid_reason		VARCHAR(1)		NULL)
+  concept_id_1            INTEGER            NOT NULL,
+  concept_id_2            INTEGER            NOT NULL,
+  relationship_id        VARCHAR(20)        NOT NULL,
+  valid_start_date        DATE            NOT NULL,
+  valid_end_date        DATE            NOT NULL,
+  invalid_reason        VARCHAR(1)        NULL)
 ;
 
 CREATE TABLE relationship (
-  relationship_id			VARCHAR(20)		NOT NULL,
-  relationship_name			VARCHAR(255)	NOT NULL,
-  is_hierarchical			VARCHAR(1)		NOT NULL,
-  defines_ancestry			VARCHAR(1)		NOT NULL,
-  reverse_relationship_id	VARCHAR(20)		NOT NULL,
-  relationship_concept_id	INTEGER			NOT NULL
+  relationship_id            VARCHAR(20)        NOT NULL,
+  relationship_name            VARCHAR(255)    NOT NULL,
+  is_hierarchical            VARCHAR(1)        NOT NULL,
+  defines_ancestry            VARCHAR(1)        NOT NULL,
+  reverse_relationship_id    VARCHAR(20)        NOT NULL,
+  relationship_concept_id    INTEGER            NOT NULL
 )
 ;
 
 CREATE TABLE concept_synonym (
-  concept_id			INTEGER			NOT NULL,
-  concept_synonym_name	VARCHAR(1000)	NOT NULL,
-  language_concept_id	INTEGER			NOT NULL
+  concept_id            INTEGER            NOT NULL,
+  concept_synonym_name    VARCHAR(1000)    NOT NULL,
+  language_concept_id    INTEGER            NOT NULL
 )
 ;
 
 CREATE TABLE concept_ancestor (
-  ancestor_concept_id		INTEGER		NOT NULL,
-  descendant_concept_id		INTEGER		NOT NULL,
-  min_levels_of_separation	INTEGER		NOT NULL,
-  max_levels_of_separation	INTEGER		NOT NULL)
+  ancestor_concept_id        INTEGER        NOT NULL,
+  descendant_concept_id        INTEGER        NOT NULL,
+  min_levels_of_separation    INTEGER        NOT NULL,
+  max_levels_of_separation    INTEGER        NOT NULL)
 ;
 
 CREATE TABLE source_to_concept_map (
-  source_code				VARCHAR(50)		NOT NULL,
-  source_vocabulary_id		VARCHAR(20)		NOT NULL,
-  source_code_description	VARCHAR(255)	NULL,
-  target_concept_id			INTEGER			NOT NULL,
-  target_vocabulary_id		VARCHAR(20)		NOT NULL,
-  valid_start_date			DATE			NOT NULL,
-  valid_end_date			DATE			NOT NULL,
-  invalid_reason			VARCHAR(1)		NULL)
+  source_code                VARCHAR(50)        NOT NULL,
+  source_vocabulary_id        VARCHAR(20)        NOT NULL,
+  source_code_description    VARCHAR(255)    NULL,
+  target_concept_id            INTEGER            NOT NULL,
+  target_vocabulary_id        VARCHAR(20)        NOT NULL,
+  valid_start_date            DATE            NOT NULL,
+  valid_end_date            DATE            NOT NULL,
+  invalid_reason            VARCHAR(1)        NULL)
 ;
 
 CREATE TABLE drug_strength (
-  drug_concept_id				INTEGER		NOT NULL,
-  ingredient_concept_id			INTEGER		NOT NULL,
-  amount_value					FLOAT		NULL,
-  amount_unit_concept_id		INTEGER		NULL,
-  numerator_value				FLOAT		NULL,
-  numerator_unit_concept_id		INTEGER		NULL,
-  denominator_unit_concept_id	INTEGER		NULL,
-  valid_start_date				DATE		NOT NULL,
-  valid_end_date				DATE		NOT NULL,
-  invalid_reason				VARCHAR(1)	NULL)
+  drug_concept_id                INTEGER        NOT NULL,
+  ingredient_concept_id            INTEGER        NOT NULL,
+  amount_value                    FLOAT        NULL,
+  amount_unit_concept_id        INTEGER        NULL,
+  numerator_value                FLOAT        NULL,
+  numerator_unit_concept_id        INTEGER        NULL,
+  denominator_unit_concept_id    INTEGER        NULL,
+  valid_start_date                DATE        NOT NULL,
+  valid_end_date                DATE        NOT NULL,
+  invalid_reason                VARCHAR(1)    NULL)
 ;
+
+CREATE TABLE CONCEPT_STAGE
+(
+   concept_id         INTEGER,
+   concept_name       VARCHAR2 (256),
+   domain_id          VARCHAR (20),
+   vocabulary_id      VARCHAR (20) NOT NULL,
+   concept_class_id   VARCHAR (20),
+   standard_concept   VARCHAR2 (1 BYTE),
+   concept_code       VARCHAR2 (40 BYTE) NOT NULL,
+   VALID_START_DATE   DATE
+                         DEFAULT TO_DATE ('01-Jan-1970', 'DD-MM-RRRR')
+                         NOT NULL,
+   VALID_END_DATE     DATE
+                         DEFAULT TO_DATE ('31-Dec-2099', 'DD-MM-RRRR')
+                         NOT NULL,
+   invalid_reason     VARCHAR2 (1 BYTE)
+);
 
 -- Create FKs
 
@@ -124,7 +142,7 @@ ALTER TABLE concept_ancestor ADD CONSTRAINT fpk_concept_ancestor_concept_2 FOREI
 ALTER TABLE source_to_concept_map ADD CONSTRAINT fpk_source_to_concept_map_v_1 FOREIGN KEY (source_vocabulary_id) REFERENCES vocabulary (vocabulary_id);
 ALTER TABLE source_to_concept_map ADD CONSTRAINT fpk_source_to_concept_map_v_2 FOREIGN KEY (target_vocabulary_id) REFERENCES vocabulary (vocabulary_id);
 ALTER TABLE source_to_concept_map ADD CONSTRAINT fpk_source_to_concept_map_c_1 FOREIGN KEY (target_concept_id) REFERENCES concept (concept_id);
-ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_concept_1 FOREIGN KEY (drug_coCREATE INDEX idx_concept_concept_id ON concept (concept_id ASC);
+ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_concept_1 FOREIGN KEY (drug_concept_id) REFERENCES concept (concept_id);
 
 -- Create indices
 
@@ -132,13 +150,9 @@ CREATE INDEX idx_concept_code ON concept (concept_code ASC);
 CREATE INDEX idx_concept_vocabluary_id ON concept (vocabulary_id ASC);
 CREATE INDEX idx_concept_domain_id ON concept (domain_id ASC);
 CREATE INDEX idx_concept_class_id ON concept (concept_class_id ASC);
-CREATE INDEX idx_vocabulary_vocabulary_id ON vocabulary (vocabulary_id ASC);
-CREATE INDEX idx_domain_domain_id ON domain (domain_id ASC);
-CREATE INDEX idx_concept_class_class_id ON concept_class (concept_class_id ASC);
 CREATE INDEX idx_concept_relationship_id_1 ON concept_relationship (concept_id_1 ASC); 
 CREATE INDEX idx_concept_relationship_id_2 ON concept_relationship (concept_id_2 ASC); 
 CREATE INDEX idx_concept_relationship_id_3 ON concept_relationship (relationship_id ASC); 
-CREATE INDEX idx_relationship_relationship_id ON relationship (relationship_id ASC);
 CREATE INDEX idx_concept_synonym_id ON concept_synonym (concept_id ASC);
 CREATE INDEX idx_concept_ancestor_id_1 ON concept_ancestor (ancestor_concept_id ASC);
 CREATE INDEX idx_concept_ancestor_id_2 ON concept_ancestor (descendant_concept_id ASC);
@@ -147,9 +161,8 @@ CREATE INDEX idx_source_to_concept_map_id_2 ON source_to_concept_map (target_voc
 CREATE INDEX idx_source_to_concept_map_id_3 ON source_to_concept_map (target_concept_id ASC);
 CREATE INDEX idx_source_to_concept_map_code ON source_to_concept_map (source_code ASC);
 CREATE INDEX idx_drug_strength_id_1 ON drug_strength (drug_concept_id ASC);
-CREATE INDEX idx_drug_strength_id_2 ON drug_strength (ingredient_concept_id ASC);ncept_id) REFERENCES concept (concept_id);
+CREATE INDEX idx_drug_strength_id_2 ON drug_strength (ingredient_concept_id ASC);
 ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_concept_2 FOREIGN KEY (ingredient_concept_id) REFERENCES concept (concept_id);
 ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_unit_1 FOREIGN KEY (amount_unit_concept_id) REFERENCES concept (concept_id);
 ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_unit_2 FOREIGN KEY (numerator_unit_concept_id) REFERENCES concept (concept_id);
 ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_unit_3 FOREIGN KEY (denominator_unit_concept_id) REFERENCES concept (concept_id);
-

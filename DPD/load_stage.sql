e9bb7cdf767b70502322fe06da5ff4f996306c0d
Christian Reich <reich@ohdsi.org>
2016-03-25 15:02:23 -0400

    Created DPD

---
+++ DPD/load_stage.sql
@@ -0,0 +1,44 @@
+/**************************************************************************
+* Copyright 2016 Observational Health Data Sciences and Informatics (OHDSI)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* 
+* Authors: Timur Vakhitov, Christian Reich, Dimitry Dimschitz
+* Date: 2016
+**************************************************************************/
+
+-- 1. Update latest_update field to new date 
+BEGIN
+   EXECUTE IMMEDIATE 'ALTER TABLE vocabulary DROP COLUMN latest_update';
+EXCEPTION WHEN OTHERS THEN NULL;
+END;
+
+ALTER TABLE vocabulary ADD latest_update DATE;
+UPDATE vocabulary SET latest_update='1-Jan-1970', vocabulary_version='2016-03-04' WHERE vocabulary_id='DPD'; 
+
+COMMIT;
+
+-- 2. Truncate all working tables and remove indices
+TRUNCATE TABLE concept_stage;
+TRUNCATE TABLE concept_relationship_stage;
+TRUNCATE TABLE concept_synonym_stage;
+ALTER SESSION SET SKIP_UNUSABLE_INDEXES = TRUE; --disables error reporting of indexes and index partitions marked UNUSABLE
+ALTER INDEX idx_cs_concept_code UNUSABLE;
+ALTER INDEX idx_cs_concept_id UNUSABLE;
+ALTER INDEX idx_concept_code_1 UNUSABLE;
+ALTER INDEX idx_concept_code_2 UNUSABLE;
+
+--3. Run Input script
+
+--4. Run createNewDrugVocabulary
+

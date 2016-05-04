935e6693de693bd7770fb6288a419be1a85445c2
Christian Reich <reich@ohdsi.org>
2016-05-04 12:20:59 -0400

    create input tables only for dm+d

--- dm+d/create_input.sql
+++ dm+d/create_input.sql
@@ -18,7 +18,7 @@ COMMIT;
 
 -- 2. Create drug_concept_stage
 
-drop table drug_concept_stage;
+drop table drug_concept_stage purge;
 CREATE TABLE drug_concept_stage NOLOGGING AS SELECT * FROM concept_stage WHERE 1=0;
 ALTER TABLE drug_concept_stage ADD insert_id NUMBER;   
 INSERT /*+ APPEND */
@@ -233,13 +233,13 @@ INSERT /*+ APPEND */
              XMLSEQUENCE (
                 t_xml.xmlfield.EXTRACT ('ACTUAL_MEDICINAL_PRODUCTS/AMPS/AMP'))) t
    UNION ALL
-   --VMPPs (clinical packs)
+   --VMPPs (Clinical Drug Box)
    SELECT NULL AS concept_id,
           SUBSTR (EXTRACTVALUE (VALUE (t), 'VMPP/NM'), 1, 255)
              AS concept_name,
           'Drug' AS domain_id,
           'dm+d' AS vocabulary_id,
-          'Clinical Pack' AS concept_class_id,
+          'Clinical Drug Box' AS concept_class_id,
           NULL AS standard_concept,
           EXTRACTVALUE (VALUE (t), 'VMPP/VPPID') AS concept_code,
           TO_DATE ('1970-01-01', 'YYYY-MM-DD') AS valid_start_date,
@@ -263,13 +263,13 @@ INSERT /*+ APPEND */
                 t_xml.xmlfield.EXTRACT (
                    'VIRTUAL_MED_PRODUCT_PACK/VMPPS/VMPP'))) t
    UNION ALL
-   --AMPPs (branded packs)
+   --AMPPs (Branded Drug Box)
    SELECT NULL AS concept_id,
           SUBSTR (EXTRACTVALUE (VALUE (t), 'AMPP/NM'), 1, 255)
              AS concept_name,
           'Drug' AS domain_id,
           'dm+d' AS vocabulary_id,
-          'Branded Pack' AS concept_class_id,
+          'Branded Drug Box' AS concept_class_id,
           NULL AS standard_concept,
           EXTRACTVALUE (VALUE (t), 'AMPP/APPID') AS concept_code,
           TO_DATE ('1970-01-01', 'YYYY-MM-DD') AS valid_start_date,
@@ -299,136 +299,3 @@ DELETE FROM drug_concept_stage
   WHERE ROWID NOT IN (SELECT LAST_VALUE (ROWID) OVER (PARTITION BY concept_code ORDER BY invalid_reason, ROWID ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
 FROM drug_concept_stage);                   
 COMMIT;    
-
--- Delete all concepts that exists in SNOMED with concept_class_id = 'Physical Object'. They are devices, not drugs
-DELETE FROM drug_concept_stage d
-WHERE EXISTS (
-  SELECT 1
-  FROM concept c
-  WHERE c.concept_code = d.concept_code
-     AND c.vocabulary_id = 'SNOMED'
-     AND c.concept_class_id = 'Physical Object'
-);
-COMMIT;					   
-
--- Remove all the drugs that have no entry in f_vmp2 or f_amp2. They are devices
-delete from drug_concept_stage where concept_code in (
-  select concept_code from drug_concept_stage where concept_class_id like 'Branded%' or concept_class_id like 'Clinical%'
-minus
-  select apid from f_amp2_ex
-minus 
-  select vpid from f_vmp2_ex
-);
-
--- Remove combination 
--- !!!! Check whether the singletons all exist and whether they aer mapped
-delete from drug_concept_stage where concept_name like '% + %' and concept_class_id='Ingredient';
-
--- Remove duplicate Ingredients
-  select 
-    i.concept_code, i.concept_name, 
-    ins.concept_code, ins.concept_name
-  from drug_concept_stage i
-  join devv5.concept s1 on s1.vocabulary_id='SNOMED' and s1.concept_code=i.concept_code
-  join devv5.concept_relationship r on r.concept_id_1=s1.concept_id and r.invalid_reason is null and r.relationship_id in ('Subsumes', 'Active ing of')
-  join devv5.concept s2 on s2.concept_id=r.concept_id_2 and s2.vocabulary_id='SNOMED' 
-  join drug_concept_stage ins on ins.concept_code=s2.concept_code and ins.concept_class_id='Ingredient'
-  where i.concept_class_id='Ingredient'
-union
-  select 
-    i.concept_code, i.concept_name, 
-    ins.concept_code, ins.concept_name
-  from drug_concept_stage i
-  join devv5.concept s1 on s1.vocabulary_id='SNOMED' and s1.concept_code=i.concept_code
-  join devv5.concept s2 on instr(lower(s2.concept_name), lower(s1.concept_name))>0 and s2.vocabulary_id='SNOMED' and s1.concept_name!=s2.concept_name
-  join drug_concept_stage ins on ins.concept_code=s2.concept_code and ins.concept_class_id='Ingredient'
-  where i.concept_class_id='Ingredient'
-;
-
-
-
-
-
-
-
-create index x_drug_concept_stage on drug_concept_stage(concept_code);
-
--- 2. Create internal relationships
--- From SNOMED
-truncate table internal_relationship_stage;
-insert into internal_relationship_stage
-with ds as (
-  select d.concept_code, s.concept_id
-  from drug_concept_stage d
-  join devv5.concept s on s.vocabulary_id='SNOMED' and s.concept_code=d.concept_code
-)
-select distinct
-  c1.concept_code as concept_code_1, 'dm+d' as vocabulary_id_1,
-  c2.concept_code as concept_code_2, 'dm+d' as vocabulary_id_2
-from ds c1
-join devv5.concept_relationship r on r.concept_id_1=c1.concept_id and r.invalid_reason is null
-join ds c2 on c2.concept_id=r.concept_id_2
-join drug_concept_stage d on d.concept_code=c1.concept_code and (d.concept_class_id like 'Clinical%' or d.concept_class_id='Branded%')
-;
-
-commit;
-
--- 3. Create relationships to RxNorm
--- Ingredients
-
-insert into relationship_to_concept;
-select count(distinct concept_code_1) from (
-select concept_code_1, vocabulary_id_1, concept_id_2, 
-  row_number() over (partition by concept_code_1 order by 1) as precedence,
-  null as conversion_factor
-from (
-  select distinct
-    dmd.concept_code as concept_code_1, dmd.vocabulary_id as vocabulary_id_1,
-    rxn.concept_id as concept_id_2
-  from drug_concept_stage dmd
-  join devv5.concept s on s.vocabulary_id='SNOMED' and s.concept_code=dmd.concept_code
-  join devv5.concept_relationship r on r.concept_id_1=s.concept_id and r.invalid_reason is null
-  join devv5.concept rxn on rxn.concept_id=r.concept_id_2 and rxn.vocabulary_id='RxNorm' and rxn.concept_class_id='Ingredient' and rxn.standard_concept='S'
-  where dmd.concept_class_id='Ingredient'
-union
--- Take 2 jumps
-  select distinct
-    dmd.concept_code as concept_code_1, dmd.vocabulary_id as vocabulary_id_1,
-    rxn.concept_id as concept_id_2
-  from drug_concept_stage dmd
-  join devv5.concept s on s.vocabulary_id='SNOMED' and s.concept_code=dmd.concept_code
-  join devv5.concept_relationship r on r.concept_id_1=s.concept_id and r.invalid_reason is null
--- if the resulting RxConcept is not a standard RxNorm ingredient, but instead a precise ingredient, a 'U' concept, a Brand Name, an NDFRT code , 
-  join devv5.concept_relationship rp on rp.concept_id_1=r.concept_id_2 and rp.invalid_reason is null and rp.relationship_id in ('Form of', 'Concept replaced by', 'Brand name of', 'NDFRT - RxNorm eq')
-  join devv5.concept rxn on rxn.concept_id=rp.concept_id_2 and rxn.vocabulary_id='RxNorm' and rxn.concept_class_id='Ingredient'
-  where dmd.concept_class_id='Ingredient'
-)
-);
-
-  select distinct
-dmd.concept_name, rxn.concept_name,
-    dmd.concept_code as concept_code_1, dmd.vocabulary_id as vocabulary_id_1,
-    rxn.concept_id as concept_id_2
-;
-select dmd.concept_name, s.concept_name, rxn.concept_name 
-  from drug_concept_stage dmd
-  join internal_relationship_stage di on di.concept_code_2=dmd.concept_code
--- limit to onesie drugs only
-  join (
-    select concept_code_1 from internal_relationship_stage join drug_concept_stage on concept_code_2=concept_code and concept_class_id='Ingredient' group by concept_code_1 having count(8)=1
-  ) onesie on onesie.concept_code_1=di.concept_code_1
-  join devv5.concept s on s.vocabulary_id='SNOMED' and s.concept_code=di.concept_code_1
-left  join devv5.concept_relationship r on r.concept_id_1=s.concept_id and r.invalid_reason is null and r.relationship_id in ('SNOMED - ATC eq')
-left  join devv5.concept rxn on rxn.concept_id=r.concept_id_2 and rxn.vocabulary_id='ATC' -- and rxn.concept_class_id='Ingredient'
-  where dmd.concept_class_id='Ingredient'
-;
-select * from drug_concept_stage where concept_class_id='Ingredient';
-select * from internal_relationship_stage join drug_concept_stage on concept_code_2=concept_code where concept_code_1='324880001';
-select *
-from internal_relationship_stage di 
-join drug_concept_stage d on d.concept_code=di.concept_code_1
-join drug_concept_stage i on i.concept_code=di.concept_code_2 and i.concept_class_id='Ingredient'
-join (
-  select concept_code_1 from internal_relationship_stage join drug_concept_stage on concept_code_2=concept_code and concept_class_id='Ingredient' group by concept_code_1 having count(8)=1
-) onesie on onesie.concept_code_1=di.concept_code_1
-;

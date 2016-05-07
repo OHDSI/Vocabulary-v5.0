dc887862a1f9c328591a536f1e508181459455e4
Christian Reich <reich@ohdsi.org>
2016-05-07 10:49:02 -0400

    Added EphMRA ATC and Supplier

--- working/manual_changes.sql
+++ working/manual_changes.sql
@@ -13,4 +13,24 @@ BEGIN
 END;
 */
 
+-- Add DPD
+insert into concept (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date, invalid_reason)
+  values(v5_concept.nextval, 'Anatomical Classification of Pharmaceutical Products (EphMRA)', 'Metadata', 'Vocabulary', 'Vocabulary', null, 'OMOP generated', '1-Jan-1970', '31-Dec-2099', null);
+insert into vocabulary (vocabulary_id, vocabulary_name, vocabulary_reference, vocabulary_version, vocabulary_concept_id) 
+  values ('EphMRA ATC', 'Anatomical Classification of Pharmaceutical Products (EphMRA)', 'http://www.ephmra.org/Anatomical-Classification', 'V2016', (select concept_id from concept where concept_name='Anatomical Classification of Pharmaceutical Products (EphMRA)'));
+insert into vocabulary_conversion (vocabulary_id_v4, vocabulary_id_v5, omop_req, click_default, available, url) values (80, 'EphMRA ATC', null, null, null, null);
+
+
+
 commit;
+
+-- Add old NDC from GPI
+select distinct n.gpi, n.gpi_desc, n.ndc, n.mkted_prod_formltn_nm as ndw_name, ndc.concept_name as ndc_name, rx.concept_id as rx_id, rx.concept_name as rx_name, rx.concept_class_id as rx_class, cd.concept_id as cd_id, cd.concept_name as cd_name, cd.concept_class_id as cd_class
+from ndw_v_product n
+join concept ndc on ndc.concept_code=n.ndc and ndc.vocabulary_id='NDC' 
+join concept_relationship r on r.invalid_reason is null and r.concept_id_1=ndc.concept_id and r.relationship_id='Maps to'
+join concept rx on rx.concept_id=r.concept_id_2
+left join concept_relationship r2 on r2.concept_id_1=rx.concept_id and r2.invalid_reason is null and r2.relationship_id='Tradename of'
+left join concept cd on cd.concept_id=r2.concept_id_2 
+where n.gpi='83100020302005'
+  ;

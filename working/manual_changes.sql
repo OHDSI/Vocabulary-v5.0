9552303435e1acf31e883a491e4de17b5534a825
Christian Reich <reich@ohdsi.org>
2016-07-30 18:47:53 -0400

    Added dm+d concept_classes

--- working/manual_changes.sql
+++ working/manual_changes.sql
@@ -13,6 +13,65 @@ BEGIN
 END;
 */
 
+/*
+-- start new sequence
+drop sequence v5_concept;
+DECLARE
+ ex NUMBER;
+BEGIN
+  SELECT MAX(concept_id)+1 INTO ex FROM concept WHERE concept_id>=5000 and concept_id<8000; -- Last valid value in the 5000-8000 slot
+  BEGIN
+    EXECUTE IMMEDIATE 'CREATE SEQUENCE v5_concept INCREMENT BY 1 START WITH ' || ex || ' NOCYCLE CACHE 20 NOORDER';
+    EXCEPTION
+      WHEN OTHERS THEN NULL;
+  END;
+END;
+*/
+
+-- Add THIN Gemscript Class
+insert into concept (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date, invalid_reason)
+  values (v5_concept.nextval, 'Gemscript THIN', 'Metadata', 'Concept Class', 'Concept Class', null, 'OMOP generated', '01-JAN-1970', '31-DEC-2099', null);
+insert into concept_class (concept_class_id, concept_class_name, concept_class_concept_id)
+  values ('Gemscript THIN', 'Encrypted Gemscript for the THIN database', (select concept_id from concept where concept_name = 'Gemscript THIN'));
+
+-- Add Measurement Type for ordering
+insert into concept (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date, invalid_reason)
+  values (v5_concept.nextval, 'Test ordered through EHR', 'Type Concept', 'Meas Type', 'Meas Type', 'S', 'OMOP generated', '01-JAN-1970', '31-DEC-2099', null);
+
+-- Add parenteral to Route
+update concept set domain_id='Route' where concept_id=40491411;
+
+-- Add concept_classes for dm+d
+insert into concept (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date, invalid_reason)
+  values (v5_concept.nextval, 'Actual Medicinal Product', 'Metadata', 'Concept Class', 'Concept Class', null, 'OMOP generated', '01-JAN-1970', '31-DEC-2099', null);
+insert into concept_class (concept_class_id, concept_class_name, concept_class_concept_id)
+  values ('AMP', 'Actual Medicinal Product', (select concept_id from concept where concept_name = 'Actual Medicinal Product'));
+
+insert into concept (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date, invalid_reason)
+  values (v5_concept.nextval, 'Actual Medicinal Product Pack', 'Metadata', 'Concept Class', 'Concept Class', null, 'OMOP generated', '01-JAN-1970', '31-DEC-2099', null);
+insert into concept_class (concept_class_id, concept_class_name, concept_class_concept_id)
+  values ('AMPP', 'Actual Medicinal Product Pack', (select concept_id from concept where concept_name = 'Actual Medicinal Product Pack'));
+
+insert into concept (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date, invalid_reason)
+  values (v5_concept.nextval, 'Virtual Medicinal Product', 'Metadata', 'Concept Class', 'Concept Class', null, 'OMOP generated', '01-JAN-1970', '31-DEC-2099', null);
+insert into concept_class (concept_class_id, concept_class_name, concept_class_concept_id)
+  values ('VMP', 'Virtual Medicinal Product', (select concept_id from concept where concept_name = 'Virtual Medicinal Product'));
+
+insert into concept (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date, invalid_reason)
+  values (v5_concept.nextval, 'Virtual Medicinal Product Pack', 'Metadata', 'Concept Class', 'Concept Class', null, 'OMOP generated', '01-JAN-1970', '31-DEC-2099', null);
+insert into concept_class (concept_class_id, concept_class_name, concept_class_concept_id)
+  values ('VMPP', 'Virtual Medicinal Product Pack', (select concept_id from concept where concept_name = 'Virtual Medicinal Product Pack'));
+
+insert into concept (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date, invalid_reason)
+  values (v5_concept.nextval, 'dm+d Dose Form', 'Metadata', 'Concept Class', 'Concept Class', null, 'OMOP generated', '01-JAN-1970', '31-DEC-2099', null);
+insert into concept_class (concept_class_id, concept_class_name, concept_class_concept_id)
+  values ('Form', 'dm+d Dose Form', (select concept_id from concept where concept_name = 'dm+d Dose Form'));
+
+insert into concept (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date, invalid_reason)
+  values (v5_concept.nextval, 'Virtual Therapeutic Moiety', 'Metadata', 'Concept Class', 'Concept Class', null, 'OMOP generated', '01-JAN-1970', '31-DEC-2099', null);
+insert into concept_class (concept_class_id, concept_class_name, concept_class_concept_id)
+  values ('VTM', 'Virtual Therapeutic Moiety', (select concept_id from concept where concept_name = 'Virtual Therapeutic Moiety'));
+
 commit;
 
 -- Add old NDC from GPI

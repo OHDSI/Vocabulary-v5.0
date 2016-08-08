be70b87879ed3e8e21d00e6835a44c59671bb2e9
Timur <timur.vakhitov@firstlinesoftware.com>
2016-08-08 16:29:02 +0300

    1. remove EXISTING_DS 2. added creating_the_release.sql

---
+++ working/creating_the_release.sql
@@ -0,0 +1,22 @@
+--1. create copy of base tables
+--run in DEVV5
+drop table concept_old purge;
+create table concept_old nologging as select * From concept;
+drop table concept_relationship_old purge;
+create table concept_relationship_old nologging as select * From concept_relationship;
+drop table concept_synonym_old purge;
+create table concept_synonym_old nologging as select * From concept_synonym;
+
+--2. update vocabulaies (with generic_update)
+
+--3. make checks
+exec DEVV5.QA_TESTS.PURGE_CACHE;
+select * from table(DEVV5.QA_TESTS.GET_SUMMARY('concept')); 
+select * from table(DEVV5.QA_TESTS.GET_SUMMARY('concept_relationship'));
+select * from table(DEVV5.QA_TESTS.GET_SUMMARY('concept_ancestor'));
+select * from table(DEVV5.QA_TESTS.GET_CHECKS);
+
+--4. start the release
+--run in DEVV5
+exec vocabulary_pack.StartRelease;
+

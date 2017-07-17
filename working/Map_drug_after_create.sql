9ca3bb14484c28ea2f8e8182f0cdec3d29b6d736
Dmytry Dymshyts <ddymshyts@sciforce.solutions>
2017-07-17 23:07:17 +0300

    Map_drug_after_create.sql bugs fixed

--- working/Map_drug_after_create.sql
+++ working/Map_drug_after_create.sql
@@ -2,7 +2,15 @@
 --run ancestor on the data get with RxE builder
 exec devv5.psmallconceptancestor
 ;
+--r_bn
+create table r_bn nologging as
+select r.concept_id_1 as concept_id, r.concept_id_2 as bn_id from concept_relationship r
+join concept d on d.concept_id=r.concept_id_1 and d.vocabulary_id in ('RxNorm', 'RxNorm Extension') and d.standard_concept='S'
+join concept f on f.concept_id=r.concept_id_2 and f.concept_class_id ='Brand Name' and f.invalid_reason is null
+where r.invalid_reason is null and r.relationship_id='Has brand name'
+;
 --choose the closest by hierarchy concept --how will "first value" work with duplicates??
+drop table anc_lev;
 create table anc_lev as
 select c.concept_id, min (a.MAX_LEVELS_OF_SEPARATION) S_level from concept c
 join concept_relationship r on concept_id = concept_id_1 and relationship_id='Maps to'
@@ -15,6 +23,7 @@ and c.invalid_reason is null and c.vocabulary_id = (select vocabulary_id from dr
 and cr.VALID_START_DATE <  (SELECT latest_update FROM vocabulary_conversion WHERE vocabulary_id_v5=(select vocabulary_id from drug_concept_stage where rownum=1)) --exclude RxNorm concepts made in this release --not a problem because in dev_vocab schema there will be no other vocabularies updated
 group by c.concept_id
 ;
+drop table rel_anc;
 create table rel_anc as
 select c.concept_id as s_c_1, cr.concept_id, a.MAX_LEVELS_OF_SEPARATION S_level from concept c
 join concept_relationship r on concept_id = concept_id_1 and relationship_id='Maps to'
@@ -27,6 +36,7 @@ and c.invalid_reason is null and c.vocabulary_id = (select vocabulary_id from dr
 and cr.VALID_START_DATE <  (SELECT latest_update FROM vocabulary_conversion WHERE vocabulary_id_v5=(select vocabulary_id from drug_concept_stage where rownum=1)) --exclude RxNorm concepts made in this release --not a problem because in dev_vocab schema there will be no other vocabularies updated
 ;
 --add codes
+drop table rel_fin;
 create table rel_fin as 
 select a.* from rel_anc a join anc_lev b  on a.s_level = b.s_level and b.concept_id = a.s_c_1
 ;
@@ -49,9 +59,9 @@ and  c2.vocabulary_id like 'RxNorm%'  and c2.invalid_reason is null
 --define order as combination of attributes number and each attribute weight
 drop table attrib_cnt; 
 create table attrib_cnt as
-select concept_id_1, count (1)|| max(weight) as weight  from (
+select concept_id, count (1)|| max(weight) as weight  from (
 --need to go throught Drug Form / Component to get the Brand Name
-select distinct concept_id_1, 3 as weight from 
+select distinct concept_id, 3 as weight from 
 r_bn
 union ALL
 select concept_id_1, 1 from cnc_rel_class where concept_class_id_2 in ('Supplier')
@@ -67,21 +77,21 @@ select * from drug_strength where Box_size is not null)
 union ALL
 select distinct drug_concept_id, 4 from  (
 select * from drug_strength where DENOMINATOR_VALUE is not null)
-) group by concept_id_1
+) group by concept_id
 union
 select concept_id , '0' from concept where concept_class_id ='Ingredient' and vocabulary_id like 'RxNorm%'
 ;
 --duplicates analysis
---drop table Q_DCODE_to_hlc
+drop table Q_DCODE_to_hlc;
 create table Q_DCODE_to_hlc as
 select q.Q_DCODE from q_to_rn q join concept c on concept_id = q.R_DID where ( CONCEPT_CLASS_ID in 
 ('Branded Drug Box', 'Quant Branded Box', 'Quant Branded Drug', 'Branded Drug', 'Marketed Product', 'Branded Pack', 'Clinical Pack' , 
 'Clinical Drug Box', 'Quant Clinical Box', 'Clinical Branded Drug',  'Clinical Drug', 'Marketed Product')
 or concept_name like '% / %' ) and c.standard_concept = 'S' 
 ;
---drop table dupl;
+drop table dupl;
 create table dupl as(
-select st.*, c.concept_class_id,attrib_cnt.*  from q_to_rn q join attrib_cnt on r_did  = concept_id_1 
+select st.*, c.concept_class_id,attrib_cnt.*  from q_to_rn q join attrib_cnt on r_did  = concept_id 
 join drug_concept_stage ds on Q_DCODE = ds.concept_code
 join concept c on c.concept_id = q.R_DID 
 join (select drug_concept_code, count (1) as cnt from ds_stage group by drug_concept_code having count(1) >1)st on drug_concept_code = Q_DCODE 
@@ -91,10 +101,10 @@ where Q_DCODE not in (select Q_DCODE from Q_DCODE_to_hlc)
  --best map
 drop table best_map;
 create table best_map as 
-select distinct  first_value(concept_id_1) over (partition by q_dcode order by weight desc) as r_did , q_dcode
-from attrib_cnt join q_to_rn on r_did  = concept_id_1 
+select distinct  first_value(concept_id) over (partition by q_dcode order by weight desc) as r_did , q_dcode
+from attrib_cnt join q_to_rn on r_did  = concept_id
 where Q_DCODE not in ( select drug_concept_code from dupl)
-union select CONCEPT_ID_1, drug_concept_code from dupl where WEIGHT = 0
+union select CONCEPT_ID, drug_concept_code from dupl where WEIGHT = 0
 ;
 --postprocessing with all excess information removal
 --still a question - should we keep all the Attributes for local vocabularies
@@ -102,6 +112,11 @@ union select CONCEPT_ID_1, drug_concept_code from dupl where WEIGHT = 0
 -- all the newly generated concepts should be removed 
 --devices already added in RxE builder
 delete from concept_stage where concept_code like '%OMOP%' 
+;
+--make devices standard
+update concept_stage set standard_concept = 'S' where domain_id = 'Device'
+;
+commit
 ;
 --only mappings (results of "best_map") should exist in concept_relationship_stage
 truncate table concept_relationship_stage
@@ -129,6 +144,8 @@ select
 from best_map m
 join concept c on c.concept_id=m.r_did and c.vocabulary_id in ('RxNorm', 'RxNorm Extension')
 ;
+commit
+;
 --add mapping of Devices to itself (do we have it in RxE build?)
 insert /*+ APPEND */ into concept_relationship_stage
 						(concept_code_1,
@@ -148,4 +165,7 @@ select
   (SELECT latest_update FROM vocabulary_conversion WHERE vocabulary_id_v5=(select vocabulary_id from drug_concept_stage where rownum=1)) as valid_start_date,
   TO_DATE ('20991231', 'yyyymmdd') as valid_end_date,
   null as invalid_reason
-from concept_stage  
+from concept_stage  where domain_id ='Device'
+;
+commit
+;

025c245c7d222a798815d7e87fe568917b596a04
odysseus@sciforce.solutions <odysseus@sciforce.solutions>
2017-07-28 22:58:03 +0300

    11

--- CVX/load_stage.sql
+++ CVX/load_stage.sql
@@ -17,17 +17,20 @@
 * Date: 2016
 **************************************************************************/
 
+
+
 --1. Update latest_update field to new date 
 DECLARE
-   cCVXFDate   DATE;
+   cCVXFDate1   DATE;
 BEGIN
-   SELECT MAX (last_updated_date) INTO cCVXFDate FROM CVX;
+   SELECT max(to_date (LAST_UPDATED_DATE, 'mm/dd/yyyy')) INTO cCVXFDate1 FROM CVX;
 
    DEVV5.VOCABULARY_PACK.SetLatestUpdate (pVocabularyName        => 'CVX',
-                                          pVocabularyDate        => cCVXFDate,
-                                          pVocabularyVersion     => 'CVX code set ' || cCVXFDate,
+                                          pVocabularyDate        => cCVXFDate1,
+                                          pVocabularyVersion     => 'CVX code set ' || cCVXFDate1,
                                           pVocabularyDevSchema   => 'DEV_CVX');
 END;
+/
 COMMIT;
 
 --2. Truncate all working tables
@@ -50,10 +53,10 @@ INSERT INTO concept_stage (concept_name,
    SELECT SUBSTR (full_vaccine_name, 1, 255) AS concept_name,
           'CVX' AS vocabulary_id,
           'Drug' AS domain_id,
-          'Drg Class' AS concept_class_id,
+          'Drug Class' AS concept_class_id,
           'C' AS standard_concept,
           cvx_code AS concept_code,
-          (SELECT MIN(concept_date) FROM CVX_DATES d WHERE D.CVX_CODE=C.CVX_CODE) AS valid_start_date, --get concept date from true source
+          nvl((SELECT MIN(concept_date) FROM CVX_DATES d WHERE D.CVX_CODE=C.CVX_CODE),to_date (LAST_UPDATED_DATE, 'mm/dd/yyyy'))  AS valid_start_date, --get concept date from true source
           TO_DATE ('20991231', 'yyyymmdd') AS valid_end_date,
           NULL AS invalid_reason
      FROM CVX c;
@@ -76,6 +79,22 @@ INSERT INTO concept_synonym_stage (synonym_concept_id,
              FOR DESCRIPTIONS
              IN (full_vaccine_name, short_description));			  
 COMMIT;
+
+insert into concept_relationship_stage (CONCEPT_CODE_1,CONCEPT_CODE_2,VOCABULARY_ID_1,VOCABULARY_ID_2,RELATIONSHIP_ID,VALID_START_DATE,valid_end_date)
+select distinct
+CVX_CODE,b.concept_code,'CVX',b.VOCABULARY_ID,'CVX_to_RxNorm' , TO_DATE ('20170728', 'yyyymmdd') AS VALID_START_DATE,
+TO_DATE ('20991231', 'yyyymmdd') AS valid_end_date from 
+(select distinct cvx_code,concept_id,VOCABULARY_ID from CVX_TO_RX_USING_LENA
+union 
+select distinct cvx.code, c.concept_id,c.VOCABULARY_ID from dev_rxnorm.rxnconso cvx
+join concept c on concept_code = rxcui and vocabulary_id = 'RxNorm'
+join   concept_stage cs  on cs.concept_code =  cvx.CODE
+where sab= 'CVX'
+and c.standard_concept='S') a join concept b on a.concept_id=b.concept_id and a.vocabulary_id=b.vocabulary_id
+;
+
+COMMIT;
+
 
 ---------------
 --???

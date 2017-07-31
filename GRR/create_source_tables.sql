6132059c3ba507c1802e9ac2634749954ef4e828
Anna Ostropolets <aostropolets@sciforce.solutions>
2017-07-31 20:04:16 +0300

    add GRR

---
+++ GRR/create_source_tables.sql
@@ -0,0 +1,136 @@
+﻿/**************************************************************************
+* Copyright 2016 Observational Health Data Sciences and Informatics (OHDSI)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+**************************************************************************/
+-- input tables creation
+CREATE TABLE DRUG_CONCEPT_STAGE
+(
+   CONCEPT_NAME       	 	 VARCHAR2(255 Byte),
+   VOCABULARY_ID      		 VARCHAR2(20 Byte),
+   CONCEPT_CLASS_ID   		 VARCHAR2(25 Byte),
+   SOURCE_CONCEPT_CLASS_ID       VARCHAR2(25 Byte),
+   STANDARD_CONCEPT   		 VARCHAR2(1 Byte),
+   CONCEPT_CODE       		 VARCHAR2(50 Byte),
+   POSSIBLE_EXCIPIENT 		 VARCHAR2(1 Byte),
+   DOMAIN_ID           		 VARCHAR2(25 Byte),
+   VALID_START_DATE   		 DATE,
+   VALID_END_DATE     		 DATE,
+   INVALID_REASON     		 VARCHAR2(1 Byte)
+);
+
+CREATE TABLE DS_STAGE
+(
+   DRUG_CONCEPT_CODE        VARCHAR2(255 Byte),
+   INGREDIENT_CONCEPT_CODE  VARCHAR2(255 Byte),
+   BOX_SIZE                 INTEGER,
+   AMOUNT_VALUE             FLOAT(126),
+   AMOUNT_UNIT              VARCHAR2(255 Byte),
+   NUMERATOR_VALUE          FLOAT(126),
+   NUMERATOR_UNIT           VARCHAR2(255 Byte),
+   DENOMINATOR_VALUE        FLOAT(126),
+   DENOMINATOR_UNIT         VARCHAR2(255 Byte)
+);
+
+CREATE TABLE INTERNAL_RELATIONSHIP_STAGE
+(
+   CONCEPT_CODE_1     VARCHAR2(50 Byte),
+   CONCEPT_CODE_2     VARCHAR2(50 Byte)
+);
+
+CREATE TABLE RELATIONSHIP_TO_CONCEPT
+(
+   CONCEPT_CODE_1     VARCHAR2(255 Byte),
+   VOCABULARY_ID_1    VARCHAR2(20 Byte),
+   CONCEPT_ID_2       INTEGER,
+   PRECEDENCE         INTEGER,
+   CONVERSION_FACTOR  FLOAT(126)
+);
+
+CREATE TABLE PC_STAGE
+(
+   PACK_CONCEPT_CODE  VARCHAR2(255 Byte),
+   DRUG_CONCEPT_CODE  VARCHAR2(255 Byte),
+   AMOUNT             NUMBER,
+   BOX_SIZE           NUMBER
+);
+
+CREATE TABLE CONCEPT_SYNONYM_STAGE
+(
+   SYNONYM_CONCEPT_ID     NUMBER,
+   SYNONYM_NAME           VARCHAR2(255 Byte)   NOT NULL,
+   SYNONYM_CONCEPT_CODE   VARCHAR2(255 Byte)     NOT NULL,
+   SYNONYM_VOCABULARY_ID  VARCHAR2(255 Byte)     NOT NULL,
+   LANGUAGE_CONCEPT_ID    NUMBER
+)
+TABLESPACE USERS;
+
+--SEQUENCE FOR OMOP-GENERATED CODES STARTING WITH THE LAST CODE USED IN PREVIOUS VOCABULARY
+CREATE sequence conc_stage_seq 
+  MINVALUE 97124
+  MAXVALUE 1000000
+  START WITH 97124
+  INCREMENT BY 1
+  CACHE 20;
+  ;
+
+CREATE TABLE SOURCE_DATA
+(
+   ID                       NUMBER,
+   THERAPY_NAME_CODE        VARCHAR2(255 Byte),
+   THERAPY_NAME             VARCHAR2(255 Byte),
+   PRODUCT_NO               VARCHAR2(255 Byte),
+   PRODUCT_LAUNCH_DATE      VARCHAR2(255 Byte),
+   PRODUCT_FORM             VARCHAR2(255 Byte),
+   PRODUCT_FORM_NAME        VARCHAR2(255 Byte),
+   STRENGTH                 VARCHAR2(255 Byte),
+   STRENGTH_UNIT_CODE       VARCHAR2(255 Byte),
+   STRENGTH_UNIT            VARCHAR2(255 Byte),
+   VOLUME                   VARCHAR2(255 Byte),
+   VOLUME_UNIT_CODE         VARCHAR2(255 Byte),
+   VOLUME_UNIT              VARCHAR2(255 Byte),
+   PACKSIZE                 VARCHAR2(255 Byte),
+   PACK_PRICE               VARCHAR2(255 Byte),
+   FORM_LAUNCH_DATE         VARCHAR2(255 Byte),
+   OUT_OF_TRADE_DATE        VARCHAR2(255 Byte),
+   MANUFACTURER             VARCHAR2(255 Byte),
+   MANUFACTURER_NAME        VARCHAR2(255 Byte),
+   MANUFACTURER_SHORT_NAME  VARCHAR2(255 Byte),
+   ATC4_CODE                VARCHAR2(255 Byte),
+   ATC4_TEXT                VARCHAR2(255 Byte),
+   ATC3_CODE                VARCHAR2(255 Byte),
+   ATC3_TEXT                VARCHAR2(255 Byte),
+   ATC2_CODE                VARCHAR2(255 Byte),
+   ATC2_TEXT                VARCHAR2(255 Byte),
+   ATC1_CODE                VARCHAR2(255 Byte),
+   ATC1_TEXT                VARCHAR2(255 Byte),
+   WHO_ATC5_CODE            VARCHAR2(255 Byte),
+   WHO_ATC5_TEXT            VARCHAR2(255 Byte),
+   WHO_ATC4_CODE            VARCHAR2(255 Byte),
+   WHO_ATC4_TEXT            VARCHAR2(255 Byte),
+   WHO_ATC3_CODE            VARCHAR2(255 Byte),
+   WHO_ATC3_TEXT            VARCHAR2(255 Byte),
+   WHO_ATC2_CODE            VARCHAR2(255 Byte),
+   WHO_ATC2_TEXT            VARCHAR2(255 Byte),
+   WHO_ATC1_CODE            VARCHAR2(255 Byte),
+   WHO_ATC1_TEXT            VARCHAR2(255 Byte),
+   SUBSTANCE                VARCHAR2(255 Byte),
+   NO_OF_SUBSTANCES         VARCHAR2(255 Byte),
+   GENERIC_ORIGINAL         VARCHAR2(255 Byte),
+   PZN                      VARCHAR2(255 Byte),
+   NFC_NO                   VARCHAR2(255 Byte),
+   NFC                      VARCHAR2(255 Byte),
+   NFC_DESCRIPTION          VARCHAR2(255 Byte),
+   FCC                      VARCHAR2(255 Byte)
+);
---
+++ GRR/drops.sql
@@ -0,0 +1,56 @@
+DROP TABLE DRUG_CONCEPT_STAGE;
+DROP TABLE DS_STAGE;
+DROP TABLE INTERNAL_RELATIONSHIP_STAGE;
+DROP TABLE RELATIONSHIP_TO_CONCEPT;
+DROP TABLE PC_STAGE;
+DROP TABLE CONCEPT_SYNONYM_STAGE;
+
+
+
+DROP TABLE NON_DRUG;
+
+drop table grr_new_3_for_form;
+DROP TABLE grr_new_3;
+DROP TABLE source_data_1;
+DROP TABLE grr_non_drug;
+drop table grr_pack_0;
+drop table  grr_pack_1 ;
+drop table grr_bn;
+drop table grr_bn_2 ;
+drop table grr_manuf_0;
+drop table grr_manuf;
+drop table grr_form;
+drop table grr_form_2;
+drop table grr_form;
+drop table grr_form_2;
+drop table grr_form_1 ;
+drop table grr_ing;
+drop table grr_ing_2_0;
+drop table grr_ing_2;
+drop table list;
+drop table dcs_drugs;
+drop table dcs_unit; 
+drop table grr_new_3_for_ds;
+drop table grr_ds_abstr_qnt;
+drop table grr_ds_abstr_qnt_2;
+drop table grr_ds_abstr_qnt_3;
+drop table grr_ds_abstr_rel;
+drop table grr_new_3_1;
+drop table grr_new_3_2;
+drop table grr_new_3_3;
+drop table grr_new_3_4;
+drop table grr_ds;
+drop table grr_ds_1;
+drop  table ds_stage_sum;
+drop table ds_stage_0 ;
+truncate table ds_stage;
+drop table ds_0_sd;
+drop table spray_upd;
+drop table ds_vol_upd;
+drop table rtc_upd;
+drop table ds_sum_2;
+drop table ds_stage_cnc;
+drop table ds_stage_cnc2;
+drop table ds_stage_cnc3;
+drop table rel_to_name;
+drop table new_name;

579a88539371f4d345c2f2b9c7908d64e0cdd89b
Timur <timur.vakhitov@firstlinesoftware.com>
2018-09-27 17:33:42 +0300

    added 'timeout' and quit() method to SendMailHTML.sql

--- working/packages/DevV5_additional_functions/SendMailHTML.sql
+++ working/packages/DevV5_additional_functions/SendMailHTML.sql
@@ -13,14 +13,15 @@ mail_port = "587"
 recipients = recipient.split(',')
 message = ("From: %s\nTo: %s\nSubject: %s\nContent-type: text/html; charset=UTF-8\n\n %s" % (mail_sender,recipient,subject,send_message))
 try:
-  smtpObj=smtplib.SMTP(mail_smtp,mail_port)
+  smtpObj=smtplib.SMTP(mail_smtp,mail_port,timeout=30)
   smtpObj.starttls()
   smtpObj.docmd('AUTH LOGIN', base64.b64encode(mail_sender))
-  smtpObj.docmd('=password_in_base64=','')    
+  smtpObj.docmd('=password_in_base64=','')
   smtpObj.sendmail(mail_sender,recipients,message)
+  smtpObj.quit()
   message='Ok'
 except smtplib.SMTPException as e:
-  message='Error: '+str(e)   
+  message='Error: '+str(e)
 return message
 $body$
 LANGUAGE 'plpythonu'

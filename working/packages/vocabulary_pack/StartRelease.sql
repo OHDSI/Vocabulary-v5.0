06abcf0ddc14c940134af75c1dbb2749abc98fb9
Timur <timur.vakhitov@firstlinesoftware.com>
2019-02-07 13:04:45 +0300

    Create the devv5.vocabulary_release_stat table (AVOF-1454)

--- working/packages/vocabulary_pack/StartRelease.sql
+++ working/packages/vocabulary_pack/StartRelease.sql
@@ -13,13 +13,14 @@ DECLARE
   email CONSTANT VARCHAR(1000) := (SELECT var_value FROM devv5.config$ WHERE var_name='vocabulary_release_email');
   cRet TEXT;
   cVocabs VARCHAR(4000);
+  cVocabsDelim CONSTANT VARCHAR(1000) :=', ';
 BEGIN
   perform vocabulary_pack.pConceptAncestor();
   perform DEVV4.v5_to_v4();
   UPDATE VOCABULARY SET VOCABULARY_VERSION = 'v5.0 '||TO_CHAR(current_date,'DD-MON-YY') WHERE VOCABULARY_ID = 'None';
   perform vocabulary_pack.pCreateBaseDump();
   
-  SELECT string_agg(DISTINCT vocabulary_id, ', ' ORDER BY vocabulary_id)
+  SELECT string_agg(DISTINCT vocabulary_id, cVocabsDelim ORDER BY vocabulary_id)
   INTO cVocabs
   FROM (
         SELECT *
@@ -38,6 +39,16 @@ BEGIN
   IF cVocabs IS NOT NULL
     THEN
     cRet := cRet || crlf || 'Affected vocabularies: ' || cVocabs;
+    --store result in vocabulary_release_stat (20190207)
+    update devv5.vocabulary_release_stat vrs set latest_release_date=clock_timestamp()::date where vrs.vocabulary_id = any (string_to_array(cVocabs,cVocabsDelim));
+    insert into devv5.vocabulary_release_stat
+    (
+      select vocabulary_id, clock_timestamp()::date from (
+      select unnest(string_to_array(cVocabs,cVocabsDelim)) as vocabulary_id
+      except
+      select vocabulary_id from devv5.vocabulary_release_stat
+      ) as s0
+    );
   END IF;
 
   perform devv5.SendMailHTML (email, 'Release status [OK]', cRet);
@@ -47,7 +58,7 @@ BEGIN
   THEN
     GET STACKED DIAGNOSTICS cRet = PG_EXCEPTION_CONTEXT;
     cRet:='ERROR: '||SQLERRM||crlf||'CONTEXT: '||regexp_replace(cRet, '\r|\n|\r\n', crlf, 'g');
-    cRet := SUBSTR ('Release completed with errors:'||crlf||'<b>'||cRet||'</b>', 1, 5000);  
+    cRet := SUBSTR ('Release completed with errors:'||crlf||'<b>'||cRet||'</b>', 1, 5000);
     perform devv5.SendMailHTML (email, 'Release status [ERROR]', cRet);
 END;
 $body$

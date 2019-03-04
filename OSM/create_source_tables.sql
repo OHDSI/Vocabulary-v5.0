7cc4e4ac4114f20b360565770d3b62648784326e
Alex Davydov <alexander.davydovv@gmail.com>
2019-03-04 18:16:58 +0300

    OSM v1 release code

---
+++ OSM/create_source_tables.sql
@@ -0,0 +1,40 @@
+/**************************************************************************
+* Copyright 2016 Observational Health Data Sciences AND Informatics (OHDSI)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may NOT use this file except IN compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to IN writing, software
+* distributed under the License is distributed ON an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* Authors: Alex Davydov, Oleg Zhuk, Christian Reich
+* Date: 2018
+**************************************************************************/
+
+DROP TABLE IF EXISTS sources.osm_2019_02_25;
+CREATE TABLE sources.osm_2019_02_25
+(
+	gid integer,
+	id integer,
+	country varchar(254),
+	name varchar(254),
+	enname varchar(254),
+	locname varchar(254),
+	offname varchar(254),
+	boundary varchar(254),
+	adminlevel integer,
+	wikidata varchar(254),
+	wikimedia varchar(254),
+	timestamp varchar(254),
+	note varchar(254),
+	rpath varchar(254),
+	iso3166_2 varchar(254),
+	geom devv5.geometry(MultiPolygon,4326)
+)
+;

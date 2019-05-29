1129ca5f84766543abd06b46a67b3ae2f4e5eb88
Timur <timur.vakhitov@firstlinesoftware.com>
2019-05-29 17:34:29 +0300

    typo in CVX

--- working/packages/load_input_tables/load_input_tables.sql
+++ working/packages/load_input_tables/load_input_tables.sql
@@ -505,7 +505,7 @@ begin
       truncate table sources.cvx, sources.cvx_cpt, sources.cvx_vaccine;
       insert into sources.cvx select TRIM(CVX_CODE),TRIM(SHORT_DESCRIPTION),TRIM(FULL_VACCINE_NAME),LAST_UPDATED_DATE, pVocabularyDate, COALESCE(pVocabularyVersion,pVocabularyID||' '||current_date) from 
         sources.py_xlsparse_cvx_codes(pVocabularyPath||'/web_cvx.xlsx');
-      insert into sources.cvx_cpt select TRIM(CVX_CODE),TRIM(CPT_DESCRIPTION),TRIM(CVX_SHORT_DESCRIPTION),TRIM(CVX_CODE),TRIM(MAP_COMMENT),LAST_UPDATED_DATE, TRIM(CPT_CODE_ID) from 
+      insert into sources.cvx_cpt select TRIM(CPT_CODE),TRIM(CPT_DESCRIPTION),TRIM(CVX_SHORT_DESCRIPTION),TRIM(CVX_CODE),TRIM(MAP_COMMENT),LAST_UPDATED_DATE, TRIM(CPT_CODE_ID) from 
         sources.py_xlsparse_cvx_cpt(pVocabularyPath||'/web_cpt.xlsx');
       insert into sources.cvx_vaccine select TRIM(CVX_SHORT_DESCRIPTION),TRIM(CVX_CODE),TRIM(VACCINE_STATUS),TRIM(VG_NAME), TRIM(CVX_VACCINE_GROUP) from 
         sources.py_xlsparse_cvx_vaccine(pVocabularyPath||'/web_vax2vg.xlsx');

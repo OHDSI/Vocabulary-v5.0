3587b217a0913ff0f93ff42478a443b7ee58491c
Timur <timur.vakhitov@firstlinesoftware.com>
2019-09-03 17:35:04 +0300

    added new vocabulary='KDC7'

--- working/generic_update.sql
+++ working/generic_update.sql
@@ -195,6 +195,7 @@ BEGIN
 		WHEN c.vocabulary_id = 'HemOnc' THEN 1
 		WHEN c.vocabulary_id = 'NAACCR' THEN 1
 		WHEN c.vocabulary_id = 'JMDC' THEN 1
+		WHEN c.vocabulary_id = 'KCD7' THEN 1
 		ELSE 0 -- in default we will not deprecate
 	END = 1
 	AND c.vocabulary_id NOT IN ('CPT4', 'HCPCS', 'ICD9Proc');

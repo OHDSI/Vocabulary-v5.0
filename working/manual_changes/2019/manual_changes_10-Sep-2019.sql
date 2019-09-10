b409f51441f26811469f259c7e7e8b69cf96989a
Timur <timur.vakhitov@firstlinesoftware.com>
2019-09-10 14:13:09 +0300

    https://github.com/OHDSI/Vocabulary-v5.0/issues/245#issuecomment-528880225

---
+++ working/manual_changes/2019/manual_changes_10-Sep-2019.sql
@@ -0,0 +1,3 @@
+https://github.com/OHDSI/Vocabulary-v5.0/issues/245#issuecomment-528880225
+insert into concept_relationship values(32693,8756,'Is a',to_date ('19700101', 'YYYYMMDD'),to_date('20991231', 'YYYYMMDD'),null);
+insert into concept_relationship values(8756,32693,'Subsumes',to_date ('19700101', 'YYYYMMDD'),to_date('20991231', 'YYYYMMDD'),null);

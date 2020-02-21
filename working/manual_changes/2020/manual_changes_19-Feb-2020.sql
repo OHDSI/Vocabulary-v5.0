4a70f186e9a51da582269699ef8deaaf5da4e1b6
ekorchmar <korchmar.eduard@gmail.com>
2020-02-21 17:46:52 +0200

    Added missing concept_class

--- working/manual_changes/2020/manual_changes_19-Feb-2020.sql
+++ working/manual_changes/2020/manual_changes_19-Feb-2020.sql
@@ -13,3 +13,27 @@ BEGIN
 	pClick_disabled			=> NULL --NULL or 'Y'
 );
 END $_$;
+
+DO $$
+DECLARE
+    z    int;
+    ex   int;
+    pConcept_class_id constant varchar(100):='ICD10 Histology';
+    pConcept_class_name constant varchar(100):= 'ICD10 Histology';
+BEGIN
+    DROP SEQUENCE IF EXISTS v5_concept;
+
+    SELECT MAX (concept_id) + 1 INTO ex FROM concept
+      --WHERE concept_id>=200 and concept_id<1000; --only for VIP concepts
+    WHERE concept_id >= 31967 AND concept_id < 72245;
+    
+    EXECUTE 'CREATE SEQUENCE v5_concept INCREMENT BY 1 START WITH ' || ex || ' CACHE 20';
+    SELECT nextval('v5_concept') INTO z;
+
+    INSERT INTO concept (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date, invalid_reason)
+      VALUES (z, pConcept_class_name, 'Metadata', 'Concept Class', 'Concept Class', null, 'OMOP generated', TO_DATE ('19700101', 'YYYYMMDD'), TO_DATE ('20991231', 'YYYYMMDD'), null);
+    INSERT INTO concept_class (concept_class_id, concept_class_name, concept_class_concept_id)
+      VALUES (pConcept_class_id, pConcept_class_name, z);
+     
+    DROP SEQUENCE v5_concept;
+END $$;

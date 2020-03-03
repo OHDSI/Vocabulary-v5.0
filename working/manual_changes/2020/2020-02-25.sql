a5ff39206e78f70cff3690c46aa51e780ca7e409
Timur <hardhouse@users.noreply.github.com>
2020-03-03 12:11:28 +0300

    refactoring

--- working/manual_changes/2020/2020-02-25.sql
+++ working/manual_changes/2020/2020-02-25.sql
@@ -1,7 +1,13 @@
---update LOINC Parts start date
+--update LOINC Parts start date (run only once after generic_update)
 UPDATE concept
-SET valid_start_date = TO_DATE('19700101','yyyymmdd')
+SET valid_start_date = TO_DATE('19700101', 'yyyymmdd')
 WHERE vocabulary_id = 'LOINC'
-    AND concept_class_id IN ('LOINC Component', 'LOINC Method', 'LOINC Property', 'LOINC Scale', 'LOINC System', 'LOINC Time')
-    AND valid_start_date = '2019-12-13'
-;
+	AND concept_class_id IN (
+		'LOINC Component',
+		'LOINC Method',
+		'LOINC Property',
+		'LOINC Scale',
+		'LOINC System',
+		'LOINC Time'
+		)
+	AND valid_start_date = TO_DATE('20191213', 'yyyymmdd');

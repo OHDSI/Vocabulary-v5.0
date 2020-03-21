e8d516390073ae10fb1b7222285a97dfe2cfe71e
Vlad.korsik@odysseusinc.com <Vlad.korsik@odysseusinc.com>
2020-03-21 16:25:09 +0300

    preliminary ingested to dev_cap

--- CAP/load_stage.sql
+++ CAP/load_stage.sql
@@ -336,13 +336,93 @@ VALUES ('CAP Value of',
             WHERE concept_name='Protocol of')
        )
 ;
+--1. Update latest_update field to new date
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.SetLatestUpdate(
+	pVocabularyName			=> 'CAP',
+	pVocabularyDate			=> to_date('20190828', 'yyyymmdd'), -- here i put the date version of first version;
+	pVocabularyVersion		=> 'CAP eCC release, Aug 2019',
+	pVocabularyDevSchema	=> 'DEV_CAP'
+);
+END $_$;
 
+
+--2. Truncate all working tables
+TRUNCATE TABLE concept_stage;
+TRUNCATE TABLE concept_relationship_stage;
+TRUNCATE TABLE concept_synonym_stage;
+TRUNCATE TABLE pack_content_stage;
+TRUNCATE TABLE drug_strength_stage;
 
 
 
 
 
 -- Load into concept_stage from cap_breast_2019_concept_stage_preliminary
+DROP TABLE IF EXISTS dev_cap.cap_breast_2019_concept_stage_preliminary
+CREATE UNLOGGED TABLE dev_cap.cap_breast_2019_concept_stage_preliminary WITH OIDS AS
+    (
+        SELECT NULL                        AS concept_id,
+               source_code                 AS concept_code,
+               source_description          AS concept_name,
+               alt_source_description AS alternative_concept_name,
+     CASE
+         WHEN source_class ='CAP Protocol'       or    (source_class='S'       AND source_description !~*'^Distance')  THEN 'Observation' -- todo How to treat 'CAP Protocol' in domain_id?
+         WHEN source_class = 'LI' /*AND source_description !~* '^\.*other|^\.*specif.*'*/ THEN  'Meas Value' --decided to leave them as values
+         ELSE 'Measurement'
+         END                               AS domain_id,
+               'CAP'                       AS vocabulary_id,
+     CASE
+         WHEN source_class = 'S'    AND source_description !~*'^Distance'                                     THEN 'CAP Header' -- or 'CAP section'
+         WHEN source_class = 'LI' /*AND source_description !~* '^\.*other|^\.*specif.*'*/  THEN 'CAP Value' -- ^.*expla.* todo do we need them to be variables, decided to leave them as values
+         WHEN source_class = 'CAP Protocol'                              THEN 'CAP Protocol'
+         ELSE 'CAP Variable'
+         END                               AS concept_class_id,
+               NULL                        AS standard_concept,
+               NULL                        AS invalid_reason,
+               '1970-01-01'                AS valid_start_date, -- AT LEAST FOR NOW
+               '2099-12-31'                AS valid_end_date,
+               source_filename,
+               source_class
+        FROM cap_prepared_breast_2019_source
+        WHERE source_class <> 'DI' -- to exclude them from CS because of lack of sense
+        ORDER BY concept_name, concept_code, concept_class_id
+    )
+;
+DROP TABLE IF EXISTS dev_cap.cap_breast_2020_concept_stage_preliminary
+CREATE UNLOGGED TABLE dev_cap.cap_breast_2020_concept_stage_preliminary WITH OIDS AS
+    (
+        SELECT NULL                        AS concept_id,
+               source_code                 AS concept_code,
+               source_description          AS concept_name,
+               alt_source_description AS alternative_concept_name,
+     CASE
+         WHEN source_class ='CAP Protocol'       or    (source_class='S'       AND source_description !~*'^Distance')  THEN 'Observation' -- todo How to treat 'CAP Protocol' in domain_id?
+         WHEN source_class = 'LI' /*AND source_description !~* '^\.*other|^\.*specif.*'*/ THEN  'Meas Value' --decided to leave them as values
+         ELSE 'Measurement'
+         END                               AS domain_id,
+               'CAP'                       AS vocabulary_id,
+     CASE
+         WHEN source_class = 'S'    AND source_description !~*'^Distance'                                     THEN 'CAP Header' -- or 'CAP section'
+         WHEN source_class = 'LI' /*AND source_description !~* '^\.*other|^\.*specif.*'*/  THEN 'CAP Value' -- ^.*expla.* todo do we need them to be variables, decided to leave them as values
+         WHEN source_class = 'CAP Protocol'                              THEN 'CAP Protocol'
+         ELSE 'CAP Variable'
+         END                               AS concept_class_id,
+               NULL                        AS standard_concept,
+               NULL                        AS invalid_reason,
+               '1970-01-01'                AS valid_start_date, -- AT LEAST FOR NOW
+               '2099-12-31'                AS valid_end_date,
+               source_filename,
+               source_class
+        FROM cap_prepared_breast_2020_source
+        WHERE source_class <> 'DI' -- to exclude them from CS because of lack of sense
+        ORDER BY concept_name, concept_code, concept_class_id
+    )
+;
+
+
+
 INSERT INTO dev_cap.CONCEPT_STAGE (
                            concept_name,
                            domain_id,
@@ -717,25 +797,18 @@ AND e.level_of_separation =1
 
 --- CHECKS CRS resulting
 -- check af multiple relationships
-SELECT distinct concept_code,concept_name,concept_class_id
-FROM cap_breast_2020_concept_stage_preliminary c
-WHERE NOT EXISTS (select 1
-                FROM cap_breast_2020_concept_relationship_stage_preliminary cr1
-    WHERE cr1.concept_code_1=c.concept_code)
-ORDER BY concept_name
-;
 -- for only one relationship created
 SELECT *
-FROM cap_breast_2020_concept_relationship_stage_preliminary
+FROM concept_relationship_stage
 WHERE concept_code_1 IN
 (SELECT concept_code_1
-    FROM cap_breast_2020_concept_relationship_stage_preliminary
+    FROM concept_relationship_stage
     GROUP BY concept_code_1
     having count(relationship_id)=1)
 ;
 -- Check for uniqueness of pair concept_code_1, concept_code_2
 select concept_code_1, concept_code_2
-from dev_cap.cap_breast_2020_concept_relationship_stage_preliminary
+from dev_cap.concept_relationship_stage
 group by concept_code_1, concept_code_2 having count(1) > 1
 ;
 
@@ -743,82 +816,64 @@ group by concept_code_1, concept_code_2 having count(1) > 1
 --all the selects below should return null
 
 
-select relationship_id from cap_breast_2020_concept_relationship_stage_preliminary
+select relationship_id from concept_relationship_stage
 except
 select relationship_id from relationship;
 
 
-select concept_class_id from cap_breast_2020_concept_stage_preliminary
+select concept_class_id from concept_stage
 except
 select concept_class_id from concept_class;
 
 
-select domain_id from cap_breast_2020_concept_stage_preliminary
+select domain_id from concept_stage
 except
 select domain_id from domain;
 
 
-select vocabulary_id from cap_breast_2020_concept_stage_preliminary
+select vocabulary_id from concept_stage
 except
 select vocabulary_id from vocabulary;
 
 
-select * from cap_breast_2020_concept_stage_preliminary where concept_name is null or domain_id is null or concept_class_id is null or concept_code is null or valid_start_date is null or valid_end_date is null
+select * from concept_stage where concept_name is null or domain_id is null or concept_class_id is null or concept_code is null or valid_start_date is null or valid_end_date is null
 or valid_end_date is null or concept_name<>trim(concept_name) or concept_code<>trim(concept_code);
 
-select concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id  from cap_breast_2020_concept_relationship_stage_preliminary
+select concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id  from concept_relationship_stage
 group by concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id  having count(*)>1;
 
-select concept_code, vocabulary_id  from cap_breast_2020_concept_stage_preliminary
+select concept_code, vocabulary_id  from concept_stage
 group by concept_code, vocabulary_id  having count(*)>1;
 
 
-select * From cap_breast_2020_concept_relationship_stage_preliminary where valid_start_date is null or valid_end_date is null or (invalid_reason is null and valid_end_date::date<>to_date ('20991231', 'yyyymmdd'))
-or (invalid_reason is not null and valid_end_date::date=to_date ('20991231', 'yyyymmdd'));
+select * From concept_relationship_stage where valid_start_date is null or valid_end_date is null or (invalid_reason is null and valid_end_date<>to_date ('20991231', 'yyyymmdd'))
+or (invalid_reason is not null and valid_end_date=to_date ('20991231', 'yyyymmdd'));
 
-select * from cap_breast_2020_concept_stage_preliminary where valid_start_date is null or valid_end_date is null
+select * from concept_stage where valid_start_date is null or valid_end_date is null
 or (invalid_reason is null and valid_end_date::date <> to_date ('20991231', 'yyyymmdd') and vocabulary_id not in ('CPT4', 'HCPCS', 'ICD9Proc'))
 or (invalid_reason is not null and valid_end_date::date = to_date ('20991231', 'yyyymmdd'))
 or valid_start_date::date < to_date ('19000101', 'yyyymmdd'); -- some concepts have a real date < 1970
 ;
 
-select relationship_id from cap_breast_2020_concept_relationship_stage_preliminary
-except
-select relationship_id from relationship;
 
 
-select concept_class_id from cap_breast_2020_concept_stage_preliminary
-except
-select concept_class_id from concept_class;
-
-
-select domain_id from cap_breast_2020_concept_stage_preliminary
-except
-select domain_id from domain;
-
-
-select vocabulary_id from cap_breast_2020_concept_stage_preliminary
-except
-select vocabulary_id from vocabulary;
-
-
-select * from cap_breast_2020_concept_stage_preliminary where concept_name is null or domain_id is null or concept_class_id is null or concept_code is null or valid_start_date is null or valid_end_date is null
+select * from concept_stage where concept_name is null or domain_id is null or concept_class_id is null or concept_code is null or valid_start_date is null or valid_end_date is null
 or valid_end_date is null or concept_name<>trim(concept_name) or concept_code<>trim(concept_code);
 
-select concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id  from cap_breast_2020_concept_relationship_stage_preliminary
+select concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id  from concept_relationship_stage
 group by concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id  having count(*)>1;
 
-select concept_code, vocabulary_id  from cap_breast_2020_concept_stage_preliminary
+select concept_code, vocabulary_id  from concept_stage
 group by concept_code, vocabulary_id  having count(*)>1;
 
 
 
 SELECT crm.*
-FROM cap_breast_2020_concept_relationship_stage_preliminary crm
+FROM concept_relationship_stage crm
 	 LEFT JOIN concept c1 ON c1.concept_code = crm.concept_code_1 AND c1.vocabulary_id = crm.vocabulary_id_1
-	 LEFT JOIN cap_breast_2020_concept_stage_preliminary cs1 ON cs1.concept_code = crm.concept_code_1 AND cs1.vocabulary_id = crm.vocabulary_id_1
+	 LEFT JOIN concept_stage cs1 ON cs1.concept_code = crm.concept_code_1 AND cs1.vocabulary_id = crm.vocabulary_id_1
 	 LEFT JOIN concept c2 ON c2.concept_code = crm.concept_code_2 AND c2.vocabulary_id = crm.vocabulary_id_2
-	 LEFT JOIN cap_breast_2020_concept_stage_preliminary cs2 ON cs2.concept_code = crm.concept_code_2 AND cs2.vocabulary_id = crm.vocabulary_id_2
+	 LEFT JOIN concept_stage cs2 ON cs2.concept_code = crm.concept_code_2 AND cs2.vocabulary_id = crm.vocabulary_id_2
 	 LEFT JOIN vocabulary v1 ON v1.vocabulary_id = crm.vocabulary_id_1
 	 LEFT JOIN vocabulary v2 ON v2.vocabulary_id = crm.vocabulary_id_2
 	 LEFT JOIN relationship rl ON rl.relationship_id = crm.relationship_id
@@ -830,13 +885,12 @@ WHERE    (c1.concept_code IS NULL AND cs1.concept_code IS NULL)
 	 OR crm.valid_start_date::date > CURRENT_DATE
 	 OR crm.valid_end_date::date < crm.valid_start_date::date;
 
+-- generic update()
+select devv5.genericupdate()
 
 
 
 
-
-
--- TODO reverse relationships need to be created
 --dev_lexicon - for Nebraska_Lexicon mappings
 select distinct cs.concept_code,cs.concept_name, cs.concept_class_id, cs.alternative_concept_name, n.*,
                 CASE WHEN length(n.concept_name)>20 then 'CHECK' else '' END AS comment

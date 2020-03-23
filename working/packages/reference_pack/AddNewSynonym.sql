e82b82601c217117ca9314dd4191cc48d4c725be
Timur <timur.vakhitov@firstlinesoftware.com>
2020-03-23 12:18:40 +0300

    improvements

--- working/packages/reference_pack/AddNewSynonym.sql
+++ working/packages/reference_pack/AddNewSynonym.sql
@@ -28,6 +28,28 @@ BEGIN
     pLanguage_concept_id =>4180186
 );
 END $_$;
+
+OR you can combine it with itself:
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewSynonym(
+  pConcept_id=>vocabulary_pack.AddNewSynonym(
+      pConcept_id          =>vocabulary_pack.AddNewConcept(
+                              pConcept_name     =>'test concept name 5',
+                              pDomain_id        =>'Drug',
+                              pVocabulary_id    =>'SNOMED',
+                              pConcept_class_id =>'Ingredient',
+                              pStandard_concept =>'S',
+                              pConcept_code     =>'123_test5'
+                          ),
+      pSynonym_name        =>'test synonym 5',
+      pLanguage_concept_id =>4180186
+  ),
+  pSynonym_name        =>'test synonym 6',
+  pLanguage_concept_id =>4180186
+);
+END $_$;
 */
 
 CREATE OR REPLACE FUNCTION vocabulary_pack.AddNewSynonym (
@@ -35,11 +57,8 @@ CREATE OR REPLACE FUNCTION vocabulary_pack.AddNewSynonym (
   pSynonym_name concept_synonym.concept_synonym_name%TYPE,
   pLanguage_concept_id concept_synonym.language_concept_id%TYPE = 4180186 /*English*/
 )
-RETURNS void AS
+RETURNS int4 AS
 $BODY$
-DECLARE
-  z  INT;
-  ex INT;
 BEGIN
   pSynonym_name:=REGEXP_REPLACE(pSynonym_name, '[[:cntrl:]]+', ' ', 'g');
   pSynonym_name:=REGEXP_REPLACE(pSynonym_name, ' {2,}', ' ', 'g');
@@ -48,6 +67,8 @@ BEGIN
 
   INSERT INTO concept_synonym (concept_id, concept_synonym_name, language_concept_id)
     VALUES (pConcept_id, pSynonym_name, pLanguage_concept_id);
+  
+  RETURN pConcept_id;
 END;
 $BODY$
 LANGUAGE 'plpgsql'

f5f27aeeaf68bd9a4a0bc67d77ba55e342f47367
Alexander <32357692+Alexdavv@users.noreply.github.com>
2020-03-25 15:35:16 +0300

    Update BVRS-GamVac-Combi.sql

--- working/manual_changes/2020/BVRS-GamVac-Combi.sql
+++ working/manual_changes/2020/BVRS-GamVac-Combi.sql
@@ -1,14 +1,18 @@
---add new concept with synonyms
+--add new concepts with synonyms
 DO $_$
 BEGIN
+PERFORM vocabulary_pack.AddNewSynonym(
       pConcept_id =>vocabulary_pack.AddNewConcept(
-                     pConcept_name     =>'BVRS-GamVac-Combi',
+                     pConcept_name     =>'Middle East Respiratory Syndrome combined heterologous adenoviral-based vector vaccine',
                      pDomain_id        =>'Drug',
                      pVocabulary_id    =>'RxNorm Extension',
                      pConcept_class_id =>'Ingredient',
                      pStandard_concept =>'S',
                      pConcept_code     =>'OMOP4873977'
-      );
+                          ),
+      pSynonym_name        =>'BVRS-GamVac-Combi',
+      pLanguage_concept_id =>4180186
+  );
 END $_$;
 
 DO $_$
@@ -23,17 +27,28 @@ BEGIN
       );
 END $_$;
 
+
 DO $_$
 BEGIN
+PERFORM vocabulary_pack.AddNewSynonym(
+  pConcept_id=>vocabulary_pack.AddNewSynonym(
       pConcept_id =>vocabulary_pack.AddNewConcept(
-                     pConcept_name     =>'CD24Fc',
+                     pConcept_name     =>'CD24 Extracellular Domain-IgG1 Fc Domain Recombinant Fusion Protein CD24Fc',
                      pDomain_id        =>'Drug',
                      pVocabulary_id    =>'RxNorm Extension',
                      pConcept_class_id =>'Ingredient',
                      pStandard_concept =>'S',
                      pConcept_code     =>'OMOP4873979'
-      );
+                          ),
+      pSynonym_name        =>'CD24Fc CD24IgG',
+      pLanguage_concept_id =>4180186
+  ),
+  pSynonym_name        =>'CD24Fc',
+  pLanguage_concept_id =>4180186
+);
 END $_$;
+
+
 
 
 DO $_$
@@ -51,14 +66,19 @@ PERFORM vocabulary_pack.AddNewSynonym(
       pSynonym_name        =>'Tetradrine',
       pLanguage_concept_id =>4180186
   ),
-  pSynonym_name        =>'Isotetrandrine',
+  pSynonym_name        =>'Fanchinine',
   pLanguage_concept_id =>4180186
 );
 END $_$;
 
 
+
+
+
+
 DO $_$
 BEGIN
+PERFORM vocabulary_pack.AddNewSynonym(
       pConcept_id =>vocabulary_pack.AddNewConcept(
                      pConcept_name     =>'Xiyanping',
                      pDomain_id        =>'Drug',
@@ -66,5 +86,8 @@ BEGIN
                      pConcept_class_id =>'Ingredient',
                      pStandard_concept =>'S',
                      pConcept_code     =>'OMOP4873981'
-);
+                          ),
+      pSynonym_name        =>'喜炎平',
+      pLanguage_concept_id =>4182948
+  );
 END $_$;

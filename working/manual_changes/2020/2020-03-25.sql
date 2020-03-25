bf1719f36671f577f87b6cc757def78cc72a4a68
Timur <timur.vakhitov@firstlinesoftware.com>
2020-03-25 15:47:30 +0300

    fix typo

--- working/manual_changes/2020/2020-03-25.sql
+++ working/manual_changes/2020/2020-03-25.sql
@@ -17,14 +17,14 @@ END $_$;
 
 DO $_$
 BEGIN
-      pConcept_id =>vocabulary_pack.AddNewConcept(
-                     pConcept_name     =>'Carrimycin',
-                     pDomain_id        =>'Drug',
-                     pVocabulary_id    =>'RxNorm Extension',
-                     pConcept_class_id =>'Ingredient',
-                     pStandard_concept =>'S',
-                     pConcept_code     =>'OMOP4873978'
-      );
+PERFORM vocabulary_pack.AddNewConcept(
+      pConcept_name     =>'Carrimycin',
+      pDomain_id        =>'Drug',
+      pVocabulary_id    =>'RxNorm Extension',
+      pConcept_class_id =>'Ingredient',
+      pStandard_concept =>'S',
+      pConcept_code     =>'OMOP4873978'
+);
 END $_$;
 
 DO $_$

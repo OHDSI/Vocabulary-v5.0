e2e1389d9febe0cd734bfa9775ff7c5ecf1db157
Timur <timur.vakhitov@firstlinesoftware.com>
2020-05-12 12:35:32 +0300

    added OMOP Extension

--- working/generic_update.sql
+++ working/generic_update.sql
@@ -201,6 +201,7 @@ BEGIN
 		WHEN c.vocabulary_id = 'ICD10CN' THEN 1
 		WHEN c.vocabulary_id = 'ICD9ProcCN' THEN 1
 		WHEN c.vocabulary_id = 'CAP' THEN 1
+		WHEN c.vocabulary_id = 'OMOP Extension' THEN 0
 		ELSE 0 -- in default we will not deprecate
 	END = 1
 	AND c.vocabulary_id NOT IN ('CPT4', 'HCPCS', 'ICD9Proc');

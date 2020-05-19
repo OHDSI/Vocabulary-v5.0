16f2addcf86b5f4ef582e59ca84fe1b0f71b053b
Timur <timur.vakhitov@firstlinesoftware.com>
2020-05-19 13:40:50 +0300

    added 'Language' parsing and loading [AVOF-2497]

--- SNOMED/readme.md
+++ SNOMED/readme.md
@@ -13,28 +13,36 @@ Prerequisites:
 - sct2_Relationship_Full_INT_YYYYMMDD.txt
 from the folder \Full\Refset\Map:
 - der2_sRefset_SimpleMapFull_INT_YYYYMMDD.txt
-Rename files to sct2_Concept_Full_INT.txt, sct2_Description_Full-en_INT.txt, sct2_Relationship_Full_INT.txt, der2_sRefset_SimpleMapFull_INT.txt
+from the folder \Full\Refset\Language
+- der2_cRefset_LanguageFull-en_INT_YYYYMMDD.txt
+Rename files to sct2_Concept_Full_INT.txt, sct2_Description_Full-en_INT.txt, sct2_Relationship_Full_INT.txt, der2_sRefset_SimpleMapFull_INT.txt, der2_cRefset_LanguageFull_INT.txt
 
 4. Download the British SNOMED file uk_sct2cl_xx.x.x__YYYYMMDD000001.zip from https://isd.digital.nhs.uk/trud3/user/authenticated/group/0/pack/26/subpack/101/releases.
 5. Extract the following files from the folder SnomedCT_UKClinicalRF2_Production_YYYYMMDDTzzzzzz\Full\Terminology into a working folder:
 - sct2_Concept_Full_GB1000000_YYYYMMDD.txt
 - sct2_Description_Full-en-GB_GB1000000_YYYYMMDD.txt
 - sct2_Relationship_Full-GB_GB1000000_YYYYMMDD.txt
-Rename files to sct2_Concept_Full-UK.txt, sct2_Description_Full-UK.txt, sct2_Relationship_Full-UK.txt
+from the folder \Full\Refset\Language
+- der2_cRefset_LanguageFull-en-GB_GB1000000_YYYYMMDD.txt
+Rename files to sct2_Concept_Full-UK.txt, sct2_Description_Full-UK.txt, sct2_Relationship_Full-UK.txt, der2_cRefset_LanguageFull_UK.txt
 
 6. Download the US SNOMED file SnomedCT_USEditionRF2_PRODUCTION_YYYYMMDDTzzzzzzZ.zip from https://www.nlm.nih.gov/healthit/snomedct/us_edition.html
 7. Extract the following files from the folder \Full\Terminology\ into a working folder:
 - sct2_Concept_Full_US1000124_YYYYMMDD.txt
 - sct2_Description_Full-en_US1000124_YYYYMMDD.txt
 - sct2_Relationship_Full_US1000124_YYYYMMDD.txt
-Remove date from file name and rename to sct2_Concept_Full_US.txt, sct2_Description_Full-en_US.txt, sct2_Relationship_Full_US.txt
+from the folder \Full\Refset\Language
+- der2_cRefset_LanguageFull-en_US1000124_YYYYMMDD.txt
+Remove date from file name and rename to sct2_Concept_Full_US.txt, sct2_Description_Full-en_US.txt, sct2_Relationship_Full_US.txt, der2_cRefset_LanguageFull_US.txt
 
 8. Download the UK SNOMED CT Drug Extension, RF2 file uk_sct2dr_xx.x.x__YYYYMMDD000001.zip from https://isd.digital.nhs.uk/trud3/user/authenticated/group/0/pack/26/subpack/105/releases
 9. Extract the following files from the folder SnomedCT_UKDrugRF2_Production_20180516T000001Z\Full\Terminology\ into a working folder:
 - sct2_Concept_Full_GB1000000_YYYYMMDD.txt
 - sct2_Description_Full-en-GB_GB1000000_YYYYMMDD.txt
 - sct2_Relationship_Full_GB1000000_YYYYMMDD.txt
-Rename files to sct2_Concept_Full_GB_DE.txt, sct2_Description_Full-en-GB_DE.txt, sct2_Relationship_Full_GB_DE.txt
+from the folder \Full\Refset\Language
+- der2_cRefset_LanguageFull-en-GB_GB1000001_YYYYMMDD.txt
+Rename files to sct2_Concept_Full_GB_DE.txt, sct2_Description_Full-en-GB_DE.txt, sct2_Relationship_Full_GB_DE.txt, der2_cRefset_LanguageFull_GB_DE.txt
 
 10. Extract
 - der2_cRefset_AssociationFull_INT_YYYYMMDD.txt from SnomedCT_InternationalRF2_Production_YYYYMMDDTzzzzzz\Full\Refset\Content
--- working/packages/vocabulary_download/bash_functions_snomed.sql
+++ working/packages/vocabulary_download/bash_functions_snomed.sql
@@ -1,6 +1,7 @@
 /*
 	20190204 added flag -C to unzip (only SNOMED part) = case insensitive
 	20200225 added extraction of der2_Refset_SimpleFull_INT.txt
+	20200519 added extraction of der2_cRefset_LanguageFull.txt
 */
 
 --INT part
@@ -19,16 +20,18 @@ BEGIN
     unzip -oqjC "$2" "SnomedCT_InternationalRF2_PRODUCTION_*/Full/Terminology/sct2_Description_Full-en_INT_*.txt" -d . && \
     unzip -oqjC "$2" "SnomedCT_InternationalRF2_PRODUCTION_*/Full/Terminology/sct2_Relationship_Full_INT_*.txt" -d . && \
     unzip -oqjC "$2" "SnomedCT_InternationalRF2_PRODUCTION_*/Full/Refset/Content/der2_cRefset_AssociationFull_INT_*.txt" -d . && \
-    unzip -oqjC "$2" "SnomedCT_InternationalRF2_PRODUCTION_*/Full/Refset/Map/der2_sRefset_SimpleMapFull_INT_*.txt" -d .
+    unzip -oqjC "$2" "SnomedCT_InternationalRF2_PRODUCTION_*/Full/Refset/Map/der2_sRefset_SimpleMapFull_INT_*.txt" -d . && \
+    unzip -oqjC "$2" "SnomedCT_InternationalRF2_PRODUCTION_*/Full/Refset/Language/der2_cRefset_LanguageFull*.txt" -d .
         
     #move result to original folder
     cd "$1"
-    rm -f "sct2_Concept_Full_INT.txt" "sct2_Description_Full-en_INT.txt" "sct2_Relationship_Full_INT.txt" "der2_cRefset_AssociationFull_INT.txt" "der2_sRefset_SimpleMapFull_INT.txt"
+    rm -f "sct2_Concept_Full_INT.txt" "sct2_Description_Full-en_INT.txt" "sct2_Relationship_Full_INT.txt" "der2_cRefset_AssociationFull_INT.txt" "der2_sRefset_SimpleMapFull_INT.txt" "der2_sRefset_LanguageFull_INT.txt"
     mv work/sct2_Concept_Full_INT_*.txt "sct2_Concept_Full_INT.txt" && \
     mv work/sct2_Description_Full-en_INT_*.txt "sct2_Description_Full-en_INT.txt" && \
     mv work/sct2_Relationship_Full_INT_*.txt "sct2_Relationship_Full_INT.txt" && \
-    mv work/der2_cRefset_AssociationFull_INT_*.txt "der2_cRefset_AssociationFull_INT.txt"
-    mv work/der2_sRefset_SimpleMapFull_INT_*.txt "der2_sRefset_SimpleMapFull_INT.txt"
+    mv work/der2_cRefset_AssociationFull_INT_*.txt "der2_cRefset_AssociationFull_INT.txt" && \
+    mv work/der2_sRefset_SimpleMapFull_INT_*.txt "der2_sRefset_SimpleMapFull_INT.txt" && \
+    mv work/der2_cRefset_LanguageFull*.txt "der2_sRefset_LanguageFull_INT.txt"
     $BODY$
     LANGUAGE 'plsh'
     SECURITY DEFINER;
@@ -52,15 +55,17 @@ BEGIN
     unzip -oqjC "$2" "SnomedCT_UKClinicalRF2_PRODUCTION_*/Full/Terminology/sct2_Concept_Full*.txt" -d . && \
     unzip -oqjC "$2" "SnomedCT_UKClinicalRF2_PRODUCTION_*/Full/Terminology/sct2_Description_Full*.txt" -d . && \
     unzip -oqjC "$2" "SnomedCT_UKClinicalRF2_PRODUCTION_*/Full/Terminology/sct2_Relationship_Full*.txt" -d . && \
-    unzip -oqjC "$2" "SnomedCT_UKClinicalRF2_PRODUCTION_*/Full/Refset/Content/der2_cRefset_AssociationFull_GB*.txt" -d .
+    unzip -oqjC "$2" "SnomedCT_UKClinicalRF2_PRODUCTION_*/Full/Refset/Content/der2_cRefset_AssociationFull_GB*.txt" -d . && \
+    unzip -oqjC "$2" "SnomedCT_UKClinicalRF2_PRODUCTION_*/Full/Refset/Language/der2_cRefset_LanguageFull*.txt" -d .
         
     #move result to original folder
     cd "$1"
-    rm -f "sct2_Concept_Full-UK.txt" "sct2_Description_Full-UK.txt" "sct2_Relationship_Full-UK.txt" "der2_cRefset_AssociationFull_UK.txt"
+    rm -f "sct2_Concept_Full-UK.txt" "sct2_Description_Full-UK.txt" "sct2_Relationship_Full-UK.txt" "der2_cRefset_AssociationFull_UK.txt" "der2_sRefset_LanguageFull_UK.txt"
     mv work/sct2_Concept_Full*.txt "sct2_Concept_Full-UK.txt" && \
     mv work/sct2_Description_Full*.txt "sct2_Description_Full-UK.txt" && \
     mv work/sct2_Relationship_Full*.txt "sct2_Relationship_Full-UK.txt" && \
-    mv work/der2_cRefset_AssociationFull*.txt "der2_cRefset_AssociationFull_UK.txt"
+    mv work/der2_cRefset_AssociationFull*.txt "der2_cRefset_AssociationFull_UK.txt" && \
+    mv work/der2_cRefset_LanguageFull*.txt "der2_sRefset_LanguageFull_UK.txt"
     $BODY$
     LANGUAGE 'plsh'
     SECURITY DEFINER;
@@ -84,15 +89,17 @@ BEGIN
     unzip -oqjC "$2" "SnomedCT_USEditionRF2_PRODUCTION_*/Full/Terminology/sct2_Concept_Full_*.txt" -d . && \
     unzip -oqjC "$2" "SnomedCT_USEditionRF2_PRODUCTION_*/Full/Terminology/sct2_Description_Full-en_US*.txt" -d . && \
     unzip -oqjC "$2" "SnomedCT_USEditionRF2_PRODUCTION_*/Full/Terminology/sct2_Relationship_Full_*.txt" -d . && \
-    unzip -oqjC "$2" "SnomedCT_USEditionRF2_PRODUCTION_*/Full/Refset/Content/der2_cRefset_AssociationFull_US*.txt" -d .
+    unzip -oqjC "$2" "SnomedCT_USEditionRF2_PRODUCTION_*/Full/Refset/Content/der2_cRefset_AssociationFull_US*.txt" -d . && \
+    unzip -oqjC "$2" "SnomedCT_USEditionRF2_PRODUCTION_*/Full/Refset/Language/der2_cRefset_LanguageFull*.txt" -d .
         
     #move result to original folder
     cd "$1"
-    rm -f "sct2_Concept_Full_US.txt" "sct2_Description_Full-en_US.txt" "sct2_Relationship_Full_US.txt" "der2_cRefset_AssociationFull_US.txt"
+    rm -f "sct2_Concept_Full_US.txt" "sct2_Description_Full-en_US.txt" "sct2_Relationship_Full_US.txt" "der2_cRefset_AssociationFull_US.txt" "der2_sRefset_LanguageFull_US.txt"
     mv work/sct2_Concept_Full_US*.txt "sct2_Concept_Full_US.txt" && \
     mv work/sct2_Description_Full-en_US*.txt "sct2_Description_Full-en_US.txt" && \
     mv work/sct2_Relationship_Full_US*.txt "sct2_Relationship_Full_US.txt" && \
-    mv work/der2_cRefset_AssociationFull_US*.txt "der2_cRefset_AssociationFull_US.txt"
+    mv work/der2_cRefset_AssociationFull_US*.txt "der2_cRefset_AssociationFull_US.txt" && \
+    mv work/der2_cRefset_LanguageFull*.txt "der2_sRefset_LanguageFull_US.txt"
     $BODY$
     LANGUAGE 'plsh'
     SECURITY DEFINER;
@@ -116,15 +123,17 @@ BEGIN
     unzip -oqjC "$2" "SnomedCT_UKDrugRF2_Production_*/Full/Terminology/sct2_Concept_Full_GB*.txt" -d . && \
     unzip -oqjC "$2" "SnomedCT_UKDrugRF2_Production_*/Full/Terminology/sct2_Description_Full-en_GB*.txt" -d . && \
     unzip -oqjC "$2" "SnomedCT_UKDrugRF2_Production_*/Full/Terminology/sct2_Relationship_Full_GB*.txt" -d . && \
-    unzip -oqjC "$2" "SnomedCT_UKDrugRF2_Production_*/Full/Refset/Content/der2_cRefset_AssociationFull_GB*.txt" -d .
+    unzip -oqjC "$2" "SnomedCT_UKDrugRF2_Production_*/Full/Refset/Content/der2_cRefset_AssociationFull_GB*.txt" -d . && \
+    unzip -oqjC "$2" "SnomedCT_UKDrugRF2_Production_*/Full/Refset/Language/der2_cRefset_LanguageFull*.txt" -d .
         
     #move result to original folder
     cd "$1"
-    rm -f "sct2_Concept_Full_GB_DE.txt" "sct2_Description_Full-en-GB_DE.txt" "sct2_Relationship_Full_GB_DE.txt" "der2_cRefset_AssociationFull_GB_DE.txt"
+    rm -f "sct2_Concept_Full_GB_DE.txt" "sct2_Description_Full-en-GB_DE.txt" "sct2_Relationship_Full_GB_DE.txt" "der2_cRefset_AssociationFull_GB_DE.txt" "der2_sRefset_LanguageFull_GB_DE.txt"
     mv work/sct2_Concept_Full_GB*.txt "sct2_Concept_Full_GB_DE.txt" && \
     mv work/sct2_Description_Full-en_GB*.txt "sct2_Description_Full-en-GB_DE.txt" && \
     mv work/sct2_Relationship_Full_GB*.txt "sct2_Relationship_Full_GB_DE.txt" && \
-    mv work/der2_cRefset_AssociationFull_GB*.txt "der2_cRefset_AssociationFull_GB_DE.txt"
+    mv work/der2_cRefset_AssociationFull_GB*.txt "der2_cRefset_AssociationFull_GB_DE.txt" && \
+    mv work/der2_cRefset_LanguageFull*.txt "der2_sRefset_LanguageFull_GB_DE.txt"
     $BODY$
     LANGUAGE 'plsh'
     SECURITY DEFINER;

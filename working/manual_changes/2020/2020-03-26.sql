1384861a86db2d07358a6c854f6addc6412b57d4
Vlad.korsik@odysseusinc.com <Vlad.korsik@odysseusinc.com>
2020-05-26 18:47:42 +0300

    Load_stage changes: 1)New restriction is added: Concepts with NULL populated alternative_names were not included 2) To incorporate all CAP protocols filename ilike '%breast%' was deleted 3) To ingest all concept_names according to CDM permissions, substring and concatenation with '...'-symbol were applied 4) For newly created CAP Protocol concept_class_codes the limitation for length >50 chars was used. (this approach was applied to build the 'Has CAP protocol' relationship, too.
    Deleted 2 conflicted files:
    1) working/manual_changes/2020/2020-03-26.sql 2)  CAP/C_R_M_preparation.sql.
    2020-03-26.sql File recreated
    Attempt to rebase.

---
+++ working/manual_changes/2020/2020-03-26.sql
@@ -0,0 +1,91 @@
+--add new vocabulary='CAP'
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.AddNewVocabulary(
+	pVocabulary_id			=> 'CAP',
+	pVocabulary_name		=> 'College of American Pathologists electronic cancer checklists',
+	pVocabulary_reference	=> 'https://fileshare.cap.org/human.aspx?Arg12=filelist&Arg06=136884410',
+	pVocabulary_version		=> NULL,
+	pOMOP_req				=> NULL,
+	pClick_default			=> NULL, --NULL or 'Y'
+	pAvailable				=> 'License required', --NULL, 'Currently not available','License required' or 'EULA required'
+	pURL					=> NULL,
+	pClick_disabled			=> NULL --NULL or 'Y'
+);
+END $_$;
+
+--new classes for CAP
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.AddNewConceptClass(
+	pConcept_class_id	=>'CAP Value',
+	pConcept_class_name	=>'CAP Value'
+);
+END $_$;
+
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.AddNewConceptClass(
+	pConcept_class_id	=>'CAP Variable',
+	pConcept_class_name	=>'CAP Variable'
+);
+END $_$;
+
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.AddNewConceptClass(
+	pConcept_class_id	=>'CAP Header',
+	pConcept_class_name	=>'CAP Header'
+);
+END $_$;
+
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.AddNewConceptClass(
+	pConcept_class_id	=>'CAP Protocol',
+	pConcept_class_name	=>'CAP Protocol'
+);
+END $_$;
+
+--new relationships for CAP
+DO $_$
+BEGIN
+	PERFORM vocabulary_pack.AddNewRelationship(
+	pRelationship_name			=>'CAP value of',
+	pRelationship_id			=>'CAP value of',
+	pIs_hierarchical			=>0,
+	pDefines_ancestry			=>0,
+	pReverse_relationship_id	=>'Has CAP value',
+	pRelationship_name_rev		=>'Has CAP value',
+	pIs_hierarchical_rev		=>0,
+	pDefines_ancestry_rev		=>0
+);
+END $_$;
+
+DO $_$
+BEGIN
+	PERFORM vocabulary_pack.AddNewRelationship(
+	pRelationship_name			=>'Has CAP parent item',
+	pRelationship_id			=>'Has CAP parent item',
+	pIs_hierarchical			=>0,
+	pDefines_ancestry			=>0,
+	pReverse_relationship_id	=>'CAP parent item of',
+	pRelationship_name_rev		=>'CAP parent item of',
+	pIs_hierarchical_rev		=>0,
+	pDefines_ancestry_rev		=>0
+);
+END $_$;
+
+DO $_$
+BEGIN
+	PERFORM vocabulary_pack.AddNewRelationship(
+	pRelationship_name			=>'Has CAP protocol',
+	pRelationship_id			=>'Has CAP protocol',
+	pIs_hierarchical			=>0,
+	pDefines_ancestry			=>0,
+	pReverse_relationship_id	=>'CAP protocol of',
+	pRelationship_name_rev		=>'CAP protocol of',
+	pIs_hierarchical_rev		=>0,
+	pDefines_ancestry_rev		=>0
+);
+END $_$;

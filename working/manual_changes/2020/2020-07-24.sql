61c5b3672e1fa99ed36addb85dc8c9687d5eea06
Dmytry Dymshyts <ddymshyts@odysseusinc.com>
2020-07-29 23:54:33 +0300

    cim10 to manual changes

--- working/manual_changes/2020/2020-07-24.sql
+++ working/manual_changes/2020/2020-07-24.sql
@@ -1,10 +1,10 @@
 DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.AddNewVocabulary(
-	pVocabulary_id			=> 'CIM10Fr',
+	pVocabulary_id			=> 'CIM10',
 	pVocabulary_name		=> 'International Classification of Diseases, Tenth Revision, French Edition',
 	pVocabulary_reference	=> 'https://www.atih.sante.fr/nomenclatures-de-recueil-de-linformation/cim',
-	pVocabulary_version		=> 'CIM10Fr 2020-04-14',
+	pVocabulary_version		=> 'CIM10 2020',
 	pOMOP_req				=> NULL, --NULL or 'Y'
 	pClick_default			=> NULL, --NULL or 'Y'
 	pAvailable				=> NULL, --NULL, 'Currently not available','License required' or 'EULA required'

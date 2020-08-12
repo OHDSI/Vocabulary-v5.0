244030c54f2a70b70b23962e9df8d139dcacec8c
Polina Talapova <42737398+p-talapova@users.noreply.github.com>
2020-08-12 17:37:55 +0300

    new vocabulary added

---
+++ NCCD/manual_changes.sql
@@ -0,0 +1,15 @@
+-- Add a new vocabulary
+DO $_$
+BEGIN
+    PERFORM VOCABULARY_PACK.AddNewVocabulary(
+    pVocabulary_id          => 'NCCD',
+    pVocabulary_name        => 'Normalized Chinese Clinical Drug',
+    pVocabulary_reference   => 'https://www.ohdsi.org/wp-content/uploads/2020/07/NCCD_RxNorm_Mapping_0728.pdf',
+    pVocabulary_version     => NULL,
+    pOMOP_req               => NULL, 
+    pClick_default          => NULL, 
+    pAvailable              => NULL, -- unrestricted license
+    pURL                    => NULL,
+    pClick_disabled         => NULL 
+);
+END $_$;

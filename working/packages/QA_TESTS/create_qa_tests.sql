3aee7dd6e05657ce11acc9cbe8bcfb6f3f6bf25e
Timur <timur.vakhitov@firstlinesoftware.com>
2020-08-18 14:04:02 +0300

    more readable output

--- working/packages/QA_TESTS/create_qa_tests.sql
+++ working/packages/QA_TESTS/create_qa_tests.sql
@@ -657,107 +657,94 @@ STABLE PARALLEL RESTRICTED SECURITY INVOKER;
 CREATE OR REPLACE FUNCTION qa_tests.check_stage_tables () RETURNS void
 AS $BODY$
 DECLARE
-z int;
+z TEXT;
 BEGIN
-  SELECT SUM (cnt) INTO z
-	FROM (SELECT COUNT (*) cnt
+	SELECT reason INTO z FROM (
+		--concept_relationship_stage
+		SELECT
+			CASE WHEN v1.vocabulary_id IS NOT NULL AND v2.vocabulary_id IS NOT NULL
+					AND COALESCE (v1.latest_update, v2.latest_update) IS NULL THEN 'concept_relationship_stage contains a vocabulary, that is not affected by the SetLatestUpdate: '||crs.vocabulary_id_1
+				WHEN crs.valid_start_date IS NULL THEN 'concept_relationship_stage.valid_start_date is null'
+				WHEN crs.valid_end_date IS NULL THEN 'concept_relationship_stage.valid_end_date is null'
+				WHEN ((crs.invalid_reason IS NULL AND crs.valid_end_date <> TO_DATE('20991231', 'yyyymmdd'))
+					OR (crs.invalid_reason IS NOT NULL AND crs.valid_end_date = TO_DATE('20991231', 'yyyymmdd')))
+					THEN 'wrong concept_relationship_stage.invalid_reason: '||crs.invalid_reason||' for '||TO_CHAR(crs.valid_end_date,'YYYYMMDD')
+				WHEN crs.valid_end_date < crs.valid_start_date THEN 'concept_relationship_stage.valid_end_date < concept_relationship_stage.valid_start_date: '||TO_CHAR(crs.valid_end_date,'YYYYMMDD')||'+'||TO_CHAR(crs.valid_start_date,'YYYYMMDD')
+				WHEN date_trunc('day', (crs.valid_start_date)) <> crs.valid_start_date THEN 'wrong format for concept_relationship_stage.valid_start_date (not truncated): '||TO_CHAR(crs.valid_start_date,'YYYYMMDD HH24:MI:SS')
+				WHEN date_trunc('day', (crs.valid_end_date)) <> crs.valid_end_date THEN 'wrong format for concept_relationship_stage.valid_end_date (not truncated to YYYYMMDD): '||TO_CHAR(crs.valid_end_date,'YYYYMMDD HH24:MI:SS')
+				WHEN COALESCE(crs.invalid_reason, 'D') NOT IN ('D','U') THEN 'wrong value for concept_relationship_stage.invalid_reason: '||crs.invalid_reason
+				WHEN c1.concept_code IS NULL AND cs1.concept_code IS NULL THEN 'concept_code_1+vocabulary_id_1 not found in the concept/concept_stage: '||crs.concept_code_1||'+'||crs.vocabulary_id_1
+				WHEN c2.concept_code IS NULL AND cs2.concept_code IS NULL THEN 'concept_code_2+vocabulary_id_2 not found in the concept/concept_stage: '||crs.concept_code_2||'+'||crs.vocabulary_id_2
+				WHEN v1.vocabulary_id IS NULL THEN 'vocabulary_id_1 not found in the vocabulary: '||crs.vocabulary_id_1
+				WHEN v2.vocabulary_id IS NULL THEN 'vocabulary_id_2 not found in the vocabulary: '||crs.vocabulary_id_2
+				WHEN rl.relationship_id IS NULL THEN 'relationship_id not found in the relationship: '||crs.relationship_id
+				WHEN crs.valid_start_date > CURRENT_DATE THEN 'concept_relationship_stage.valid_start_date is greater than the current date: '||TO_CHAR(crs.valid_start_date,'YYYYMMDD')
+				WHEN crs.valid_start_date < TO_DATE ('19000101', 'yyyymmdd') THEN 'concept_stage.valid_start_date is before 1900: '||TO_CHAR(crs.valid_start_date,'YYYYMMDD')
+				ELSE NULL
+			END AS reason
 			FROM concept_relationship_stage crs
-				 LEFT JOIN vocabulary v1 ON v1.vocabulary_id = crs.vocabulary_id_1 AND v1.latest_update IS NOT NULL
-				 LEFT JOIN vocabulary v2 ON v2.vocabulary_id = crs.vocabulary_id_2 AND v2.latest_update IS NOT NULL
-		   WHERE COALESCE (v1.latest_update, v2.latest_update) IS NULL
-		  UNION ALL
-		  SELECT COUNT (*)
-			FROM concept_stage cs LEFT JOIN vocabulary v ON v.vocabulary_id = cs.vocabulary_id AND v.latest_update IS NOT NULL
-		   WHERE v.latest_update IS NULL
-		  UNION ALL
-		  SELECT COUNT (*)
-			FROM concept_relationship_stage
-		   WHERE    valid_start_date IS NULL
-				 OR valid_end_date IS NULL
-				 OR (invalid_reason IS NULL AND valid_end_date <> TO_DATE ('20991231', 'yyyymmdd'))
-				 OR (invalid_reason IS NOT NULL AND valid_end_date = TO_DATE ('20991231', 'yyyymmdd'))
-		  UNION ALL
-		  SELECT COUNT (*)
-			FROM concept_stage
-		   WHERE    valid_start_date IS NULL
-				 OR valid_end_date IS NULL
-				 OR (invalid_reason IS NULL AND valid_end_date <> TO_DATE ('20991231', 'yyyymmdd') AND vocabulary_id NOT IN ('CPT4', 'HCPCS', 'ICD9Proc', 'ICD10PCS'))
-				 OR (invalid_reason IS NOT NULL AND valid_end_date = TO_DATE ('20991231', 'yyyymmdd'))
-				 OR valid_start_date < TO_DATE ('19000101', 'yyyymmdd') -- some concepts have a real date < 1970
-		  UNION ALL
-		  SELECT COUNT (*)
-			FROM (SELECT relationship_id FROM concept_relationship_stage
-				  EXCEPT
-				  SELECT relationship_id FROM relationship) AS s0
-		  UNION ALL
-		  SELECT COUNT (*)
-			FROM (SELECT concept_class_id FROM concept_stage
-				  EXCEPT
-				  SELECT concept_class_id FROM concept_class) AS s0
-		  UNION ALL
-		  SELECT COUNT (*)
-			FROM (SELECT domain_id FROM concept_stage
-				  EXCEPT
-				  SELECT domain_id FROM domain) AS s0
-		  UNION ALL
-		  SELECT COUNT (*)
-			FROM (SELECT vocabulary_id FROM concept_stage
-				  EXCEPT
-				  SELECT vocabulary_id FROM vocabulary) AS s0
-		  UNION ALL
-		  SELECT COUNT (*)
-			FROM concept_stage
-		   WHERE concept_name IS NULL OR domain_id IS NULL OR concept_class_id IS NULL OR concept_code IS NULL OR valid_start_date IS NULL
-		   OR valid_end_date IS NULL OR concept_name<>TRIM(concept_name) OR concept_code<>TRIM(concept_code)
-		  UNION ALL
-			SELECT COUNT (*)
-			  FROM concept_relationship_stage
-		  GROUP BY concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id
-			HAVING COUNT (*) > 1
-		  UNION ALL
-			SELECT COUNT (*)
-			  FROM concept_stage
-		  GROUP BY concept_code, vocabulary_id
-			HAVING COUNT (*) > 1
-		  UNION ALL
-			SELECT COUNT (*)
-			  FROM pack_content_stage
-		  GROUP BY pack_concept_code,
-				   pack_vocabulary_id,
-				   drug_concept_code,
-				   drug_vocabulary_id,
-				   amount
-			HAVING COUNT (*) > 1
-		  UNION ALL
-			SELECT COUNT (*)
-			  FROM drug_strength_stage
-		  GROUP BY drug_concept_code,
-				   vocabulary_id_1,
-				   ingredient_concept_code,
-				   vocabulary_id_2,
-				   amount_value
-			HAVING COUNT (*) > 1
-		  UNION ALL
-		  SELECT COUNT (*)
-			FROM concept_relationship_stage crm
-				 LEFT JOIN concept c1 ON c1.concept_code = crm.concept_code_1 AND c1.vocabulary_id = crm.vocabulary_id_1
-				 LEFT JOIN concept_stage cs1 ON cs1.concept_code = crm.concept_code_1 AND cs1.vocabulary_id = crm.vocabulary_id_1
-				 LEFT JOIN concept c2 ON c2.concept_code = crm.concept_code_2 AND c2.vocabulary_id = crm.vocabulary_id_2
-				 LEFT JOIN concept_stage cs2 ON cs2.concept_code = crm.concept_code_2 AND cs2.vocabulary_id = crm.vocabulary_id_2
-				 LEFT JOIN vocabulary v1 ON v1.vocabulary_id = crm.vocabulary_id_1
-				 LEFT JOIN vocabulary v2 ON v2.vocabulary_id = crm.vocabulary_id_2
-				 LEFT JOIN relationship rl ON rl.relationship_id = crm.relationship_id
-		   WHERE    (c1.concept_code IS NULL AND cs1.concept_code IS NULL)
-				 OR (c2.concept_code IS NULL AND cs2.concept_code IS NULL)
-				 OR v1.vocabulary_id IS NULL
-				 OR v2.vocabulary_id IS NULL
-				 OR rl.relationship_id IS NULL
-				 OR crm.valid_start_date > CURRENT_DATE
-				 OR crm.valid_end_date < crm.valid_start_date) AS s1;
+				LEFT JOIN concept c1 ON c1.concept_code = crs.concept_code_1 AND c1.vocabulary_id = crs.vocabulary_id_1
+				LEFT JOIN concept_stage cs1 ON cs1.concept_code = crs.concept_code_1 AND cs1.vocabulary_id = crs.vocabulary_id_1
+				LEFT JOIN concept c2 ON c2.concept_code = crs.concept_code_2 AND c2.vocabulary_id = crs.vocabulary_id_2
+				LEFT JOIN concept_stage cs2 ON cs2.concept_code = crs.concept_code_2 AND cs2.vocabulary_id = crs.vocabulary_id_2
+				LEFT JOIN vocabulary v1 ON v1.vocabulary_id = crs.vocabulary_id_1
+				LEFT JOIN vocabulary v2 ON v2.vocabulary_id = crs.vocabulary_id_2
+				LEFT JOIN relationship rl ON rl.relationship_id = crs.relationship_id
+		UNION ALL
+		SELECT
+			'duplicates in concept_relationship_stage were found: '||crs.concept_code_1||'+'||crs.concept_code_2||'+'||crs.vocabulary_id_1||'+'||crs.vocabulary_id_2||'+'||crs.relationship_id AS reason
+			FROM concept_relationship_stage crs
+			GROUP BY crs.concept_code_1, crs.concept_code_2, crs.vocabulary_id_1, crs.vocabulary_id_2, crs.relationship_id HAVING COUNT (*) > 1
+		UNION ALL
+		--concept_stage
+		SELECT
+			CASE WHEN v.vocabulary_id IS NOT NULL AND v.latest_update IS NULL THEN 'concept_stage contains a vocabulary, that is not affected by the SetLatestUpdate: '||cs.vocabulary_id
+				WHEN v.vocabulary_id IS NULL THEN 'concept_stage.vocabulary_id not found in the vocabulary: '||cs.vocabulary_id
+				WHEN cs.valid_end_date < cs.valid_start_date THEN 'concept_stage.valid_end_date < concept_stage.valid_start_date: '||TO_CHAR(cs.valid_end_date,'YYYYMMDD')||'+'||TO_CHAR(cs.valid_start_date,'YYYYMMDD')
+				WHEN date_trunc('day', (cs.valid_start_date)) <> cs.valid_start_date THEN 'wrong format for concept_stage.valid_start_date (not truncated): '||TO_CHAR(cs.valid_start_date,'YYYYMMDD HH24:MI:SS')
+				WHEN date_trunc('day', (cs.valid_end_date)) <> cs.valid_end_date THEN 'wrong format for concept_stage.valid_end_date (not truncated to YYYYMMDD): '||TO_CHAR(cs.valid_end_date,'YYYYMMDD HH24:MI:SS')
+				WHEN (((cs.invalid_reason IS NULL AND cs.valid_end_date <> TO_DATE('20991231', 'yyyymmdd')) AND cs.vocabulary_id NOT IN ('CPT4', 'HCPCS', 'ICD9Proc', 'ICD10PCS'))
+					OR (cs.invalid_reason IS NOT NULL AND cs.valid_end_date = TO_DATE('20991231', 'yyyymmdd'))) THEN 'wrong concept_stage.invalid_reason: '||cs.invalid_reason||' for '||TO_CHAR(cs.valid_end_date,'YYYYMMDD')
+				WHEN d.domain_id IS NULL AND cs.domain_id IS NOT NULL THEN 'domain_id not found in the domain: '||cs.domain_id
+				WHEN cc.concept_class_id IS NULL AND cs.concept_class_id IS NOT NULL THEN 'concept_class_id not found in the concept_class: '||cs.concept_class_id
+				WHEN COALESCE(cs.standard_concept, 'S') NOT IN ('C','S') THEN 'wrong value for standard_concept: '||cs.standard_concept
+				WHEN COALESCE(cs.invalid_reason, 'D') <> 'D' THEN 'wrong value for concept_stage.invalid_reason: '||cs.invalid_reason
+				WHEN cs.valid_start_date IS NULL THEN 'concept_stage.valid_start_date is null'
+				WHEN cs.valid_end_date IS NULL THEN 'concept_stage.valid_end_date is null'
+				WHEN cs.valid_start_date < TO_DATE ('19000101', 'yyyymmdd') THEN 'concept_stage.valid_start_date is before 1900: '||TO_CHAR(cs.valid_start_date,'YYYYMMDD')
+				WHEN cs.concept_name IS NULL THEN 'empty concept_stage.concept_name'
+				WHEN cs.concept_code IS NULL THEN 'empty concept_stage.concept_code'
+				WHEN cs.concept_name<>TRIM(cs.concept_name) THEN 'concept_stage.concept_name not trimmed for concept_code: '||cs.concept_code
+				WHEN cs.concept_code<>TRIM(cs.concept_code) THEN 'concept_stage.concept_code not trimmed for concept_name: '||cs.concept_name
+				ELSE NULL
+			END AS reason
+		FROM concept_stage cs
+			LEFT JOIN vocabulary v ON v.vocabulary_id = cs.vocabulary_id
+			LEFT JOIN domain d ON d.domain_id = cs.domain_id
+			LEFT JOIN concept_class cc ON cc.concept_class_id = cs.concept_class_id
+		UNION ALL
+		SELECT
+			'duplicates in concept_stage were found: '||cs.concept_code||'+'||cs.vocabulary_id AS reason
+			FROM concept_stage cs
+			GROUP BY cs.concept_code, cs.vocabulary_id HAVING COUNT (*) > 1
+		UNION ALL
+		--pack_content_stage
+		SELECT
+			'duplicates in pack_content_stage were found: '||pcs.pack_concept_code||'+'||pcs.pack_vocabulary_id||pcs.drug_concept_code||'+'||pcs.drug_vocabulary_id||'+'||TO_CHAR(pcs.amount) AS reason
+			FROM pack_content_stage pcs
+			GROUP BY pcs.pack_concept_code, pcs.pack_vocabulary_id, pcs.drug_concept_code, pcs.drug_vocabulary_id, pcs.amount HAVING COUNT (*) > 1
+		UNION ALL
+		--drug_strength_stage
+		SELECT
+			'duplicates in drug_strength_stage were found: '||dcs.drug_concept_code||'+'||dcs.vocabulary_id_1||
+				dcs.ingredient_concept_code||'+'||dcs.vocabulary_id_2||'+'||TO_CHAR(dcs.amount_value, 'FM9999999999999999999990.999999999999999999999') AS reason
+			FROM drug_strength_stage dcs
+			GROUP BY dcs.drug_concept_code, dcs.vocabulary_id_1, dcs.ingredient_concept_code, dcs.vocabulary_id_2, dcs.amount_value HAVING COUNT (*) > 1
+	) AS s0
+	WHERE reason IS NOT NULL LIMIT 1;
 
-  IF z <> 0
-  THEN
-	  RAISE EXCEPTION '% error(s) found in stage tables. Check working\QA_stage_tables.sql', z;
-  END IF;
+	IF z IS NOT NULL THEN
+		RAISE EXCEPTION '%', z;
+	END IF;
 END;
 $BODY$ LANGUAGE 'plpgsql' SECURITY INVOKER;

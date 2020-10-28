23174851c3d9978e3a0069064d4b5d1b420ebbfc
Alexander <32357692+Alexdavv@users.noreply.github.com>
2020-10-28 14:50:27 +0300

    Update load_stage.sql

--- MedDRA/load_stage.sql
+++ MedDRA/load_stage.sql
@@ -355,75 +355,31 @@ INSERT INTO  concept_relationship_stage (concept_code_1,
      FROM SOURCES.low_level_term
     WHERE llt_currency = 'Y' AND llt_code <> pt_code;
 
---6. Copy existing relationships
-/*INSERT INTO concept_relationship_stage (
-	concept_code_1,
-	concept_code_2,
-	vocabulary_id_1,
-	vocabulary_id_2,
-	relationship_id,
-	valid_start_date,
-	valid_end_date,
-	invalid_reason
-	)
-SELECT DISTINCT c1.concept_code AS concept_code_1, --use distinct for SMQ: one concept_code but different concept_ids
-	c2.concept_code AS concept_code_2,
-	c1.vocabulary_id AS vocabulary_id_1,
-	c2.vocabulary_id AS vocabulary_id_2,
-	r.relationship_id AS relationship_id,
-	r.valid_start_date AS valid_start_date,
-	r.valid_end_date AS valid_end_date,
-	r.invalid_reason AS invalid_reason
-FROM concept_relationship r,
-	concept c1,
-	concept c2
-WHERE c1.concept_id = r.concept_id_1
-	AND c2.concept_id = r.concept_id_2
-	AND r.relationship_id IN (
-		'MedDRA - SNOMED eq',
-		'MedDRA - SMQ',
-		'MedDRA - ICD9CM'
-		);*/
-
---7. Create a relationship file for the Medical Coder
-/*
-SELECT c.concept_code,
-	c.concept_name,
-	c.domain_id,
-	c.concept_class_id,
-	c1.concept_code concept_code_snomed
-FROM concept_stage c
-LEFT JOIN concept_relationship_stage r ON c.concept_code = r.concept_code_1
-	AND r.relationship_id = 'MedDRA - SNOMED eq'
-LEFT JOIN concept c1 ON c1.concept_code = r.concept_code_2
-	AND c1.vocabulary_id = 'SNOMED';
-*/
-
---8. Append result to concept_relationship_stage table
+--6. Append result to concept_relationship_stage table
 /*DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.ProcessManualRelationships();
 END $_$;*/
 
---9. Working with replacement mappings
+--7. Working with replacement mappings
 /*DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.CheckReplacementMappings();
 END $_$;*/
 
---10. Add mapping from deprecated to fresh concepts
+--8. Add mapping from deprecated to fresh concepts
 /*DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.AddFreshMAPSTO();
 END $_$;*/
 
---11. Deprecate 'Maps to' mappings to deprecated and upgraded concepts
+--9. Deprecate 'Maps to' mappings to deprecated and upgraded concepts
 /*DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.DeprecateWrongMAPSTO();
 END $_$;*/
 
---12. Delete ambiguous 'Maps to' mappings
+--10. Delete ambiguous 'Maps to' mappings
 /*DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.DeleteAmbiguousMAPSTO();

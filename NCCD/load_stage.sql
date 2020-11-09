cf0a1180736bda311e6801e9154758c28432ab27
divakhnenko <69844472+divakhnenko@users.noreply.github.com>
2020-11-09 18:28:53 +0200

    Update and rename finilize_load_stage.sql to load_stage.sql

--- NCCD/load_stage.sql
+++ NCCD/load_stage.sql
@@ -16,7 +16,48 @@
 * Authors: Polina Talapova, Daryna Ivakhnenko, Dmitry Dymshyts
 * Date: 2020
 **************************************************************************/
---1. Add original Chinese names for NCCD concepts
+
+-- Truncate all working tables
+TRUNCATE TABLE concept_stage;
+TRUNCATE TABLE concept_relationship_stage;
+TRUNCATE TABLE concept_synonym_stage;
+TRUNCATE TABLE pack_content_stage;
+TRUNCATE TABLE drug_strength_stage;
+
+-- Add NCCD concepts (Drug Products and Drug Attributes)
+INSERT INTO concept_stage
+(concept_name,domain_id,vocabulary_id,concept_class_id,standard_concept,concept_code,valid_start_date,valid_end_date,invalid_reason)
+SELECT  t_nm as concept_name,       -- English name
+       'Drug' as domain_id,
+       'NCCD' as vocabulary_id,
+       CASE
+         WHEN nccd_type = 'IN' THEN 'Ingredient'
+         WHEN nccd_type = 'DF' THEN 'Dose Form'
+         WHEN nccd_type = 'BN' THEN 'Brand Name'
+         ELSE 'Drug Product' 
+       END as concept_class_id,
+       NULL as standard_concept,
+       nccd_code as concept_code,
+       TO_DATE('19700101','yyyymmdd') as valid_start_date,
+       TO_DATE('20991231','yyyymmdd') AS valid_end_date,
+       NULL AS invalid_reason
+FROM nccd_full_done;
+
+-- Add mappings for NCCD concepts
+INSERT INTO concept_relationship_stage
+(concept_code_1,concept_code_2,vocabulary_id_1,vocabulary_id_2,relationship_id,valid_start_date,valid_end_date,invalid_reason)
+SELECT nccd_code as concept_code_1,
+       concept_code as concept_code_2,
+       'NCCD' as vocabulary_id_1,
+       vocabulary_id as vocabulary_id_2,
+       CASE
+         WHEN nccd_type in ('DF', 'BN') THEN 'Source - RxNorm eq'
+        ELSE 'Maps to' END as relationship_id,
+       TO_DATE('19700101','yyyymmdd') as valid_start_date,
+       TO_DATE('20991231','yyyymmdd') as valid_end_date,
+       NULL AS invalid_reason
+FROM nccd_full_done;
+-- Add original Chinese names for NCCD concepts
 INSERT INTO concept_synonym_stage
 (
   synonym_name,
@@ -33,7 +74,7 @@ WHERE nccd_name not in ('X', '')
 AND   nccd_code IN (SELECT concept_code FROM concept_stage)
 ; -- 51026
 
---2. Add everything from the Manual tables - PLEASE CHECK NEXT TIME the results of all these VOCABULARY_PACKs
+-- Add everything from the Manual tables - PLEASE CHECK NEXT TIME the results of all these VOCABULARY_PACKs
 --Working with manual concepts
 DO $_$
 BEGIN

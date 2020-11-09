180815ca9185f5614a3372389db933ca2f648317
divakhnenko <69844472+divakhnenko@users.noreply.github.com>
2020-11-09 12:38:47 +0200

    Update manual_table.sql

--- NCCD/manual_table.sql
+++ NCCD/manual_table.sql
@@ -28,3 +28,59 @@ CREATE TABLE nccd_manual
   concept_code   VARCHAR,
   concept_name   VARCHAR
 );
+
+
+/**********************************
+********* CONCEPT MANUAL **********
+***********************************/
+--truncate concept_manual;
+INSERT INTO concept_manual
+(
+  concept_name,
+  domain_id,
+  vocabulary_id,
+  concept_class_id,
+  standard_concept,
+  concept_code,
+  valid_start_date,
+  valid_end_date,
+  invalid_reason
+)
+SELECT  DISTINCT  trim(regexp_replace(initcap (t_nm), '\s+', ' ', 'g')),
+c.domain_id AS domain_id,
+       'NCCD' AS vocabulary_id,
+       case when c.domain_id = 'Drug' then 'Drug Product' else 'Device' end AS concept_class_id,
+       NULL AS standard_concept,
+       nccd_code AS concept_code,
+       TO_DATE('20200313','yyyymmdd') AS valid_start_date,
+       TO_DATE('20991231','yyyymmdd') AS valid_end_date,
+       NULL AS invalid_reason
+       FROM nccd_manual a
+       join devv5.concept c on a.concept_Id = c.concept_id and c.standard_concept = 'S'; 
+/***************************************
+***** CONCEPT RELATIONSHIP MANUAL ******
+****************************************/
+--truncate concept_relationship_manual;
+INSERT INTO concept_relationship_manual
+(concept_code_1,concept_code_2,vocabulary_id_1,vocabulary_id_2,relationship_id,valid_start_date,valid_end_date)
+SELECT DISTINCT  nccd_code AS concept_code_1,
+        c.concept_code AS concept_code_2,
+       'NCCD' AS vocabulary_id_1,
+       c.vocabulary_id AS vocabulary_id_2,
+       'Maps to' AS relationship_id,
+       CURRENT_DATE AS valid_start_date,
+       TO_DATE('20991231','yyyymmdd') AS valid_end_date
+       FROM nccd_manual a
+       join devv5.concept c on a.concept_Id = c.concept_id and c.standard_concept = 'S'; 
+       
+/******************************************
+********* CONCEPT SYNONYM MANUAL **********
+*******************************************/
+--truncate concept_synonym_manual;
+INSERT INTO concept_synonym_manual
+(synonym_name,synonym_concept_code,synonym_vocabulary_id, language_concept_id)
+SELECT DISTINCT nccd_name as synonym_name,
+       nccd_code as synonym_concept_code,
+       'NCCD' as synonym_vocabulary_id,
+       4180186 as language_concept_id
+FROM nccd_manual; 

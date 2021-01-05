bf7ba5ae8225334a9c1da9c7dea444ca7d70ea5c
Alex Davydov <alexander.davydovv@gmail.com>
2021-01-05 11:29:13 +0300

    RxE attribute flaw QA

--- RxNorm_E/manual_work/specific_qa/manual_checks_after_generic.sql
+++ RxNorm_E/manual_work/specific_qa/manual_checks_after_generic.sql
@@ -1,7 +1,31 @@
---Drug Standard concepts aren't linked to the Standard Drug Ingredients
---1893499
---19783 (1%)
+--1. Drug Standard RxNorm/RxE concepts aren't linked to the Standard Drug Ingredients
+
+--1a. statistics
+--vocabulary version v20201222:
+-- 15711/1893499 = 0.8%
 SELECT c.vocabulary_id, c.concept_class_id, COUNT (*)
+FROM concept c
+
+WHERE c.domain_id = 'Drug'
+    AND c.standard_concept = 'S'
+    AND c.vocabulary_id IN ('RxNorm', 'RxNorm Extension')
+    AND c.concept_class_id NOT IN ('Ingredient')
+
+AND NOT EXISTS (
+                SELECT 1
+                FROM concept_ancestor ca
+                JOIN concept c2
+                    ON ca.ancestor_concept_id = c2.concept_id
+                        AND c2.concept_class_id = 'Ingredient'
+                        AND c2.domain_id = 'Drug'
+                        AND c2.standard_concept = 'S'
+                WHERE ca.descendant_concept_id = c.concept_id
+    )
+GROUP BY 1,2
+;
+
+--1b. full list
+SELECT c.*
 FROM concept c
 
 WHERE c.domain_id = 'Drug'

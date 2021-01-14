00427e897fb940ee6c69cba2bcc4faf1300138ee
tsemharb <gorbachevart@gmail.com>
2021-01-14 13:54:14 +0300

    initial update 01_21

--- JMDC/load_stage_2.sql
+++ JMDC/load_stage_2.sql
@@ -1,59 +1,82 @@
+
 /************************************************
 * 9. Populate relationship_to_concept *
 ************************************************/
 
 -- 9.1 Forms
 -- insert mapped forms back to aut_form_mapped
-insert into relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
-select distinct dc.concept_code,'JMDC',concept_id_2,precedence
-from aut_form_mapped a
-join drug_concept_stage dc on dc.concept_name = coalesce (a.new_name,a.concept_name)
-where dc.concept_class_id = 'Dose Form'
-and not exists (select 1 from relationship_to_concept rtc2 where rtc2.concept_code_1 =  dc.concept_code)
+INSERT INTO relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
+SELECT DISTINCT dc.concept_code, 'JMDC', concept_id_2, precedence
+FROM aut_form_mapped a
+JOIN drug_concept_stage dc
+    ON dc.concept_name = COALESCE(a.new_name, a.concept_name)
+WHERE dc.concept_class_id = 'Dose Form'
+  AND NOT EXISTS(SELECT 1
+                 FROM relationship_to_concept rtc2
+                 WHERE rtc2.concept_code_1 = dc.concept_code)
 ;
 --9.2 Units
 -- insert mapped forms back to aut_unit_mapped
-insert into relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
-select distinct concept_code_1, vocabulary_id_1, concept_id_2, precedence, conversion_factor
-from aut_unit_mapped a
-where not exists (select 1 from relationship_to_concept rtc2 where rtc2.concept_code_1 =  a.concept_code_1)
+INSERT INTO relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence, conversion_factor)
+SELECT DISTINCT concept_code_1, vocabulary_id_1, concept_id_2, precedence, conversion_factor
+FROM aut_unit_mapped a
+WHERE NOT EXISTS(SELECT 1
+                 FROM relationship_to_concept rtc2
+                 WHERE rtc2.concept_code_1 = a.concept_code_1)
 ;
 --9.3 Ingredients
 --insert mappings back to aut_ingredient_mapped or aut_parsed_ingr (for ingredients that need parsing)
-insert into relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
-select distinct dc.concept_code,'JMDC',cast(concept_id_2 as int), precedence
-from aut_ingredient_mapped a
-join drug_concept_stage dc on dc.concept_name = a.concept_name and concept_class_id = 'Ingredient'
-where not exists (select 1 from relationship_to_concept rtc2 where rtc2.concept_code_1 =  dc.concept_code)
+INSERT INTO relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
+SELECT DISTINCT dc.concept_code, 'JMDC', CAST(concept_id_2 AS int), precedence
+FROM aut_ingredient_mapped a
+JOIN drug_concept_stage dc
+    ON dc.concept_name = a.concept_name AND concept_class_id = 'Ingredient'
+WHERE NOT EXISTS(SELECT 1
+                 FROM relationship_to_concept rtc2
+                 WHERE rtc2.concept_code_1 = dc.concept_code)
 ;
 
-insert into relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
-select distinct dc.concept_code,'JMDC',concept_id_2, rank() over (partition by dc.concept_code order by concept_id_2)
-from aut_parsed_ingr a
-join drug_concept_stage dc
-on lower(dc.concept_name) = lower(a.ing_name) and dc.concept_class_id = 'Ingredient'
-where not exists (select 1 from relationship_to_concept rtc2 where rtc2.concept_code_1 =  dc.concept_code);
+INSERT INTO relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
+SELECT DISTINCT dc.concept_code, 'JMDC', concept_id_2, RANK() OVER (PARTITION BY dc.concept_code ORDER BY concept_id_2)
+FROM aut_parsed_ingr a
+JOIN drug_concept_stage dc
+    ON LOWER(dc.concept_name) = LOWER(a.ing_name) AND dc.concept_class_id = 'Ingredient'
+WHERE NOT EXISTS(SELECT 1
+                 FROM relationship_to_concept rtc2
+                 WHERE rtc2.concept_code_1 = dc.concept_code);
 
-insert into relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
-select distinct dc.concept_code,'JMDC',concept_id_2, rank() over (partition by dc.concept_code order by concept_id_2)
-from aut_parsed_ingr a
-join drug_concept_stage dc
-on lower(dc.concept_name) = lower(a.concept_name) and dc.concept_class_id = 'Ingredient'
-where not exists (select 1 from relationship_to_concept rtc2 where rtc2.concept_code_1 =  dc.concept_code);
+INSERT INTO relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
+SELECT DISTINCT dc.concept_code, 'JMDC', concept_id_2, RANK() OVER (PARTITION BY dc.concept_code ORDER BY concept_id_2)
+FROM aut_parsed_ingr a
+JOIN drug_concept_stage dc
+    ON LOWER(dc.concept_name) = LOWER(a.concept_name) AND dc.concept_class_id = 'Ingredient'
+WHERE NOT EXISTS(SELECT 1
+                 FROM relationship_to_concept rtc2
+                 WHERE rtc2.concept_code_1 = dc.concept_code);
 
 -- 9.4 BN
-insert into relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
-select distinct dc.concept_code,'JMDC',cast(concept_id_2 as int), precedence
-from aut_bn_mapped a
-join drug_concept_stage dc on dc.concept_name = a.concept_name and concept_class_id = 'Brand Name'
-where not exists (select 1 from relationship_to_concept rtc2 where rtc2.concept_code_1 =  dc.concept_code)
+INSERT INTO relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
+SELECT DISTINCT dc.concept_code, 'JMDC', CAST(concept_id_2 AS int), precedence
+FROM aut_bn_mapped a
+JOIN drug_concept_stage dc
+    ON dc.concept_name = a.concept_name AND concept_class_id = 'Brand Name'
+WHERE NOT EXISTS(SELECT 1
+                 FROM relationship_to_concept rtc2
+                 WHERE rtc2.concept_code_1 = dc.concept_code)
 ;
 
 -- 9.5 Supplier
 -- insert mappings back to aut_supplier_mapped
-insert into relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
-select distinct dc.concept_code,'JMDC',a.concept_id_2, rank() over (partition by dc.concept_code order by a.concept_id_2)
-from aut_suppliers_mapped a
-join drug_concept_stage dc on concept_name = concept_name
-where a.concept_id is not null
-and dc.concept_code not in (select concept_code_1 from relationship_to_concept);
+INSERT INTO relationship_to_concept (concept_code_1, vocabulary_id_1, concept_id_2, precedence)
+SELECT DISTINCT
+    dc.concept_code, 'JMDC', a.concept_id_2,
+    RANK() OVER (PARTITION BY dc.concept_code ORDER BY a.concept_id_2)
+FROM aut_suppliers_mapped a
+JOIN drug_concept_stage dc
+    ON a.concept_name = dc.concept_name
+WHERE a.concept_id_2 IS NOT NULL
+  AND dc.concept_code NOT IN (
+                             SELECT concept_code_1
+                             FROM relationship_to_concept
+                             );
+

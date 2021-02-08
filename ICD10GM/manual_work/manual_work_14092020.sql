e4b72b61a41d36eed8b80e287609420bff5d5bb9
divakhnenko <69844472+divakhnenko@users.noreply.github.com>
2021-02-08 13:39:32 +0200

    Deleting dead concepts step was added

--- ICD10GM/manual_work/manual_work_14092020.sql
+++ ICD10GM/manual_work/manual_work_14092020.sql
@@ -1,3 +1,7 @@
+-- delete from concept_manual table "dead" concepts
+DELETE FROM concept_manual
+WHERE concept_code NOT IN (SELECT concept_code FROM sources.icd10gm);
+
 --add COVID and E-cig concepts
 INSERT INTO concept_synonym_manual (
 	synonym_name,

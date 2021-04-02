095c448b6aed08e250613dcf688cd10e1ac0fd5b
Alex Davydov <alexander.davydovv@gmail.com>
2021-04-02 13:23:40 +0300

    new concept add

---
+++ working/manual_changes/2021/2021-04-02.sql
@@ -0,0 +1,20 @@
+--Add new concept
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewSynonym(
+  pConcept_id=>vocabulary_pack.AddNewSynonym(
+      pConcept_id =>vocabulary_pack.AddNewConcept(
+                     pConcept_name     =>'COVID-19 convalescent plasma',
+                     pDomain_id        =>'Drug',
+                     pVocabulary_id    =>'RxNorm Extension',
+                     pConcept_class_id =>'Ingredient',
+                     pStandard_concept =>'S',
+                     pConcept_code     =>''
+                          ),
+      pSynonym_name        =>'Disease caused by Severe acute respiratory syndrome coronavirus 2 convalescent plasma',
+      pLanguage_concept_id =>4180186
+  ),
+  pSynonym_name        =>'Anti-SARS-CoV-2 convalescent plasma',
+  pLanguage_concept_id =>4180186
+);
+END $_$;

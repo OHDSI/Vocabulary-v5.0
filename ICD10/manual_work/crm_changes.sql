0032c20a4c712ee525fdb1f0c652fd0c1ea0d290
divakhnenko <69844472+divakhnenko@users.noreply.github.com>
2021-04-08 12:50:06 +0300

    Update crm_changes.sql
    case added

--- ICD10/manual_work/crm_changes.sql
+++ ICD10/manual_work/crm_changes.sql
@@ -19,4 +19,4 @@
 -- deprecate previous inaccurate mapping
 UPDATE concept_relationship_manual SET valid_end_date = CURRENT_DATE, invalid_reason = 'D' WHERE concept_code_1 IN (SELECT icd_code FROM refresh_lookup_done) AND concept_code_2 NOT IN (SELECT repl_by_code FROM refresh_lookup_done);
 -- insert new mapping
-INSERT INTO concept_relationship_manual SELECT icd_code, repl_by_code, 'ICD10', repl_by_vocabulary, 'Maps to', CURRENT_DATE, TO_DATE('20991231','YYYYMMDD'), NULL FROM refresh_lookup_done;
+INSERT INTO concept_relationship_manual SELECT icd_code, repl_by_code, 'ICD10', repl_by_vocabulary, CASE WHEN repl_by_relationship = 'Is a' THEN 'Maps to' ELSE repl_by_relationship END, CURRENT_DATE, TO_DATE('20991231','YYYYMMDD'), NULL FROM refresh_lookup_done;

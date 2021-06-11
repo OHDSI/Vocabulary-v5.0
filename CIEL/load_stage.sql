9257bd18fbc8500cf0b2da8b569fc2afe27fa5ed
Michael Kallfelz <60365423+mik-ohdsi@users.noreply.github.com>
2021-06-11 15:22:59 +0200

    updated create source tables and load stage scripts

--- CIEL/load_stage.sql
+++ CIEL/load_stage.sql
@@ -1,12 +1,29 @@
+/**************************************************************************
+* Copyright 2016 Observational Health Data Sciences and Informatics (OHDSI)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* 
+* Authors: Christian Reich, Timur Vakhitov, Michael Kallfelz
+* Date: 2020, 2021
+**************************************************************************/
+
 --1. Update latest_update field to new date 
 DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.SetLatestUpdate(
 	pVocabularyName			=> 'CIEL',
-	pVocabularyDate			=> ('2021-03-12'),
---  pVocabularyDate			=> (SELECT vocabulary_date FROM dev_ciel.ciel_concept_class LIMIT 1),
---	pVocabularyVersion		=> (SELECT vocabulary_version FROM dev_ciel.ciel_concept_class LIMIT 1),
-	pVocabularyVersion		=> ('openMRS March 2021'),
+	pVocabularyDate			=> (SELECT vocabulary_date FROM dev_ciel.ciel_concept_class LIMIT 1),
+	pVocabularyVersion		=> (SELECT vocabulary_version FROM dev_ciel.ciel_concept_class LIMIT 1),
 	pVocabularyDevSchema	=> 'DEV_CIEL'
 );
 END $_$;

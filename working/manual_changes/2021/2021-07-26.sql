0eed396ad78466ee761f5850cc1d231192596a8f
Timur <hardhouse@users.noreply.github.com>
2021-07-27 16:35:01 +0300

    refactoring

--- working/manual_changes/2021/2021-07-26.sql
+++ working/manual_changes/2021/2021-07-26.sql
@@ -1,100 +1,96 @@
--- Create new vocabulary OMOP Genomic
 DO $_$
-BEGIN
-	PERFORM VOCABULARY_PACK.AddNewVocabulary(
-	pVocabulary_id			=> 'OMOP Genomic',
-	pVocabulary_name		=> 'OMOP Genomic vocabulary',
-	pVocabulary_reference	=> 'OMOP generated',
-	pVocabulary_version		=> NULL,
-	pOMOP_req				=> NULL,
-	pClick_default			=> NULL, --NULL or 'Y'
-	pAvailable				=> NULL, --NULL, 'Currently not available','License required' or 'EULA required'
-	pURL					=> NULL,
-	pClick_disabled			=> NULL --NULL or 'Y'
-);
+	--Create new vocabulary 'OMOP Genomic'
+	BEGIN
+		PERFORM VOCABULARY_PACK.AddNewVocabulary(
+		pVocabulary_id			=> 'OMOP Genomic',
+		pVocabulary_name		=> 'OMOP Genomic vocabulary',
+		pVocabulary_reference	=> 'OMOP generated',
+		pVocabulary_version		=> NULL,
+		pOMOP_req				=> NULL,
+		pClick_default			=> NULL, --NULL or 'Y'
+		pAvailable				=> NULL, --NULL, 'Currently not available','License required' or 'EULA required'
+		pURL					=> NULL,
+		pClick_disabled			=> NULL --NULL or 'Y'
+	);
+
+	--Change hierarchy direction
+	UPDATE relationship
+	SET is_hierarchical = 1,
+		defines_ancestry = 1
+	WHERE relationship_concept_id IN (
+			32919,
+			32921
+			);
+
+	UPDATE relationship
+	SET is_hierarchical = 0,
+		defines_ancestry = 0
+	WHERE relationship_concept_id IN (
+			32920,
+			32922
+			);
+
+	--Rename concept_class_ids and names
+	ALTER TABLE concept DROP CONSTRAINT fpk_concept_class;
+
+	-- update concept_classes
+	UPDATE concept_class
+	SET concept_class_id = 'DNA Variant',
+		concept_class_name = 'DNA Variant'
+	WHERE concept_class_concept_id = 32924;
+
+	UPDATE concept
+	SET concept_name = 'DNA Variant'
+	WHERE concept_id = 32924;
+
+	UPDATE concept
+	SET concept_class_id = 'DNA Variant'
+	WHERE concept_class_id = 'Genomic Variant';
+
+	UPDATE concept_class
+	SET concept_class_id = 'Genetic Variation',
+		concept_class_name = 'Genetic Variation'
+	WHERE concept_class_concept_id = 32925;
+
+	UPDATE concept
+	SET concept_name = 'Genetic Variation'
+	WHERE concept_id = 32925;
+
+	UPDATE concept
+	SET concept_class_id = 'Genetic Variation'
+	WHERE concept_class_id = 'Gene';
+
+	UPDATE concept_class
+	SET concept_class_id = 'RNA Variant',
+		concept_class_name = 'RNA Variant'
+	WHERE concept_class_concept_id = 32923;
+
+	UPDATE concept
+	SET concept_name = 'RNA Variant'
+	WHERE concept_id = 32923;
+
+	UPDATE concept
+	SET concept_class_id = 'RNA Variant'
+	WHERE concept_class_id = 'Transcript Variant';
+
+	ALTER TABLE concept ADD CONSTRAINT fpk_concept_class FOREIGN KEY (concept_class_id) REFERENCES concept_class (concept_class_id);
+
+	--Replace concept_code and vocabulary for genomic concepts
+	UPDATE concept c
+	SET concept_code = omop_can_code,
+		vocabulary_id = 'OMOP Genomic'
+	FROM dev_dkaduk.upd_concept_june a
+	WHERE a.concept_id = c.concept_id;
+
+	--Replace 'OMOP Extension' vocabulary to 'OMOP Genomic' for genomic concepts
+	UPDATE concept c
+	SET vocabulary_id = 'OMOP Genomic'
+	WHERE vocabulary_id = 'OMOP Extension'
+		AND concept_class_id LIKE '%Variant';
+
+	--Replace 'HGNC' vocabulary to 'OMOP Genomic' for genomic concepts
+	UPDATE concept c
+	SET vocabulary_id = 'OMOP Genomic'
+	WHERE vocabulary_id = 'HGNC';
 END $_$;
-
--- change hierarchy direction
-update relationship
-set is_hierarchical = 1,
-defines_ancestry = 1 
-where relationship_concept_id = 32919;
-
-update relationship
-set is_hierarchical = 0,
-defines_ancestry = 0
-where relationship_concept_id = 32920;
-
-update relationship
-set is_hierarchical = 1,
-defines_ancestry = 1 
-where relationship_concept_id = 32921;
-
-update relationship
-set is_hierarchical = 0,
-defines_ancestry = 0
-where relationship_concept_id = 32922;
-
-
--- remove constraints for changing concept_classes
-ALTER TABLE concept drop CONSTRAINT fpk_concept_class;
-ALTER TABLE concept_class drop CONSTRAINT xpk_concept_class;
-ALTER TABLE concept_class drop CONSTRAINT fpk_concept_class_concept;
-
-
--- update concept_classes
-update concept_class
-set concept_class_id = 'DNA Variant',
-concept_class_name = 'DNA Variant'
-where concept_class_concept_id = 32924;
-
-update concept 
-set concept_class_id = 'DNA Variant'
-where concept_class_id = 'Genomic Variant';
-
-update concept_class
-set concept_class_id = 'Genetic Variation',
-concept_class_name = 'Genetic Variation'
-where concept_class_concept_id = 32925;
-
-update concept 
-set concept_class_id = 'Genetic Variation'
-where concept_class_id = 'Gene';
-
-update concept_class
-set concept_class_id = 'RNA Variant',
-concept_class_name = 'RNA Variant'
-where concept_class_concept_id = 32923;
-
-update concept 
-set concept_class_id = 'RNA Variant'
-where concept_class_id = 'Transcript Variant';
-
-
---add constraints for changed concept_classes
-ALTER TABLE concept_class ADD CONSTRAINT xpk_concept_class PRIMARY KEY (concept_class_id);
-ALTER TABLE concept_class ADD CONSTRAINT fpk_concept_class_concept FOREIGN KEY (concept_class_concept_id) REFERENCES concept (concept_id);
-ALTER TABLE concept ADD CONSTRAINT fpk_concept_class FOREIGN KEY (concept_class_id) REFERENCES concept_class (concept_class_id);
-
-
--- replace concept_code and vocabulary for genomic concepts
-update concept c
-set concept_code = omop_can_code,
-vocabulary_id = 'OMOP Genomic'
-from dev_dkaduk.upd_concept_june a 
-where a.concept_id = c.concepT_id;
-
--- replace 'OMOP Extension' vocabulary to 'OMOP Genomic' for genomic concepts
-update concept c
-set vocabulary_id = 'OMOP Genomic'
-where vocabulary_id = 'OMOP Extension'
-and concept_class_id like '%Variant';
-
--- replace 'HGNC' vocabulary to 'OMOP Genomic' for genomic concepts
-update concept c
-set vocabulary_id = 'OMOP Genomic'
-where vocabulary_id = 'HGNC'
-;
-
-
 

99afbd3ffaec1407359ae0cbd358aac580117b9a
Dmytry Dymshyts <ddymshyts@odysseusinc.com>
2021-07-28 23:12:06 +0300

    Update and rename 2021-04-05.sql to 2021-07-29.sql

--- working/manual_changes/2021/2021-07-29.sql
+++ working/manual_changes/2021/2021-07-29.sql
@@ -1,3 +1,4 @@
+--new vocabulary
 DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.AddNewVocabulary(
@@ -10,5 +11,20 @@ BEGIN
 	pAvailable				=> NULL, --NULL, 'Currently not available','License required' or 'EULA required'
 	pURL					=> NULL,
 	pClick_disabled			=> NULL --NULL or 'Y'
+);
+END $_$;
+
+--new relationships for OncoTree to ICDO
+DO $_$
+BEGIN
+	PERFORM vocabulary_pack.AddNewRelationship(
+	pRelationship_name			=>'OncoTree to ICDO',
+	pRelationship_id			=>'OncoTree to ICDO',
+	pIs_hierarchical			=>0,
+	pDefines_ancestry			=>0,
+	pReverse_relationship_id	=>'ICDO to OncoTree',
+	pRelationship_name_rev		=>'ICDO to OncoTree',
+	pIs_hierarchical_rev		=>0,
+	pDefines_ancestry_rev		=>0
 );
 END $_$;

012eab0cf6669b6058ad7c209716c2403ae20fa1
Dmytry Dymshyts <ddymshyts@odysseusinc.com>
2021-07-28 23:14:33 +0300

    Update load_stage.sql
    no mapping_ot is used, only the manual ones

--- OncoTree/load_stage.sql
+++ OncoTree/load_stage.sql
@@ -39,7 +39,7 @@ select
 	'OncoTree',
 	'Condition',
 	o.descendant_code,
-	(
+	(-- need to get the right dates for the first release
 		select latest_update
 		from vocabulary
 		where vocabulary_id = 'OncoTree'
@@ -70,61 +70,6 @@ BEGIN
 	PERFORM VOCABULARY_PACK.ProcessManualRelationships();
 END $_$;
 ;
---6. Put mapping to ICDO/SNOMED concepts from MAPPING_OT
---Mapping to ICDO3/SNOMED
-insert into concept_relationship_stage (concept_code_1,concept_code_2,vocabulary_id_1,vocabulary_id_2,relationship_id,valid_start_date,valid_end_date)
-with cond_code as
-	(
-		select *, o.icdo_morphology_code || '-' || o.icdo_topography_code as condition_code
-		from mapping_ot o
-		where o.icdo_morphology_code is not null
-	)
-select
-	o.oncotree_code,
-	c2.concept_code,
-	'OncoTree',
-	c2.vocabulary_id,
-	'Maps to',
-	to_date ('19700101','yyyymmdd'),
-	to_date ('19700101','20991231')
-from cond_code o
-join concept c on
-	c.concept_code = o.condition_code and
-	c.vocabulary_id = 'ICDO3'
-join concept_relationship r on
-	r.relationship_id = 'Maps to' and
-	r.concept_id_1 = c.concept_id and
-	r.invalid_reason is null
-join concept c2 on
-	c2.concept_id = r.concept_id_2
-where not exists
-	(
-		select 1
-		from concept_relationship_stage
-		where
-			concept_code_1 = o.oncotree_code and
-			vocabulary_id_2 != 'OncoTree'
-	)
-;
---6.1. Check for concepts that end up without mappings or hierarchical relationships
-DO $_$
-declare
-	codes text;
-BEGIN
-	select string_agg (concept_code, ''',''')
-	into codes
-	from concept_stage
-	left join mapping_ot on oncotree_code = concept_code
-	where concept_code not in 
-		(
-			select concept_code_1 from concept_relationship_stage
-			where vocabulary_id_2 != 'OncoTree' --external
-		);
-	IF codes IS NOT NULL THEN
-			RAISE EXCEPTION 'Following concepts lack proper mappings: ''%''', codes ;
-	END IF;
-END $_$
-;
 --7. Vocabulary pack procedures
 --7.1. Working with replacement mappings
 DO $_$
@@ -149,20 +94,3 @@ DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.DeleteAmbiguousMAPSTO();
 END $_$;
-
---Currently not in convention
-/*
---8. Make concepts without mapping standard
-update concept_stage s
-set standard_concept = 'S'
-where
-	not exists
-		(
-			select 1
-			from concept_relationship_stage r
-			where
-				s.concept_code = r.concept_code_1 and
-				r.relationship_id = 'Maps to' and
-				r.invalid_reason is null
-		)
-*/

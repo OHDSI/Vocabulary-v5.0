6d33ce3d0c7cff328a9448ebc778314eacb8f21e
Dmytry Dymshyts <ddymshyts@odysseusinc.com>
2021-07-30 09:57:58 +0300

    Update load_stage.sql
    load_stage fixes

--- OncoTree/load_stage.sql
+++ OncoTree/load_stage.sql
@@ -1,5 +1,5 @@
 /**************************************************************************
-* Copyright 2020 Observational Health Data Sciences and Informatics (OHDSI)
+* Copyright 2021 Observational Health Data Sciences and Informatics (OHDSI)
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -17,18 +17,18 @@
 * Date: 2021
 **************************************************************************/
 
--- 1. Vocabulary update routine
+--1. Update latest_update field to new date 
 DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.SetLatestUpdate(
 	pVocabularyName			=> 'OncoTree',
-	pVocabularyDate			=> TO_DATE ('20201001', 'yyyymmdd'), -- http://oncotree.mskcc.org/
-	pVocabularyVersion		=> 'OncoTree version 2020_10_01',
+	pVocabularyDate			=> (SELECT vocabulary_date FROM sources.oncotree_tree LIMIT 1), 
+	pVocabularyVersion		=> (SELECT vocabulary_version FROM sources.oncotree_tree LIMIT 1), 
 	pVocabularyDevSchema	=> 'DEV_ONCOTREE'
 );
 END $_$
 ;
---2. Initial cleanup
+--2. Truncate all working tables
 truncate table concept_stage, concept_relationship_stage, concept_synonym_stage, drug_strength_stage, pack_content_stage
 ;
 --3. Fill concept_stage with concepts
@@ -39,17 +39,15 @@ select
 	'OncoTree',
 	'Condition',
 	o.descendant_code,
-	(-- need to get the right dates for the first release
+/*	(
 		select latest_update
 		from vocabulary
 		where vocabulary_id = 'OncoTree'
-	) as valid_start_date,
+	) */ -- remove comments in the next release when we get the actual dates, while there's totally new release we treat all concepts as created sometimes in a past ('19700101')
+		to_date ('19700101','yyyymmdd')
+as valid_start_date,
 	to_date ('20991231','yyyymmdd')
-from oncotree o
-left join concept c on
-	c.vocabulary_id = 'OncoTree' and
-	c.concept_code = o.descendant_code
-where o.ancestor_code is not null
+from sources.oncotree_tree o
 ;
 --4. Put internal hierarchy in concept_relationship_stage
 insert into concept_relationship_stage (concept_code_1,concept_code_2,vocabulary_id_1,vocabulary_id_2,relationship_id,valid_start_date,valid_end_date)
@@ -61,8 +59,8 @@ select
 	'Is a',
 	to_date ('19700101','yyyymmdd'),
 	to_date ('20991231','yyyymmdd')
-from oncotree
-where ancestor_code != 'TISSUE'
+from sources.oncotree_tree 
+where ancestor_code is not null
 ;
 --5. Process manual relationships
 DO $_$
@@ -70,27 +68,21 @@ BEGIN
 	PERFORM VOCABULARY_PACK.ProcessManualRelationships();
 END $_$;
 ;
---7. Vocabulary pack procedures
---7.1. Working with replacement mappings
+--6. Vocabulary pack procedures
+--6.1. Working with replacement mappings
 DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.CheckReplacementMappings();
 END $_$;
 
---7.2, Add mapping from deprecated to fresh concepts
+--6.2, Add mapping from deprecated to fresh concepts
 DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.AddFreshMAPSTO();
 END $_$;
 
---7.3. Deprecate 'Maps to' mappings to deprecated and upgraded concepts
+--6.3. Deprecate 'Maps to' mappings to deprecated and upgraded concepts
 DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.DeprecateWrongMAPSTO();
-END $_$;
-
---7.4. Delete ambiguous 'Maps to' mappings
-DO $_$
-BEGIN
-	PERFORM VOCABULARY_PACK.DeleteAmbiguousMAPSTO();
 END $_$;

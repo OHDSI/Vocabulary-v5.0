ebf8b651e6765c977e7ec5454c6825f4a22bb2c7
Dmytry Dymshyts <ddymshyts@odysseusinc.com>
2021-08-17 17:41:19 +0300

    added broad and eq relationships

--- working/manual_changes/2021/2021-07-29.sql
+++ working/manual_changes/2021/2021-07-29.sql
@@ -18,12 +18,27 @@ END $_$;
 DO $_$
 BEGIN
 	PERFORM vocabulary_pack.AddNewRelationship(
-	pRelationship_name			=>'OncoTree to ICDO',
-	pRelationship_id			=>'OncoTree to ICDO',
+	pRelationship_name			=>'OncoTree to ICDO equivalent',
+	pRelationship_id			=>'OncoTree to ICDO eq',
 	pIs_hierarchical			=>0,
 	pDefines_ancestry			=>0,
-	pReverse_relationship_id	=>'ICDO to OncoTree',
-	pRelationship_name_rev		=>'ICDO to OncoTree',
+	pReverse_relationship_id	=>'ICDO to OncoTree eq',
+	pRelationship_name_rev		=>'ICDO to OncoTree equivalent',
+	pIs_hierarchical_rev		=>0,
+	pDefines_ancestry_rev		=>0
+);
+END $_$;
+
+--new relationships for Oncotre
+DO $_$
+BEGIN
+	PERFORM vocabulary_pack.AddNewRelationship(
+	pRelationship_name			=>'OncoTree to ICDO broader',
+	pRelationship_id			=>'OncoTree to ICDO br',
+	pIs_hierarchical			=>0,
+	pDefines_ancestry			=>0,
+	pReverse_relationship_id	=>'ICDO br to OncoTree',
+	pRelationship_name_rev		=>'ICDO broader to OncoTree',
 	pIs_hierarchical_rev		=>0,
 	pDefines_ancestry_rev		=>0
 );

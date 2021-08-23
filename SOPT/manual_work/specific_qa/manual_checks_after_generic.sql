9369f9a5ff96db88e35b870a7a72c9e96eed5e91
Alexander <32357692+Alexdavv@users.noreply.github.com>
2021-08-23 17:40:51 +0300

    Update manual_checks_after_generic.sql

--- SOPT/manual_work/specific_qa/manual_checks_after_generic.sql
+++ SOPT/manual_work/specific_qa/manual_checks_after_generic.sql
@@ -1,1 +1,7 @@
-
+--01 check names correctness/consistency
+SELECT s.concept_code, c.concept_name as old_name, s.concept_name as new_name, devv5.similarity (c.concept_name, s.concept_name)
+FROM sources.sopt_source s
+JOIN concept c ON c.concept_code = s.concept_code
+	AND c.vocabulary_id = 'SOPT'
+WHERE devv5.similarity (c.concept_name, s.concept_name) != 1
+;

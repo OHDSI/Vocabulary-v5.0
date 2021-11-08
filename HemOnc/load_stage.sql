d2d3a0d273f9e43461c376eac6e3482c58282f96
Dmytry Dymshyts <ddymshyts@odysseusinc.com>
2021-11-08 23:33:48 +0200

    added relationships exclusions

--- HemOnc/load_stage.sql
+++ HemOnc/load_stage.sql
@@ -126,7 +126,12 @@ WHERE r.relationship_id NOT IN (
 		'Has been compared to',
 		'Can be preceded by',
 		'Can be followed by',
-		'May require'
+		'May require',
+		--duplicated, not really know how to merge them with the existing hierarchy ('Is a' relationships)
+ 'Has major class',
+'Has minor class'
+)
+;
 		)
 	--Antithymocyte globulin rabbit ATG was mapped to Thymoglobulin (Brand Name) , correct mapping will be added below
 	AND NOT (

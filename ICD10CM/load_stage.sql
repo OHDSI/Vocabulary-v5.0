9dce0da3cab65006852695f56223969c240bb66a
Iryna Zh <zherko.irina@mail.ru>
2021-12-04 16:24:56 +0300

    Cancer modifier add

--- ICD10CM/load_stage.sql
+++ ICD10CM/load_stage.sql
@@ -178,7 +178,7 @@ FROM (
 		AND cs1.vocabulary_id = 'ICD10CM'
 	JOIN concept c2 ON c2.concept_code = crs.concept_code_2
 		AND c2.vocabulary_id = crs.vocabulary_id_2
-		AND c2.vocabulary_id in ( 'SNOMED', 'LOINC', 'Cancer Modifier', 'OMOP Extension')
+		AND c2.vocabulary_id in ( 'SNOMED', 'LOINC', 'Cancer Modifier')
 	WHERE crs.relationship_id = 'Maps to'
 		AND crs.invalid_reason IS NULL
 
@@ -204,7 +204,7 @@ FROM (
 	JOIN concept c1 ON c1.concept_id = cr.concept_id_1
 		AND c1.vocabulary_id = 'ICD10CM'
 	JOIN concept c2 ON c2.concept_id = cr.concept_id_2
-		AND c2.vocabulary_id in ('SNOMED', 'LOINC', 'Cancer Modifier', 'OMOP Extension')
+		AND c2.vocabulary_id in ('SNOMED', 'LOINC', 'Cancer Modifier')
 	JOIN concept_stage cs1 ON cs1.concept_code = c1.concept_code
 		AND cs1.vocabulary_id = c1.vocabulary_id
 	WHERE cr.relationship_id = 'Maps to'
@@ -326,3 +326,4 @@ WHERE 'ICD10CM' IN (
 			AND crs_int.relationship_id = r.relationship_id
 		);
 -- At the end, the three tables concept_stage, concept_relationship_stage and concept_synonym_stage should be ready to be fed into the generic_update.sql script
+

a4469616f04207001e2abf345e3868761e7a31f0
Irina <irina.zherko@odysseusinc.com>
2022-01-24 20:11:06 +0300

    code to run

---
+++ working/manual_changes/2022/2022-01-25.sql
@@ -0,0 +1,373 @@
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'milliliter per kilogram per minute',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'ml/kg/min'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'nanogram per kilogram per minute',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'ng/kg/min'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'Wood units * square meter',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'WU.m2'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'(dyne-second per centimeter to the fifth power)*square meter',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'(dyn.s/cm5).m2'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'million per kilogram',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'10*6/kg'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'meters per 6 minutes',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'m/(6.min)'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'millivolt',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'mV'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'gram per 72 hour',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'g/(72.h)'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'kilojoule/mole',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'kJ/mol'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'kiloarbitary unit/liters',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'k[arb''U]/L'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'gram per 48 hours',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'g/(48.h)'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'units per 10 to the 10th power cells',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'U/10*10{cells}'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'microns per second',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'um/s'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'picogram per gram of creatinine',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'pg/g{creat}'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'nanomole per minute per mg of proteine',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'nmol/min/mg{protein}'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'circulating tumor cells per 7.5 mililiters',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'{CTCs}/(7.5.mL)'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'femtoliter/nanoliter',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'fL/nL'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'unit per kilogram',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'U/kg{Hb}'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'unit per 2 hour',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'U/(2.h)'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'threshold cycle value',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'{Ct_value}'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'milliliter/pound',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'mL/[lb_av]'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'nanogram/nanogram',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'ng/ng'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'kilounits per liter',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'kU/L'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'free thyroxine index',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'{FTI%}'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'cells per mililiter',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'cells/ml'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'milligram/deciliter]/[24*hour]',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'mg/dL/(24.h)'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'picogram per milligram of creatinine',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'pg/mg{creat}'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'millimeter of induration',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'{MM_INDURATION}'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'picomole per hour per microliter',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'pmol/hr/uL'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'nanomole per day per milligram',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'nmol/(24.h)/mg'
+);
+END $_$;
+
+DO $_$
+BEGIN
+PERFORM vocabulary_pack.AddNewConcept(
+pConcept_name =>'milliliter per minute per 1.73 square meter',
+pDomain_id =>'Unit',
+pVocabulary_id =>'UCUM',
+pConcept_class_id =>'Unit',
+pStandard_concept =>'S',
+pConcept_code =>'mL/min/(173.10*-2.m2)'
+);
+END $_$;
+
+

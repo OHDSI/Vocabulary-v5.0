65df6213cab7dcf8601b972da70e07dc0465da24
Irina <irina.zherko@odysseusinc.com>
2022-01-30 18:21:32 +0300

    code change

--- NDC_SPL/manual_work/NDC_SPL_refresh.sql
+++ NDC_SPL/manual_work/NDC_SPL_refresh.sql
@@ -52,71 +52,131 @@ AND concept_id in ('1799556', '1799558', '1799562', '1799559', '1799560', '17795
 --4. Truncate the NDC_manual_mapped table. Save the spreadsheet as the NDC_manual_mapped table and upload it into the working schema.
 TRUNCATE TABLE dev_ndc.NDC_manual_mapped;
 
---5. Deprecate all mappings that differ from the new version of resulting mapping file.
+--5. Perform mapping (NDC_manual_mapped) checks
+SELECT *
+FROM dev_ndc.NDC_manual_mapped j1
+WHERE NOT EXISTS (  SELECT *
+                    FROM dev_ndc.NDC_manual_mapped j2
+                    JOIN dev_ndc.concept c
+                        ON j2.target_concept_id = c.concept_id
+                            AND c.concept_code = j2.target_concept_code
+                            AND replace(lower(c.concept_name), ' ', '') =
+                                replace(lower(j2.target_concept_name), ' ', '')
+                            AND c.vocabulary_id = j2.target_vocabulary_id
+                            AND c.domain_id = j2.target_domain_id
+                            AND c.standard_concept = 'S'
+                            AND c.invalid_reason is NULL
+                           WHERE j1.source_concept_id = j2.source_concept_id
+                  )
+    AND j1.target_concept_id != '17'
+    AND j1.target_concept_id BETWEEN 2 AND 2000000000
+    ;
+
+--6. Deprecate all mappings that differ from the new version of resulting mapping file.
+--SELECT (try-out for the following UPDATE)
+SELECT *
+FROM dev_ndc.concept_relationship_manual crm
+WHERE crm.vocabulary_id_1 = 'NDC'
+    AND crm.invalid_reason IS NULL
+    AND crm.concept_code_1 IN (SELECT source_code FROM dev_ndc.NDC_manual_mapped WHERE source_code IS NOT NULL)
+    AND NOT EXISTS (SELECT 1
+                    FROM dev_ndc.NDC_manual_mapped m
+                    JOIN dev_ndc.concept c
+                        ON  m.target_concept_id = c.concept_id
+                    WHERE crm.concept_code_1 = m.source_code
+                        AND crm.concept_code_2 = c.concept_code
+                        AND crm.vocabulary_id_2 = c.vocabulary_id
+                    )
+;
+
 UPDATE dev_ndc.concept_relationship_manual
 SET invalid_reason = 'D',
     valid_end_date = current_date
---SELECT*FROM concept_relationship_manual
-WHERE (concept_code_1, concept_code_2, relationship_id, vocabulary_id_2) IN
-      (SELECT concept_code_1, concept_code_2, relationship_id, vocabulary_id_2
-       FROM concept_relationship_manual crm_old
-
-       WHERE NOT exists(SELECT source_code,
-                               target_concept_code,
-                               'NDC',
-                               target_vocabulary_id,
-                               CASE
-                                   WHEN to_value ~* 'value' THEN 'Maps to value'
-                                   WHEN to_value ~* 'Is a' THEN 'Is a'
-                                   WHEN to_value ~* 'Subsumes' THEN 'Subsumes'
-                                   ELSE 'Maps to' END
-                        FROM dev_ndc.NDC_manual_mapped crm_new
-                        WHERE source_code = crm_old.concept_code_1
-                          AND target_concept_code = crm_old.concept_code_2
-                          AND target_vocabulary_id = crm_old.vocabulary_id_2
-                          AND CASE
-                                  WHEN to_value ~* 'value' THEN 'Maps to value'
-                    WHEN to_value ~* 'Is a' THEN 'Is a'
-                    WHEN to_value ~* 'Subsumes' THEN 'Subsumes'
-                   ELSE 'Maps to' END = crm_old.relationship_id
-
-    )
-    AND invalid_reason IS NULL
-    )
+WHERE (concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id, valid_start_date, valid_end_date, coalesce(invalid_reason, '1')) in
+      (SELECT concept_code_1,
+              concept_code_2,
+              vocabulary_id_1,
+              vocabulary_id_2,
+              relationship_id,
+              valid_start_date,
+              valid_end_date,
+              coalesce(crm.invalid_reason, '1')
+       FROM dev_ndc.concept_relationship_manual crm
+       WHERE crm.vocabulary_id_1 = 'NDC'
+           AND crm.invalid_reason IS NULL
+           AND crm.concept_code_1 IN (SELECT source_code FROM dev_ndc.NDC_manual_mapped WHERE source_code IS NOT NULL)
+           AND NOT EXISTS (SELECT 1
+                           FROM dev_ndc.NDC_manual_mapped m
+                           JOIN dev_ndc.concept c
+                               ON  m.target_concept_id = c.concept_id
+                           WHERE crm.concept_code_1 = m.source_code
+                               AND crm.concept_code_2 = c.concept_code
+                               AND crm.vocabulary_id_2 = c.vocabulary_id
+                            )
+)
 ;
 
---6. Insert new and corrected mappings into the concept_relationship_manual table.
-with mapping AS
-    (
-        SELECT DISTINCT source_code AS concept_code_1,
-               target_concept_code AS concept_code_2,
-               'NDC' AS vocabulary_id_1,
-               target_vocabulary_id AS vocabulary_id_2,
-               CASE WHEN to_value ~* 'value' THEN 'Maps to value'
-                    WHEN to_value ~* 'Is a' THEN 'Is a'
-                    WHEN to_value ~* 'Subsumes' THEN 'Subsumes'
-                   ELSE 'Maps to' END AS relationship_id,
-               current_date AS valid_start_date,
-               to_date('20991231','yyyymmdd') AS valid_end_date,
-               NULL AS invalid_reason
-        FROM dev_ndc.NDC_manual_mapped
-        WHERE target_concept_id != 0
+--7. Insert new and corrected mappings into the concept_relationship_manual table.
+--mapping insertion
+with tab as (
+    SELECT DISTINCT s.*
+    FROM dev_ndc.NDC_manual_mapped s
+)
+INSERT INTO dev_ndc.concept_relationship_manual
+      (concept_code_1,
+      concept_code_2,
+       vocabulary_id_1,
+       vocabulary_id_2,
+       relationship_id,
+       valid_start_date,
+       valid_end_date,
+      invalid_reason)
+
+SELECT DISTINCT m.source_code as concept_code_1,
+                c.concept_code as concept_code_2,
+                cc.vocabulary_id as vocabulary_id_1,
+                c.vocabulary_id as vocabulary_id_2,
+                'Maps to' as relationship_id,
+                current_date as valid_start_date,
+                TO_DATE('20991231', 'YYYYMMDD') AS valid_end_date,
+                NULL as invalid_reason
+FROM tab m
+
+LEFT JOIN devv5.concept c
+    ON m.target_concept_id = c.concept_id
+
+LEFT JOIN devv5.concept cc
+    ON m.source_code = cc.concept_code AND cc.vocabulary_id = 'NDC'
+
+WHERE m.target_concept_id NOT IN (0, 17) AND m.target_concept_id IS NOT NULL AND c.concept_id IS NOT NULL AND cc.concept_code IS NOT NULL
+
+AND (
+
+    (NOT EXISTS (SELECT 1
+                FROM devv5.concept_relationship cr
+                WHERE cr.concept_id_1 = m.source_concept_id
+                    AND cr.relationship_id = 'Maps to'
+                    AND cr.invalid_reason IS NULL)
+        )
+
+    OR
+          (m.source_code, cc.vocabulary_id) IN (SELECT concept_code_1, vocabulary_id_1 FROM dev_ndc.concept_relationship_manual)
+        )
+
+AND     (NOT EXISTS (SELECT 1
+                FROM dev_ndc.concept_relationship_manual crm
+                WHERE m.source_code = crm.concept_code_1
+                    AND cc.vocabulary_id = crm.vocabulary_id_1
+                    AND c.concept_code = crm.concept_code_2
+                    AND c.vocabulary_id = crm.vocabulary_id_2
+                    AND crm.relationship_id = 'Maps to'
+                    AND crm.invalid_reason IS NULL
+                    )
     )
 
-INSERT INTO dev_ndc.concept_relationship_manual(concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id, valid_start_date, valid_end_date, invalid_reason)
-    (SELECT concept_code_1,
-            concept_code_2,
-            vocabulary_id_1,
-            vocabulary_id_2,
-            relationship_id,
-            valid_start_date,
-            valid_end_date,
-            invalid_reason
-     FROM mapping m
-        WHERE (concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id)
-                  NOT IN (SELECT concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id FROM dev_ndc.concept_relationship_manual)
-    )
+ORDER BY 1,2,3,4,5,6,7,8
 ;
+
 
 
 
--- NDC_SPL/readme.md
+++ NDC_SPL/readme.md
@@ -28,13 +28,13 @@ Open the site http://www.fda.gov/Drugs/InformationOnDrugs/ucm142438.htm
 SELECT devv5.FastRecreateSchema(main_schema_name=>'devv5', include_concept_ancestor=>true, include_deprecated_rels=>true, include_synonyms=>true);
 ```
 
-8. Run load_stage.sql
+8. Run [load_stage.sql]
 
 9. Perform manual work described in manual_work folder
 
 10. Run [load_stage.sql]
 
-11. Stage tables checks (should retrieve NULL)
+11. Perform stage tables checks (should retrieve NULL)
 ```sql
  SELECT * FROM qa_tests.check_stage_tables ();
 ```
@@ -44,6 +44,42 @@ SELECT devv5.FastRecreateSchema(main_schema_name=>'devv5', include_concept_ances
 SELECT devv5.GenericUpdate();
 ```
 
-13. Run [manual_checks_after_generic.sql](https://github.com/OHDSI/Vocabulary-v5.0/blob/master/working/manual_checks_after_generic.sql)
+13. Perform QA checks (should retrieve NULL)
+```sql
+SELECT * FROM QA_TESTS.GET_CHECKS();
+```
 
-14. If no problems, enjoy!
+14. Run [manual_checks_after_generic.sql](https://github.com/OHDSI/Vocabulary-v5.0/blob/master/working/manual_checks_after_generic.sql)
+
+15. Get_summary - changes in tables between dev-schema (current) and devv5/prodv5/any other schema
+
+--15.1. first clean cache
+select * from qa_tests.purge_cache();
+
+--15.2. summary (table to check, schema to compare)
+select * from qa_tests.get_summary (table_name=>'concept',pCompareWith=>'devv5');
+
+--15.3. summary (table to check, schema to compare)
+select * from qa_tests.get_summary (table_name=>'concept_relationship',pCompareWith=>'devv5');
+
+--15.4. summary (table to check, schema to compare)
+select * from qa_tests.get_summary (table_name=>'concept_ancestor',pCompareWith=>'devv5');
+
+
+16. Statistics QA checks
+--changes in tables between dev-schema (current) and devv5/prodv5/any other schema
+
+--16.1. Domain changes
+select * from qa_tests.get_domain_changes(pCompareWith=>'devv5');
+
+--16.2. Newly added concepts grouped by vocabulary_id and domain
+select * from qa_tests.get_newly_concepts(pCompareWith=>'devv5');
+
+--16.3. Standard concept changes
+select * from qa_tests.get_standard_concept_changes(pCompareWith=>'devv5');
+
+--16.4. Newly added concepts and their standard concept status
+select * from qa_tests.get_newly_concepts_standard_concept_status(pCompareWith=>'devv5');
+
+--16.5. Changes of concept mapping status grouped by target domain
+select * from qa_tests.get_changes_concept_mapping(pCompareWith=>'devv5');

cb5aee23b1879a9bc4c33f0f1cdd3a06337b924e
Timur <timur.vakhitov@firstlinesoftware.com>
2022-03-04 16:43:26 +0300

    Create_DEV_from_DevV5_DDL.sql file replaced with FillNewDevSchema.sql function

--- working/FillNewDevSchema.sql
+++ working/FillNewDevSchema.sql
@@ -14,11 +14,26 @@
 * limitations under the License.
 * 
 * Authors: Timur Vakhitov
-* Date: 2017
+* Date: 2022
 **************************************************************************/
 
-DO $$ 
+CREATE OR REPLACE FUNCTION devv5.FillNewDevSchema (
+	include_concept_ancestor BOOLEAN DEFAULT FALSE,
+	include_deprecated_rels BOOLEAN DEFAULT FALSE,
+	include_synonyms BOOLEAN DEFAULT FALSE
+)
+RETURNS void AS
+$BODY$
+/*
+	Use this script to fill main tables (concept, concept_relationship, concept_synonym etc) for newly created schema from devv5
+	Examples:
+	SELECT devv5.FillNewDevSchema(); --filling with default settings (w/o ancestor, deprecated relationships and synonyms)
+	SELECT devv5.FillNewDevSchema(include_concept_ancestor=>true); --same as above, but the concept_ancestor is included
+	SELECT devv5.FillNewDevSchema(include_concept_ancestor=>true,include_deprecated_rels=>true,include_synonyms=>true); --all data will be copied
+*/
 BEGIN
+	IF CURRENT_SCHEMA = 'devv5' THEN RAISE EXCEPTION 'You cannot use this script in the DevV5!'; END IF;
+
 	--Create stage and 'manual' tables
 	DROP TABLE IF EXISTS drug_strength_stage;
 	CREATE TABLE drug_strength_stage (LIKE devv5.drug_strength_stage);
@@ -43,11 +58,11 @@ BEGIN
 	--Create copies of table
 	DROP TABLE IF EXISTS concept_ancestor, concept, concept_relationship, relationship, vocabulary, vocabulary_conversion, concept_class, domain, concept_synonym, drug_strength, pack_content CASCADE;
 	CREATE TABLE concept_ancestor (LIKE devv5.concept_ancestor);
-	INSERT INTO concept_ancestor SELECT * FROM devv5.concept_ancestor;
+	INSERT INTO concept_ancestor SELECT * FROM devv5.concept_ancestor WHERE include_concept_ancestor;
 	CREATE TABLE concept (LIKE devv5.concept INCLUDING CONSTRAINTS);
 	INSERT INTO concept SELECT * FROM devv5.concept;
 	CREATE TABLE concept_relationship (LIKE devv5.concept_relationship INCLUDING CONSTRAINTS);
-	INSERT INTO concept_relationship SELECT * FROM devv5.concept_relationship;
+	INSERT INTO concept_relationship SELECT * FROM devv5.concept_relationship WHERE (invalid_reason IS NULL AND NOT include_deprecated_rels) OR include_deprecated_rels;
 	CREATE TABLE relationship (LIKE devv5.relationship);
 	INSERT INTO relationship SELECT * FROM devv5.relationship;
 	CREATE TABLE vocabulary (LIKE devv5.vocabulary);
@@ -59,7 +74,7 @@ BEGIN
 	CREATE TABLE domain (LIKE devv5.domain);
 	INSERT INTO domain SELECT * FROM devv5.domain;
 	CREATE TABLE concept_synonym (LIKE devv5.concept_synonym INCLUDING CONSTRAINTS);
-	INSERT INTO concept_synonym SELECT * FROM devv5.concept_synonym;
+	INSERT INTO concept_synonym SELECT * FROM devv5.concept_synonym WHERE include_synonyms;
 	CREATE TABLE drug_strength (LIKE devv5.drug_strength);
 	INSERT INTO drug_strength SELECT * FROM devv5.drug_strength;
 	CREATE TABLE pack_content (LIKE devv5.pack_content);
@@ -75,28 +90,28 @@ BEGIN
 	ALTER TABLE concept_ancestor ADD CONSTRAINT xpkconcept_ancestor PRIMARY KEY (ancestor_concept_id,descendant_concept_id);
 	ALTER TABLE drug_strength ADD CONSTRAINT xpk_drug_strength PRIMARY KEY (drug_concept_id, ingredient_concept_id);
 
-	--Create external keys
-	ALTER TABLE concept ADD CONSTRAINT fpk_concept_domain FOREIGN KEY (domain_id) REFERENCES domain (domain_id);
-	ALTER TABLE concept ADD CONSTRAINT fpk_concept_class FOREIGN KEY (concept_class_id) REFERENCES concept_class (concept_class_id);
-	ALTER TABLE concept ADD CONSTRAINT fpk_concept_vocabulary FOREIGN KEY (vocabulary_id) REFERENCES vocabulary (vocabulary_id);
-	ALTER TABLE vocabulary ADD CONSTRAINT fpk_vocabulary_concept FOREIGN KEY (vocabulary_concept_id) REFERENCES concept (concept_id);
-	ALTER TABLE domain ADD CONSTRAINT fpk_domain_concept FOREIGN KEY (domain_concept_id) REFERENCES concept (concept_id);
-	ALTER TABLE concept_class ADD CONSTRAINT fpk_concept_class_concept FOREIGN KEY (concept_class_concept_id) REFERENCES concept (concept_id);
-	ALTER TABLE concept_relationship ADD CONSTRAINT fpk_concept_relationship_c_1 FOREIGN KEY (concept_id_1) REFERENCES concept (concept_id);
-	ALTER TABLE concept_relationship ADD CONSTRAINT fpk_concept_relationship_c_2 FOREIGN KEY (concept_id_2) REFERENCES concept (concept_id);
-	ALTER TABLE concept_relationship ADD CONSTRAINT fpk_concept_relationship_id FOREIGN KEY (relationship_id) REFERENCES relationship (relationship_id);
-	ALTER TABLE relationship ADD CONSTRAINT fpk_relationship_concept FOREIGN KEY (relationship_concept_id) REFERENCES concept (concept_id);
-	ALTER TABLE relationship ADD CONSTRAINT fpk_relationship_reverse FOREIGN KEY (reverse_relationship_id) REFERENCES relationship (relationship_id);
-	ALTER TABLE concept_synonym ADD CONSTRAINT fpk_concept_synonym_concept FOREIGN KEY (concept_id) REFERENCES concept (concept_id);
-	ALTER TABLE concept_synonym ADD CONSTRAINT fpk_concept_synonym_language FOREIGN KEY (language_concept_id) REFERENCES concept (concept_id);
+	--Create external keys and UNIQUE constraints
+	ALTER TABLE concept ADD CONSTRAINT fpk_concept_domain FOREIGN KEY (domain_id) REFERENCES domain (domain_id) NOT VALID;
+	ALTER TABLE concept ADD CONSTRAINT fpk_concept_class FOREIGN KEY (concept_class_id) REFERENCES concept_class (concept_class_id) NOT VALID;
+	ALTER TABLE concept ADD CONSTRAINT fpk_concept_vocabulary FOREIGN KEY (vocabulary_id) REFERENCES vocabulary (vocabulary_id) NOT VALID;
+	ALTER TABLE vocabulary ADD CONSTRAINT fpk_vocabulary_concept FOREIGN KEY (vocabulary_concept_id) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE domain ADD CONSTRAINT fpk_domain_concept FOREIGN KEY (domain_concept_id) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE concept_class ADD CONSTRAINT fpk_concept_class_concept FOREIGN KEY (concept_class_concept_id) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE concept_relationship ADD CONSTRAINT fpk_concept_relationship_c_1 FOREIGN KEY (concept_id_1) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE concept_relationship ADD CONSTRAINT fpk_concept_relationship_c_2 FOREIGN KEY (concept_id_2) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE concept_relationship ADD CONSTRAINT fpk_concept_relationship_id FOREIGN KEY (relationship_id) REFERENCES relationship (relationship_id) NOT VALID;
+	ALTER TABLE relationship ADD CONSTRAINT fpk_relationship_concept FOREIGN KEY (relationship_concept_id) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE relationship ADD CONSTRAINT fpk_relationship_reverse FOREIGN KEY (reverse_relationship_id) REFERENCES relationship (relationship_id) NOT VALID;
+	ALTER TABLE concept_synonym ADD CONSTRAINT fpk_concept_synonym_concept FOREIGN KEY (concept_id) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE concept_synonym ADD CONSTRAINT fpk_concept_synonym_language FOREIGN KEY (language_concept_id) REFERENCES concept (concept_id) NOT VALID;
 	ALTER TABLE concept_synonym ADD CONSTRAINT unique_synonyms UNIQUE (concept_id,concept_synonym_name,language_concept_id);
-	ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_concept_1 FOREIGN KEY (drug_concept_id) REFERENCES concept (concept_id);
-	ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_concept_2 FOREIGN KEY (ingredient_concept_id) REFERENCES concept (concept_id);
-	ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_unit_1 FOREIGN KEY (amount_unit_concept_id) REFERENCES concept (concept_id);
-	ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_unit_2 FOREIGN KEY (numerator_unit_concept_id) REFERENCES concept (concept_id);
-	ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_unit_3 FOREIGN KEY (denominator_unit_concept_id) REFERENCES concept (concept_id);
-	ALTER TABLE pack_content ADD CONSTRAINT fpk_pack_content_concept_1 FOREIGN KEY (pack_concept_id) REFERENCES concept (concept_id);
-	ALTER TABLE pack_content ADD CONSTRAINT fpk_pack_content_concept_2 FOREIGN KEY (drug_concept_id) REFERENCES concept (concept_id);
+	ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_concept_1 FOREIGN KEY (drug_concept_id) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_concept_2 FOREIGN KEY (ingredient_concept_id) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_unit_1 FOREIGN KEY (amount_unit_concept_id) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_unit_2 FOREIGN KEY (numerator_unit_concept_id) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE drug_strength ADD CONSTRAINT fpk_drug_strength_unit_3 FOREIGN KEY (denominator_unit_concept_id) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE pack_content ADD CONSTRAINT fpk_pack_content_concept_1 FOREIGN KEY (pack_concept_id) REFERENCES concept (concept_id) NOT VALID;
+	ALTER TABLE pack_content ADD CONSTRAINT fpk_pack_content_concept_2 FOREIGN KEY (drug_concept_id) REFERENCES concept (concept_id) NOT VALID;
 
 	--Create indexes
 	CREATE UNIQUE INDEX idx_unique_concept_code ON concept (vocabulary_id, concept_code) WHERE vocabulary_id NOT IN ('DRG', 'SMQ') AND concept_code <> 'OMOP generated';
@@ -132,4 +147,7 @@ BEGIN
 	ANALYZE drug_strength;
 	ANALYZE pack_content;
 	ANALYZE concept_ancestor;
-END$$;
+END;
+$BODY$
+LANGUAGE 'plpgsql' SECURITY INVOKER
+SET client_min_messages = error;

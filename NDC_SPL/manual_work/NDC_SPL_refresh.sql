4791f5b2603d4dfcfb66f85338b900ee71f4e47f
Irina <irina.zherko@odysseusinc.com>
2022-04-12 17:46:41 +0300

    NDC script update

---
+++ NDC_SPL/manual_work/NDC_SPL_refresh.sql
@@ -0,0 +1,136 @@
+--1. Backup concept_relationship_manual table
+DO
+$body$
+    DECLARE
+        update text;
+    BEGIN
+        SELECT TO_CHAR(CURRENT_DATE, 'YYYY_MM_DD')
+        INTO update;
+        EXECUTE FORMAT('create table %I as select * from concept_relationship_manual',
+                       'concept_relationship_manual_backup_' || update);
+
+    END
+$body$;
+
+--restore concept_relationship_manual table (run it only if something went wrong)
+/*TRUNCATE TABLE dev_ndc.concept_relationship_manual;
+INSERT INTO dev_ndc.concept_relationship_manual
+SELECT * FROM dev_ndc.concept_relationship_manual_backup_YYYY_MM_DD;*/
+
+--2. Create NDC_manual_mapped table and pre-populate it with the resulting manual table of the previous NDC refresh
+--DROP TABLE dev_ndc.NDC_manual_mapped;
+CREATE TABLE dev_ndc.NDC_manual_mapped (
+    source_concept_id int,
+    source_code varchar(255),
+    source_code_description varchar(1000),
+    comments varchar,
+    --flag varchar,
+    target_concept_id int,
+    target_concept_code varchar(255),
+    target_concept_name varchar(255),
+    target_concept_class_id varchar(255),
+    target_standard_concept varchar(255),
+    target_invalid_reason varchar(255),
+    target_domain_id varchar(255),
+    target_vocabulary_id varchar(255)
+);
+
+--3. Select concepts to map and add them to the manual file in the spreadsheet editor.
+
+--4. Truncate the NDC_manual_mapped table. Save the spreadsheet as the NDC_manual_mapped table and upload it into the working schema.
+TRUNCATE TABLE dev_ndc.NDC_manual_mapped;
+
+--5. Perform mapping (NDC_manual_mapped) checks
+
+--6. Deprecate all mappings that differ from the new version of resulting mapping file.
+
+-- Perform UPDATE after review SELECT result
+UPDATE dev_ndc.concept_relationship_manual
+SET invalid_reason = 'D',
+    valid_end_date = current_date
+WHERE (concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id, valid_start_date, valid_end_date, coalesce(invalid_reason, '1')) in
+
+--SELECT (try-out for the following UPDATE)
+      (SELECT concept_code_1,
+              concept_code_2,
+              vocabulary_id_1,
+              vocabulary_id_2,
+              relationship_id,
+              valid_start_date,
+              valid_end_date,
+              coalesce(crm.invalid_reason, '1')
+       FROM dev_ndc.concept_relationship_manual crm
+       WHERE crm.vocabulary_id_1 = 'NDC'
+           AND crm.invalid_reason IS NULL
+           AND crm.concept_code_1 IN (SELECT source_code FROM dev_ndc.NDC_manual_mapped WHERE source_code IS NOT NULL)
+           AND NOT EXISTS (SELECT 1
+                           FROM dev_ndc.NDC_manual_mapped m
+                           JOIN dev_ndc.concept c
+                               ON  m.target_concept_id = c.concept_id
+                               WHERE crm.concept_code_1 = m.source_code
+                               AND crm.concept_code_2 = c.concept_code
+                               AND crm.vocabulary_id_2 = c.vocabulary_id
+                            )
+)
+;
+
+--7. Insert new and corrected mappings into the concept_relationship_manual table.
+--mapping insertion
+with tab as (
+    SELECT DISTINCT s.*
+    FROM dev_ndc.NDC_manual_mapped s
+)
+INSERT INTO dev_ndc.concept_relationship_manual
+      (concept_code_1,
+      concept_code_2,
+       vocabulary_id_1,
+       vocabulary_id_2,
+       relationship_id,
+       valid_start_date,
+       valid_end_date,
+      invalid_reason)
+
+SELECT DISTINCT m.source_code as concept_code_1,
+                c.concept_code as concept_code_2,
+                cc.vocabulary_id as vocabulary_id_1,
+                c.vocabulary_id as vocabulary_id_2,
+                'Maps to' as relationship_id,
+                current_date as valid_start_date,
+                TO_DATE('20991231', 'YYYYMMDD') AS valid_end_date,
+                NULL as invalid_reason
+FROM tab m
+
+LEFT JOIN devv5.concept c
+    ON m.target_concept_id = c.concept_id
+
+LEFT JOIN devv5.concept cc
+    ON m.source_code = cc.concept_code AND cc.vocabulary_id = 'NDC'
+
+WHERE m.target_concept_id NOT IN (0, 17) AND m.target_concept_id IS NOT NULL AND c.concept_id IS NOT NULL AND cc.concept_code IS NOT NULL
+
+AND (
+
+    (NOT EXISTS (SELECT 1
+                FROM devv5.concept_relationship cr
+                WHERE cr.concept_id_1 = m.source_concept_id
+                    AND cr.relationship_id = 'Maps to'
+                    AND cr.invalid_reason IS NULL)
+        )
+
+    OR
+          (m.source_code, cc.vocabulary_id) IN (SELECT concept_code_1, vocabulary_id_1 FROM dev_ndc.concept_relationship_manual)
+        )
+
+AND     (NOT EXISTS (SELECT 1
+                FROM dev_ndc.concept_relationship_manual crm
+                WHERE m.source_code = crm.concept_code_1
+                    AND cc.vocabulary_id = crm.vocabulary_id_1
+                    AND c.concept_code = crm.concept_code_2
+                    AND c.vocabulary_id = crm.vocabulary_id_2
+                    AND crm.relationship_id = 'Maps to'
+                    AND crm.invalid_reason IS NULL
+                    )
+    )
+
+ORDER BY 1,2,3,4,5,6,7,8
+;
--- NDC_SPL/readme.md
+++ NDC_SPL/readme.md
@@ -7,15 +7,78 @@ Prerequisites:
 - Created schema ApiGrabber (\working\packages\APIgrabber). You must execute all functions in ApiGrabber at least once
 
 1. Run create_source_tables.sql
+
 2. Download NDC code distrbution file
 Open the site http://www.fda.gov/Drugs/InformationOnDrugs/ucm142438.htm
 - Download the latest NDC Database File
 - Extract product.txt and package.txt files
+
 3. Download additional source for SPL concepts and relationships from https://dailymed.nlm.nih.gov/dailymed/spl-resources-all-drug-labels.cfm and https://dailymed.nlm.nih.gov/dailymed/spl-resources-all-mapping-files.cfm
 - Full Releases of HUMAN PRESCRIPTION LABELS, HUMAN OTC LABELS, HOMEOPATHIC LABELS and REMAINDER LABELS (1st link)
 - SPL-RXNORM MAPPINGS (2d link)
+
 4. Extract LABELS using unzipxml.sh
+
 5. Extract rxnorm_mappings.txt from rxnorm_mappings.zip
-6. Run in devv5 (with fresh vocabulary date and version): SELECT sources.load_input_tables('NDC_SPL',TO_DATE('20180420','YYYYMMDD'),'NDC 20180420');
-7. Run load_stage.sql
-8. Run generic_update: devv5.GenericUpdate();
+
+6. Run in devv5 (with fresh vocabulary date and version): SELECT sources.load_input_tables('NDC',TO_DATE('20180420','YYYYMMDD'),'NDC 20180420');
+
+7. Run the FastRecreate (Full recreate, all tables are included):
+```sql
+SELECT devv5.FastRecreateSchema(main_schema_name=>'devv5', include_concept_ancestor=>true, include_deprecated_rels=>true, include_synonyms=>true);
+```
+8. Run [load_stage.sql]
+
+9. Perform manual work described in manual_work folder
+
+10. Run [load_stage.sql]
+
+11. Perform stage tables checks (should retrieve NULL)
+```sql
+ SELECT * FROM qa_tests.check_stage_tables ();
+```
+
+12. Run generic_update:
+```sql
+SELECT devv5.GenericUpdate();
+```
+
+13. Perform QA checks (should retrieve NULL)
+```sql
+SELECT * FROM QA_TESTS.GET_CHECKS();
+```
+
+14. Run [manual_checks_after_generic.sql](https://github.com/OHDSI/Vocabulary-v5.0/blob/master/working/manual_checks_after_generic.sql)
+
+15. Get_summary - changes in tables between dev-schema (current) and devv5/prodv5/any other schema
+
+--15.1. first clean cache
+select * from qa_tests.purge_cache();
+
+--15.2. summary (table to check, schema to compare)
+select * from qa_tests.get_summary (table_name=>'concept',pCompareWith=>'devv5');
+
+--15.3. summary (table to check, schema to compare)
+select * from qa_tests.get_summary (table_name=>'concept_relationship',pCompareWith=>'devv5');
+
+--15.4. summary (table to check, schema to compare)
+select * from qa_tests.get_summary (table_name=>'concept_ancestor',pCompareWith=>'devv5');
+
+
+16. Statistics QA checks
+--changes in tables between dev-schema (current) and devv5/prodv5/any other schema
+
+--16.1. Domain changes
+select * from qa_tests.get_domain_changes(pCompareWith=>'devv5');
+
+--16.2. Newly added concepts grouped by vocabulary_id and domain
+select * from qa_tests.get_newly_concepts(pCompareWith=>'devv5');
+
+--16.3. Standard concept changes
+select * from qa_tests.get_standard_concept_changes(pCompareWith=>'devv5');
+
+--16.4. Newly added concepts and their standard concept status
+select * from qa_tests.get_newly_concepts_standard_concept_status(pCompareWith=>'devv5');
+
+--16.5. Changes of concept mapping status grouped by target domain
+select * from qa_tests.get_changes_concept_mapping(pCompareWith=>'devv5');

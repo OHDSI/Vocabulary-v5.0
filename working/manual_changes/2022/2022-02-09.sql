dc9970399d09480ce45f8cc68f270cbd3f318e9d
Dmytry Dymshyts <dimshitc@gmail.com>
2022-05-24 12:52:09 +0300

    added INXIGHT to the vocabulary reference

--- working/manual_changes/2022/2022-02-09.sql
+++ working/manual_changes/2022/2022-02-09.sql
@@ -4,7 +4,7 @@ BEGIN
 	PERFORM VOCABULARY_PACK.AddNewVocabulary(
 	pVocabulary_id			=> 'OMOP Invest Drug',
 	pVocabulary_name		=> 'OMOP Investigational Drugs',
-	pVocabulary_reference	=> 'https://go.drugbank.com/drugs, https://evs.nci.nih.gov/ftp1/NCI_Thesaurus/Drug_or_Substance/Antineoplastic_Agent.txt',
+	pVocabulary_reference	=> 'https://gsrs.ncats.nih.gov/, https://evs.nci.nih.gov/ftp1/NCI_Thesaurus/Drug_or_Substance/Antineoplastic_Agent.txt',
 	pVocabulary_version		=> NULL,
 	pOMOP_req				=> NULL,
 	pClick_default			=> NULL, --NULL or 'Y'

1cde380fda8669c41ab43b87316856d36c785223
olegzhuk <oleg.zhuk@odysseusinc.com>
2022-05-27 14:16:42 +0200

    Review notes fixed: files transferred to google drive, renamed, footer added, DeleteAmbiguousMAPSTO added, date corrected

--- LPD_Australia/load_stage.sql
+++ LPD_Australia/load_stage.sql
@@ -4,7 +4,7 @@ BEGIN
 	PERFORM VOCABULARY_PACK.SetLatestUpdate(
 	pVocabularyName			=>'LPD_Australia',
 	pVocabularyDate			=> CURRENT_DATE,
-	pVocabularyVersion		=> CONCAT('LPD_Australia ', CURRENT_DATE ),
+	pVocabularyVersion		=> 'LPD_Australia '||TO_CHAR(CURRENT_DATE,'YYYY-MM-DD'),
 	pVocabularyDevSchema	=> 'dev_aus'
 );
 END $_$;
@@ -33,3 +33,12 @@ DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.AddFreshMAPSTO();
 END $_$;
+
+--6. Delete ambiguous 'Maps to' mappings
+
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.DeleteAmbiguousMAPSTO();
+END $_$;
+
+-- At the end, the three tables concept_stage, concept_relationship_stage and concept_synonym_stage should be ready to be fed into the generic_update.sql script

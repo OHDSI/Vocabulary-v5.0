12f78d0b2a4d3f01901109b7fc333379e34bf93c
MariaRohozhkina <mariaspirit228@gmail.com>
2022-09-15 15:15:48 +0300

    add script for Domain and Class

--- OMOP Extension/load_stage.sql
+++ OMOP Extension/load_stage.sql
@@ -78,7 +78,187 @@ BEGIN
 END $_$;*/
 
 --10. Assign Domain for concepts with unassigned Domain using the Domain of an ancestor
---TODO: AVOF-3548
+DO $$
+DECLARE
+	DOMAINS_ARRAY VARCHAR[]:=ARRAY['Condition','Observation','Procedure','Measurement','Device']; --order does matter
+	CONCEPT_CLASS_ARRAY VARCHAR[]:=ARRAY['Condition','Observation','Procedure','Measurement','Device']; --order does matter
+BEGIN
+	--build the concept_ancestor
+	DROP TABLE IF EXISTS omop_ext_ancestor;
+	CREATE UNLOGGED TABLE omop_ext_ancestor AS
+		WITH RECURSIVE hierarchy_concepts (
+			ancestor_concept_code,
+			ancestor_vocabulary_id,
+			descendant_concept_code,
+			descendant_vocabulary_id,
+			descendant_domain_id,
+			descendant_concept_class_id,
+			root_ancestor_concept_code,
+			root_ancestor_vocabulary_id,
+			full_path,
+			hierarchy_path
+			)
+		AS (
+			SELECT ancestor_concept_code,
+				ancestor_vocabulary_id,
+				descendant_concept_code,
+				descendant_vocabulary_id,
+				descendant_domain_id,
+				descendant_concept_class_id,
+				ancestor_concept_code AS root_ancestor_concept_code,
+				ancestor_vocabulary_id AS root_ancestor_vocabulary_id,
+				ARRAY [ROW (descendant_concept_code, descendant_vocabulary_id)] AS full_path,
+				--ARRAY [ancestor_concept_code||' ('||ancestor_vocabulary_id||')', descendant_concept_code||' ('||descendant_vocabulary_id||')'] AS hierarchy_path
+				ARRAY [ancestor_concept_code, descendant_concept_code]::TEXT[] AS hierarchy_path --w/o casting to TEXT[] we get an error:  recursive query "hierarchy_concepts" column 8 has type character varying(50)[] in non-recursive term but type character varying[] overall
+			FROM concepts
+
+			UNION ALL
+
+			SELECT c.ancestor_concept_code,
+				c.ancestor_vocabulary_id,
+				c.descendant_concept_code,
+				c.descendant_vocabulary_id,
+				c.descendant_domain_id,
+				c.descendant_concept_class_id,
+				root_ancestor_concept_code,
+				root_ancestor_vocabulary_id,
+				hc.full_path || ROW(c.descendant_concept_code, c.descendant_vocabulary_id) AS full_path,
+				--hc.hierarchy_path || ARRAY [c.descendant_concept_code||' ('||c.descendant_vocabulary_id||')']
+				hc.hierarchy_path || ARRAY [c.descendant_concept_code]::TEXT[]
+			FROM concepts c
+			JOIN hierarchy_concepts hc ON hc.descendant_concept_code = c.ancestor_concept_code
+				AND hc.descendant_vocabulary_id = c.ancestor_vocabulary_id
+			WHERE ROW(c.descendant_concept_code, c.descendant_vocabulary_id) <> ALL (full_path)
+			),
+		concepts
+		AS (
+			SELECT DISTINCT CASE WHEN crs.relationship_id = 'Is a' THEN crs.concept_code_1 ELSE crs.concept_code_2 END AS ancestor_concept_code,
+				CASE WHEN crs.relationship_id = 'Is a' THEN crs.vocabulary_id_1 ELSE crs.vocabulary_id_2 END AS ancestor_vocabulary_id,
+				CASE WHEN crs.relationship_id = 'Is a' THEN crs.concept_code_2 ELSE crs.concept_code_1 END AS descendant_concept_code,
+				CASE WHEN crs.relationship_id = 'Is a' THEN crs.vocabulary_id_2 ELSE crs.vocabulary_id_1 END AS descendant_vocabulary_id,
+				CASE WHEN crs.relationship_id = 'Is a' THEN c_info_2.domain_id ELSE c_info_1.domain_id END AS descendant_domain_id,
+				CASE WHEN crs.relationship_id = 'Is a' THEN c_info_2.concept_class_id ELSE c_info_1.concept_class_id END AS descendant_concept_class_id
+			FROM concept_relationship_stage crs
+			CROSS JOIN vocabulary_pack.GetActualConceptInfo(crs.concept_code_1, crs.vocabulary_id_1) c_info_1
+			CROSS JOIN vocabulary_pack.GetActualConceptInfo(crs.concept_code_2, crs.vocabulary_id_2) c_info_2
+			WHERE c_info_1.invalid_reason IS NULL
+				AND c_info_1.standard_concept = 'S'
+				AND c_info_2.invalid_reason IS NULL
+				AND c_info_2.standard_concept = 'S'
+				AND crs.relationship_id IN (
+					'Is a',
+					'Subsumes'
+					)
+				AND crs.invalid_reason IS NULL
+			)
+		SELECT hc.root_ancestor_concept_code AS ancestor_concept_code,
+			hc.root_ancestor_vocabulary_id AS ancestor_vocabulary_id,
+			hc.descendant_concept_code,
+			hc.descendant_vocabulary_id,
+			hc.descendant_domain_id,
+			hc.descendant_concept_class_id,
+			hc.hierarchy_path,
+			DOMAINS_ARRAY as approved_domains_array,
+			CONCEPT_CLASS_ARRAY as approved_concept_class_array
+		FROM hierarchy_concepts hc
+		--filter by OMOP Ext as ancestor_concept_code
+		JOIN concept_stage cs ON cs.concept_code = hc.root_ancestor_concept_code
+			AND cs.vocabulary_id = hc.root_ancestor_vocabulary_id;
+
+	--update domain if not defined
+	UPDATE concept_stage cs
+	SET domain_id = i.domain_id
+	FROM (
+		SELECT DISTINCT ON (
+				cs_int.concept_code,
+				cs_int.vocabulary_id
+				) cs_int.concept_code,
+			cs_int.vocabulary_id,
+			CASE WHEN an.approved_domains_array @> ARRAY_AGG(an.descendant_domain_id) OVER (
+						--collect all domains we have
+						PARTITION BY cs_int.concept_code,
+						cs_int.vocabulary_id
+						) THEN an.descendant_domain_id ELSE '-1' END AS domain_id
+		FROM omop_ext_ancestor an
+		JOIN concept_stage cs_int ON cs_int.concept_code = an.ancestor_concept_code
+			AND cs_int.vocabulary_id = an.ancestor_vocabulary_id
+			AND cs_int.domain_id IS NULL
+		LEFT JOIN UNNEST(an.approved_domains_array) WITH ORDINALITY AS domains(domain_id, domain_position) ON domains.domain_id = an.descendant_domain_id
+		WHERE an.descendant_domain_id IS NOT NULL
+		ORDER BY cs_int.concept_code,
+			cs_int.vocabulary_id,
+			domains.domain_position
+		) i
+	WHERE cs.concept_code = i.concept_code
+		AND cs.vocabulary_id = i.vocabulary_id;
+
+	--update concept_class if not defined
+	UPDATE concept_stage cs
+	SET concept_class_id = i.concept_class_id
+	FROM (
+		SELECT DISTINCT ON (
+				cs_int.concept_code,
+				cs_int.vocabulary_id
+				) cs_int.concept_code,
+			cs_int.vocabulary_id,
+			CASE WHEN an.approved_concept_class_array @> ARRAY_AGG(an.descendant_concept_class_id) OVER (
+						--collect all classes we have
+						PARTITION BY cs_int.concept_code,
+						cs_int.vocabulary_id
+						) THEN an.descendant_concept_class_id ELSE '-1' END AS concept_class_id
+		FROM omop_ext_ancestor an
+		JOIN concept_stage cs_int ON cs_int.concept_code = an.ancestor_concept_code
+			AND cs_int.vocabulary_id = an.ancestor_vocabulary_id
+			AND cs_int.concept_class_id IS NULL
+		LEFT JOIN UNNEST(an.approved_concept_class_array) WITH ORDINALITY AS classes(concept_class_id, class_position) ON classes.concept_class_id = an.descendant_concept_class_id
+		WHERE an.descendant_concept_class_id IS NOT NULL
+		ORDER BY cs_int.concept_code,
+			cs_int.vocabulary_id,
+			classes.class_position
+		) i
+	WHERE cs.concept_code = i.concept_code
+		AND cs.vocabulary_id = i.vocabulary_id;
+END $$;
+
+--now check for domain/class assignment error
+DO $$
+DECLARE
+	z int4;
+BEGIN
+	SELECT COUNT(*)
+	INTO z
+	FROM concept_stage
+	WHERE '-1' IN (
+			domain_id,
+			concept_class_id
+			);
+
+	IF z > 0 THEN
+		RAISE EXCEPTION '% assignment error(s) found', z;
+	END IF;
+END $$;
+
+--get specific concept_codes
+SELECT an.ancestor_concept_code,
+	an.ancestor_vocabulary_id,
+	an.descendant_concept_code,
+	an.descendant_vocabulary_id,
+	CASE WHEN NOT an.approved_domains_array @> ARRAY [an.descendant_domain_id]
+			AND cs.domain_id = '-1' THEN an.descendant_domain_id END AS domain_not_in_list,
+	CASE WHEN NOT an.approved_concept_class_array @> ARRAY [an.descendant_concept_class_id]
+			AND cs.concept_class_id = '-1' THEN an.descendant_concept_class_id END AS class_not_in_list,
+	array_to_string(an.hierarchy_path, ' -> ') AS hierarchy_path
+FROM omop_ext_ancestor an
+JOIN concept_stage cs ON cs.concept_code = an.ancestor_concept_code
+	AND cs.vocabulary_id = an.ancestor_vocabulary_id
+	AND '-1' IN (
+		cs.domain_id,
+		cs.concept_class_id
+		)
+ORDER BY 1, 2, 3, 4;
+
+--clean up
+DROP TABLE omop_ext_ancestor
 
 --11. Assign Concept_class for concepts with unassigned Concept_class using the Concept_class of an ancestor
 --TODO: AVOF-3548
---
+++ OMOP Extension/manual_work/omop_ext_manual.sql
@@ -0,0 +1,45 @@
+--backup for concept_manual
+DO
+$body$
+    DECLARE
+        update text;
+    BEGIN
+        SELECT TO_CHAR(CURRENT_DATE, 'YYYY_MM_DD')
+        INTO update;
+        EXECUTE format('create table %I as select * from concept_manual',
+                       'concept_manual_backup_' || update);
+
+    END
+$body$;
+
+--backup for concept_relationship_manual
+DO
+$body$
+    DECLARE
+        update text;
+    BEGIN
+        SELECT TO_CHAR(CURRENT_DATE, 'YYYY_MM_DD')
+        INTO update;
+        EXECUTE format('create table %I as select * from concept_relationship_manual',
+                       'concept_relationship_manual_backup_' || update);
+
+    END
+$body$;
+
+--backup for concept_synonym_manual
+DO
+$body$
+    DECLARE
+        update text;
+    BEGIN
+        SELECT TO_CHAR(CURRENT_DATE, 'YYYY_MM_DD')
+        INTO update;
+        EXECUTE format('create table %I as select * from concept_synonym_manual',
+                       'concept_synonym_manual_backup_' || update);
+
+    END
+$body$;
+
+/*select * from concept_manual_backup_2022_09_12;
+select * from concept_relationship_manual_backup_2022_09_12;
+select * from concept_synonym_manual_backup_2022_09_12;*/

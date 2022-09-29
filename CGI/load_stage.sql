ab5aeb89b032d72d68beccf414971376e1ae98bf
Vlad.korsik@odysseusinc.com <Vlad.korsik@odysseusinc.com>
2022-09-29 18:11:21 +0300

    CGI upd

--- CGI/load_stage.sql
+++ CGI/load_stage.sql
@@ -35,11 +35,10 @@ truncate concept_synonym_stage;
 
 drop table cgi_source;
 create table cgi_source as (
-select distinct concat(gene, ':', regexp_replace(protein, 'p.', '')) as concept_name, 'CGI' as vocabulary_id,  concat(gene, ':', regexp_replace(protein, 'p.', '')) as concept_code,gdna as hgvs
+select distinct gdna as concept_name, 'CGI' as vocabulary_id,  regexp_split_to_table(gdna,'__') as concept_code,regexp_split_to_table(gdna,'__') as hgvs,gene,protein
 from dev_cgi.genomic_cgi_source
 where gdna != ''
 and protein != '.');
-
 
 --insert into concept_stage
 insert into concept_stage
@@ -50,12 +49,16 @@ with s as (SELECT DISTINCT
        n.vocabulary_id AS vocabulary_id,
        'Variant' AS concept_class_id,
        NULL AS standard_concept,
-       n.concept_code AS concept_code,
+  trim(substr(n.concept_code,1,50)) AS concept_code,
       COALESCE(c.valid_start_date, v.latest_update)   as valid_start_date, -- defines valid_start_date  based on previous vocabulary run
-       COALESCE(c.valid_end_date,TO_DATE('20991231', 'yyyymmdd'))  as valid_end_date -- defines valid_end_date  based on previous vocabulary run
+       COALESCE(c.valid_end_date,TO_DATE('20991231', 'yyyymmdd'))  as valid_end_date, -- defines valid_end_date  based on previous vocabulary run
+gene,
+protein
 FROM cgi_source n
 JOIN vocabulary v ON v.vocabulary_id = 'CGI'
-LEFT JOIN concept c ON c.concept_code = n.concept_code -- already existing LOINC concepts
+LEFT JOIN concept c ON c.concept_code =
+                      --  n.concept_code
+                       concat(gene, ':', regexp_replace(protein, 'p.', '')) -- Should be dropped after Fall2022 release and replaced with n.concept_code
     	AND c.vocabulary_id = 'CGI')
 
 SELECT distinct
@@ -70,9 +73,10 @@ SELECT distinct
        s.valid_end_date ,
       CASE WHEN s.concept_code is null then 'D' else null end as invalid_reason
 from s
-FULL OUTER JOIN concept c ON c.concept_code = s.concept_code -- already existing LOINC concepts
-    	and c.vocabulary_id = 'CGI'
+FULL OUTER JOIN concept c ON c.concept_code =    concat(gene, ':', regexp_replace(protein, 'p.', '')) -- already existing CGI concepts
+	and c.vocabulary_id = 'CGI'
 where s.concept_code is not null
+
 ;
 
 
@@ -81,7 +85,7 @@ with tab as (
     SELECT *,
            regexp_replace((regexp_match(reference, 'NM_.\d.+\(p\..+\)\s|NM_.\d.+\(p\..+\)(?<!__PMID)|NM_.\d.+\(p\..+\)$'))[1],
                           'AND .+__Clinvar:|__PMID.+__Clinvar:|__PMID', ' @ ', 'gi') as hgvs_array,
-           concat(gene, ':', regexp_replace(protein, 'p.', ''))               as concept_code
+           trim(substr(regexp_split_to_table(gdna,'__'),1,50))         as concept_code
     FROM dev_cgi.genomic_cgi_source
     where reference ilike '%Clinvar%'
       and protein != '.'
@@ -116,8 +120,8 @@ SELECT
     'gdna' as flag,
     'CGI' AS synonym_vocabulary_id,
     4180186 as language_concept_id,
-       trim(gdna) as synonym_name,
-       concat(gene, ':', regexp_replace(protein, 'p.', '')) as synonym_concept_code
+            trim(regexp_split_to_table(gdna,'__'))  as synonym_name,
+             trim(substr(regexp_split_to_table(gdna,'__'),1,50))           as synonym_concept_code
 
 FROM  dev_cgi.genomic_cgi_source
       where protein != '.'
@@ -129,7 +133,7 @@ SELECT
     'CGI' AS synonym_vocabulary_id,
     4180186 as language_concept_id,
        trim(concat(gene,':',protein)) as synonym_name,
-       concat(gene, ':', regexp_replace(protein, 'p.', '')) as synonym_concept_code
+             trim(substr(regexp_split_to_table(gdna,'__'),1,50))           as synonym_concept_code
 
 FROM  dev_cgi.genomic_cgi_source
           where protein != '.'
@@ -144,5 +148,8 @@ synonyms as (
 )
 INSERT INTO concept_synonym_stage(synonym_vocabulary_id, language_concept_id, synonym_name, synonym_concept_code)
 SELECT distinct synonym_vocabulary_id, language_concept_id, synonym_name, synonym_concept_code
-FROM synonyms
+FROM synonyms s
+JOIN concept_stage cs
+ON cs.concept_code=s.synonym_concept_code
+and s.synonym_name<>cs.concept_name
 ;

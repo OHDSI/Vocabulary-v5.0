058042a1a77fd48bcc8c774810c1778a8682fbd1
Varvara <varvara.savitskaya@odysseusinc.com>
2022-10-03 15:00:02 +0300

    cosmic work

--- COSMIC/create_source_tables.sql
+++ COSMIC/create_source_tables.sql
@@ -1,5 +1,5 @@
-drop table dev_genomic.cosmicmutantexportcensus;
-create table dev_genomic.cosmicmutantexportcensus (
+drop table cosmicmutantexportcensus;
+create table cosmicmutantexportcensus (
     gene_name varchar(100),
     accession_number varchar(200),
     gene_cds_length int,
@@ -41,7 +41,8 @@ create table dev_genomic.cosmicmutantexportcensus (
 );
 
 select *
-from cosmicmutantexportcensus;
+from cosmicmutantexportcensus
+where legacy_mutation_id = 'COSM7170946';
 
 
 select *
@@ -84,3 +85,196 @@ where length(genomic_mutation_id) = 0
 and mutation_aa != 'p.?'
 group by mutation_aa
 having count(mutation_aa) > 1;
+
+
+
+select distinct gene_name, accession_number, gene_cds_length, hgnc_id, sample_name, id_sample, id_tumour, primary_site, site_subtype_1, site_subtype_2, site_subtype_3, primary_histology, histology_subtype_1, histology_subtype_2, histology_subtype_3, genome_wide_screen, legacy_mutation_id, mutation_id, mutation_cds, mutation_aa, mutation_description, mutation_zygosity, loh, grch, mutation_genome_position, mutation_strand, resistance_mutation, mutation_somatic_status, pubmed_pmid, id_study, sample_type, tumour_origin, age, tier, hgvsp, hgvsc, hgvsg from cosmicmutantexportcensus;
+
+select distinct * from cosmicmutantexportcensus
+where legacy_mutation_id in (
+    select legacy_mutation_id from (
+    select distinct gene_name, accession_number, gene_cds_length, hgnc_id, sample_name, id_sample, id_tumour, primary_site, site_subtype_1, site_subtype_2, site_subtype_3, primary_histology, histology_subtype_1, histology_subtype_2, histology_subtype_3, genome_wide_screen, legacy_mutation_id, mutation_id, mutation_cds, mutation_aa, mutation_description, mutation_zygosity, loh, grch, mutation_genome_position, mutation_strand, resistance_mutation, mutation_somatic_status, pubmed_pmid, id_study, sample_type, tumour_origin, age, tier, hgvsp, hgvsc, hgvsg from cosmicmutantexportcensus
+    )a);
+
+
+select distinct legacy_mutation_id, resistance_mutation, tier, mutation_description from cosmicmutantexportcensus
+where legacy_mutation_id in (
+    select legacy_mutation_id from (
+    select distinct gene_name, accession_number, gene_cds_length, hgnc_id, sample_name, id_sample, id_tumour, primary_site, site_subtype_1, site_subtype_2, site_subtype_3, primary_histology, histology_subtype_1, histology_subtype_2, histology_subtype_3, genome_wide_screen, legacy_mutation_id, mutation_id, mutation_cds, mutation_aa, mutation_description, mutation_zygosity, loh, grch, mutation_genome_position, mutation_strand, resistance_mutation, mutation_somatic_status, pubmed_pmid, id_study, sample_type, tumour_origin, age, tier, hgvsp, hgvsc, hgvsg from cosmicmutantexportcensus
+    )a);
+
+select distinct legacy_mutation_id, resistance_mutation, tier, mutation_description from cosmicmutantexportcensus
+group by 1,2,3,4
+having count(legacy_mutation_id)>1;
+
+
+select distinct gene_name, legacy_mutation_id, resistance_mutation, tier, mutation_description, mutation_cds, mutation_aa,
+                hgvsp, hgvsc, hgvsg from cosmicmutantexportcensus
+where length(genomic_mutation_id) != 0
+                                                                                                                                                                                                                                                               group by 1,2,3,4,5,6,7,8,9,10
+having count(genomic_mutation_id)>1;
+
+
+
+select gene_name, accession_number, gene_cds_length, hgnc_id, legacy_mutation_id, mutation_id, mutation_cds, mutation_aa, mutation_description, loh, grch, mutation_genome_position, mutation_strand, resistance_mutation, tier, hgvsp, hgvsc, hgvsg from cosmicmutantexportcensus
+where legacy_mutation_id in (
+    select legacy_mutation_id from (
+    select distinct gene_name, accession_number, gene_cds_length, hgnc_id, legacy_mutation_id, mutation_id, mutation_cds, mutation_aa, mutation_description, loh, grch, mutation_genome_position, mutation_strand, resistance_mutation, tier, hgvsp, hgvsc, hgvsg from cosmicmutantexportcensus
+group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14, 15,16,17,18
+having count(legacy_mutation_id)>1
+    )a);
+
+
+with tab as (select distinct gene_name,
+                             accession_number,
+                             gene_cds_length,
+                             hgnc_id,
+                             legacy_mutation_id,
+                             mutation_id,
+                             mutation_cds,
+                             mutation_aa,
+                             mutation_description,
+                             loh,
+                             grch,
+                             mutation_genome_position,
+                             mutation_strand,
+                             resistance_mutation,
+                             tier,
+                             hgvsp,
+                             hgvsc,
+                             hgvsg
+             from cosmicmutantexportcensus)
+select legacy_mutation_id from tab
+group by 1
+having count(legacy_mutation_id)>1;
+
+
+select count(distinct sample_name) from cosmicmutantexportcensus;
+
+
+with tab as (select distinct gene_name,
+                             accession_number,
+                             gene_cds_length,
+                             hgnc_id,
+                             genomic_mutation_id,
+                             mutation_id,
+                             mutation_cds,
+                             mutation_aa,
+                             mutation_description,
+                             loh,
+                             grch,
+                             mutation_genome_position,
+                             mutation_strand,
+                             resistance_mutation,
+                             tier,
+                             hgvsp,
+                             hgvsc,
+                             hgvsg
+             from cosmicmutantexportcensus)
+select genomic_mutation_id from tab
+                           where length(genomic_mutation_id)!=0
+group by 1
+having count(genomic_mutation_id)>1
+;
+
+
+select gene_name, accession_number, gene_cds_length, hgnc_id, legacy_mutation_id, mutation_id, mutation_cds, mutation_aa, mutation_description, loh, grch, mutation_genome_position, mutation_strand, resistance_mutation, tier, hgvsp, hgvsc, hgvsg from cosmicmutantexportcensus
+where legacy_mutation_id in (
+    select legacy_mutation_id from (
+    select distinct gene_name, accession_number, gene_cds_length, hgnc_id, legacy_mutation_id, mutation_id, mutation_cds, mutation_aa, mutation_description, loh, grch, mutation_genome_position, mutation_strand, resistance_mutation, tier, hgvsp, hgvsc, hgvsg from cosmicmutantexportcensus
+group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14, 15,16,17,18
+having count(legacy_mutation_id)>1
+    )a)
+and legacy_mutation_id in (select legacy_mutation_id from (
+    with tab as (select distinct gene_name,
+                             accession_number,
+                             gene_cds_length,
+                             hgnc_id,
+                             legacy_mutation_id,
+                             mutation_id,
+                             mutation_cds,
+                             mutation_aa,
+                             mutation_description,
+                             loh,
+                             grch,
+                             mutation_genome_position,
+                             mutation_strand,
+                             resistance_mutation,
+                             tier,
+                             hgvsp,
+                             hgvsc,
+                             hgvsg
+             from cosmicmutantexportcensus)
+select tier from tab
+group by 1
+having count(legacy_mutation_id)>1
+    )b);
+
+with tab1 as
+(with tab as (select distinct gene_name,
+                             accession_number,
+                             gene_cds_length,
+                             hgnc_id,
+                             genomic_mutation_id,
+                             mutation_id,
+                             mutation_cds,
+                             mutation_aa,
+                             mutation_description,
+                             loh,
+                             grch,
+                             mutation_genome_position,
+                             mutation_strand,
+                             resistance_mutation,
+                             tier,
+                             hgvsp,
+                             hgvsc,
+                             hgvsg
+             from cosmicmutantexportcensus
+             where length(genomic_mutation_id)!=0
+             )
+select genomic_mutation_id from tab
+group by 1
+having count(genomic_mutation_id)>1)
+select gene_name, genomic_mutation_id from tab1
+
+left join cosmicmutantexportcensus c
+using (genomic_mutation_id)
+where length(genomic_mutation_id)!=0;
+
+--656564 + 148
+select distinct gene_name, genomic_mutation_id, resistance_mutation, tier, mutation_description, mutation_cds, mutation_aa,
+                hgvsp, hgvsc, hgvsg from cosmicmutantexportcensus
+where genomic_mutation_id not in (
+    select genomic_mutation_id from (
+        with tab as (select distinct gene_name,
+                             accession_number,
+                             gene_cds_length,
+                             hgnc_id,
+                             genomic_mutation_id,
+                             mutation_id,
+                             mutation_cds,
+                             mutation_aa,
+                             mutation_description,
+                             loh,
+                             grch,
+                             mutation_genome_position,
+                             mutation_strand,
+                             resistance_mutation,
+                             tier,
+                             hgvsp,
+                             hgvsc,
+                             hgvsg
+             from cosmicmutantexportcensus
+             where length(genomic_mutation_id)!=0)
+select genomic_mutation_id from tab
+group by 1
+having count(genomic_mutation_id)>1
+                                   )c
+    )
+and length(genomic_mutation_id)!=0;
+
+select distinct gene_name, genomic_mutation_id, resistance_mutation, tier, mutation_description, mutation_cds, mutation_aa,
+                hgvsp, hgvsc, hgvsg from cosmicmutantexportcensus;
+
+
+--656860

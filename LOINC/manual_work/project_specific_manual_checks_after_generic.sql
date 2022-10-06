3232fc7ed5c5ee950da7310b212cb107411d356a
MariaRohozhkina <mariaspirit228@gmail.com>
2022-10-06 15:42:11 +0300

    refactor

--- LOINC/manual_work/project_specific_manual_checks_after_generic.sql
+++ LOINC/manual_work/project_specific_manual_checks_after_generic.sql
@@ -24,17 +24,18 @@ join devv5.concept b on b.concept_id = concept_id_2 and b.vocabulary_id = 'SNOME
 where a.vocabulary_id = 'LOINC' and a.invalid_reason is null
 group by a.concept_code, a.concept_name
 )
-select a.concept_code as source_code,
-       a.concept_name as source_name,
+select a.concept_code     as source_code,
+       a.concept_name     as source_name,
        a.relationship_agg as old_relat_agg,
-       a.code_agg as old_code_agg,
-       a.name_agg as old_name_agg,
+       a.code_agg         as old_code_agg,
+       a.name_agg         as old_name_agg,
        b.relationship_agg as new_relat_agg,
-       b.code_agg as new_code_agg,
-       b.name_agg as new_name_agg
-from old_map  a
-join new_map b
-on a.concept_code = b.concept_code and (a.code_agg != b.code_agg or a.relationship_agg != b.relationship_agg)
+       b.code_agg         as new_code_agg,
+       b.name_agg         as new_name_agg
+from old_map a
+         join new_map b
+              on a.concept_code = b.concept_code and
+                 (a.code_agg != b.code_agg or a.relationship_agg != b.relationship_agg)
 order by a.concept_code
 ;
 

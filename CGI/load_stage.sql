8daa79da900b098de7d05f6541924545b0fefb3b
Timur <timur.vakhitov@firstlinesoftware.com>
2022-10-13 18:13:07 +0300

    refactoring

--- CGI/load_stage.sql
+++ CGI/load_stage.sql
@@ -14,19 +14,19 @@
 * limitations under the License.
 *
 * Authors: Varvara Savitskaya, Vlad Korsik, Alexander Davydov
-* Date: Fall-2022
+* Date: 2022
 **************************************************************************/
 
 --1. Update latest_update field to new date
 DO $_$
-    BEGIN
-        PERFORM VOCABULARY_PACK.SetLatestUpdate(
-                pVocabularyName => 'CGI',
-                pVocabularyDate => TO_DATE('20180216', 'yyyymmdd'),
-                pVocabularyVersion => 'CGI' || '20180216', -- should be hardcoded when API will not provide reproducible way for Updates
-                pVocabularyDevSchema => 'dev_cgi'
-            );
-    END $_$;
+BEGIN
+	PERFORM VOCABULARY_PACK.SetLatestUpdate(
+	pVocabularyName			=> 'CGI',
+	pVocabularyDate			=> TO_DATE('20180216', 'yyyymmdd'),
+	pVocabularyVersion		=> 'CGI' || '20180216', -- should be hardcoded when API will not provide reproducible way for Updates
+	pVocabularyDevSchema	=> 'dev_cgi'
+);
+END $_$;
 
 --2. Truncate all working tables
 TRUNCATE TABLE concept_stage;
@@ -36,109 +36,135 @@ TRUNCATE TABLE pack_content_stage;
 TRUNCATE TABLE drug_strength_stage;
 
 --3. Create temporary table
-DROP TABLE if exists cgi_source;
-CREATE TABLE cgi_source as (
-   SELECT DISTINCT regexp_split_to_table(gdna, '__') as concept_name,
-                    'CGI'                             as vocabulary_id,
-                    regexp_split_to_table(gdna, '__') as concept_code,
-                    regexp_split_to_table(gdna, '__') as hgvs,
-                    gene,
-                    protein,
-                    gdna
-    from dev_cgi.genomic_cgi_source
-    WHERE gdna != ''
-      AND protein != '.'
-    );
+DROP TABLE IF EXISTS cgi_source;
+CREATE UNLOGGED TABLE cgi_source
+SELECT DISTINCT REGEXP_SPLIT_TO_TABLE(gdna, '__') AS concept_name,
+	'CGI' AS vocabulary_id,
+	REGEXP_SPLIT_TO_TABLE(gdna, '__') AS concept_code,
+	REGEXP_SPLIT_TO_TABLE(gdna, '__') AS hgvs,
+	gene,
+	protein,
+	gdna
+FROM dev_cgi.genomic_cgi_source
+WHERE gdna <> ''
+	AND protein <> '.';
 
--- 4 Fill the concept_stage
-INSERT INTO concept_stage (concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date)
-select                  vocabulary_pack.CutConceptName(n.concept_name)                     AS concept_name,
-                           'Measurement'                                               AS domain_id,
-                           n.vocabulary_id                                             AS vocabulary_id,
-                           'Variant'                                                   AS concept_class_id,
-                           NULL                                                        AS standard_concept,
-                           trim(substr(n.concept_code, 1, 50))                         AS concept_code,
-                           v.latest_update           as valid_start_date,
-                           TO_DATE('20991231', 'yyyymmdd') as valid_end_date
-           FROM cgi_source n
-                    JOIN vocabulary v ON v.vocabulary_id = 'CGI'
-;
+--4. Fill the concept_stage
+INSERT INTO concept_stage (
+	concept_name,
+	domain_id,
+	vocabulary_id,
+	concept_class_id,
+	standard_concept,
+	concept_code,
+	valid_start_date,
+	valid_end_date
+	)
+SELECT vocabulary_pack.CutConceptName(n.concept_name) AS concept_name,
+	'Measurement' AS domain_id,
+	n.vocabulary_id AS vocabulary_id,
+	'Variant' AS concept_class_id,
+	NULL AS standard_concept,
+	TRIM(SUBSTR(n.concept_code, 1, 50)) AS concept_code,
+	v.latest_update AS valid_start_date,
+	TO_DATE('20991231', 'yyyymmdd') AS valid_end_date
+FROM cgi_source n
+JOIN vocabulary v ON v.vocabulary_id = 'CGI';
 
-
--- 5 Fill the concept_synonym_stage
-with tab as (
-    select *,
-           regexp_replace(
-                   (regexp_match(reference, 'NM_.\d.+\(p\..+\)\s|NM_.\d.+\(p\..+\)(?<!__PMID)|NM_.\d.+\(p\..+\)$'))[1],
-                   'AND .+__Clinvar:|__PMID.+__Clinvar:|__PMID', ' @ ', 'gi') as hgvs_array,
-           trim(substr(regexp_split_to_table(gdna, '__'), 1, 50))             as concept_code
-    FROM dev_cgi.genomic_cgi_source
-    WHERE reference ilike '%Clinvar%'
-      AND protein != '.'
-)
-        ,
-     ref_hgvs as (
-         select gene,
-                gdna,
-                protein,
-                transcript,
-                info,
-                context,
-                cancer_acronym,
-                source,
-                reference,
-                coalesce(hgvs_array,
-                         (regexp_match(split_part(reference, ' AND ', 1), 'NM_.\d.+\s|NM_.\d.+$'))[1]) as hgvs,
-                concept_code
-         FROM tab)
-        ,
-     hgvs_synonyms as (
-         select 'ref'                                    as flag,
-                'CGI'                                    AS synonym_vocabulary_id,
-                4180186                                  as language_concept_id,
-                trim(regexp_split_to_table(hgvs, ' @ ')) as synonym_name,
-                concept_code                             as synonym_concept_code
-         FROM ref_hgvs
-         UNION ALL
-
-         select 'gdna'                                  as flag,
-                'CGI'                                   AS synonym_vocabulary_id,
-                4180186                                 as language_concept_id,
-                trim(regexp_split_to_table(gdna, '__')) as synonym_name,
-                concept_code                            as synonym_concept_code
-
-         FROM cgi_source
-         WHERE protein != '.'
-
-         UNION ALL
-
-         select 'protein'                        as flag,
-                'CGI'                            AS synonym_vocabulary_id,
-                4180186                          as language_concept_id,
-                trim(concat(gene, ':', protein)) as synonym_name,
-                concept_code                     as synonym_concept_code
-
-         FROM cgi_source
-         WHERE protein != '.'
-     )
-        ,
-     synonyms as (
-        SELECT DISTINCT flag, synonym_vocabulary_id, language_concept_id, synonym_name, synonym_concept_code
-         FROM hgvs_synonyms
-         WHERE flag IN ('protein', 'gdna')
-            or (flag = 'ref' AND synonym_name ilike 'NM%')
-     )
-
-INSERT INTO concept_synonym_stage(synonym_concept_id, synonym_vocabulary_id, language_concept_id, synonym_name,
-                           synonym_concept_code)
-
-select  cs.concept_id, synonym_vocabulary_id, language_concept_id, synonym_name, synonym_concept_code
+--5. Fill the concept_synonym_stage
+WITH tab
+AS (
+	SELECT *,
+		REGEXP_REPLACE((REGEXP_MATCH(reference, 'NM_.\d.+\(p\..+\)\s|NM_.\d.+\(p\..+\)(?<!__PMID)|NM_.\d.+\(p\..+\)$')) [1], 'AND .+__Clinvar:|__PMID.+__Clinvar:|__PMID', ' @ ', 'gi') AS hgvs_array,
+		TRIM(SUBSTR(REGEXP_SPLIT_TO_TABLE(gdna, '__'), 1, 50)) AS concept_code
+	FROM dev_cgi.genomic_cgi_source
+	WHERE reference ILIKE '%Clinvar%'
+		AND protein <> '.'
+	),
+ref_hgvs
+AS (
+	SELECT gene,
+		gdna,
+		protein,
+		transcript,
+		info,
+		context,
+		cancer_acronym,
+		source,
+		reference,
+		COALESCE(hgvs_array, (REGEXP_MATCH(SPLIT_PART(reference, ' AND ', 1), 'NM_.\d.+\s|NM_.\d.+$')) [1]) AS hgvs,
+		concept_code
+	FROM tab
+	),
+hgvs_synonyms
+AS (
+	SELECT 'ref' AS flag,
+		'CGI' AS synonym_vocabulary_id,
+		4180186 AS language_concept_id,
+		TRIM(REGEXP_SPLIT_TO_TABLE(hgvs, ' @ ')) AS synonym_name,
+		concept_code AS synonym_concept_code
+	FROM ref_hgvs
+	
+	UNION ALL
+	
+	SELECT 'gdna' AS flag,
+		'CGI' AS synonym_vocabulary_id,
+		4180186 AS language_concept_id,
+		TRIM(REGEXP_SPLIT_TO_TABLE(gdna, '__')) AS synonym_name,
+		concept_code AS synonym_concept_code
+	FROM cgi_source
+	WHERE protein <> '.'
+	
+	UNION ALL
+	
+	SELECT 'protein' AS flag,
+		'CGI' AS synonym_vocabulary_id,
+		4180186 AS language_concept_id,
+		TRIM(CONCAT (
+				gene,
+				':',
+				protein
+				)) AS synonym_name,
+		concept_code AS synonym_concept_code
+	FROM cgi_source
+	WHERE protein <> '.'
+	),
+synonyms
+AS (
+	SELECT DISTINCT flag,
+		synonym_vocabulary_id,
+		language_concept_id,
+		synonym_name,
+		synonym_concept_code
+	FROM hgvs_synonyms
+	WHERE flag IN (
+			'protein',
+			'gdna'
+			)
+		OR (
+			flag = 'ref'
+			AND synonym_name ILIKE 'NM%'
+			)
+	)
+INSERT INTO concept_synonym_stage (
+	synonym_concept_id,
+	synonym_vocabulary_id,
+	language_concept_id,
+	synonym_name,
+	synonym_concept_code
+	)
+SELECT cs.concept_id,
+	synonym_vocabulary_id,
+	language_concept_id,
+	synonym_name,
+	synonym_concept_code
 FROM synonyms s
-         JOIN concept_stage cs
-              ON cs.concept_code = s.synonym_concept_code
-                  AND s.synonym_name <> cs.concept_name
-                    AND s.synonym_name NOT IN (select concept_code from concept_stage)
-;
+JOIN concept_stage cs ON cs.concept_code = s.synonym_concept_code
+	AND s.synonym_name <> cs.concept_name
+	AND s.synonym_name NOT IN (
+		SELECT concept_code
+		FROM concept_stage
+		);
 
 --6. Clean up
 DROP TABLE cgi_source;
--- working/manual_changes/2022/2022-10-04.sql
+++ working/manual_changes/2022/2022-10-04.sql
@@ -2,27 +2,41 @@
 --Leftover codes will be deprecated
 UPDATE concept c
 SET concept_code = s.concept_code,
-    valid_start_date= CASE WHEN s.valid_start_date<TO_DATE('20180216', 'yyyymmdd') then s.valid_start_date else TO_DATE('20180216', 'yyyymmdd') end
-
+	valid_start_date = CASE 
+		WHEN s.valid_start_date < TO_DATE('20180216', 'yyyymmdd')
+			THEN s.valid_start_date
+		ELSE TO_DATE('20180216', 'yyyymmdd')
+		END
 FROM (
-with tab as (SELECT b.concept_id,trim(substr(regexp_split_to_table(gdna,'__'),1,50)) as concept_code,valid_start_date
-FROM dev_cgi.genomic_cgi_source a
-JOIN concept b
- ON   concat(a.gene, ':', regexp_replace(a.protein, 'p.', '')) = b.concept_code
-and b.vocabulary_id='CGI')
-    SELECT concept_id, concept_code,valid_start_date
-from tab
-    where concept_id IN (SELECT concept_id from tab group by 1 having count(distinct concept_code)=1)
-                          ) as s
-where c.concept_id=s.concept_id
-and c.vocabulary_id='CGI'
-;
+	WITH tab AS (
+			SELECT b.concept_id,
+				TRIM(SUBSTR(REGEXP_SPLIT_TO_TABLE(a.gdna, '__'), 1, 50)) AS concept_code,
+				b.valid_start_date
+			FROM dev_cgi.genomic_cgi_source a
+			JOIN concept b ON CONCAT (
+					a.gene,
+					':',
+					REGEXP_REPLACE(a.protein, 'p.', '')
+					) = b.concept_code
+				AND b.vocabulary_id = 'CGI'
+			)
+	SELECT concept_id,
+		concept_code,
+		valid_start_date
+	FROM tab
+	WHERE concept_id IN (
+			SELECT concept_id
+			FROM tab
+			GROUP BY 1
+			HAVING COUNT(DISTINCT concept_code) = 1
+			)
+	) AS s
+WHERE c.concept_id = s.concept_id
+	AND c.vocabulary_id = 'CGI';
 
 --Set correct validity for 37 concepts
 UPDATE concept c
-SET
-    valid_end_date= current_date,
-    invalid_reason ='D'
-where  c.vocabulary_id='CGI'
-and concept_code  not like '%:g.%'
-;
+SET valid_end_date = CURRENT_DATE,
+	invalid_reason = 'D'
+WHERE c.vocabulary_id = 'CGI'
+	AND concept_code NOT LIKE '%:g.%';

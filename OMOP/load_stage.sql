25c7f8db2aebef3082656e75308cf6bf5a319403
m-khitrun <maria.khitrun@odysseusinc.com>
2022-10-24 14:33:21 +0300

    add_new_concept.sql created load_stage.sql updated

--- OMOP/load_stage.sql
+++ OMOP/load_stage.sql
@@ -17,7 +17,7 @@
 * Date: 2021
 **************************************************************************/
 
-/*DO $_$
+DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.SetLatestUpdate(
 	pVocabularyName			=>'Type Concept',
@@ -37,7 +37,7 @@ BEGIN
 );
 END $_$;*/
 
-DO $_$
+/*DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.SetLatestUpdate(
 	pVocabularyName			=>'UCUM',
@@ -45,7 +45,7 @@ BEGIN
 	pVocabularyVersion		=> 'Version 1.8.2',
 	pVocabularyDevSchema	=> 'DEV_OMOP'
 );
-END $_$;
+END $_$;*/
 
 -- 2. Truncate all working tables
 TRUNCATE TABLE concept_stage;
@@ -78,4 +78,15 @@ BEGIN
 	PERFORM VOCABULARY_PACK.AddFreshMAPSTO();
 END $_$;
 
+--7. Workaround to drop the concepts that are not affected by the SetLatestUpdate
+DELETE
+FROM concept_stage
+WHERE vocabulary_id <> 'Type Concept'
+;
+
+--8. Workaround to drop the relationships between the vocabularies that are not affected by the SetLatestUpdate
+DELETE
+FROM concept_relationship_stage
+WHERE vocabulary_id_1 NOT IN ('Type Concept')
+;
 --At the end, the three tables concept_stage, concept_relationship_stage and concept_synonym_stage should be ready to be fed into the generic_update.sql script

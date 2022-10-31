a5b9c403f80295ab22e7027702273b97f315df4c
Timur <timur.vakhitov@firstlinesoftware.com>
2022-10-31 21:28:04 +0300

    update for https://github.com/OHDSI/Vocabulary-v5.0/commit/8d364af688f10ea0b525e741ec0eb94376522e50

--- working/packages/vocabulary_pack/CheckManualSynonyms.sql
+++ working/packages/vocabulary_pack/CheckManualSynonyms.sql
@@ -16,7 +16,7 @@ BEGIN
 			LEFT JOIN concept c ON c.concept_code = csm.synonym_concept_code AND c.vocabulary_id = csm.synonym_vocabulary_id
 			LEFT JOIN concept_stage cs ON cs.concept_code = csm.synonym_concept_code AND cs.vocabulary_id = csm.synonym_vocabulary_id
 			LEFT JOIN vocabulary v ON v.vocabulary_id = csm.synonym_vocabulary_id
-			LEFT JOIN concept c_l ON c_l.concept_id = csm.language_concept_id AND c_l.vocabulary_id='SNOMED' AND c_l.domain_id='Observation' AND c_l.concept_class_id='Qualifier Value' AND c_l.concept_name ilike '%language'
+			LEFT JOIN concept c_l ON c_l.concept_id = csm.language_concept_id AND c_l.vocabulary_id IN ('SNOMED','Language') AND c_l.domain_id='Language' AND c_l.concept_class_id='Qualifier Value' AND c_l.concept_name ilike '%language'
 	) AS s0
 	WHERE reason IS NOT NULL LIMIT 1;
 

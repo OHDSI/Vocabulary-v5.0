c354a1947379091b073602c0b855e35c55334e42
m-khitrun <maria.khitrun@odysseusinc.com>
2022-10-31 20:26:35 +0300

    fixes

--- HCPCS/manual work/hcpcs_refresh.sql
+++ HCPCS/manual work/hcpcs_refresh.sql
@@ -15,7 +15,7 @@ $body$;
 --restore concept_relationship_manual table (!!!run it only if something went wrong!!!)
 /*TRUNCATE TABLE dev_hcpcs.concept_relationship_manual;
 INSERT INTO dev_hcpcs.concept_relationship_manual
-SELECT * FROM dev_hcpcs.concept_relationship_manual_backup_YYYY_MM_DD;*/
+SELECT * FROM dev_hcpcs.concept_relationship_manual_backup_2022_10_26;*/
 
 DO
 $body$

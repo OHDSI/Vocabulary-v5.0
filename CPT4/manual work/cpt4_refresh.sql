d336eaca9da545b8d58a7a5d36a8ecf9c78dc13b
m-khitrun <maria.khitrun@odysseusinc.com>
2022-11-16 15:56:25 +0200

    fixes

--- CPT4/manual work/cpt4_refresh.sql
+++ CPT4/manual work/cpt4_refresh.sql
@@ -63,8 +63,40 @@ SELECT * FROM dev_cpt4.concept_manual_backup_YYYY_MM_DD;*/
 --9.2.5 Truncate the 'cpt4_mapped' table. Save the spreadsheet as the 'cpt4_mapped table' and upload it into the working schema.
 --TRUNCATE TABLE dev_cpt4.cpt4_mapped;
 
+--9.2.6. Deprecate all mappings that differ from the new version of resulting mapping file.
+UPDATE dev_cpt4.concept_relationship_manual
+SET invalid_reason = 'D',
+    valid_end_date = current_date
+WHERE (concept_code_1, concept_code_2, relationship_id, vocabulary_id_2) IN
+      (SELECT concept_code_1, concept_code_2, relationship_id, vocabulary_id_2
+       FROM concept_relationship_manual crm_old
 
---9.2.8 Insert new and corrected mappings into the concept_relationship_manual table.
+       WHERE NOT exists(SELECT source_code,
+                               target_concept_code,
+                               'cpt4',
+                               target_vocabulary_id,
+                               CASE
+                                   WHEN to_value ~* 'value' THEN 'Maps to value'
+                                   WHEN to_value ~* 'Is a' THEN 'Is a'
+                                   WHEN to_value ~* 'Subsumes' THEN 'Subsumes'
+                                   ELSE 'Maps to' END
+                        FROM dev_cpt4.cpt4_mapped crm_new
+                        WHERE source_code = crm_old.concept_code_1
+                          AND target_concept_code = crm_old.concept_code_2
+                          AND target_vocabulary_id = crm_old.vocabulary_id_2
+                          AND CASE
+                                  WHEN to_value ~* 'value' THEN 'Maps to value'
+                                  WHEN to_value ~* 'Is a' THEN 'Is a'
+                                  WHEN to_value ~* 'Subsumes' THEN 'Subsumes'
+                               ELSE 'Maps to' END = crm_old.relationship_id
+
+    )
+    AND invalid_reason IS NULL
+    AND crm_old.relationship_id NOT IN ('CPT4 - SNOMED cat', 'CPT4 - SNOMED eq', 'CPT4 - LOINC eq')
+    )
+;
+
+--9.2.7 Insert new and corrected mappings into the concept_relationship_manual table.
 with mapping AS
     (
         SELECT DISTINCT source_code AS concept_code_1,
@@ -94,6 +126,24 @@ INSERT INTO concept_relationship_manual(concept_code_1, concept_code_2, vocabula
      FROM mapping m
         WHERE (concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id)
                   NOT IN (SELECT concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id FROM dev_cpt4.concept_relationship_manual)
+    )
+;
+
+--9.2.8 Activate mapping, that became valid again
+UPDATE concept_relationship_manual crm
+SET invalid_reason = null,
+valid_end_date = to_date('20991231','yyyymmdd'),
+valid_start_date =current_date
+
+--SELECT * FROM concept_relationship_manual crm --use this SELECT for QA
+WHERE invalid_reason = 'D' -- activate only deprecated mappings
+
+AND EXISTS (SELECT 1 -- activate mapping if the same exists in the current manual file
+                FROM cpt4_mapped m
+                WHERE m.source_code = crm.concept_code_1 --the same source_code is mapped
+                    AND m.target_concept_code = crm.concept_code_2 --to the same concept_code
+                    AND m.target_vocabulary_id = crm.vocabulary_id_2 --of the same vocabulary
+                    AND m.to_value = crm.relationship_id --with the same relationship
     )
 ;
 

3610507dbad46d68829b6d8fe898d82c0cafec2f
olegzhuk <oleg.zhuk@odysseusinc.com>
2022-11-21 11:05:16 +0100

    Codestyle changes

--- dmd/postprocessing.sql
+++ dmd/postprocessing.sql
@@ -31,10 +31,12 @@ where
 		) and
 	relationship_id = 'Mapped from'*/
 ; -- old mappings to RxN* are not deprecated automatically
-insert into concept_relationship_stage
-select distinct
-	null :: int4,
-	null :: int4,
+
+
+INSERT INTO concept_relationship_stage
+SELECT DISTINCT
+	NULL :: int4,
+	NULL :: int4,
 	c.concept_code,
 	c2.concept_code,
 	'dm+d',
@@ -43,24 +45,26 @@ select distinct
 	r.valid_start_date,
 	current_date - 1,
 	'D'
-from concept_relationship r
-join concept c on 
-	c.concept_id = r.concept_id_1 and
-	c.vocabulary_id = 'dm+d' and
+FROM concept_relationship r
+JOIN concept c ON
+	c.concept_id = r.concept_id_1 AND
+	c.vocabulary_id = 'dm+d' AND
 	r.relationship_id = 'Maps to'
-join relationship_to_concept t on
+JOIN relationship_to_concept t ON
 	c.concept_code = t.concept_code_1
-join concept cx on
-	cx.concept_id = t.concept_id_2 and
+JOIN concept cx ON
+	cx.concept_id = t.concept_id_2 AND
 	cx.vocabulary_id = 'CVX'
-join concept c2 on
+JOIN concept c2 ON
 	c2.concept_id = r.concept_id_2
-where c2.vocabulary_id like 'RxN%'
+WHERE c2.vocabulary_id LIKE 'RxN%'
 ;--save CVX mappings from relationship_to_concept
-insert into concept_relationship_stage
-select
-	null,
-	null,
+
+
+INSERT INTO concept_relationship_stage
+SELECT
+	NULL,
+	NULL,
 	r.concept_code_1,
 	c.concept_code,
 	'dm+d',
@@ -69,19 +73,21 @@ select
 	current_date,
 	TO_DATE('20991231','yyyymmdd'),
 	null
-from relationship_to_concept r
-join concept c on
-	r.concept_id_2 = c.concept_id and
+FROM relationship_to_concept r
+JOIN concept c ON
+	r.concept_id_2 = c.concept_id AND
 	(
-		c.vocabulary_id = 'CVX' or
+		c.vocabulary_id = 'CVX' OR
 		c.concept_class_id ~ '(Drug|Pack)'
 	)
 ;
+
+
 --Devices can and should be mapped to SNOMED as they are the same concepts
-insert into concept_relationship_stage
-select distinct
-	null :: int4 as concept_id_1,
-	null :: int4 as concept_id_2,
+INSERT INTO concept_relationship_stage
+SELECT DISTINCT
+	NULL :: int4 as concept_id_1,
+	NULL :: int4 as concept_id_2,
 	c.concept_code as concept_code_1,
 	x.concept_code as concept_code_2,
 	'dm+d',
@@ -89,22 +95,24 @@ select distinct
 	'Maps to',
 	current_date as valid_start_date,
 	to_date ('20991231','yyyymmdd') as valid_end_date,
-	null as invalid_reason
-from concept_stage c 
-join concept x on
-	x.concept_code = c.concept_code and
+	NULL as invalid_reason
+FROM concept_stage c
+JOIN concept x ON
+	x.concept_code = c.concept_code AND
 
-	x.invalid_reason is null and
-	x.vocabulary_id = 'SNOMED' and
-	x.standard_concept = 'S' and 
-	x.domain_id = 'Device' and -- some are Observations, we don't want them
+	x.invalid_reason IS NULL AND
+	x.vocabulary_id = 'SNOMED' AND
+	x.standard_concept = 'S' AND
+	x.domain_id = 'Device' AND -- some are Observations, we don't want them
 
-	c.vocabulary_id = 'dm+d' and
+	c.vocabulary_id = 'dm+d' AND
 	c.domain_id = 'Device'
 ;
+
+
 --SNOMED mappings now take precedence
-update concept_relationship_stage r
-set 
+UPDATE concept_relationship_stage r
+SET
 	invalid_reason = 'D',
 	valid_end_date = 
 	(
@@ -113,87 +121,96 @@ set
         WHERE vocabulary_id IN (r.vocabulary_id_1, r.vocabulary_id_2)
               AND latest_update IS NOT NULL
       )
-where
-	vocabulary_id_2 != 'SNOMED' and
-	relationship_id = 'Maps to' and
-	invalid_reason is null and
+WHERE
+	vocabulary_id_2 != 'SNOMED' AND
+	relationship_id = 'Maps to' AND
+	invalid_reason IS NULL AND
 	exists
 		(
-			select 
-			from concept_relationship_stage
-			where
-				concept_code_1 = r.concept_code_1 and
-				vocabulary_id_2 = 'SNOMED' and
+			SELECT
+			FROM concept_relationship_stage
+			WHERE
+				concept_code_1 = r.concept_code_1 AND
+				vocabulary_id_2 = 'SNOMED' AND
 				relationship_id = 'Maps to'
 		)
 ;
-update concept_stage 
-set standard_concept = NULL
-where
-	domain_id = 'Device' and
-	vocabulary_id = 'dm+d' and
+
+
+UPDATE concept_stage
+SET standard_concept = NULL
+WHERE
+	domain_id = 'Device' AND
+	vocabulary_id = 'dm+d' AND
 	exists
 		(
-			select
-			from concept_relationship_stage
-			where
-				concept_code_1 = concept_code and
-				relationship_id = 'Maps to' and
+			SELECT
+			FROM concept_relationship_stage
+			WHERE
+				concept_code_1 = concept_code AND
+				relationship_id = 'Maps to' AND
 				vocabulary_id_2 = 'SNOMED'
 		)
 ;
-analyze concept_relationship_stage;
+
+
+ANALYZE concept_relationship_stage;
+
+
 --delete useless deprecations (non-existent relations)
-delete from concept_relationship_stage i
-where
-	(concept_code_1, vocabulary_id_1, concept_code_2, vocabulary_id_2/*, relationship_id*/) not in
+DELETE FROM concept_relationship_stage i
+WHERE
+	(concept_code_1, vocabulary_id_1, concept_code_2, vocabulary_id_2/*, relationship_id*/) NOT IN
 	(
-		select
+		SELECT
 			c1.concept_code,
 			c1.vocabulary_id,
 			c2.concept_code,
 			c2.vocabulary_id/*,
 			r.relationship_id*/
-		from concept_relationship r
-		join concept c1 on
-			c1.concept_id = r.concept_id_1 and
+		FROM concept_relationship r
+		JOIN concept c1 ON
+			c1.concept_id = r.concept_id_1 AND
 			(c1.concept_code, c1.vocabulary_id) = (i.concept_code_1, i.vocabulary_id_1)
-		join concept c2 on
-			c2.concept_id = r.concept_id_2 and
+		JOIN concept c2 ON
+			c2.concept_id = r.concept_id_2 AND
 			(c2.concept_code, c2.vocabulary_id) = (i.concept_code_2, i.vocabulary_id_2)
-	) and
-	invalid_reason is not null
+	) AND
+	invalid_reason IS NOT NULL
 ;
 
+
 --add replacements for VMPs, replaced by source
-insert into concept_stage
-select
-	null :: int4 as concept_id,
-	coalesce (v.nmprev, v.nm) as concept_name,
-	case --take domain ID from replacement drug
-		when d.vpid is null then 'Drug'
-		else 'Device'
-	end as domain_id,
+INSERT INTO concept_stage
+SELECT
+	NULL :: int4 AS concept_id,
+	coalesce (v.nmprev, v.nm) AS concept_name,
+	CASE --take domain ID from replacement drug
+		WHEN d.vpid IS NULL THEN 'Drug'
+		ELSE 'Device'
+	END AS domain_id,
 	'dm+d',
 	'VMP',
-	null :: varchar as standard_concept,
-	v.vpidprev as concept_code,
-	to_date ('19700101','yyyymmdd') as valid_start_date,
-	coalesce (v.NMDT, current_date - 1) as valid_end_date,
-	'U' as invalid_reason
-from vmps v
-left join vmps u on --make sure old code was not processed on it's own
+	NULL :: varchar AS standard_concept,
+	v.vpidprev AS concept_code,
+	to_date ('19700101','yyyymmdd') AS valid_start_date,
+	coalesce (v.NMDT, current_date - 1) AS valid_end_date,
+	'U' AS invalid_reason
+FROM vmps v
+LEFT JOIN vmps u ON --make sure old code was not processed on it's own
 	v.vpidprev = u.vpid
-left join devices d on u.vpid = d.vpid
-where
-	v.vpidprev is not null and
-	u.vpid is null
+LEFT JOIN devices d ON u.vpid = d.vpid
+WHERE
+	v.vpidprev IS NOT NULL AND
+	u.vpid IS NULL
 ;
+
+
 --Get replacement mappings for deprecated VMPs
-insert into concept_relationship_stage
-select distinct
-	null :: int4,
-	null :: int4,
+INSERT INTO concept_relationship_stage
+SELECT DISTINCT
+	NULL :: int4,
+	NULL :: int4,
 	v.vpidprev,
 	r.concept_code_2,
 	'dm+d',
@@ -201,19 +218,19 @@ select distinct
 	'Maps to',
 	(SELECT vocabulary_date FROM sources.f_lookup2 LIMIT 1),
 	TO_DATE('20991231','yyyymmdd'),
-	null
-from vmps v
-join concept_relationship_stage r on
+	NULL
+FROM vmps v
+JOIN concept_relationship_stage r ON
 	v.vpid = r.concept_code_1
-where vpidprev is not null and
-	vpidprev not in (select concept_code_1 from concept_relationship_stage where invalid_reason is null)
+WHERE vpidprev IS NOT NULL AND
+	vpidprev NOT IN (SELECT concept_code_1 FROM concept_relationship_stage WHERE invalid_reason IS NULL)
 ;
 
 --deprecate all old maps
-insert into concept_relationship_stage
-select distinct
-	null :: int4,
-	null :: int4,
+INSERT INTO concept_relationship_stage
+SELECT DISTINCT
+	NULL :: int4,
+	NULL :: int4,
 	c.concept_code,
 	c2.concept_code,
 	'dm+d',
@@ -222,26 +239,28 @@ select distinct
 	r.valid_start_date,
 	current_date - 1,
 	'D'
-from concept_relationship r
-join concept c on 
-	c.concept_id = r.concept_id_1 and
-	c.vocabulary_id = 'dm+d' and
+FROM concept_relationship r
+JOIN concept c ON
+	c.concept_id = r.concept_id_1 AND
+	c.vocabulary_id = 'dm+d' AND
 	r.relationship_id = 'Maps to'
-join concept_stage cs on
+JOIN concept_stage cs ON
 	cs.concept_code = c.concept_code
-join concept c2 on 
+JOIN concept c2 ON
 	c2.concept_id = r.concept_id_2
-where
-	not exists
+WHERE
+	NOT exists
 		(
-			select 1
-			from concept_relationship_stage
-			where
-				concept_code_1 = c.concept_code and
-				concept_code_2 = c2.concept_code and
+			SELECT 1
+			FROM concept_relationship_stage
+			WHERE
+				concept_code_1 = c.concept_code AND
+				concept_code_2 = c2.concept_code AND
 				vocabulary_id_2 = c2.vocabulary_id
 		)
 ;
+
+
 -- Working with replacement mappings
 DO $_$
 BEGIN
@@ -255,10 +274,10 @@ BEGIN
 END $_$;
 
 --deprecate old ingredient mappings
-insert into concept_relationship_stage
-select
-	null :: int4,
-	null :: int4,
+INSERT INTO concept_relationship_stage
+SELECT
+	NULL :: int4,
+	NULL :: int4,
 	c.concept_code,
 	c2.concept_code,
 	c.vocabulary_id,
@@ -267,24 +286,23 @@ select
 	r.valid_start_date,
 	current_date - 1,
 	'D'
-from concept c
-join concept_relationship r on
-	r.concept_id_1 = c.concept_id and
-	r.relationship_id = 'Maps to' and
-	r.invalid_reason is null and
+FROM concept c
+JOIN concept_relationship r ON
+	r.concept_id_1 = c.concept_id AND
+	r.relationship_id = 'Maps to' AND
+	r.invalid_reason IS NULL AND
 	c.vocabulary_id = 'dm+d'
-join concept c2 on
-	c2.concept_id = r.concept_id_2 and
+JOIN concept c2 ON
+	c2.concept_id = r.concept_id_2 AND
 	c2.concept_class_id = 'Ingredient'
-left join internal_relationship_stage i on
+LEFT JOIN internal_relationship_stage i ON
 	i.concept_code_2 = c.concept_code
-where
-	i.concept_code_2 is null and
-	c.concept_class_id not in
-		('VMP','AMP','VMPP','AMPP')
+WHERE
+	i.concept_code_2 IS NULL AND
+	c.concept_class_id NOT IN
+		('VMP','AMP','VMPP','AMPP');
 
-update concept_stage set concept_name = trim(concept_name)
-;
 
-delete from concept_relationship_stage where concept_code_1 = '8203003' and invalid_reason is null
-;
+--Final manual changes
+UPDATE concept_stage SET concept_name = trim(concept_name);
+DELETE FROM concept_relationship_stage WHERE concept_code_1 = '8203003' AND invalid_reason IS NULL;

5a8f2785dcde510e1fdbba24545b9c1d5452de66
m-khitrun <maria.khitrun@odysseusinc.com>
2023-01-10 00:41:20 +0200

    fixes

--- working/manual_checks_after_generic.sql
+++ working/manual_checks_after_generic.sql
@@ -436,7 +436,7 @@ WITH home_visit AS (SELECT ('(?!(morp))home(?!(tr|opath))|domiciliary') as home_
     pharmacy_visit AS (SELECT ('(\W)pharm(\s)|pharmacy') AS pharmacy_visit),
     inpatient_visit AS (SELECT ('inpatient|in.patient|(\W)hosp(?!(ice|h|ira))') AS inpatient_visit),
     telehealth AS (SELECT ('(?!(pla))tele(?!(t|scop))|remote|video') as telehealth),
-    other_visit AS (SELECT ('clinic(?!(al))|center|(\W)facility|visit|institution|encounter|rehab|hospice|nurs') AS other_visit),
+    other_visit AS (SELECT ('clinic(?!(al))|center|(\W)facility|visit|institution|encounter|rehab|hospice|nurs|school') AS other_visit),
 
 flag AS (SELECT DISTINCT c.concept_code,
                 c.concept_name,
@@ -485,16 +485,15 @@ FROM concept c
 LEFT JOIN concept_relationship cr ON cr.concept_id_1 = c.concept_id AND relationship_id ='Maps to' AND cr.invalid_reason IS NULL
 LEFT JOIN concept b ON b.concept_id = cr.concept_id_2
 WHERE c.vocabulary_id IN (:your_vocabs)
-and b.domain_id = 'Visit'
---AND b.concept_id IN (581476, 9202, 581478, 9203, 581458, 9201, 5083)
+AND b.concept_id IN (581476, 9202, 581478, 9203, 581458, 9201, 5083)
 )
-
-SELECT * FROM correct_mapping
-
-UNION ALL
 
 SELECT * FROM flag
          WHERE flag_visit_should_be IS NOT NULL
+
+UNION ALL
+
+SELECT * FROM correct_mapping
 
 ORDER BY flag,
     flag_visit_should_be

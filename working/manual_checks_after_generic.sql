ed335ffbb7e94050d5d7dd92d84cc6c08b601b94
Alex Davydov <alexander.davydovv@gmail.com>
2023-01-11 17:50:26 +0200

    style fix

--- working/manual_checks_after_generic.sql
+++ working/manual_checks_after_generic.sql
@@ -439,15 +439,18 @@ AND NOT EXISTS (SELECT 1
 -- This check retrieves the mapping of vaccine concepts to Standard targets.
 -- Because of mapping complexity and trickiness, and depending on the way the mappings were produced, full manual review may be needed.
 --move to the project-specific QA folder and adjust exclusion criteria in there
+--use mask_array field for prioritization
 --adjust inclusion criteria here if needed: https://github.com/OHDSI/Vocabulary-v5.0/blob/master/RxNorm_E/manual_work/specific_qa/vaccine%20selection.sql
+
 with vaccine_exclusion as (SELECT
     'placeholder|placeholder' as vaccine_exclusion
     )
 ,
      vaccine_inclusion as (
-         SELECT  unnest(regexp_split_to_array(vaccine_inclusion,  '\|(?![^(]*\))')) as mask from  dev_rxe.vaccine_inclusion)
+         SELECT  unnest(regexp_split_to_array(vaccine_inclusion,  '\|(?![^(]*\))')) as mask FROM dev_rxe.vaccine_inclusion)
 
-select distinct c.vocabulary_id,
+SELECT DISTINCT array_agg(DISTINCT coalesce(vi.mask,vi2.mask )) as mask_array,
+                c.vocabulary_id,
                 c.concept_name,
                 c.concept_class_id,
                 CASE WHEN c.concept_id = b.concept_id THEN '<Mapped to itself>'
@@ -455,17 +458,21 @@ select distinct c.vocabulary_id,
                 CASE WHEN c.concept_id = b.concept_id THEN '<Mapped to itself>'
                     ELSE b.concept_class_id END as target_concept_class_id,
                 CASE WHEN c.concept_id = b.concept_id THEN '<Mapped to itself>'
-                    ELSE b.vocabulary_id END as target_vocabulary_id,
-                array_agg(DISTINCT coalesce(vi.mask,vi2.mask )) as mask_array
-from concept c
-left join concept_relationship cr on cr.concept_id_1 = c.concept_id and relationship_id ='Maps to' and cr.invalid_reason is null
-left join concept b on b.concept_id = cr.concept_id_2
-left join vaccine_inclusion vi on c.concept_name ~* vi.mask
-left join vaccine_inclusion vi2 on b.concept_name ~* vi2.mask
-where c.vocabulary_id IN (:your_vocabs)
-    and ((c.concept_name ~* (select vaccine_inclusion from dev_rxe.vaccine_inclusion) and c.concept_name !~* (select vaccine_exclusion from vaccine_exclusion))
-        or
-        (b.concept_name ~* (select vaccine_inclusion from dev_rxe.vaccine_inclusion) and b.concept_name !~* (select vaccine_exclusion from vaccine_exclusion)))
+                    ELSE b.vocabulary_id END as target_vocabulary_id
+FROM concept c
+LEFT JOIN concept_relationship cr
+    ON cr.concept_id_1 = c.concept_id
+           AND relationship_id ='Maps to' and cr.invalid_reason IS NULL
+LEFT JOIN concept b
+    ON b.concept_id = cr.concept_id_2
+LEFT JOIN vaccine_inclusion vi
+    ON c.concept_name ~* vi.mask
+LEFT JOIN vaccine_inclusion vi2
+    ON b.concept_name ~* vi2.mask
+WHERE c.vocabulary_id IN (:your_vocabs)
+    AND ((c.concept_name ~* (SELECT vaccine_inclusion FROM dev_rxe.vaccine_inclusion) AND c.concept_name !~* (SELECT vaccine_exclusion FROM vaccine_exclusion))
+        OR
+        (b.concept_name ~* (SELECT vaccine_inclusion FROM dev_rxe.vaccine_inclusion) AND b.concept_name !~* (SELECT vaccine_exclusion FROM vaccine_exclusion)))
 
 GROUP BY
 c.vocabulary_id,

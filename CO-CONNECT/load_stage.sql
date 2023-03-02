8f10fe4d1cb20d724d919933fcecd9eff671a70b
olegzhuk <oleg.zhuk@odysseusinc.com>
2023-03-02 11:12:19 +0100

    Changes of source data implemented to run generic update

--- CO-CONNECT/load_stage.sql
+++ CO-CONNECT/load_stage.sql
@@ -51,14 +51,35 @@ ALTER TABLE concept_relationship_stage
 ALTER COLUMN vocabulary_id_1 TYPE varchar(255);
 
 
---Automatic QA
+--Automatic QA of stage tables
 SELECT * FROM qa_tests.check_stage_tables();
 
 
+--Fixes to run a generic update to check basic tables
+--Concept_stage
+UPDATE concept_stage SET vocabulary_id = 'CO-CONNECT';
+UPDATE concept_relationship_stage SET vocabulary_id_1 = 'CO-CONNECT';
+UPDATE concept_stage SET concept_class_id = 'Precoordinated pair' WHERE concept_class_id = 'precoordinated pair';
+UPDATE concept_stage SET standard_concept = NULL WHERE standard_concept IS NOT NULL;
+UPDATE concept_stage SET concept_name = vocabulary_pack.cutconceptname(concept_name);
 
-SELECT DISTINCT crs.concept_code_2, crs.vocabulary_id_2, c.*
-FROM dev_co_connect.concept_relationship_stage crs
-LEFT JOIN dev_co_connect.concept c
-ON crs.concept_code_2 = c.concept_code AND crs.vocabulary_id_2 = c.vocabulary_id
-WHERE c.concept_id IS NULL
-;
+--Concept_relationship_stage
+--quick deduplication
+CREATE TABLE concept_relationship_stage_dedup AS (SELECT DISTINCT * FROM concept_relationship_stage);
+TRUNCATE concept_relationship_stage;
+INSERT INTO concept_relationship_stage (SELECT * FROM concept_relationship_Stage_dedup);
+DROP TABLE concept_relationship_stage_dedup;
+
+
+
+DO $_$
+BEGIN
+	PERFORM devv5.GenericUpdate();
+END $_$;
+
+
+--Automatic QA  of basic tables
+SELECT * FROM QA_TESTS.GET_CHECKS();
+
+--Manual checks after generic
+--(other files)

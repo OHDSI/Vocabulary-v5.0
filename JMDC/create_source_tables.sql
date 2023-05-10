4ab30b1e0abd4b870f4ad1a7f76df2e3c0992299
Dmytry Dymshyts <dimshitc@gmail.com>
2023-05-10 17:02:42 +0200

    Update create_source_tables.sql
    jmdc_drug_code is used in scripts intead of jmdc_code, 
    added version and formulation columns

--- JMDC/create_source_tables.sql
+++ JMDC/create_source_tables.sql
@@ -1,15 +1,14 @@
-
-create table jmdc
+create table scratch_ddymshyt.jmdc 
 (
-	drug_code varchar(255),
-	claim_code varchar(255),
+	jmdc_drug_code varchar(255),
 	who_atc_code varchar(255),
 	who_atc_name varchar(255),
 	general_name varchar(255),
 	brand_name varchar(255),
 	standardized_unit varchar(100),
-	frequency varchar(10),
-	concept_id integer,
-	concept_name varchar(255),
-	concept_class_id varchar(20)
+	formulation_large_classification_name varchar ,
+	formulation_medium_classification_name varchar,
+	formulation_small_classification_name varchar,
+	vocabulary_version date,
+	frequency varchar(10)
 );

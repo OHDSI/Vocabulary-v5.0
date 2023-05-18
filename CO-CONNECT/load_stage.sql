9152d5cbb5d0e7d9969e6fdc2bc68302b04e7be7
olegzhuk <oleg.zhuk@odysseusinc.com>
2023-05-18 17:36:26 +0200

    DeprecateWrongMAPSTO added; 2 missing truncates added

--- CO-CONNECT/load_stage.sql
+++ CO-CONNECT/load_stage.sql
@@ -37,6 +37,8 @@ BEGIN
 TRUNCATE TABLE concept_stage;
 TRUNCATE TABLE concept_relationship_stage;
 TRUNCATE TABLE concept_synonym_stage;
+TRUNCATE TABLE pack_content_stage;
+TRUNCATE TABLE drug_strength_stage;
 
 
 --3. Manual concepts
@@ -59,7 +61,13 @@ BEGIN
 	PERFORM VOCABULARY_PACK.AddFreshMAPSTO();
 END $_$;
 
---6. Delete ambiguous 'Maps to' mappings
+--6. Deprecate 'Maps to' mappings to deprecated and upgraded concepts
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.DeprecateWrongMAPSTO();
+END $_$;
+
+--7. Delete ambiguous 'Maps to' mappings
 DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.DeleteAmbiguousMAPSTO();

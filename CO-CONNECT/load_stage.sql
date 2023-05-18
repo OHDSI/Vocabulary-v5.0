bc6a7c85a49787ee7c13d10313966f80b41807f8
olegzhuk <oleg.zhuk@odysseusinc.com>
2023-05-18 13:41:23 +0200

    Preparing for the release: removing fastrecreate/genericupdate, moving adding new vocabularies to manual changes

--- CO-CONNECT/load_stage.sql
+++ CO-CONNECT/load_stage.sql
@@ -1,59 +1,10 @@
-SELECT devv5.FastRecreateSchema(main_schema_name=>'devv5', include_concept_ancestor=>true, include_deprecated_rels=>true, include_synonyms=>true);
-
--- 0. Add New Vocabularies
-DO $_$
-BEGIN
-	PERFORM VOCABULARY_PACK.AddNewVocabulary(
-	pVocabulary_id			=> 'CO-CONNECT',
-	pVocabulary_name		=> 'IQVIA CO-CONNECT',
-	pVocabulary_reference	=> 'To be populated',
-	pVocabulary_version		=> NULL,
-	pOMOP_req				=> NULL,
-	pClick_default			=> NULL, --NULL or 'Y'
-	pAvailable				=> 'License required', --NULL, 'Currently not available','License required' or 'EULA required'
-	pURL					=> NULL,
-	pClick_disabled			=> NULL --NULL or 'Y'
-);
-END $_$;
-
-DO $_$
-BEGIN
-	PERFORM VOCABULARY_PACK.AddNewVocabulary(
-	pVocabulary_id			=> 'CO-CONNECT MIABIS',
-	pVocabulary_name		=> 'IQVIA CO-CONNECT MIABIS',
-	pVocabulary_reference	=> 'To be populated',
-	pVocabulary_version		=> NULL,
-	pOMOP_req				=> NULL,
-	pClick_default			=> NULL, --NULL or 'Y'
-	pAvailable				=> 'License required', --NULL, 'Currently not available','License required' or 'EULA required'
-	pURL					=> NULL,
-	pClick_disabled			=> NULL --NULL or 'Y'
-);
-END $_$;
-
-DO $_$
-BEGIN
-	PERFORM VOCABULARY_PACK.AddNewVocabulary(
-	pVocabulary_id			=> 'CO-CONNECT TWINS',
-	pVocabulary_name		=> 'IQVIA CO-CONNECT TWINS',
-	pVocabulary_reference	=> 'To be populated',
-	pVocabulary_version		=> NULL,
-	pOMOP_req				=> NULL,
-	pClick_default			=> NULL, --NULL or 'Y'
-	pAvailable				=> 'License required', --NULL, 'Currently not available','License required' or 'EULA required'
-	pURL					=> NULL,
-	pClick_disabled			=> NULL --NULL or 'Y'
-);
-END $_$;
-
-
 -- 1. Set latest update
 DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.SetLatestUpdate(
 	pVocabularyName			=> 'CO-CONNECT',
-    pVocabularyDate			=> to_date ('2018-03-02', 'yyyy-mm-dd'),
-	pVocabularyVersion		=> 'CO-CONNECT test',
+    pVocabularyDate			=> CURRENT_DATE,
+	pVocabularyVersion		=> 'CO-CONNECT '||TO_CHAR(CURRENT_DATE,'YYYY-MM-DD'),
 	pVocabularyDevSchema	=> 'dev_co_connect'
 	);
 	END $_$;
@@ -62,8 +13,8 @@ DO $_$
 BEGIN
     PERFORM VOCABULARY_PACK.SetLatestUpdate(
 	pVocabularyName			=> 'CO-CONNECT MIABIS',
-    pVocabularyDate			=> to_date ('2018-03-02', 'yyyy-mm-dd'),
-	pVocabularyVersion		=> 'CO-CONNECT test',
+    pVocabularyDate			=> CURRENT_DATE,
+	pVocabularyVersion		=> 'CO-CONNECT MIABIS '||TO_CHAR(CURRENT_DATE,'YYYY-MM-DD'),
 	pVocabularyDevSchema	=> 'dev_co_connect',
     pAppendVocabulary		=> TRUE
 	);
@@ -74,8 +25,8 @@ DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.SetLatestUpdate(
 	pVocabularyName			=> 'CO-CONNECT TWINS',
-    pVocabularyDate			=> to_date ('2018-03-02', 'yyyy-mm-dd'),
-	pVocabularyVersion		=> 'CO-CONNECT test',
+    pVocabularyDate			=> CURRENT_DATE,
+	pVocabularyVersion		=> 'CO-CONNECT TWINS '||TO_CHAR(CURRENT_DATE,'YYYY-MM-DD'),
 	pVocabularyDevSchema	=> 'dev_co_connect',
 	pAppendVocabulary		=> TRUE
 	);
@@ -86,8 +37,6 @@ BEGIN
 TRUNCATE TABLE concept_stage;
 TRUNCATE TABLE concept_relationship_stage;
 TRUNCATE TABLE concept_synonym_stage;
-TRUNCATE TABLE pack_content_stage;
-TRUNCATE TABLE drug_strength_stage;
 
 
 --3. Manual concepts
@@ -117,9 +66,3 @@ BEGIN
 END $_$;
 
 -- At the end, the three tables concept_stage, concept_relationship_stage and concept_synonym_stage should be ready to be fed into the generic_update.sql script
-
-
-DO $_$
-BEGIN
-	PERFORM devv5.GenericUpdate();
-END $_$;

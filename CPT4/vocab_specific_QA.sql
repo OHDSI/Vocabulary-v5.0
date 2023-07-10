d1ee5fee05639ae9ce4b5909906aa190799d0596
m-khitrun <maria.khitrun@odysseusinc.com>
2023-07-10 11:10:02 +0300

    vocab_specific_QA.sql edit

--- CPT4/vocab_specific_QA.sql
+++ CPT4/vocab_specific_QA.sql
@@ -1,12 +1,57 @@
 -- 1. Our aim is to build a unique hierarchy of Procedures with CPT4 embedded in SNOMED/OMOP Ext hierarchy.
 --- The script below retrieves following data:
 ---- 'concepts_in_hierarchy' - the number of concepts in hierarchy through 'Is a' relationships;
----- 'mapped_concepts' - the number of mapped to SNOMED/OMOP Ext. concepts;
----- 'concepts_not_in_hierarchy' - the number of concepts that are not yet in hierarchy.
+---- 'mapped' - the number of mapped to SNOMED/OMOP Ext./etc. concepts;
+---- 'concepts_not_in_hierarchy' - the number of concepts that are not yet in SNOMED/OMOP Ext hierarchy.
+--- The script below allows you to compare current status of hierarchy in devv5 with new status after manual work performed
 --- These counts don't include classification concepts and modifiers, and also concepts mapped to other vocabularies.
 --- Use this counts for analysis and renew respective numbers in https://github.com/OHDSI/Vocabulary-v5.0/wiki/Known-Issues-in-Vocabularies
 
-WITH concepts_in_hierarchy AS (SELECT DISTINCT cr.concept_id_1
+WITH current_status AS (
+       SELECT 'concepts_in_hierarchy' AS flag,
+              count (DISTINCT cr.concept_id_1) AS count
+                                FROM devv5.concept_relationship cr
+                                        JOIN concept c ON cr.concept_id_1 = c.concept_id
+                                        JOIN concept c1 ON cr.concept_id_2 = c1.concept_id
+                               WHERE c.vocabulary_id = 'CPT4'
+                                 AND c.concept_class_id = 'CPT4'
+                                 AND c1.vocabulary_id IN ('SNOMED', 'OMOP Extension')
+                                 AND cr.relationship_id IN ('Is a', 'Maps to')
+                                 AND cr.invalid_reason IS NULL
+
+       UNION
+
+       SELECT 'mapped' AS flag,
+              count (DISTINCT cr.concept_id_1)
+                           FROM devv5.concept_relationship cr
+                                        JOIN concept c ON cr.concept_id_1 = c.concept_id
+                                        JOIN concept c1 ON cr.concept_id_2 = c1.concept_id
+                               WHERE c.vocabulary_id = 'CPT4'
+                                 AND c.concept_class_id IN ('CPT4', 'Visit')
+                                 AND c1.vocabulary_id IN ('SNOMED', 'OMOP Extension')
+                                 AND cr.relationship_id IN ('Maps to')
+                                 AND cr.invalid_reason IS NULL
+
+       UNION
+
+       SELECT 'concepts_not_in_hierarchy' AS flag,
+              count (DISTINCT concept_id)
+                            FROM concept
+                            WHERE vocabulary_id = 'CPT4'
+                                  AND concept_class_id IN ('CPT4', 'Visit')
+                                  AND concept_id NOT IN (SELECT concept_id_1
+														FROM devv5.concept_relationship cr
+														   JOIN concept c ON cr.concept_id_1 = c.concept_id
+														   JOIN concept c1 ON cr.concept_id_2 = c1.concept_id
+															   AND c.vocabulary_id = 'CPT4'
+															   AND c1.vocabulary_id IN ('SNOMED', 'OMOP Extension')
+															   AND cr.relationship_id in ('Maps to', 'Is a')
+															   AND cr.invalid_reason IS NULL)
+),
+
+new_status AS (
+              SELECT 'concepts_in_hierarchy' AS flag,
+              count (DISTINCT cr.concept_id_1) AS count
                                 FROM concept_relationship cr
                                         JOIN concept c ON cr.concept_id_1 = c.concept_id
                                         JOIN concept c1 ON cr.concept_id_2 = c1.concept_id
@@ -14,40 +59,43 @@ WITH concepts_in_hierarchy AS (SELECT DISTINCT cr.concept_id_1
                                  AND c.concept_class_id = 'CPT4'
                                  AND c1.vocabulary_id IN ('SNOMED', 'OMOP Extension')
                                  AND cr.relationship_id IN ('Is a', 'Maps to')
-                                 AND cr.invalid_reason IS NULL),
+                                 AND cr.invalid_reason IS NULL
 
-    mapped_to_snomed as (SELECT DISTINCT cr.concept_id_1
+       UNION
+
+       SELECT 'mapped' AS flag,
+              count (DISTINCT cr.concept_id_1)
                            FROM concept_relationship cr
                                         JOIN concept c ON cr.concept_id_1 = c.concept_id
                                         JOIN concept c1 ON cr.concept_id_2 = c1.concept_id
                                WHERE c.vocabulary_id = 'CPT4'
-                                 AND c.concept_class_id = 'CPT4'
+                                 AND c.concept_class_id IN ('CPT4', 'Visit')
                                  AND c1.vocabulary_id IN ('SNOMED', 'OMOP Extension')
                                  AND cr.relationship_id IN ('Maps to')
-                                 AND cr.invalid_reason IS NULL),
+                                 AND cr.invalid_reason IS NULL
 
-     concepts_not_in_hierarchy AS (SELECT concept_id
-                                   FROM concept
-                                   WHERE concept_id NOT IN (SELECT concept_id_1
-                                                            FROM concepts_in_hierarchy)
-                                     AND vocabulary_id = 'CPT4'
-                                     AND standard_concept = 'S'
-                                     AND concept_class_id IN ('CPT4', 'Visit'))
-SELECT 'concepts_in_hierarchy' AS flag,
-       COUNT(concept_id_1)     AS count
-FROM concepts_in_hierarchy
+       UNION
 
-UNION
+       SELECT 'concepts_not_in_hierarchy' AS flag,
+              count (DISTINCT concept_id)
+                            FROM concept
+                            WHERE vocabulary_id = 'CPT4'
+                                  AND concept_class_id IN ('CPT4', 'Visit')
+                                  AND concept_id NOT IN (SELECT concept_id_1
+														FROM concept_relationship cr
+														   JOIN concept c ON cr.concept_id_1 = c.concept_id
+														   JOIN concept c1 ON cr.concept_id_2 = c1.concept_id
+															   AND c.vocabulary_id = 'CPT4'
+															   AND c1.vocabulary_id IN ('SNOMED', 'OMOP Extension')
+															   AND cr.relationship_id IN ('Maps to', 'Is a')
+															   AND cr.invalid_reason IS NULL)
+)
 
-SELECT 'mapped_to_snomed'    AS flag,
-       COUNT(concept_id_1)  AS count
-FROM mapped_to_snomed
-
-UNION
-
-SELECT 'concepts_not_in_hierarchy' AS flag,
-       COUNT(concept_id)
-FROM concepts_not_in_hierarchy
+SELECT flag,
+       a.count AS current_status,
+       b.count AS new_status
+       FROM current_status a
+	JOIN new_status b USING (flag)
 ;
 
 -- 2. In CPT4 concepts may migrate between categories and acquire new concept_code. These changes are made in source.

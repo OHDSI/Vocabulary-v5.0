d074c7cb5f2af7ffb41d2c39faad726f2c755981
Irina <irina.zherko@odysseusinc.com>
2023-08-21 21:21:44 +0300

    PPI long names update

--- PPI/manual_work/Jul_2023/process_source_table.sql
+++ PPI/manual_work/Jul_2023/process_source_table.sql
@@ -47,11 +47,11 @@ UPDATE bhp_pr SET question_code = trim(question_code) ;
 UPDATE bhp_pr SET answer_name = trim(answer_name) ;
 UPDATE bhp_pr SET question_name = trim(question_name) ;
 
-SELECT * FROM bhp_pr;
+SELECT * FROM bhp_pr; --209
 
 SELECT DISTINCT question_code, question_name FROM dev_ppi.bhp_pr where length (question_name) > 255;
 
-UPDATE bhp_pr SET question_name = 'Some people have a lot of fear about things like going out of the house alone, being in a crowd, going over bridges, or traveling by bus. Were you ever in your life so frightened by any of these situations that it got in the way of you having a normal life?'
+UPDATE bhp_pr SET question_name = 'Some people have a lot of fear about things like going out of the house alone, being in a crowd, going over bridges, or traveling by bus. Were you ever in your life frightened by any of these situations?'
 WHERE question_code = 'cidi5_30';
 UPDATE bhp_pr SET question_name = 'Have you ever been bothered with thoughts, images, or urges that kept coming back over and over, like concerns with germs, order, or experiencing horrific images or intrusive sexual thoughts, or urges to knock objects, or harming a loved one?'
 WHERE question_code = 'pmi_1';
@@ -60,8 +60,27 @@ WHERE question_code = 'mhqukb_55';
 
 SELECT DISTINCT answer_code, answer_name FROM bhp_pr where length (answer_name) > 255;
 
-UPDATE bhp_pr SET question_name = 'Some people have a lot of fear about things like going out of the house alone, being in a crowd, going over bridges, or traveling by bus. Were you ever in your life so frightened by any of these situations that it got in the way of you having a normal life?'
-WHERE question_code = 'bhp_6';
+UPDATE bhp_pr SET answer_name = 'Some people have a lot of fear about things like going out of the house alone, being in a crowd, going over bridges, or traveling by bus. Were you ever in your life frightened by any of these situations?-Yes'
+WHERE answer_code = 'cidi5_30-bhp_6';
+UPDATE bhp_pr SET answer_name = 'Some people have a lot of fear about things like going out of the house alone, being in a crowd, going over bridges, or traveling by bus. Were you ever in your life frightened by any of these situations?-No'
+WHERE answer_code = 'cidi5_30-bhp_7';
+UPDATE bhp_pr SET answer_name = 'How distressing did you find having any of these experiences (such as seeing a vision, hearing a voice)?-Not distressing at all. It was a positive experience'
+WHERE answer_code = 'mhqukb_54-bhp_96';
+UPDATE bhp_pr SET answer_name = 'Did you ever talk to a health professional about any of these experiences (such as seeing a vision, hearing a voice, believing that something strange was trying to communicate with you)?-Yes'
+WHERE answer_code = 'mhqukb_55-bhp_6';
+UPDATE bhp_pr SET answer_name = 'Did you ever talk to a health professional about any of these experiences (such as seeing a vision, hearing a voice, believing that something strange was trying to communicate with you)?-No'
+WHERE answer_code = 'mhqukb_55-bhp_7';
+UPDATE bhp_pr SET answer_name = 'Did you ever talk to a health professional about any of these experiences (such as seeing a vision, hearing a voice, believing that something strange was trying to communicate with you)?-Don''t know'
+WHERE answer_code = 'mhqukb_55-pmi_dontknow';
+UPDATE bhp_pr SET answer_name = 'Did you ever talk to a health professional about any of these experiences (such as seeing a vision, hearing a voice, believing that something strange was trying to communicate with you)?-Prefer not to answer'
+WHERE answer_code = 'mhqukb_55-pmi_prefernottoanswer';
+UPDATE bhp_pr SET answer_name = 'Were you ever prescribed a medication by a health professional for any of these experiences (such as seeing a vision, hearing a voice)? -Prefer not to answer'
+WHERE answer_code = 'mhqukb_56-pmi_prefernottoanswer';
+UPDATE bhp_pr SET answer_name = 'Have you ever been bothered with thoughts, images, or urges that kept coming back over and over, like concerns with germs, order, or experiencing horrific images or intrusive sexual thoughts, or urges to knock objects, or harming a loved one?-Yes'
+WHERE answer_code = 'pmi_1-bhp_6';
+UPDATE bhp_pr SET answer_name = 'Have you ever been bothered with thoughts, images, or urges that kept coming back over and over, like concerns with germs, order, or experiencing horrific images or intrusive sexual thoughts, or urges to knock objects, or harming a loved one?-No'
+WHERE answer_code = 'pmi_1-bhp_7';
+
 
 -- EHH (Emotional Health history and Wellbeing) source table
 TRUNCATE TABLE ehh;
@@ -112,7 +131,7 @@ update ehh_pr set question_code = trim(question_code) ;
 update ehh_pr set answer_name = trim(answer_name) ;
 update ehh_pr set question_name = trim(question_name) ;
 
-SELECT * FROM ehh_pr;
+SELECT * FROM ehh_pr; --408
 
 SELECT DISTINCT question_code, question_name FROM ehh_pr where length (question_name) > 255;
 SELECT DISTINCT answer_code, answer_name FROM ehh_pr where length (answer_name) > 255;

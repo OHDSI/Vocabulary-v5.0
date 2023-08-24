0a9fc7249d2727d5df1594466a99c87df4c9dda9
olegzhuk <oleg.zhuk@odysseusinc.com>
2023-08-24 16:03:04 +0200

    Merging branches

--- working/manual_changes/2023/2023-08-25.sql
+++ working/manual_changes/2023/2023-08-25.sql
@@ -1,3 +1,17 @@
+--Change of vocabulary license requirements and vocabulary_reference for CO-CONNECT family of the vocabularies
+--AVOC-4015
+UPDATE vocabulary 
+SET vocabulary_name = replace(vocabulary_name, 'IQVIA ', ''),
+    vocabulary_reference = 'https://co-connect.ac.uk/'
+WHERE vocabulary_id IN ('CO-CONNECT', 'CO-CONNECT TWINS', 'CO-CONNECT MIABIS');
+
+UPDATE vocabulary_conversion
+SET available = NULL,
+    URL = NULL
+WHERE vocabulary_id_v5 IN ('CO-CONNECT', 'CO-CONNECT TWINS', 'CO-CONNECT MIABIS');
+
+
+
 --Adding two new vocabularies
 --https://github.com/OHDSI/Vocabulary-v5.0/issues/843
 

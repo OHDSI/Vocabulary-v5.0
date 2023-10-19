34443a3674509b4fc2683d29a1fa0217fb6ba071
Timur <timur.vakhitov@odysseusinc.com>
2023-10-19 18:26:49 +0300

    fix bug with incorrect processing of mappings if the same concept_code belongs to different vocabularies [AVOC-4085] + small performance improvement

--- working/packages/vocabulary_pack/AddFreshMapsToValue.sql
+++ working/packages/vocabulary_pack/AddFreshMapsToValue.sql
@@ -1,106 +1,100 @@
 CREATE OR REPLACE FUNCTION vocabulary_pack.AddFreshMapsToValue (pVocabulary VARCHAR DEFAULT NULL)
 RETURNS VOID AS
 $BODY$
-/*
- Adds mapping from deprecated to fresh concepts for 'Maps to value'
- Works similar to AddFreshMAPSTO function, but with differences:
- 1. The function doesn't automatically add the 'Maps to value' for replacement relationships (they are not used at all)
- 2. The function can use both 'Maps to value' and 'Maps to' relationships
- For example, if we have A 'Maps to' B 'Maps to value' C, then A 'Maps to value' C will be created
- 
- Also, if there is already 'Maps to' between the target concepts, then 'Maps to value' will not be created
- For example, if we have A 'Maps to' B 'Maps to value' C, and A 'Maps to' C, then A 'Maps to value' C will NOT be created
- 
- NOTE: for the function to work correctly, it must be launched after AddFreshMAPSTO
-*/
+	/*
+	Adds mapping from deprecated to fresh concepts for 'Maps to value'
+	Works similar to AddFreshMAPSTO function, but with differences:
+	1. The function doesn't automatically add the 'Maps to value' for replacement relationships (they are not used at all)
+	2. The function can use both 'Maps to value' and 'Maps to' relationships
+	For example, if we have A 'Maps to' B 'Maps to value' C, then A 'Maps to value' C will be created
+
+	Also, if there is already 'Maps to' between the target concepts, then 'Maps to value' will not be created
+	For example, if we have A 'Maps to' B 'Maps to value' C, and A 'Maps to' C, then A 'Maps to value' C will NOT be created
+
+	NOTE: for the function to work correctly, it must be launched after AddFreshMAPSTO
+	*/
 BEGIN
 	ANALYZE concept_relationship_stage;
+	ANALYZE concept_stage;
 
-	CREATE TEMP TABLE new_relationships ON COMMIT DROP AS
-		WITH RECURSIVE rec AS (
-				SELECT u.concept_code_1,
-					u.vocabulary_id_1,
-					u.concept_code_2,
-					u.vocabulary_id_2,
-					u.concept_code_1 AS root_concept_code_1,
-					u.vocabulary_id_1 AS root_vocabulary_id_1,
-					ARRAY [ROW (u.concept_code_2, u.vocabulary_id_2)] AS full_path
-				FROM upgraded_concepts u
-				
-				UNION ALL
-				
-				SELECT uc.concept_code_1,
-					uc.vocabulary_id_1,
-					uc.concept_code_2,
-					uc.vocabulary_id_2,
-					r.root_concept_code_1,
-					r.root_vocabulary_id_1,
-					r.full_path || ROW (uc.concept_code_2, uc.vocabulary_id_2)
-				FROM upgraded_concepts uc
-				JOIN rec r ON r.concept_code_2 = uc.concept_code_1
-					AND r.vocabulary_id_2 = uc.vocabulary_id_1
-				WHERE ROW (uc.concept_code_2, uc.vocabulary_id_2) <> ALL (full_path) --excluding loops
-				),
-			upgraded_concepts AS (
-				SELECT *
-				FROM (
-					SELECT DISTINCT s1.concept_code_1,
-						CASE 
-							WHEN s1.in_base_tables = MIN(s1.in_base_tables) OVER (PARTITION BY s1.concept_code_1)
-								THEN s1.concept_code_2
-							END AS concept_code_2,
-						s1.vocabulary_id_1,
-						s1.vocabulary_id_2
-					FROM (
-						SELECT crs.concept_code_1,
-							crs.concept_code_2,
-							crs.vocabulary_id_1,
-							crs.vocabulary_id_2,
-							0 AS in_base_tables
-						FROM concept_relationship_stage crs
-						WHERE crs.relationship_id IN (
-								'Maps to',
-								'Maps to value'
-								)
-							AND crs.invalid_reason IS NULL
-							AND NOT (
-								--exclude mappings to self
-								crs.concept_code_1 = crs.concept_code_2
-								AND crs.vocabulary_id_1 = crs.vocabulary_id_2
-								)
-						
-						UNION ALL
-						
-						--some concepts might be in 'base' tables
-						SELECT c1.concept_code,
-							c2.concept_code,
-							c1.vocabulary_id,
-							c2.vocabulary_id,
-							1 AS in_base_tables
-						FROM concept_relationship r
-						JOIN concept c1 ON c1.concept_id = r.concept_id_1
-						JOIN concept c2 ON c2.concept_id = r.concept_id_2
-						WHERE r.invalid_reason IS NULL
-							AND r.concept_id_1 <> r.concept_id_2 --exclude mappings to self
-							AND r.relationship_id IN (
-								'Maps to',
-								'Maps to value'
-								)
-							--don't use already deprecated relationships
-							AND NOT EXISTS (
-								SELECT 1
-								FROM concept_relationship_stage crs_int
-								WHERE crs_int.concept_code_1 = c1.concept_code
-									AND crs_int.vocabulary_id_1 = c1.vocabulary_id
-									AND crs_int.concept_code_2 = c2.concept_code
-									AND crs_int.vocabulary_id_2 = c2.vocabulary_id
-									AND crs_int.relationship_id = r.relationship_id
-									AND crs_int.invalid_reason IS NOT NULL
-								)
-						) AS s1
-					) AS s2
-				WHERE concept_code_2 IS NOT NULL
-				)
+	CREATE TEMP TABLE new_relationships_prepare$ AS
+	SELECT crs.concept_code_1,
+		crs.concept_code_2,
+		crs.vocabulary_id_1,
+		crs.vocabulary_id_2
+	FROM concept_relationship_stage crs
+	WHERE crs.relationship_id IN (
+			'Maps to',
+			'Maps to value'
+			)
+		AND crs.invalid_reason IS NULL
+		AND NOT (
+			--exclude mappings to self
+			crs.concept_code_1 = crs.concept_code_2
+			AND crs.vocabulary_id_1 = crs.vocabulary_id_2
+			)
+
+	UNION ALL
+
+	--some concepts might be in 'base' tables
+	SELECT c1.concept_code,
+		c2.concept_code,
+		c1.vocabulary_id,
+		c2.vocabulary_id
+	FROM concept_relationship r
+	JOIN concept c1 ON c1.concept_id = r.concept_id_1
+	JOIN concept c2 ON c2.concept_id = r.concept_id_2
+	LEFT JOIN concept_relationship_stage crs ON crs.concept_code_1 = c1.concept_code
+		AND crs.vocabulary_id_1 = c1.vocabulary_id
+		AND crs.relationship_id = r.relationship_id
+		AND crs.invalid_reason IS NULL
+	WHERE r.invalid_reason IS NULL
+		AND r.concept_id_1 <> r.concept_id_2 --exclude mappings to self
+		AND crs.concept_code_1 IS NULL --we need only fresh 'Maps to/value' which contains in stage-tables (per each concept_code_1), but if we doesn't have them - take from base tables
+		AND r.relationship_id IN (
+			'Maps to',
+			'Maps to value'
+			)
+		--don't use already deprecated relationships
+		AND NOT EXISTS (
+			SELECT 1
+			FROM concept_relationship_stage crs_int
+			WHERE crs_int.concept_code_1 = c1.concept_code
+				AND crs_int.vocabulary_id_1 = c1.vocabulary_id
+				AND crs_int.concept_code_2 = c2.concept_code
+				AND crs_int.vocabulary_id_2 = c2.vocabulary_id
+				AND crs_int.relationship_id = r.relationship_id
+				AND crs_int.invalid_reason IS NOT NULL
+			);
+
+	CREATE INDEX idx_new_relationships_prepare$ ON new_relationships_prepare$ (concept_code_1,vocabulary_id_1) WITH (FILLFACTOR=100);
+	ANALYZE new_relationships_prepare$;
+
+	CREATE TEMP TABLE new_relationships$ AS
+	WITH RECURSIVE rec AS (
+		SELECT u.concept_code_1,
+			u.vocabulary_id_1,
+			u.concept_code_2,
+			u.vocabulary_id_2,
+			u.concept_code_1 AS root_concept_code_1,
+			u.vocabulary_id_1 AS root_vocabulary_id_1,
+			ARRAY [ROW (u.concept_code_2, u.vocabulary_id_2)] AS full_path
+		FROM new_relationships_prepare$ u
+		
+		UNION ALL
+		
+		SELECT uc.concept_code_1,
+			uc.vocabulary_id_1,
+			uc.concept_code_2,
+			uc.vocabulary_id_2,
+			r.root_concept_code_1,
+			r.root_vocabulary_id_1,
+			r.full_path || ROW (uc.concept_code_2, uc.vocabulary_id_2)
+		FROM new_relationships_prepare$ uc
+		JOIN rec r ON r.concept_code_2 = uc.concept_code_1
+			AND r.vocabulary_id_2 = uc.vocabulary_id_1
+		WHERE ROW (uc.concept_code_2, uc.vocabulary_id_2) <> ALL (full_path) --excluding loops
+		)
 		SELECT s3.root_concept_code_1,
 			s3.concept_code_2,
 			s3.root_vocabulary_id_1,
@@ -163,7 +157,7 @@ BEGIN
 		invalid_reason
 		)
 	SELECT nr.*
-	FROM new_relationships nr
+	FROM new_relationships$ nr
 	ON CONFLICT ON CONSTRAINT idx_pk_crs
 	DO UPDATE
 	SET invalid_reason = NULL,
@@ -173,7 +167,7 @@ BEGIN
 	ROW (excluded.valid_start_date, excluded.valid_end_date, excluded.invalid_reason);
 
 	--if the function is executed in a transaction, then by the time of the next call the temp table will exist
-	DROP TABLE new_relationships;
+	DROP TABLE new_relationships_prepare$, new_relationships$;
 END;
 $BODY$
 LANGUAGE 'plpgsql';

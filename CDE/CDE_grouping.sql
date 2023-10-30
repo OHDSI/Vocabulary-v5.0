8071c03041ec30aa63f08552a8b605c47450061e
maleman <snax2251@gmail.com>
2023-10-30 12:49:26 -0600

    Update CDE_grouping.sql
    Add input table parameter

--- CDE/CDE_grouping.sql
+++ CDE/CDE_grouping.sql
@@ -17,20 +17,28 @@ SELECT * FROM cde_manual_group;
 -- Merge group function
 ---------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION cde_merge_group(pgroup_id int, pgroup_code character varying [])
+CREATE OR REPLACE FUNCTION cde_merge_group(pinput_table TEXT, pgroup_id int, pgroup_code character varying [])
 RETURNS void
 LANGUAGE plpgsql
 AS $function$
 
 /*
  * Example:
- * 		select * from cde_merge_group(2, ARRAY['A15.5:ICD10','Q12.0:KCD7'])
+ * 		select * from cde_merge_group('cde_manual_group', 2, ARRAY['A15.5:ICD10','Q12.0:KCD7'])
  */
 
+DECLARE
+	z int4;
 BEGIN
-	IF pgroup_id IS NULL OR NOT EXISTS(SELECT 1 FROM cde_manual_group WHERE group_id = pgroup_id)THEN
+	
+	EXECUTE FORMAT ($$
+		SELECT COUNT(*) FROM %1$I WHERE group_id = %2$s;
+		$$, pinput_table, pgroup_id) INTO z;
+	
+	IF pgroup_id IS NULL OR Z <= 0 THEN
 			RAISE EXCEPTION 'The supplied group_id parameter does not exist!';
 	END IF;
+
 
 	IF pgroup_code  IS NULL OR array_length(pgroup_code, 1) IS NULL THEN
 		RAISE EXCEPTION 'The group code parameter is required!';
@@ -39,42 +47,45 @@ BEGIN
 	DROP TABLE IF EXISTS group_ids;
 	CREATE TEMP TABLE group_ids(ids INT);
 
-	INSERT INTO group_ids
-	SELECT DISTINCT group_id
-	FROM(
-		SELECT group_id
-		FROM cde_manual_group
-		WHERE group_id = pgroup_id
-		UNION ALL
-		SELECT group_id AS	group_id
-		FROM cde_manual_group	
-		WHERE group_code && pgroup_code
-	)AS T;
+	EXECUTE FORMAT ($$
+		INSERT INTO group_ids
+		SELECT DISTINCT group_id
+		FROM(
+			SELECT group_id
+			FROM %1$I
+			WHERE group_id = pgroup_id
+			UNION ALL
+			SELECT group_id AS	group_id
+			FROM %1$I	
+			WHERE group_code && pgroup_code
+		)AS T;
+	$$, pinput_table);
 
-	WITH cte_dource AS (
-		SELECT pgroup_code AS group_id,source_vocabulary_id, group_name  , UNNEST (group_code) AS group_code
-		FROM cde_manual_group
-		WHERE group_id in(
-			SELECT ids
-			FROM group_ids
+	EXECUTE FORMAT ($$
+		WITH cte_dource AS (
+			SELECT pgroup_code AS group_id,source_vocabulary_id, group_name  , UNNEST (group_code) AS group_code
+			FROM %1$I
+			WHERE group_id in(
+				SELECT ids
+				FROM group_ids
+			)
+		), cte_meger AS(
+			SELECT group_id AS id, group_name AS name, array_agg(group_code)  AS code 
+			FROM (
+				SELECT DISTINCT pgroup_id AS group_id, 
+					FIRST_VALUE (group_name) over(ORDER BY source_vocabulary_id, LENGTH(group_name) DESC) AS group_name, 
+					group_code
+				FROM cte_dource
+				)meger
+			GROUP BY group_id, group_name
 		)
-	), cte_meger AS(
-		SELECT group_id AS id, group_name AS name, array_agg(group_code)  AS code 
-		FROM (
-			SELECT DISTINCT pgroup_id AS group_id, 
-				FIRST_VALUE (group_name) over(ORDER BY source_vocabulary_id, LENGTH(group_name) DESC) AS group_name, 
-				group_code
-			FROM cte_dource
-			)meger
-		GROUP BY group_id, group_name
-	)
-	UPDATE cde_manual_group
-	SET group_id = id,
-		group_name = name,
-		group_code = code
-	FROM cte_meger
-	WHERE group_id IN (SELECT ids FROM group_ids);
-
+		UPDATE %1$I
+		SET group_id = id,
+			group_name = name,
+			group_code = code
+		FROM cte_meger
+		WHERE group_id IN (SELECT ids FROM group_ids);
+	$$, pinput_table);
 
 END
 $function$;
@@ -88,25 +99,29 @@ SELECT * FROM cde_manual_group;
 --
 ---------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION cde_merge_group(pgroup_id integer[])
+CREATE OR REPLACE FUNCTION cde_merge_group(pinput_table TEXT, pgroup_id integer[])
 RETURNS void
 LANGUAGE plpgsql
 AS $function$
 	/*
 	 * Example:
-	 * 	select * from cde_merge_group(ARRAY[1,2,3,20])
+	 * 	select * from cde_merge_group('cde_manual_group',ARRAY[1,2,3,20])
 	 */
-
+DECLARE
+	z int4;
 BEGIN
 
 	IF pgroup_id IS NULL OR array_length(pgroup_id, 1) IS NULL OR array_length(pgroup_id, 1) < 2 THEN
 		RAISE EXCEPTION 'The group id parameter cannot be null, empty or contain less than one id!';
 	END IF;
 
-	UPDATE cde_manual_group t
-	SET group_id = pgroup_id[1]
-	WHERE t.group_id=ANY(pgroup_id)
-	AND t.group_id <> pgroup_id[1];
+	EXECUTE FORMAT ($$
+			UPDATE %1$I AS t
+			SET group_id = $1[1]
+			WHERE t.group_id=ANY($1)
+			AND t.group_id <> $1[1];
+		$$, pinput_table) USING pgroup_id;
+
 
 END
 $function$;
@@ -118,13 +133,13 @@ SELECT * FROM cde_manual_group;
 --
 ---------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION cde_merge_group(pgroup_code character varying []) -- Case 2b works
+CREATE OR REPLACE FUNCTION cde_merge_group(pinput_table TEXT,pgroup_code character varying []) -- Case 2b works
 RETURNS void
 LANGUAGE plpgsql
 AS $function$
 	/*
 	 * Example:
-	 * 	select * from  cde_merge_group(ARRAY['A15.0:ICD10', 'A15.0:ICD10CM','A15:ICD10'])
+	 * 	select * from  cde_merge_group('cde_manual_group',ARRAY['A15.0:ICD10', 'A15.0:ICD10CM','A15:ICD10'])
 	 */
 
 DECLARE
@@ -134,8 +149,11 @@ BEGIN
 		RAISE EXCEPTION 'The group code parameter cannot be null or empty!';
 	END IF;
 
-	INSERT INTO  cde_manual_group (group_id,group_code)
-	SELECT nextval('seq_cde_manual_group_id'), pgroup_code;
+	EXECUTE FORMAT ($$
+			INSERT INTO %1$I (group_id,group_code)
+			SELECT nextval('seq_cde_manual_group_id'), $1;
+		$$, pinput_table) USING pgroup_code;
+
 
 END
 $function$;
@@ -144,7 +162,8 @@ $function$;
 ---------------------------------------------------------------------------------------------
 -- Split group function
 ---------------------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION cde_split_group(pgroup_id int, pgroup_code character varying [] DEFAULT NULL)
+
+CREATE OR REPLACE FUNCTION cde_split_group(pinput_table TEXT,pgroup_id int, pgroup_code character varying [] DEFAULT NULL)
 RETURNS void
 LANGUAGE plpgsql
 AS $function$
@@ -152,60 +171,73 @@ AS $function$
  * Split the group into separate concepts
  * Example:
  * 	--Splits each of the elements that belong to the group_code field into a new groups
- * 		select * from cde_merge_group(2);
+ * 		select * from cde_merge_group('cde_manual_group',2);
  *
  *  --Detach selected group code values into a new group
- *		select * from cde_merge_group(2, ARRAY['Z96.7:ICD10']);
+ *		select * from cde_merge_group('cde_manual_group',2, ARRAY['Z96.7:ICD10']);
  */
 DECLARE
 	V_SEQUENCE_VAL INT;
-
+	z int4;
 BEGIN
-
-	IF NOT EXISTS(SELECT 1 FROM cde_manual_group WHERE group_id = pgroup_id)
-	THEN
-			RAISE EXCEPTION 'The supplied group_id does not exist!';
+	
+	EXECUTE FORMAT ($$
+		SELECT COUNT(*) FROM %1$I WHERE group_id = %2$s;
+		$$, pinput_table, pgroup_id) INTO z;
+	
+	IF pgroup_id IS NULL OR Z <= 0 THEN
+			RAISE EXCEPTION 'The supplied group_id parameter does not exist!';
 	END IF;
 
-	IF pgroup_code IS NOT NULL AND ARRAY_LENGTH(pgroup_code, 1) > 1
-		AND NOT EXISTS (SELECT 1 FROM cde_manual_group WHERE group_id = pgroup_id AND group_code @> pgroup_code)
+	EXECUTE FORMAT ($$
+		SELECT COUNT(*) FROM %1$I WHERE group_id = %2$s AND group_code @> $1;
+		$$, pinput_table, pgroup_id) USING pgroup_code INTO z;
+
+	IF pgroup_code IS NOT NULL AND ARRAY_LENGTH(pgroup_code, 1) > 1 AND Z <= 0
 	THEN
 			RAISE EXCEPTION 'One or more of the elements sent in the group_code parameter does not match the content of group code for group id=%!',pgroup_id;
 	END IF ;
 
 	SELECT LAST_VALUE INTO V_SEQUENCE_VAL FROM seq_cde_manual_group_id;
 
-	WITH cte_touples (group_id,group_code,group_name,tuples)AS(
-		SELECT group_id,
-				group_code, 
-				group_name,
-				regexp_split_to_array(group_code, ':') AS tuples
-		FROM  (
-			SELECT DISTINCT  group_id, group_name,unnest (group_code) as group_code
-			FROM cde_manual_group
-			WHERE group_id = pgroup_id
-			) AS T
-		WHERE (pgroup_code IS NULL OR T.group_code = ANY (pgroup_code))
-	)
-	INSERT INTO cde_manual_group
-	SELECT c.source_code, 
-			c.source_code_description,
-			c.source_vocabulary_id,
-			nextval('seq_cde_manual_group_id') AS group_id,
-			c.group_name,
-			c.group_code 
-	FROM cte_touples t
-	JOIN cde_manual_group c 
-		ON t.tuples[1] = c.source_code
-			AND t.tuples[2] = c.source_vocabulary_id 
-			AND array_length(c.group_code, 1) <= 1;
+	EXECUTE FORMAT ($$
+			WITH cte_touples (group_id,group_code,group_name,tuples)AS(
+			SELECT group_id,
+					group_code, 
+					group_name,
+					regexp_split_to_array(group_code, ':') AS tuples
+			FROM  (
+				SELECT DISTINCT  group_id, group_name,unnest (group_code) as group_code
+				FROM %1$I
+				WHERE group_id = %2$s
+				) AS T
+			WHERE ($1 IS NULL OR T.group_code = ANY ($1))
+		)
+		INSERT INTO cde_manual_group
+		SELECT c.source_code, 
+				c.source_code_description,
+				c.source_vocabulary_id,
+				nextval('seq_cde_manual_group_id') AS group_id,
+				c.group_name,
+				c.group_code 
+		FROM cte_touples t
+		JOIN cde_manual_group c 
+			ON t.tuples[1] = c.source_code
+				AND t.tuples[2] = c.source_vocabulary_id 
+				AND array_length(c.group_code, 1) <= 1;
+		$$, pinput_table, pgroup_id) USING pgroup_code;
+
+
 		
 	IF (SELECT LAST_VALUE FROM seq_cde_manual_group_id) = V_SEQUENCE_VAL THEN
 		RAISE EXCEPTION 'The supplied group_id cannot be splitted, verify that each of the group_code values exists individually!';
 	END IF;
 	
 	IF pgroup_code IS NULL THEN
-		DELETE FROM cde_manual_group WHERE group_id = pgroup_id;
+		EXECUTE FORMAT ($$
+			DELETE FROM %1$I WHERE group_id = %2$s;
+		$$, pinput_table, pgroup_id);
+		
 	END IF;
 
 END;

4f4bcf081ab60d8a482bb2537a906f2cad8d700d
Irina <irina.zherko@odysseusinc.com>
2023-11-01 19:30:09 +0300

    PPI script update

--- PPI/manual_work/Jul_2023/manual_tables.sql
+++ PPI/manual_work/Jul_2023/manual_tables.sql
@@ -1,6 +1,9 @@
 CREATE TABLE concept_manual_backup_10_2023 as SELECT * FROM concept_manual;
+SELECT * FROM concept_manual_backup_10_2023;
 CREATE TABLE concept_relationship_manual_backup_10_2023 as SELECT * FROM concept_relationship_manual;
+SELECT * FROM concept_relationship_manual_backup_10_2023;
 CREATE TABLE concept_synonym_manual_backup_10_2023 as SELECT * FROM concept_synonym_manual;
+SELECT * FROM concept_synonym_manual_backup_10_2023;
 
 --TRUNCATE concept_manual;
 --TRUNCATE concept_relationship_manual;
@@ -30,30 +33,34 @@ NULL AS invalid_reason
 -- insert questions bhp
 INSERT INTO concept_manual
 SELECT DISTINCT
-question_name AS concept_name,
+concept_name,
 'Observation' AS domain_id,
 'PPI' AS vocabulary_id,
 'Question' AS concept_class_id,
 'S' as source_standard_concept,
-question_code as concept_code,
+concept_code,
 CURRENT_DATE AS valid_start_date,
 TO_DATE('20991231','yyyymmdd') AS valid_end_date,
 NULL AS invalid_reason
-from bhp_pr;
+from bhp_pr
+WHERE flag = 'q'
+and concept_code != 'bhp';
 
 --insert answers bhp
 INSERT INTO concept_manual
 SELECT DISTINCT
-answer_name AS concept_name,
+concept_name,
 'Observation' AS domain_id,
 'PPI' AS vocabulary_id,
 'Answer' AS concept_class_id,
 null AS source_standard_concept,
-answer_code AS concept_code,
+concept_code,
 CURRENT_DATE AS valid_start_date,
 TO_DATE('20991231','yyyymmdd') AS valid_end_date,
 NULL AS invalid_reason
-from bhp_pr;
+from bhp_pr
+WHERE flag = 'a'
+and concept_code not in ('pmi_prefernottoanswer', 'pmi_dontknow'); --concepts will be reused;
 
 --insert module ehh
 INSERT INTO concept_manual
@@ -72,32 +79,70 @@ NULL AS invalid_reason
 -- insert questions ehh
 INSERT INTO concept_manual
 SELECT DISTINCT
-question_name AS concept_name,
+concept_name,
 'Observation' AS domain_id,
 'PPI' AS vocabulary_id,
 'Question' AS concept_class_id,
 'S' as source_standard_concept,
-question_code as concept_code,
+concept_code,
 CURRENT_DATE AS valid_start_date,
 TO_DATE('20991231','yyyymmdd') AS valid_end_date,
 NULL AS invalid_reason
-from ehh_pr ;
+FROM ehh_pr
+WHERE flag = 'q'
+and concept_code != 'ehhwb';
 
 --insert answers ehh
 INSERT INTO concept_manual
 SELECT DISTINCT
-answer_name AS concept_name,
+concept_name,
 'Observation' AS domain_id,
 'PPI' AS vocabulary_id,
 'Answer' AS concept_class_id,
 null AS source_standard_concept,
-answer_code AS concept_code,
+concept_code,
 CURRENT_DATE AS valid_start_date,
 TO_DATE('20991231','yyyymmdd') AS valid_end_date,
 NULL AS invalid_reason
-from ehh_pr;
+FROM ehh_pr
+WHERE flag = 'a'
+and concept_code not in ('pmi_prefernottoanswer', 'pmi_dontknow', 'pmi_none'); --concepts will be reused
+
+
 
 -- 3. insert new relationships
+--to add hierarchy 'Has PPI parent code' from Questions to Module
+INSERT INTO concept_relationship_manual
+SELECT DISTINCT
+concept_code AS concept_code_1,
+'bhp' AS concept_code_2,
+'PPI' AS vocabulary_id_1,
+'PPI' AS vocabulary_id_2,
+'Has PPI parent code' AS relationship_id,
+CURRENT_DATE AS valid_start_date,
+TO_DATE('20991231','yyyymmdd') AS valid_end_date,
+null as invalid_reason
+FROM bhp_pr
+WHERE flag = 'q'
+AND concept_code != 'bhp'
+;
+
+--to add hierarchy 'Has PPI parent code' from Questions to Module
+INSERT INTO concept_relationship_manual
+SELECT DISTINCT
+concept_code AS concept_code_1,
+'ehhwb' AS concept_code_2,
+'PPI' AS vocabulary_id_1,
+'PPI' AS vocabulary_id_2,
+'Has PPI parent code' AS relationship_id,
+CURRENT_DATE AS valid_start_date,
+TO_DATE('20991231','yyyymmdd') AS valid_end_date,
+null as invalid_reason
+FROM ehh_pr
+WHERE flag = 'q'
+AND concept_code != 'ehh'
+;
+
 --to add hierarchy 'Has PPI parent code' from Answers to Questions
 INSERT INTO concept_relationship_manual
 SELECT DISTINCT
@@ -109,35 +154,7 @@ question_code as concept_code_2,
 CURRENT_DATE AS valid_start_date,
 TO_DATE('20991231','yyyymmdd') AS valid_end_date,
 null as invalid_reason
-FROM bhp_pr;
-
---to add hierarchy 'Has PPI parent code' from Questions to Module
-INSERT INTO concept_relationship_manual
-SELECT DISTINCT
-question_code AS concept_code_1,
-'bhp' AS concept_code_2,
-'PPI' AS vocabulary_id_1,
-'PPI' AS vocabulary_id_2,
-'Has PPI parent code' AS relationship_id,
-CURRENT_DATE AS valid_start_date,
-TO_DATE('20991231','yyyymmdd') AS valid_end_date,
-null as invalid_reason
-FROM bhp_pr
-;
-
---add hierarchy 'Has answer (PPI)' from  Questions to Answers
-INSERT INTO concept_relationship_manual
-SELECT DISTINCT
-question_code AS concept_code_1,
-answer_code AS concept_code_2,
-'PPI' AS vocabulary_id_1,
-'PPI' AS vocabulary_id_2,
-'Has answer (PPI)' AS relationship_id,
-CURRENT_DATE AS valid_start_date,
-TO_DATE('20991231','yyyymmdd') AS valid_end_date,
-null as invalid_reason
-FROM bhp_pr
-;
+FROM bhp_qa;
 
 --to add hierarchy 'Has PPI parent code' from Answers to Questions
 INSERT INTO concept_relationship_manual
@@ -150,35 +167,9 @@ question_code as concept_code_2,
 CURRENT_DATE AS valid_start_date,
 TO_DATE('20991231','yyyymmdd') AS valid_end_date,
 null as invalid_reason
-FROM ehh_pr;
+FROM ehh_qa;
 
---to add hierarchy 'Has PPI parent code' from Questions to Module
-INSERT INTO concept_relationship_manual
-SELECT DISTINCT
-question_code AS concept_code_1,
-'bhp' AS concept_code_2,
-'PPI' AS vocabulary_id_1,
-'PPI' AS vocabulary_id_2,
-'Has PPI parent code' AS relationship_id,
-CURRENT_DATE AS valid_start_date,
-TO_DATE('20991231','yyyymmdd') AS valid_end_date,
-null as invalid_reason
-FROM ehh_pr
-;
 
---add hierarchy 'Has answer (PPI)' from  Questions to Answers
-INSERT INTO concept_relationship_manual
-SELECT DISTINCT
-question_code AS concept_code_1,
-answer_code AS concept_code_2,
-'PPI' AS vocabulary_id_1,
-'PPI' AS vocabulary_id_2,
-'Has answer (PPI)' AS relationship_id,
-CURRENT_DATE AS valid_start_date,
-TO_DATE('20991231','yyyymmdd') AS valid_end_date,
-null as invalid_reason
-FROM ehh_pr
-;
 
 -- 4. insert concept synonyms from manual file
 
--- PPI/manual_work/Jul_2023/process_source_table.sql
+++ PPI/manual_work/Jul_2023/process_source_table.sql
@@ -21,10 +21,77 @@ Matrix_Ranking	varchar,
 Field_Annotation	varchar);
 
 SELECT * FROM bhp; -- 69
+CREATE TABLE bhp_pr_back as SELECT * FROM bhp_pr;
 
---Process BHP source table
+--Process answers
+DROP TABLE answers;
+CREATE TABLE answers as
+    SELECT DISTINCT
+           SPLIT_PART (TRIM(REGEXP_SPLIT_TO_TABLE(choices_calculations_or_slider_labels, '\|')), ', ', 1)  AS answer_code,
+           SPLIT_PART (TRIM(REGEXP_SPLIT_TO_TABLE(choices_calculations_or_slider_labels, '\|')), ', ', 2)  AS answer_name
+    FROM bhp;
+
+SELECT * FROM answers;
+
+--Processed BHP source table
 DROP TABLE bhp_pr;
-CREATE TABLE bhp_pr as (
+TRUNCATE TABLE ABLE bhp_pr;
+CREATE TABLE bhp_pr (
+    concept_code varchar,
+    concept_name varchar,
+    field_type varchar,
+    flag varchar);
+
+--Question insertion
+INSERT INTO bhp_pr (concept_code,
+                    concept_name,
+                    field_type,
+                    flag)
+SELECT DISTINCT variable_field_name,
+                field_label,
+                field_type,
+                'q' as flag
+FROM bhp;
+
+-- Cut concept names longer than 255
+UPDATE bhp_pr SET concept_name = 'Some people have a lot of fear about things like going out of the house alone, being in a crowd, going over bridges, or traveling by bus. Were you ever in your life frightened by any of these situations?'
+WHERE concept_code = 'cidi5_30';
+UPDATE bhp_pr SET concept_name = 'Have you ever been bothered with thoughts, images, or urges that kept coming back over and over, like concerns with germs, order, or experiencing horrific images or intrusive sexual thoughts, or urges to knock objects, or harming a loved one?'
+WHERE concept_code = 'pmi_1';
+UPDATE bhp_pr SET concept_name = 'Did you ever talk to a health professional about any of these experiences (such as seeing a vision, hearing a voice, believing that something strange was trying to communicate with you)?'
+WHERE concept_code = 'mhqukb_55';
+UPDATE bhp_pr SET concept_name = 'Attention and Focus Everyone has different abilities to pay attention and get things done. Answering these next questions may help researchers learn more about the brain and attention. During the past 6 months,'
+WHERE concept_code = 'attention_focus';
+UPDATE bhp_pr SET concept_name = 'Experiences of Panic and Anxiety The next section asks about panic attacks. Your answers to these next questions may help researchers understand how to better prevent and treat these attacks.'
+WHERE concept_code = 'panic_anxiety';
+UPDATE bhp_pr SET concept_name = 'Shifts in Mood, Energy, and Activity People often experience changes in their mood or energy levels. The next questions ask about unusual moods you may have had. '
+WHERE concept_code = 'mood_energy';
+UPDATE bhp_pr SET concept_name = 'Feelings of Fear in Certain Situations For some people, being in social settings can make them feel a lot of fear and anxiety. Your answers may help researchers better identify and help people with social anxiety disorder or agoraphobia. '
+WHERE concept_code = 'social_anxiety';
+UPDATE bhp_pr SET concept_name = 'Unusual Experiences and Perceptions. Your answers could help researchers better understand why these experiences happen and how to help people who have them. With that in mind, did you ever in your life have any of the following experiences?'
+WHERE concept_code = 'unusual_experiences';
+UPDATE bhp_pr SET concept_name = 'Recurring Thoughts and Behaviors Everyone double-checks things. In the next section, we ask about thoughts and behaviors that may be hard to control and can cause anxiety.'
+WHERE concept_code = 'recurring_thoughts';
+UPDATE bhp_pr SET concept_name = 'The following questions ask about your behavioral health and personality. Some of the questions may be sensitive. You can choose not to answer any question.'
+WHERE concept_code = 'bhp_intro';
+
+--Answers insertion
+INSERT INTO bhp_pr (concept_code,
+                    concept_name,
+                    field_type,
+                    flag)
+SELECT DISTINCT answer_code,
+                answer_name,
+                NULL as field_type,
+                'a' as flag
+FROM answers;
+
+SELECT * FROM bhp_pr; --110
+DELETE FROM bhp_pr WHERE concept_code = 'bhp_44' and concept_name = 'Enter number'; -- to preserve only one variant for bhp_44
+
+-- table with q-a pairs
+DROP TABLE bhp_qa;
+CREATE TABLE bhp_qa as (
 WITH a
 AS
 (SELECT *,
@@ -35,52 +102,14 @@ AS
      field_label as question_name,
      SPLIT_PART(answer, ',', 1) AS answer_code,
      trim(SPLIT_PART(answer, ',', 2)) AS answer_name
-     FROM a) ;
+     FROM a);
 
---Precoordinated answer concepts
---SELECT DISTINCT concat (question_code, '-', answer_code), concat (question_name, '-', answer_name) FROM bhp_pr;
---UPDATE bhp_pr SET answer_name = concat (question_name, '-', answer_name);
---UPDATE bhp_pr SET answer_code = concat (question_code, '-', answer_code);
+SELECT * FROM bhp_qa;
+UPDATE bhp_qa SET answer_code = 'PMI_DontKnow' WHERE answer_code = 'pmi_dontknow';
+UPDATE bhp_qa SET answer_code = 'PMI_PreferNotToAnswer' WHERE answer_code = 'pmi_prefernottoanswer';
 
-UPDATE bhp_pr SET answer_code = trim(answer_code) ;
-UPDATE bhp_pr SET question_code = trim(question_code) ;
-UPDATE bhp_pr SET answer_name = trim(answer_name) ;
-UPDATE bhp_pr SET question_name = trim(question_name) ;
 
-SELECT * FROM bhp_pr; --209
-
-SELECT DISTINCT question_code, question_name FROM dev_ppi.bhp_pr where length (question_name) > 255;
-
-/*UPDATE bhp_pr SET question_name = 'Some people have a lot of fear about things like going out of the house alone, being in a crowd, going over bridges, or traveling by bus. Were you ever in your life frightened by any of these situations?'
-WHERE question_code = 'cidi5_30';
-UPDATE bhp_pr SET question_name = 'Have you ever been bothered with thoughts, images, or urges that kept coming back over and over, like concerns with germs, order, or experiencing horrific images or intrusive sexual thoughts, or urges to knock objects, or harming a loved one?'
-WHERE question_code = 'pmi_1';
-UPDATE bhp_pr SET question_name = 'Did you ever talk to a health professional about any of these experiences (such as seeing a vision, hearing a voice, believing that something strange was trying to communicate with you)?'
-WHERE question_code = 'mhqukb_55';*/
-
-SELECT DISTINCT answer_code, answer_name FROM bhp_pr where length (answer_name) > 255;
-
-/*UPDATE bhp_pr SET answer_name = 'Some people have a lot of fear about things like going out of the house alone, being in a crowd, going over bridges, or traveling by bus. Were you ever in your life frightened by any of these situations?-Yes'
-WHERE answer_code = 'cidi5_30-bhp_6';
-UPDATE bhp_pr SET answer_name = 'Some people have a lot of fear about things like going out of the house alone, being in a crowd, going over bridges, or traveling by bus. Were you ever in your life frightened by any of these situations?-No'
-WHERE answer_code = 'cidi5_30-bhp_7';
-UPDATE bhp_pr SET answer_name = 'How distressing did you find having any of these experiences (such as seeing a vision, hearing a voice)?-Not distressing at all. It was a positive experience'
-WHERE answer_code = 'mhqukb_54-bhp_96';
-UPDATE bhp_pr SET answer_name = 'Did you ever talk to a health professional about any of these experiences (such as seeing a vision, hearing a voice, believing that something strange was trying to communicate with you)?-Yes'
-WHERE answer_code = 'mhqukb_55-bhp_6';
-UPDATE bhp_pr SET answer_name = 'Did you ever talk to a health professional about any of these experiences (such as seeing a vision, hearing a voice, believing that something strange was trying to communicate with you)?-No'
-WHERE answer_code = 'mhqukb_55-bhp_7';
-UPDATE bhp_pr SET answer_name = 'Did you ever talk to a health professional about any of these experiences (such as seeing a vision, hearing a voice, believing that something strange was trying to communicate with you)?-Don''t know'
-WHERE answer_code = 'mhqukb_55-pmi_dontknow';
-UPDATE bhp_pr SET answer_name = 'Did you ever talk to a health professional about any of these experiences (such as seeing a vision, hearing a voice, believing that something strange was trying to communicate with you)?-Prefer not to answer'
-WHERE answer_code = 'mhqukb_55-pmi_prefernottoanswer';
-UPDATE bhp_pr SET answer_name = 'Were you ever prescribed a medication by a health professional for any of these experiences (such as seeing a vision, hearing a voice)? -Prefer not to answer'
-WHERE answer_code = 'mhqukb_56-pmi_prefernottoanswer';
-UPDATE bhp_pr SET answer_name = 'Have you ever been bothered with thoughts, images, or urges that kept coming back over and over, like concerns with germs, order, or experiencing horrific images or intrusive sexual thoughts, or urges to knock objects, or harming a loved one?-Yes'
-WHERE answer_code = 'pmi_1-bhp_6';
-UPDATE bhp_pr SET answer_name = 'Have you ever been bothered with thoughts, images, or urges that kept coming back over and over, like concerns with germs, order, or experiencing horrific images or intrusive sexual thoughts, or urges to knock objects, or harming a loved one?-No'
-WHERE answer_code = 'pmi_1-bhp_7';*/
-
+SELECT DISTINCT concept_code, concept_name FROM dev_ppi.bhp_pr where length (concept_name) > 255;
 
 -- EHH (Emotional Health history and Wellbeing) source table
 TRUNCATE TABLE ehh;
@@ -105,10 +134,70 @@ Matrix_Ranking	varchar,
 Field_Annotation	varchar) ;
 
 SELECT * FROM ehh; --122
+CREATE TABLE ehh_pr_back as SELECT * FROM ehh_pr;
 
---Process EHH source table
+--Process answers
+DROP TABLE ehh_answers;
+CREATE TABLE ehh_answers as
+    SELECT DISTINCT
+           SPLIT_PART (TRIM(REGEXP_SPLIT_TO_TABLE(choices_calculations_or_slider_labels, '\|')), ', ', 1)  AS answer_code,
+           SPLIT_PART (TRIM(REGEXP_SPLIT_TO_TABLE(choices_calculations_or_slider_labels, '\|')), ', ', 2)  AS answer_name
+    FROM ehh;
+
+SELECT * FROM ehh_answers;
+DELETE FROM ehh_answers WHERE answer_code = 'ehhwb_10' and answer_name = 'More than half of the days'; -- to preserve only one variant
+DELETE FROM ehh_answers WHERE answer_code = 'pmi_dontknow' and answer_name = 'Don''t know/Not sure';-- to preserve only one variant
+
+--Processed BHP source table
 DROP TABLE ehh_pr;
-CREATE TABLE ehh_pr as (
+TRUNCATE TABLE ehh_pr;
+CREATE TABLE ehh_pr (
+    concept_code varchar,
+    concept_name varchar,
+    field_type varchar,
+    flag varchar);
+
+--Question insertion
+INSERT INTO ehh_pr (concept_code,
+                    concept_name,
+                    field_type,
+                    flag)
+SELECT DISTINCT variable_field_name,
+                field_label,
+                field_type,
+                'q' as flag
+FROM ehh;
+
+--Answers insertion
+INSERT INTO ehh_pr (concept_code,
+                    concept_name,
+                    field_type,
+                    flag)
+SELECT DISTINCT answer_code,
+                answer_name,
+                NULL as field_type,
+                'a' as flag
+FROM ehh_answers;
+
+SELECT * FROM ehh_pr; --206
+
+SELECT DISTINCT concept_code, concept_name FROM dev_ppi.ehh_pr where length (concept_name) > 255;
+SELECT * FROM dev_ppi.ehh where length (field_label) > 255;
+
+UPDATE ehh_pr SET concept_name = 'Self-Harm Suicide is often preventable. Your answers may help researchers find better ways to help those with thoughts of harming themselves. '
+WHERE concept_code = 'self_harm';
+UPDATE ehh_pr SET concept_name = 'The following questions ask about your emotional health and well-being. If you aren''t sure how to answer a question, choose the best answer from the options given. '
+WHERE concept_code = 'ehhwb_intro';
+UPDATE ehh_pr SET concept_name = 'Feelings of General Anxiety and Worry Everyone worries from time to time. Over the last 2 weeks, how often have you been bothered by the following problems?'
+WHERE concept_code = 'anxiety_worry';
+UPDATE ehh_pr SET concept_name = 'Mood and Sadness Everyone experiences sadness every now and then. Over the last 2 weeks, how often have you been bothered by any of the following problems?'
+WHERE concept_code = 'mood_sadness';
+UPDATE ehh_pr SET concept_name = 'Experiences with Trauma Some people go through stressful and upsetting events during their life. In this section we will be asking you about your experiences with trauma in childhood (before you were 18 years old).'
+WHERE concept_code = 'trauma';
+
+--table with q-a pairs
+DROP TABLE ehh_qa;
+CREATE TABLE ehh_qa as (
 WITH a
 AS
 (SELECT *,
@@ -120,21 +209,9 @@ AS
      SPLIT_PART(answer, ',', 1) AS answer_code,
      trim(SPLIT_PART(answer, ',', 2)) AS answer_name
      FROM a) ;
+SELECT * FROM ehh_qa;
 
---Precoordinated answer concepts
---SELECT DISTINCT answer_code, answer_name FROM ehh_pr;
---UPDATE ehh_pr SET answer_name = concat (question_name, '-', answer_name);
---UPDATE ehh_pr SET answer_code = concat (question_code, '-', answer_code);
-
-update ehh_pr set answer_code = trim(answer_code) ;
-update ehh_pr set question_code = trim(question_code) ;
-update ehh_pr set answer_name = trim(answer_name) ;
-update ehh_pr set question_name = trim(question_name) ;
-
-SELECT * FROM ehh_pr; --408
-
-SELECT DISTINCT question_code, question_name FROM ehh_pr where length (question_name) > 255;
-SELECT DISTINCT answer_code, answer_name FROM ehh_pr where length (answer_name) > 255;
-
+UPDATE ehh_qa SET answer_code = 'PMI_DontKnow' WHERE answer_code = 'pmi_dontknow';
+UPDATE ehh_qa SET answer_code = 'PMI_PreferNotToAnswer' WHERE answer_code = 'pmi_prefernottoanswer';
 
 

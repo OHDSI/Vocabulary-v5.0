fa3672af9e41f7c00d234bcb2d3a99edf0e70ff6
Irina <irina.zherko@odysseusinc.com>
2023-11-07 15:42:51 +0300

    CIM10 load stage script update

--- CIM10/load_stage.sql
+++ CIM10/load_stage.sql
@@ -258,6 +258,189 @@ FROM concept c
 WHERE c.concept_code = cs.concept_code
 	AND c.vocabulary_id = 'ICD10';
 
+--16. Translate the rest of names
+DROP TABLE cim10_translated_source;
+TRUNCATE TABLE cim10_translated_source;
+CREATE TABLE cim10_translated_source
+(concept_code text,
+concept_name  text,
+concept_name_translated text);
+
+INSERT INTO  cim10_translated_source
+    SELECT concept_code,
+           concept_name,
+           null as concept_name_translated
+    FROM concept_stage
+where concept_code not in (SELECT concept_code FROM concept c where c.vocabulary_id = 'ICD10')
+;
+
+--Translation
+DO $_$
+BEGIN
+	PERFORM google_pack.GTranslate(
+		pInputTable    =>'cim10_translated_source',
+		pInputField    =>'concept_name',
+		pOutputField   =>'concept_name_translated',
+		pDestLang      =>'en',
+	    pSrcLang       =>'fr'
+	);
+END $_$;
+
+UPDATE cim10_translated_source
+SET concept_name_translated = cim10_translated_source.concept_name_translated ||' (machine translation)';
+
+WITH cut as (
+SELECT
+       ts.concept_name,
+       CASE WHEN LENGTH(TRIM(concept_name_translated)) > 255
+			THEN TRIM(SUBSTR(TRIM(concept_name_translated), 1, 252)) || '...'
+		ELSE TRIM(concept_name_translated) END as cut_name
+FROM cim10_translated_source ts)
+
+UPDATE concept_stage cs
+SET concept_name = cut.cut_name
+FROM cut
+WHERE cs.concept_name = cut.concept_name;
+
+--Manual fix for names with '!ERROR: %' patterns
+UPDATE concept_stage
+SET concept_name = 'Passenger of a car injured in a collision with a cycle, non-traffic accident, while participating in an unspecified activity (machine translation)'
+WHERE concept_code = 'V41.19';
+
+UPDATE concept_stage
+SET concept_name = 'Van driver injured in collision with train or rail vehicle, traffic accident, while resting, sleeping, eating or participating in other essential activities (machine translation)'
+WHERE concept_code = 'V55.54';
+
+UPDATE concept_stage
+SET concept_name = 'Occupant of a bus injured in a transport accident, without collision, going up or down, while participating in other specified activities (machine translation)'
+WHERE concept_code = 'V78.48';
+
+UPDATE concept_stage
+SET concept_name = 'Accident of other motorboats causing other traumatic injuries, while practicing a sport (machine translation)'
+WHERE concept_code = 'V91.30';
+
+UPDATE concept_stage
+SET concept_name = 'Accident of other private fixed-wing aircraft injuring an occupant, while participating in an unspecified activity (machine translation)'
+WHERE concept_code = 'V95.29';
+
+UPDATE concept_stage
+SET concept_name = 'Fall from a chair, industrial premises and construction site, while carrying out work for profit (machine translation)'
+WHERE concept_code = 'W07.62';
+
+UPDATE concept_stage
+SET concept_name = 'Fall from a chair, farm (machine translation)'
+WHERE concept_code = 'W07.7';
+
+UPDATE concept_stage
+SET concept_name = 'Falling from the top of a building or other structure, street or road, while resting, sleeping, eating or participating in other essential activities (machine translation)'
+WHERE concept_code = 'W13.44';
+
+UPDATE concept_stage
+SET concept_name = 'Occupant, unspecified, of a heavy vehicle, injured in a collision with motor vehicles, other and unspecified, non-traffic accident, while participating in an unspecified activity (machine translation)'
+WHERE concept_code = 'V69.29';
+
+UPDATE concept_stage
+SET concept_name = 'Water transport accident involving a passenger liner, other and unspecified, while participating in other specified activities (machine translation)'
+WHERE concept_code = 'V94.18';
+
+UPDATE concept_stage
+SET concept_name = 'Dementia of Alzheimer''s disease, atypical or mixed form G30.8, with other symptoms, mostly depressive, severe (machine translation)'
+WHERE concept_code = 'F00.232';
+
+UPDATE concept_stage
+SET concept_name = 'Compression, crushing or blocking in objects or between objects, collective establishment (machine translation)'
+WHERE concept_code = 'W23.1';
+
+UPDATE concept_stage
+SET concept_name = 'Compression, crushing or jamming in or between objects, area of commerce, while participating in play and leisure activities (machine translation)'
+WHERE concept_code = 'W23.51';
+
+UPDATE concept_stage
+SET concept_name = 'Contact with sharp glass, home, while participating in other specified activities (machine translation)'
+WHERE concept_code = 'W25.08';
+
+UPDATE concept_stage
+SET concept_name = '*** SU17 *** Contact with a knife, sword or dagger, agricultural operation (machine translation)'
+WHERE concept_code = 'W26.7';
+
+UPDATE concept_stage
+SET concept_name = 'Exposure to a high pressure jet, industrial premises and construction site, by participating in a game and leisure activities (machine translation)'
+WHERE concept_code = 'W41.61';
+
+UPDATE concept_stage
+SET concept_name = 'Drowning and submersion in a bathtub, collective establishment, while practicing a sport (machine translation)'
+WHERE concept_code = 'W65.10';
+
+UPDATE concept_stage
+SET concept_name = 'Drowning and submersion following a fall into natural waters, home, while participating in play and leisure activities (machine translation)'
+WHERE concept_code = 'W70.01';
+
+UPDATE concept_stage
+SET concept_name = 'Exposure to ignition of a highly flammable substance, street or road (machine translation)'
+WHERE concept_code = 'X04.4';
+
+UPDATE concept_stage
+SET concept_name = 'Exposure to other specified smoke, fires and flames, other specified locations, while practicing sport (machine translation)'
+WHERE concept_code = 'X08.80';
+
+UPDATE concept_stage
+SET concept_name = 'Contact with boiling water from a tap, industrial premises and construction site (machine translation)'
+WHERE concept_code = 'X11.6';
+
+UPDATE concept_stage
+SET concept_name = 'Accidental poisoning by analgesics, antipyretics and antirheumatic drugs, non-opiates and exposure to these products, industrial premises and construction site, while participating in an unspecified activity (machine translation)'
+WHERE concept_code = 'X40.69';
+
+UPDATE concept_stage
+SET concept_name = 'Accidental poisoning by non-opioid analgesics, antipyretics and antirheumatic drugs and exposure to these products, farming, while carrying out other forms of work (machine translation)'
+WHERE concept_code = 'X40.73';
+
+UPDATE concept_stage
+SET concept_name = 'Accidental poisoning by other gases and vapors while working for profit (machine translation)'
+WHERE concept_code = 'X47.8+2';
+
+UPDATE concept_stage
+SET concept_name = 'Self-intoxication with and exposure to non-opioid analgesics, antipyretics, and antirheumatic drugs, at home, while participating in other specified activities (machine translation)'
+WHERE concept_code = 'X60.08';
+
+UPDATE concept_stage
+SET concept_name = 'Self-inflicted injury by use of explosive material, collective establishment, while performing work for profit (machine translation)'
+WHERE concept_code = 'X75.12';
+
+UPDATE concept_stage
+SET concept_name = 'Self-inflicted injury by use of blunt object, industrial premises and construction site, while resting, sleeping, eating or participating in other essential activities (machine translation)'
+WHERE concept_code = 'X79.64';
+
+UPDATE concept_stage
+SET concept_name = 'Assault by carbon monoxide from domestic gas, industrial premises and construction sites, while working for profit (machine translation)'
+WHERE concept_code = 'X88.162';
+
+UPDATE concept_stage
+SET concept_name = 'Attack by other specified chemicals and harmful products, home (machine translation)'
+WHERE concept_code = 'X89.0';
+
+UPDATE concept_stage
+SET concept_name = 'Assault by explosive material, other specified locations (machine translation)'
+WHERE concept_code = 'X96.8';
+
+UPDATE concept_stage
+SET concept_name = 'Carbon monoxide poisoning from unspecified source, undetermined intent, industrial premises and construction site, while resting, sleeping, eating or participating in other essential activities (machine translation)'
+WHERE concept_code = 'Y17.464';
+
+UPDATE concept_stage
+SET concept_name = 'Discharge of a handgun, intent undetermined, location unspecified, while practicing a sport (machine translation)'
+WHERE concept_code = 'Y22.90';
+
+UPDATE concept_stage
+SET concept_name = 'Contact with water vapor, gases and burning objects, intention not determined, other places specified, while participating in a game and leisure activities (machine translation)'
+WHERE concept_code = 'Y27.81';
+
+--concept_synonym_stage update
+UPDATE concept_synonym_stage
+SET synonym_name = css.synonym_name ||', ' || ts.concept_name_translated
+FROM concept_synonym_stage css LEFT JOIN cim10_translated_source ts on css.synonym_concept_code = ts.concept_code
+WHERE LENGTH(TRIM(ts.concept_name_translated)) > 255;
+
 --16. Working with concept_manual table
 DO $_$
 BEGIN

11112521a78e27fdf646ea6ba56fd53c909a2cfa
Irina <irina.zherko@odysseusinc.com>
2023-12-21 01:58:56 +0300

    ICD_CDE scripts update

--- ICD_CDE/ICD_CDE_source.sql
+++ ICD_CDE/ICD_CDE_source.sql
@@ -26,8 +26,9 @@ CREATE TABLE dev_icd10.icd_cde_source
 );
 
 -- Run concept_stage for every Vocabulary to be included into the CDE
--- Insert the whole source with existing mappings into the CDE.
--- If there are several mapping sources all the versions should be included, excluding duplicates within one vocabularby.
+-- Insert the whole source with existing mappings into the CDE. We want to preserve mappings to non-S or non valid concepts at this stage.
+-- Concepts, that are not supposed to have mappings are not included
+-- If there are several mapping sources all the versions should be included, excluding duplicates within one vocabulary.
 -- Mapping duplicates between vocabularies are preserved
 
 --ICD10 with mappings
@@ -67,12 +68,15 @@ SELECT cs.concept_code     as source_code,
 FROM dev_icd10.concept_stage cs
 LEFT JOIN dev_icd10.concept_relationship_stage crs
     on cs.concept_code = crs.concept_code_1
-    and relationship_id in ('Maps to', 'Maps to value')
+    and crs.relationship_id in ('Maps to', 'Maps to value')
+    and crs.invalid_reason is null
 LEFT JOIN concept c
     on crs.concept_code_2 = c.concept_code
-    --and c.standard_concept = 'S'
+    and crs.vocabulary_id_2 = c.vocabulary_id
+    --and c.standard_concept = 'S' -- to preserve mappings to non-S, not valid concepts
     --and c.invalid_reason is null
-where cs.concept_class_id != 'ICD10 Hierarchy';
+where cs.concept_class_id not in ('ICD10 Chapter','ICD10 SubChapter', 'ICD10 Hierarchy')
+;
 
 --ICD10CM with mappings
 INSERT INTO icd_cde_source (source_code,
@@ -94,7 +98,7 @@ INSERT INTO icd_cde_source (source_code,
 -- Check Select before insertion --135145 S valid, 263603 non-S valid, 266773 non-S not-valid
 SELECT cs.concept_code     as source_code,
        cs.concept_name     as source_code_description,
-       'ICD10CM'             as source_vocabulary_id,
+       'ICD10CM'           as source_vocabulary_id,
        null                as group_name,
        null                as group_id,
        null                as medium_group_id,
@@ -112,8 +116,54 @@ FROM dev_icd10cm.concept_stage cs
 LEFT JOIN dev_icd10cm.concept_relationship_stage crs
     on cs.concept_code = crs.concept_code_1
     and relationship_id in ('Maps to', 'Maps to value')
+    and crs.invalid_reason is null
 LEFT JOIN concept c
     on crs.concept_code_2 = c.concept_code
+    and crs.vocabulary_id_2 = c.vocabulary_id
     --and c.standard_concept = 'S'
-    --and c.invalid_reason is null;
+    --and c.invalid_reason is null
+    ;
+
+--potential replacement mapping insertion
+INSERT INTO icd_cde_source (source_code,
+                            source_code_description,
+                            source_vocabulary_id,
+                            relationship_id,
+                            target_concept_id,
+                            target_concept_code,
+                            target_concept_name,
+                            target_concept_class_id,
+                            target_standard_concept,
+                            target_invalid_reason,
+                            target_domain_id,
+                            target_vocabulary_id,
+                            mappings_origin)
+with mis_map as
+(SELECT * FROM icd_cde_source
+WHERE target_concept_id is not null
+AND target_standard_concept is null) -- 3719
+       SELECT DISTINCT m.source_code,
+              m.source_code_description,
+              m.source_vocabulary_id,
+              cr.relationship_id,
+              c.concept_id as target_concept_id,
+              c.concept_code as target_concept_code,
+              c.concept_name as target_concept_name,
+              c.concept_class_id as target_concept_class_id,
+              c.standard_concept as target_standard_concept,
+              c.invalid_reason as target_invalid_reason,
+              c.domain_id as target_domain_id,
+              c.vocabulary_id as target_vocabulary_id,
+              'replaced through relationships' as mapping_origin
+       FROM mis_map m JOIN concept_relationship cr
+       ON m.target_concept_id = cr.concept_id_1
+       JOIN concept c
+       ON cr.concept_id_2 = c.concept_id
+       AND cr.relationship_id in ('Maps to', 'Maps to value', 'Concept replaced by')
+       AND c.standard_concept = 'S'
+       AND c.invalid_reason is null;
+
+--strict group (identical source_code_description and same codes with identical mappings)
+
+
 

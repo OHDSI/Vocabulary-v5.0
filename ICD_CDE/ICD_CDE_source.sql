387ebf1cf9f19fbf037c4910fd70c682ea4fc00e
Irina <irina.zherko@odysseusinc.com>
2023-12-21 23:49:24 +0300

    ICD_CDE scripts update

--- ICD_CDE/ICD_CDE_source.sql
+++ ICD_CDE/ICD_CDE_source.sql
@@ -7,7 +7,7 @@ CREATE TABLE dev_icd10.icd_cde_source
     source_code_description varchar(255),
     source_vocabulary_id    varchar(20),
     group_name              varchar(255),
-    group_id                integer,
+    group_id                SERIAL PRIMARY KEY ,
     --group_code              varchar, -- group code is dynamic and is assembled after grouping just before insertion data into the google sheet
     medium_group_id         integer,
     --medium_group_code       varchar,
@@ -25,10 +25,10 @@ CREATE TABLE dev_icd10.icd_cde_source
     mappings_origin         varchar
 );
 
--- Run concept_stage for every Vocabulary to be included into the CDE
+-- Run load_stage for every Vocabulary to be included into the CDE
 -- Insert the whole source with existing mappings into the CDE. We want to preserve mappings to non-S or non valid concepts at this stage.
 -- Concepts, that are not supposed to have mappings are not included
--- If there are several mapping sources all the versions should be included, excluding duplicates within one vocabulary.
+-- If there are several mapping sources all the versions should be included, excluding duplicates within one vocabulary. --Ask Mikita
 -- Mapping duplicates between vocabularies are preserved
 
 --ICD10 with mappings
@@ -36,7 +36,6 @@ INSERT INTO icd_cde_source (source_code,
                             source_code_description,
                             source_vocabulary_id,
                             group_name,
-                            group_id,
                             medium_group_id,
                             broad_group_id,
                             relationship_id,
@@ -53,7 +52,6 @@ SELECT cs.concept_code     as source_code,
        cs.concept_name     as source_code_description,
        'ICD10'             as source_vocabulary_id,
        null                as group_name,
-       null                as group_id,
        null                as medium_group_id,
        null                as broad_group_id,
        crs.relationship_id as relationship_id,
@@ -69,7 +67,7 @@ FROM dev_icd10.concept_stage cs
 LEFT JOIN dev_icd10.concept_relationship_stage crs
     on cs.concept_code = crs.concept_code_1
     and crs.relationship_id in ('Maps to', 'Maps to value')
-    and crs.invalid_reason is null
+    --and crs.invalid_reason is null -- we want to have D relationships in the CDE
 LEFT JOIN concept c
     on crs.concept_code_2 = c.concept_code
     and crs.vocabulary_id_2 = c.vocabulary_id
@@ -83,7 +81,6 @@ INSERT INTO icd_cde_source (source_code,
                             source_code_description,
                             source_vocabulary_id,
                             group_name,
-                            group_id,
                             medium_group_id,
                             broad_group_id,
                             relationship_id,
@@ -100,7 +97,6 @@ SELECT cs.concept_code     as source_code,
        cs.concept_name     as source_code_description,
        'ICD10CM'           as source_vocabulary_id,
        null                as group_name,
-       null                as group_id,
        null                as medium_group_id,
        null                as broad_group_id,
        crs.relationship_id as relationship_id,
@@ -116,7 +112,7 @@ FROM dev_icd10cm.concept_stage cs
 LEFT JOIN dev_icd10cm.concept_relationship_stage crs
     on cs.concept_code = crs.concept_code_1
     and relationship_id in ('Maps to', 'Maps to value')
-    and crs.invalid_reason is null
+    -- and crs.invalid_reason is null -- we want to have D relationships in the CDE
 LEFT JOIN concept c
     on crs.concept_code_2 = c.concept_code
     and crs.vocabulary_id_2 = c.vocabulary_id
@@ -159,11 +155,37 @@ AND target_standard_concept is null) -- 3719
        ON m.target_concept_id = cr.concept_id_1
        JOIN concept c
        ON cr.concept_id_2 = c.concept_id
-       AND cr.relationship_id in ('Maps to', 'Maps to value', 'Concept replaced by')
+       AND cr.relationship_id in ('Maps to', 'Maps to value', 'Concept replaced by') -- add other rel_id
        AND c.standard_concept = 'S'
        AND c.invalid_reason is null;
 
 --strict group (identical source_code_description and same codes with identical mappings)
+DROP TABLE grouped;
+CREATE TABLE grouped as (
+SELECT c.source_code as source_code_1,
+       c.source_code_description as source_code_description_1,
+       c.source_vocabulary_id as source_vocabulary_1,
+       c.group_id as group_id_1,
+       c1.source_code as source_code,
+       c1.source_code_description as source_code_description,
+       c1.source_vocabulary_id as source_vocabulary_id,
+       c1.group_id as group_id,
+       c1.source_code_description as group_name
+FROM dev_icd10.icd_cde_source c
+    JOIN dev_icd10.icd_cde_source c1
+    ON c.source_code_description = c1.source_code_description
+    and c.source_vocabulary_id = 'ICD10');
+
+SELECT * FROM grouped;
+
+do $$
+declare
+r record;
+begin
+for r in select source_code, source_code_description, source_vocabulary_id, group_id, group_id_1, group_name from grouped where group_id_1 != group_id loop
+perform cde_groups.MergeGroupsByGroupID ('grouped', r.group_id_1::int, r.group_id ::int);
+end loop;
+end $$;
 
 
 

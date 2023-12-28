9a0d19b263bcdcd9fd3a03cc3e240e9a6517baf6
Eduard Korchmar <eduard.korchmar@odysseusinc.com>
2023-12-28 12:26:20 +0100

    Fix misssing backreference to updated concept_relationship table

--- SNOMED/manual_work/ukde_deletion/01A_patch_dmd_after.sql
+++ SNOMED/manual_work/ukde_deletion/01A_patch_dmd_after.sql
@@ -9,25 +9,19 @@ UPDATE concept_relationship cr
 SET
     valid_end_date = to_date('31-10-2023', 'DD-MM-YYYY'),
     invalid_reason = 'D'
+FROM concept c
+JOIN concept c2 ON
+    c2.concept_id = cr.concept_id_2
+            AND c.concept_id = cr.concept_id_1
+JOIN snomed_concepts_to_steal s ON
+    s.concept_id = c.concept_id
+LEFT JOIN snomed_concepts_to_steal s2 ON
+    s2.concept_id = c2.concept_id
 WHERE
-    EXISTS (
-        SELECT 1
-        FROM concept_relationship r
-        JOIN concept c ON
-            c.concept_id = r.concept_id_1
-        JOIN concept c2 ON
-            c2.concept_id = r.concept_id_2
-        JOIN snomed_concepts_to_steal s ON
-            s.concept_id = c.concept_id
-        LEFT JOIN snomed_concepts_to_steal s2 ON
-            s2.concept_id = c2.concept_id
-        WHERE
-            NOT (
-                r.relationship_id = 'Maps to'
-                        AND s2.concept_id IS NOT NULL
-                )
-            AND r.invalid_reason IS NULL
-    )
+        cr.invalid_reason IS NULL
+    AND NOT (
+            cr.relationship_id = 'Maps to'
+        AND s2.concept_id IS NOT NULL)
 ;
 --3.3. Steal the concepts!
 UPDATE concept c SET

798ffbfde43a5cd6356c0f5af7f8dfa67f4a869c
MariaRohozhkina <mariaspirit228@gmail.com>
2024-02-19 14:40:09 +0300

    docs

--- LOINC/manual_work/loinc_refresh.sql
+++ LOINC/manual_work/loinc_refresh.sql
@@ -1,6 +1,6 @@
 --TODO: More tests required
 
---22.3.1. Create loinc_mapped table and pre-populate it with the resulting manual table of the previous LOINC refresh.
+--19.3.1. Create loinc_mapped table and pre-populate it with the resulting manual table of the previous LOINC refresh.
 --DROP TABLE dev_loinc.loinc_mapped;
 CREATE TABLE dev_loinc.loinc_mapped
 (
@@ -31,7 +31,7 @@ UPDATE dev_loinc.loinc_mapped SET source_invalid_reason = NULL WHERE source_inva
 --Adding constraints for unique records
 ALTER TABLE dev_loinc.loinc_mapped ADD CONSTRAINT idx_pk_mapped UNIQUE (source_code,target_concept_code,source_vocabulary_id,target_vocabulary_id,relationship_id);
 
---22.3.2. Select concepts to map (flag shows different reasons for mapping refresh) and add them to the manual file in the spreadsheet editor.
+--19.3.2. Select concepts to map (flag shows different reasons for mapping refresh) and add them to the manual file in the spreadsheet editor.
 with previous_mappings AS
     (SELECT concept_id_1, c.standard_concept, array_agg(concept_id_2 ORDER BY concept_id_2 DESC) AS old_maps_to
         FROM devv5.concept_relationship cr
@@ -137,7 +137,7 @@ WHERE c.concept_code NOT IN (SELECT source_code FROM loinc_mapped) --exclude cod
 ORDER BY replace(c.concept_name, 'Deprecated ', ''), c.concept_code
 ;
 
---22.3.3. Select COVID concepts lacking hierarchy and add them to the manual file in the spreadsheet editor (these concepts need 'Is a' relationships).
+--19.3.3. Select COVID concepts lacking hierarchy and add them to the manual file in the spreadsheet editor (these concepts need 'Is a' relationships).
 SELECT * FROM (
 SELECT DISTINCT
        replace (long_common_name, 'Deprecated ', '') AS source_concept_name_clean,
@@ -183,14 +183,14 @@ AND NOT EXISTS (SELECT
 ORDER BY replace (s.source_concept_name, 'Deprecated ', ''), s.source_concept_code
 ;
 
---22.3.4. Truncate the loinc_mapped table. Save the spreadsheet as the loinc_mapped table and upload it into the working schema.
+--19.3.4. Truncate the loinc_mapped table. Save the spreadsheet as the loinc_mapped table and upload it into the working schema.
 TRUNCATE TABLE dev_loinc.loinc_mapped;
 
 --Format after uploading
 UPDATE dev_loinc.loinc_mapped SET cr_invalid_reason = NULL WHERE cr_invalid_reason = '';
 UPDATE dev_loinc.loinc_mapped SET source_invalid_reason = NULL WHERE source_invalid_reason = '';
 
---22.3.5. Change concept_relationship_manual table according to loinc_mapped table.
+--19.3.5. Change concept_relationship_manual table according to loinc_mapped table.
 --Insert new relationships
 --Update existing relationships
 INSERT INTO dev_loinc.concept_relationship_manual AS mapped 

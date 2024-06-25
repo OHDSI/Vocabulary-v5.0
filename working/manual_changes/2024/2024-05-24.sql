807b66d07c289fcce65ba81a45e087e8683c435f
Vlad.korsik@odysseusinc.com <Vlad.korsik@odysseusinc.com>
2024-06-25 14:03:23 +0300

    Some minor changes in licensicng Process for CDISC (discussed with C. Riech and A. Davydov)

--- working/manual_changes/2024/2024-05-24.sql
+++ working/manual_changes/2024/2024-05-24.sql
@@ -3,12 +3,12 @@ DO $_$
 BEGIN
 	PERFORM vocabulary_pack.AddNewVocabulary(
 	pVocabulary_id          => 'CDISC',
-    pVocabulary_name        => 'CDISC',
+    pVocabulary_name        => 'Clinical Data Interchange Standards Consortium',
     pVocabulary_reference   => 'https://evs.nci.nih.gov/evs-download/metathesaurus-downloads',
     pVocabulary_version     => '2023_12D',
     pOMOP_req				=> 'Y', --NULL or 'Y'
 	pClick_default			=> NULL, --NULL or 'Y'
-	pAvailable				=> 'License required', --NULL, 'Currently not available','License required' or 'EULA required'
+	pAvailable				=> NULL, --NULL, 'Currently not available','License required' or 'EULA required'
 	pURL					=> NULL,
 	pClick_disabled			=> NULL, --NULL or 'Y'
 	pSEQ_VIP_gen			=> FALSE --TRUE if VIP

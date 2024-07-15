8c25cdc166b3638c04700d844c38b0cbe58ae15f
Vlad.korsik@odysseusinc.com <Vlad.korsik@odysseusinc.com>
2024-07-15 18:25:30 +0300

    --Units are added;

--- working/manual_changes/2024/2024-07-09.sql
+++ working/manual_changes/2024/2024-07-09.sql
@@ -71,3 +71,51 @@ BEGIN
 		pConcept_code		=>'cg/l'
 	);
 END $_$;
+
+DO $_$
+BEGIN
+	PERFORM vocabulary_pack.AddNewConcept(
+		pConcept_name		=>'square millimole per liter in square',
+		pDomain_id			=>'Unit',
+		pVocabulary_id		=>'UCUM',
+		pConcept_class_id	=>'Unit',
+		pStandard_concept	=>'S',
+		pConcept_code		=>'mmol2/L2'
+	);
+END $_$;
+
+DO $_$
+BEGIN
+	PERFORM vocabulary_pack.AddNewConcept(
+		pConcept_name		=>'per kiloohm',
+		pDomain_id			=>'Unit',
+		pVocabulary_id		=>'UCUM',
+		pConcept_class_id	=>'Unit',
+		pStandard_concept	=>'S',
+		pConcept_code		=>'/kOhm'
+	);
+END $_$;
+
+DO $_$
+BEGIN
+	PERFORM vocabulary_pack.AddNewConcept(
+		pConcept_name		=>'hectopascal',
+		pDomain_id			=>'Unit',
+		pVocabulary_id		=>'UCUM',
+		pConcept_class_id	=>'Unit',
+		pStandard_concept	=>'S',
+		pConcept_code		=>'hPa'
+	);
+END $_$;
+
+DO $_$
+BEGIN
+	PERFORM vocabulary_pack.AddNewConcept(
+		pConcept_name		=>'millimole per 48 hours',
+		pDomain_id			=>'Unit',
+		pVocabulary_id		=>'UCUM',
+		pConcept_class_id	=>'Unit',
+		pStandard_concept	=>'S',
+		pConcept_code		=>'mmol/(48.h)'
+	);
+END $_$;

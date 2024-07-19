6fcbb9c762421ec8d3dd7ffa05c40baecf2c43c3
Vlad <47443886+vladkorsik@users.noreply.github.com>
2024-07-19 16:03:45 +0300

    Update 2024-07-16.sql
    Changes in Voc ingection sql

--- working/manual_changes/2024/2024-07-16.sql
+++ working/manual_changes/2024/2024-07-16.sql
@@ -2,15 +2,15 @@
 DO $_$
 BEGIN
 	PERFORM vocabulary_pack.AddNewVocabulary(
-	pVocabulary_id          => 'CDISC',
+	pVocabulary_id       => 'CDISC',
     pVocabulary_name        => 'Clinical Data Interchange Standards Consortium',
     pVocabulary_reference   => 'https://evs.nci.nih.gov/evs-download/metathesaurus-downloads',
     pVocabulary_version     => '2023_12D',
-    pOMOP_req				=> 'Y', --NULL or 'Y'
-	pClick_default			=> NULL, --NULL or 'Y'
-	pAvailable				=> NULL, --NULL, 'Currently not available','License required' or 'EULA required'
-	pURL					=> NULL,
-	pClick_disabled			=> NULL, --NULL or 'Y'
-	pSEQ_VIP_gen			=> FALSE --TRUE if VIP
+    pOMOP_req		    => NULL, --NULL or 'Y'
+	pClick_default	    => NULL, --NULL or 'Y'
+	pAvailable	    => NULL, --NULL, 'Currently not available','License required' or 'EULA required'
+	pURL		    => 'https://evs.nci.nih.gov/evs-download/metathesaurus-downloads',
+	pClick_disabled	    => NULL, --NULL or 'Y'
+	pSEQ_VIP_gen	    => FALSE --TRUE if VIP
 );
 END $_$;

2d9e903c41e9f53ed4ddd749715fb415898f020c
Vlad.korsik@odysseusinc.com <Vlad.korsik@odysseusinc.com>
2024-08-05 14:13:36 +0300

    --CDISC Refresh UPD -- harmonisation with pre-existing Scripts (loinc-inspired)

--- CDISC/manual_work/cdisc_refresh.sql
+++ CDISC/manual_work/cdisc_refresh.sql
@@ -42,20 +42,56 @@ domain_id varchar
 );
 
 
---Population of the concept_relationship_manual table
--- concept_relationship_manual table population
-INSERT INTO  concept_relationship_manual (concept_code_1,concept_code_2,vocabulary_id_1,vocabulary_id_2,relationship_id,valid_start_date,valid_end_date,invalid_reason)
-SELECT DISTINCT concept_code as concept_code_1,
-       target_concept_code as concept_code_2,
-       vocabulary_id as vocabulary_id_1,
-       target_vocabulary_id as vocabulary_id_2,
-       relationship_id as relationship_id,
-       valid_start_date as valid_start_date,
-       valid_end_date as valid_end_date,
-       null as invalid_reason
-       FROM cdisc_mapped
-    WHERE target_concept_id is not null
-      AND target_concept_code !='No matching concept'  -- _mapped file can contain them
-    and decision is true
-        ORDER BY concept_code,relationship_id;
+--Insert new relationships
+--Update existing relationships
+INSERT INTO concept_relationship_manual AS mapped
+    (concept_code_1,
+    concept_code_2,
+    vocabulary_id_1,
+    vocabulary_id_2,
+    relationship_id,
+    valid_start_date,
+    valid_end_date,
+    invalid_reason)
 
+	SELECT concept_code as source_code,
+	       target_concept_code,
+	       vocabulary_id as source_vocabulary_id,
+	       target_vocabulary_id,
+	       m.relationship_id,
+	       current_date AS valid_start_date,
+           to_date('20991231','yyyymmdd') AS valid_end_date,
+           m.invalid_reason
+	FROM dev_cdisc.cdisc_mapped m
+	--Only related to LOINC vocabulary
+	WHERE (vocabulary_id = 'CDISC' OR target_vocabulary_id = 'CDISC')
+	    AND target_concept_id != 0
+
+	ON CONFLICT ON CONSTRAINT unique_manual_relationships
+	DO UPDATE
+	    --In case of mapping 'resuscitation' use current_date as valid_start_date; in case of mapping deprecation use previous valid_start_date
+	SET valid_start_date = CASE WHEN excluded.invalid_reason IS NULL THEN excluded.valid_start_date ELSE mapped.valid_start_date END,
+	    --In case of mapping 'resuscitation' use 2099-12-31 as valid_end_date; in case of mapping deprecation use current_date
+		valid_end_date = CASE WHEN excluded.invalid_reason IS NULL THEN excluded.valid_end_date ELSE current_date END,
+		invalid_reason = excluded.invalid_reason
+	WHERE ROW (mapped.invalid_reason)
+	IS DISTINCT FROM
+	ROW (excluded.invalid_reason);
+
+--Correction of valid_start_dates and valid_end_dates for deprecation of existing mappings, existing in base, but not manual tables
+UPDATE concept_relationship_manual crm 
+SET valid_start_date = cr.valid_start_date,
+    valid_end_date = current_date
+FROM cdisc_mapped m 
+JOIN concept c 
+ON c.concept_code = m.concept_code AND m.vocabulary_id = c.vocabulary_id
+JOIN concept_relationship cr 
+ON cr.concept_id_1 = c.concept_id AND cr.relationship_id = m.relationship_id
+JOIN concept c1 
+ON c1.concept_id = cr.concept_id_2 AND c1.concept_code = m.target_concept_code AND c1.vocabulary_id = m.target_vocabulary_id
+WHERE m.invalid_reason IS NOT NULL
+AND crm.concept_code_1 = m.concept_code AND crm.vocabulary_id_1 = m.vocabulary_id
+AND crm.concept_code_2 = m.target_concept_code AND crm.vocabulary_id_2 = m.target_vocabulary_id
+AND crm.relationship_id = m.relationship_id
+AND crm.invalid_reason IS NOT NULL
+;

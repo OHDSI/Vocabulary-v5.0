e5d557f92b8f688e6cc681f95809fc1cc63983e2
olegzhuk <oleg.zhuk@odysseusinc.com>
2024-08-29 11:25:40 +0200

    Metadata - constraint added + minor changes

--- working/metadata/concept_relationship_metadata.sql
+++ working/metadata/concept_relationship_metadata.sql
@@ -5,40 +5,19 @@ CREATE TABLE concept_relationship_metadata (
     concept_id_2 int NOT NULL,
     relationship_id varchar(20) NOT NULL,
     relationship_predicate_id VARCHAR(20),
-	relationship_group INT,
-mapping_source VARCHAR(255),
-confidence FLOAT,
-mapping_tool VARCHAR(50),
-mapper VARCHAR(50),
-reviewer VARCHAR(50),
+    relationship_group INT,
+    mapping_source VARCHAR(255),
+    confidence FLOAT,
+    mapping_tool VARCHAR(50),
+    mapper VARCHAR(50),
+    reviewer VARCHAR(50),
     FOREIGN KEY (concept_id_1, concept_id_2, relationship_id)
     REFERENCES concept_relationship (concept_id_1, concept_id_2, relationship_id)
 );
 
--- ICDs insert:
-INSERT INTO concept_relationship_metadata
-SELECT cr.concept_id_1 as concept_id_1,
-       cr.concept_id_2 as concept_id_2,
-       cr.relationship_id as relationship_id,
-       p.relationship_id_predicate as relationship_predicate_id,
-       null as relationship_group,
-       (array_agg (DISTINCT s.mappings_origin)) as mapping_source,
-       null as confidence,
-       null as mapping_tool,
-       null as mapper,
-       null as reviewer
-FROM devv5.concept_relationship cr
-JOIN devv5.concept c ON cr.concept_id_1 = c.concept_id
-JOIN dev_icd10.icd_cde_proc p ON p.source_code = c.concept_code AND p.source_vocabulary_id = c.vocabulary_id
-LEFT JOIN dev_icd10.icd_cde_source s ON p.source_code = s.source_code and p.source_vocabulary_id = s.source_vocabulary_id
-WHERE cr.relationship_id in ('Maps to', 'Maps to value')
-AND cr.invalid_reason is null
-GROUP BY cr.concept_id_1,
-         cr.concept_id_2,
-         cr.relationship_id,
-         p.relationship_id_predicate;
+ALTER TABLE concept_relationship_metadata ADD CONSTRAINT xpk_concept_relationship_metadata PRIMARY KEY (concept_id_1,concept_id_2,relationship_id);
 
--- CC insert:
+-- Community contribution:
 INSERT INTO concept_relationship_metadata
 SELECT cr.concept_id_1 as concept_id_1,
        cr.concept_id_2 as concept_id_2,
@@ -53,13 +32,41 @@ SELECT cr.concept_id_1 as concept_id_1,
        cc.mapping_tool as mapping_tool,
        null as mapper,
        null as reviewer
-FROM cc_mapping cc
+FROM dev_test4.cc_mapping cc
 JOIN devv5.concept c ON (cc.concept_code_1, cc.vocabulary_id_1) = (c.concept_code, c.vocabulary_id)
 JOIN devv5.concept c1 ON (cc.concept_code_2, cc.vocabulary_id_2) = (c1.concept_code, c1.vocabulary_id)
 JOIN devv5.concept_relationship cr ON (c.concept_id, c1.concept_id, cc.relationship_id) = (cr.concept_id_1, cr.concept_id_2, cr.relationship_id)
 WHERE cr.invalid_reason is null;
 
--- Insert for SNOMED:
+
+-- ICDs:
+INSERT INTO concept_relationship_metadata
+SELECT cr.concept_id_1 as concept_id_1,
+       cr.concept_id_2 as concept_id_2,
+       cr.relationship_id as relationship_id,
+       p.relationship_id_predicate as relationship_predicate_id,
+       NULL as relationship_group,
+       (array_agg (DISTINCT s.mappings_origin)) as mapping_source,
+       NULL as confidence,
+       NULL as mapping_tool,
+       CASE WHEN p.mapper = '' THEN NULL ELSE p.mapper END as mapper,
+       CASE WHEN p.reviewer = '' THEN NULL ELSE p.reviewer END as reviewer
+FROM devv5.concept_relationship cr
+JOIN devv5.concept c ON cr.concept_id_1 = c.concept_id
+JOIN dev_icd10.icd_cde_proc p ON p.source_code = c.concept_code AND p.source_vocabulary_id = c.vocabulary_id
+LEFT JOIN dev_icd10.icd_cde_source s ON p.source_code = s.source_code AND p.source_vocabulary_id = s.source_vocabulary_id
+WHERE cr.relationship_id IN ('Maps to', 'Maps to value')
+  AND (cr.concept_id_1, cr.concept_id_2, cr.relationship_id) NOT IN (SELECT concept_id_1, concept_id_2, relationship_id FROM concept_relationship_metadata)
+AND cr.invalid_reason IS NULL
+GROUP BY cr.concept_id_1,
+         cr.concept_id_2,
+         cr.relationship_id,
+         p.relationship_id_predicate,
+         p.mapper,
+         p.reviewer;
+
+
+--SNOMED:
 INSERT INTO concept_relationship_metadata
 SELECT cr.concept_id_1 as concept_id_1,
        cr.concept_id_2 as concept_id_2,
@@ -76,11 +83,13 @@ JOIN devv5.concept c on (m.source_code, m.source_vocabulary_id) = (c.concept_cod
 JOIN devv5.concept c1 on (m.target_concept_code, m.target_vocabulary_id) = (c1.concept_code, c1.vocabulary_id)
 JOIN devv5.concept_relationship cr on (c.concept_id, c1.concept_id, m.relationship_id) = (cr.concept_id_1, cr.concept_id_2, cr.relationship_id)
 WHERE cr.relationship_id IN ('Maps to', 'Maps to value')
+    AND (cr.concept_id_1, cr.concept_id_2, cr.relationship_id) NOT IN (SELECT concept_id_1, concept_id_2, relationship_id FROM concept_relationship_metadata)
 AND cr.invalid_reason IS NULL
 AND m.cr_invalid_reason is null
 AND m.relationship_id_predicate IS NOT NULL;
 
---CDISC Rel_Meta Integration
+
+--CDISC
 INSERT INTO concept_relationship_metadata (concept_id_1, concept_id_2, relationship_id, relationship_predicate_id, relationship_group, mapping_source, confidence, mapping_tool, mapper, reviewer)
 SELECT DISTINCT
        concept_id_1,
@@ -141,7 +150,8 @@ UNION ALL
 ORDER BY concept_id_1,relationship_id,concept_id_2
 ;
 
--- --MedDRA Rel_Meta Integration
+
+--MedDRA
 INSERT INTO concept_relationship_metadata (concept_id_1, concept_id_2, relationship_id, relationship_predicate_id, relationship_group, mapping_source, confidence, mapping_tool, mapper, reviewer)
 with tab_array as(
 SELECT concept_id_1,

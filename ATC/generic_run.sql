72e92a6758fb48b0c63b627a3555cc2964e3a11b
anton_tatur1 <ratuat@gmail.com>
2024-08-29 23:47:34 +0300

    corrected name of columns in class_to_drug.sql

--- ATC/generic_run.sql
+++ ATC/generic_run.sql
@@ -1,25 +1,18 @@
 --- 1. Step
-SELECT devv5.FastRecreateSchema(main_schema_name=>'devv5', include_concept_ancestor=>FALSE, include_deprecated_rels=>TRUE, include_synonyms=>TRUE);
-select * from devv5.qa_ddl();
-SELECT * FROM qa_tests.check_stage_tables ();
+load_stage.sql
 
 ---- 2. Step
----- update_manual_tables.sql
----- load_input.sql
----- load_stage.sql
-
----- 3. Step
-SELECT admin_pack.VirtualLogIn(:login, :password);
 DO $_$
 BEGIN
 	PERFORM devv5.GenericUpdate();
 END $_$;
-select * from QA_TESTS.GET_CHECKS();
+
+--- 3. Step
+class_to_drug.sql
 
 ---- 4. Step
----- class_to_drug.sql
-
----- 5. Step
+---- функция должна быть как в dev_atc с правками на проблему с дубликатами, но сорсовая таблица
+---- приходит уже из sources.class_to_drug
 DO $_$
 BEGIN
 	PERFORM dev_atc.pConceptAncestor();

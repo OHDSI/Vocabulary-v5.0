5a82e97d8c2b34529d70c0e0871302671aeb3ae2
m-khitrun <maria.khitrun@odysseusinc.com>
2024-08-29 16:41:09 +0300

    polyhierarchy scripts refactoring

--- working/vocab_principles_QA/pohyhierarchy.sql
+++ working/vocab_principles_QA/pohyhierarchy.sql
@@ -2,43 +2,36 @@
 
 --- 1. Number of the classification concepts without descendants:
 select vocabulary_id,
-       count(*) as count
-from devv5.concept c1
-where c1.standard_concept = 'C'
-  and c1.invalid_reason is null
-and not exists(
-select 1
-from devv5.concept c
-join devv5.concept_relationship cr on c.concept_id = cr.concept_id_1
-where c.standard_concept = 'C'
-and cr.relationship_id = 'Subsumes'
-and cr.invalid_reason is null
-and c.concept_id = c1.concept_id)
-group by vocabulary_id
-order by count desc
+       domain_id,
+       concept_class_id,
+       count(*) as number_of_concepts
+from concept c
+where c.standard_concept IN ('C', 'S')
+  and not exists (select 1
+                  from concept_ancestor ca
+                  where c.concept_id = ca.ancestor_concept_id
+                    and ca.descendant_concept_id != ca.ancestor_concept_id)
+group by vocabulary_id,
+         domain_id,
+         concept_class_id
+order by number_of_concepts desc;
 ;
 
 -- 2. Number of standard concepts without ancestors and descendants
 select vocabulary_id,
        count(*) as count
-from devv5.concept c1
+from concept c1
 where c1.standard_concept = 'S'
 and not exists(
 select 1
-from devv5.concept c
-join devv5.concept_relationship cr on c.concept_id = cr.concept_id_1
-where c.standard_concept = 'S'
-and cr.relationship_id = 'Is a'
-and cr.invalid_reason is null
-and c.concept_id = c1.concept_id)
+from concept_ancestor ca
+where c1.concept_id = ca.descendant_concept_id
+and ca.descendant_concept_id != ca.ancestor_concept_id)
 and not exists(
 select 1
-from devv5.concept c
-join devv5.concept_relationship cr on c.concept_id = cr.concept_id_1
-where c.standard_concept = 'S'
-and cr.relationship_id = 'Subsumes'
-and cr.invalid_reason is null
-and c.concept_id = c1.concept_id)
+from concept_ancestor ca
+where c1.concept_id = ca.ancestor_concept_id
+and ca.descendant_concept_id != ca.ancestor_concept_id)
 group by vocabulary_id
 order by count desc
 ;

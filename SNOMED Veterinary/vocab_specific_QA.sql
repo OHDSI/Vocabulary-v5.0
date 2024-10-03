c8573f5472a42f2a2031c70414d380f7bfdc580f
m-khitrun <maria_khitrun@epam.com>
2024-10-03 10:35:48 +0300

    vocab specific checks added

---
+++ SNOMED Veterinary/vocab_specific_QA.sql
@@ -0,0 +1,31 @@
+-- This check returns additional SNOMED-SNOMED relationships that are created due to the SNOMED Vet input in SNOMED hierarchy
+
+select distinct
+       ca.*,
+       c1.concept_name,
+       c2.concept_name
+from devv5.concept_ancestor ca
+join devv5.concept c1 on c1.concept_id = ca.ancestor_concept_id and c1.vocabulary_id = 'SNOMED'
+join devv5.concept c2 on c2.concept_id = ca.descendant_concept_id and c2.vocabulary_id = 'SNOMED'
+where (ca.ancestor_concept_id, ca.descendant_concept_id) not in (
+select ca1.ancestor_concept_id, ca1.descendant_concept_id
+from dev_veterinary.concept_ancestor ca1
+);
+
+-- This check reviews the cases where SNOMED Vet concepts is located between two SNOMED concepts on the concept_ancestor:
+select distinct a.concept_id as ancestor_id,
+               a.concept_code as ancestor_code,
+               a.concept_name as ancestor_name,
+               a.vocabulary_id as ancest_vocab,
+      cc.concept_id as vet_id,
+      cc.concept_name as vet_name,
+      cc.vocabulary_id as vet_vocab,
+      c.concept_id as descendant_id,
+      c.concept_name as descendant_name,
+      c.vocabulary_id as descendant_vocab
+from dev_veterinary.concept_ancestor ca
+join dev_veterinary.concept c on c.concept_id = ca.descendant_concept_id and c.vocabulary_id = 'SNOMED'
+join dev_veterinary.concept cc on cc.concept_id = ca.ancestor_concept_id and cc.vocabulary_id = 'SNOMED Veterinary'
+join dev_veterinary.concept_ancestor ca1 on ca1.descendant_concept_id = cc.concept_id
+left join dev_veterinary.concept a on a.concept_id = ca1.ancestor_concept_id and a.vocabulary_id = 'SNOMED'
+;

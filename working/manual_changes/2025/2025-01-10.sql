41de01998957045da7f6fdb85b8c761f19c90c89
m-khitrun <maria_khitrun@epam.com>
2025-01-13 15:06:40 +0200

    Manual changes refactoring

--- working/manual_changes/2025/2025-01-10.sql
+++ working/manual_changes/2025/2025-01-10.sql
@@ -1,20 +1,4 @@
--- 1. Create new relationship linking Dose Form and Route of Administration (Community Contribution):
-
-DO $_$
-BEGIN
-  PERFORM vocabulary_pack.AddNewRelationship(
-    pRelationship_name       =>'Dose form to route of administration (OMOP)',
-    pRelationship_id         =>'Dose form to route',
-    pIs_hierarchical         =>0,
-    pDefines_ancestry        =>0,
-    pReverse_relationship_id =>'Route to dose form',
-    pRelationship_name_rev   =>'Route of administration to dose form (OMOP)',
-    pIs_hierarchical_rev     =>0,
-    pDefines_ancestry_rev    =>0
-);
-END $_$;
-
--- 2. Make SNOMED Veterinary relationships 'Sub-specimen of' and 'Has sub-specimen' non-hierarchical:
+-- 1. Make SNOMED Veterinary relationships 'Sub-specimen of' and 'Has sub-specimen' non-hierarchical:
 --- They lead to the hierarchy loops creation in concept_ancestor
 
 UPDATE relationship

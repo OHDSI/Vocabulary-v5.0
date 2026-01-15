03bcea60ae202e800de0dcf6115cc447262baff8
m-khitrun <maria_khitrun@epam.com>
2026-01-15 13:46:32 +0200

    readme fix, edi_refresh refactoring

--- EDI/manual_work/edi_refresh.sql
+++ EDI/manual_work/edi_refresh.sql
@@ -1,7 +1,5 @@
---populate concept_relationship_manual table from a manual lookup (edi_mapped) table 
-
---add mappings into concept_relationship_manual
-CREATE TABLE edi_mapped(
+-- 1. Populate concept_relationship_manual table from a manual lookup (edi_mapped) table
+/*CREATE TABLE edi_mapped(
 source_concept_code varchar,
 source_domain_id varchar,
 source_concept_name varchar,
@@ -14,18 +12,59 @@ target_vocabulary_id varchar,
 target_standard_concept varchar,
 target_invalid_reason varchar,
 valid_start_date date,
-valid_end_date date);
+valid_end_date date);*/
 
-INSERT INTO concept_relationship_manual
-SELECT
-source_concept_code AS concept_code_1,
-target_concept_code AS concept_code_2,
-'EDI' AS vocabulary_id_1,
-target_vocabulary_id AS vocabulary_id_2,
-relationship_id,
-valid_start_date,
-valid_end_date,
-target_invalid_reason AS invalid_reason
-FROM edi_mapped
-WHERE target_concept_id IS NOT NULL
-AND target_invalid_reason IS NULL;
+-- 2. Truncate edi_mapped table and populate with the fresh data:
+TRUNCATE TABLE dev_edi.edi_mapped;
+
+-- 3. Add mappings into concept_relationship_manual
+INSERT INTO dev_edi.concept_relationship_manual AS mapped
+    (concept_code_1,
+    concept_code_2,
+    vocabulary_id_1,
+    vocabulary_id_2,
+    relationship_id,
+    valid_start_date,
+    valid_end_date,
+    invalid_reason)
+
+	SELECT source_code,
+	       target_concept_code,
+	       source_vocabulary_id,
+	       target_vocabulary_id,
+	       m.relationship_id,
+	       current_date AS valid_start_date,
+           CASE WHEN m.cr_invalid_reason IS NULL
+                  THEN to_date('20991231','yyyymmdd')
+                  ELSE current_date END AS valid_end_date,
+           m.cr_invalid_reason
+	FROM dev_edi.edi_mapped m
+
+	ON CONFLICT ON CONSTRAINT unique_manual_relationships
+	DO UPDATE
+	    --In case of mapping 'resuscitation' use current_date as valid_start_date; in case of mapping deprecation use previous valid_start_date
+	SET valid_start_date = CASE WHEN excluded.invalid_reason IS NULL THEN excluded.valid_start_date ELSE mapped.valid_start_date END,
+	    --In case of mapping 'resuscitation' use 2099-12-31 as valid_end_date; in case of mapping deprecation use current_date
+		valid_end_date = CASE WHEN excluded.invalid_reason IS NULL THEN excluded.valid_end_date ELSE current_date END,
+		invalid_reason = excluded.invalid_reason
+	WHERE ROW (mapped.invalid_reason)
+	IS DISTINCT FROM
+	ROW (excluded.invalid_reason);
+
+-- 4. Correction of valid_start_dates and valid_end_dates for deprecation of existing mappings, existing in base, but not manual tables
+UPDATE concept_relationship_manual crm
+SET valid_start_date = cr.valid_start_date,
+    valid_end_date = current_date
+FROM edi_mapped m
+JOIN concept c
+ON c.concept_code = m.source_code AND m.source_vocabulary_id = c.vocabulary_id
+JOIN concept_relationship cr
+ON cr.concept_id_1 = c.concept_id AND cr.relationship_id = m.relationship_id
+JOIN concept c1
+ON c1.concept_id = cr.concept_id_2 AND c1.concept_code = m.target_concept_code AND c1.vocabulary_id = m.target_vocabulary_id
+WHERE m.cr_invalid_reason IS NOT NULL
+AND crm.concept_code_1 = m.source_code AND crm.vocabulary_id_1 = m.source_vocabulary_id
+AND crm.concept_code_2 = m.target_concept_code AND crm.vocabulary_id_2 = m.target_vocabulary_id
+AND crm.relationship_id = m.relationship_id
+AND crm.invalid_reason IS NOT NULL
+;
--- EDI/readme.md
+++ EDI/readme.md
@@ -5,6 +5,36 @@ Prerequisites:
 - Working directory dev_edi.
 
 1. Run create_source_tables.sql
-2. Run edi_refresh.sql
-3. Run load_stage.sql
-4. Run generic_update: devv5.GenericUpdate();
+2. Run 
+```sql
+SELECT devv5.FastRecreateSchema(main_schema_name=>'devv5', include_concept_ancestor=> false, include_deprecated_rels=> true, include_synonyms=> true);
+   ```
+3. Run edi_refresh.sql
+
+4. Run load_stage.sql
+
+5. Run generic_update:
+   ```sql
+   DO $_$
+   BEGIN
+       PERFORM devv5.GenericUpdate();
+   END $_$;
+   ```
+6. Run basic tables check (should retrieve NULL):
+   ```sql
+    SELECT * FROM qa_tests.get_checks();
+    ```
+7. Run scripts to get summary, and interpret the results:
+    ```sql
+    SELECT DISTINCT * FROM qa_tests.get_summary('concept');
+    SELECT DISTINCT * FROM qa_tests.get_summary('concept_relationship');
+    ```
+8. Run scripts to collect statistics, and interpret the results:
+    ```sql
+    SELECT DISTINCT * FROM qa_tests.get_domain_changes();
+    SELECT DISTINCT * FROM qa_tests.get_newly_concepts();
+    SELECT DISTINCT * FROM qa_tests.get_standard_concept_changes();
+    SELECT DISTINCT * FROM qa_tests.get_newly_concepts_standard_concept_status();
+    SELECT DISTINCT * FROM qa_tests.get_changes_concept_mapping();
+    ```
+9. If no problems, enjoy!

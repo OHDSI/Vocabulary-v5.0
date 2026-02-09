d0b569e61c37a9cb1ad1083e8fbdf497d9c90af7
m-khitrun <maria_khitrun@epam.com>
2026-02-09 12:44:36 +0200

    manual update of the CIEL link moved to another directory

--- working/manual_changes/2026/2026-02-09.sql
+++ working/manual_changes/2026/2026-02-09.sql
@@ -1,4 +1,4 @@
--- replace outdated CIEL reference link in the vocabulary table
+-- Update CIEL vocabulary reference
 UPDATE vocabulary
    SET vocabulary_reference = 'https://app.openconceptlab.org/#/orgs/CIEL/'
 WHERE vocabulary_id = 'CIEL';

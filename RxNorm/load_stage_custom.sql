ca3c2dcac4dea909d87673b6aedd790cb81ad8b4
m-khitrun <maria_khitrun@epam.com>
2026-05-27 17:44:13 +0300

    smapp refactoring

--- RxNorm/load_stage_custom.sql
+++ RxNorm/load_stage_custom.sql
@@ -1,21 +1,4 @@
-/**************************************************************************
-* Copyright 2016 Observational Health Data Sciences and Informatics (OHDSI)
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-* Authors: Christian Reich, Timur Vakhitov
-* Date: 2021
-**************************************************************************/
+-- !! Developer's version!!
 
 -- 1. Update latest_update field to new date
 DO $_$
@@ -1519,8 +1502,6 @@ BEGIN
     PERFORM vocabulary_pack.RxECleanUP();
 END $_$;
 
-
-select admin_pack.VirtualLogIn('dev_atatur', 'wheoIFrevy!212');
 
 DO $_$
 BEGIN

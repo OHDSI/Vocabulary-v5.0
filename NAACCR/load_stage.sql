d06b3899dabd08367bb43af66d9f160cf97f89c4
vladk <korsik@ohdsi.org>
2026-07-13 12:04:23 +0300

    --NAACCR update commit via externally produces set of stge-tables (agreed with Vocab Team technical lead)

--- NAACCR/load_stage.sql
+++ NAACCR/load_stage.sql
@@ -14,7 +14,7 @@
 * limitations under the License.
 *
 * Authors: Medical team
-* Date: 2021
+* Date: 2021, 2026
 **************************************************************************/
 
 
@@ -22,8 +22,8 @@ DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.SetLatestUpdate(
 	pVocabularyName			=> 'NAACCR',
-    pVocabularyDate			=> to_date ('2018-03-02', 'yyyy-mm-dd'), -- https://www.naaccr.org/data-standards-data-dictionary/#DataDictionary -- Version 18 Data Standards and Data Dictionary - (posted 3/2/18;
-	pVocabularyVersion		=> 'NAACCR v18',
+    pVocabularyDate			=> to_date ('2026-07-13', 'yyyy-mm-dd'), -- https://www.naaccr.org/data-standards-data-dictionary/#DataDictionary -- Version 18 Data Standards and Data Dictionary - (posted 3/2/18;
+	pVocabularyVersion		=> 'NAACCR v26',
 	pVocabularyDevSchema	=> 'dev_naaccr'
 	);
 /*	PERFORM VOCABULARY_PACK.SetLatestUpdate(
@@ -42,34 +42,62 @@ TRUNCATE TABLE concept_synonym_stage;
 TRUNCATE TABLE pack_content_stage;
 TRUNCATE TABLE drug_strength_stage;
 
---1. ProcessManualConcepts
-DO $_$
-BEGIN
-	PERFORM VOCABULARY_PACK.ProcessManualConcepts();
-END $_$;
+--2. As ProcessManualRelationships to minimize the legacy-tail in manual tables,
+-- direct injection of 'externally-produced-stage' content is expected to be omitted direct insert to
+INSERT INTO concept_relationship_stage (concept_id_1, concept_id_2, concept_code_1, concept_code_2, vocabulary_id_1, vocabulary_id_2, relationship_id, valid_start_date, valid_end_date, invalid_reason)
+SELECT concept_id_1,
+       concept_id_2,
+       concept_code_1,
+       concept_code_2,
+       vocabulary_id_1,
+       vocabulary_id_2,
+       relationship_id,
+       valid_start_date,
+       valid_end_date,
+       invalid_reason
+FROM dev_naaccr.naaccr_update_concept_relationship_stage ncrs
+;
 
---2. Add manual relationships
-DO $_$
-BEGIN
-	PERFORM VOCABULARY_PACK.ProcessManualRelationships();
-END $_$;
+--3. As ProcessManualSynonyms to minimize the legacy-tail in manual tables,
+-- direct injection of 'externally-produced-stage' content is expected to be omitted direct insert to
+INSERT INTO concept_synonym_stage (synonym_concept_id, synonym_name, synonym_concept_code, synonym_vocabulary_id, language_concept_id)
+SELECT synonym_concept_id,
+       synonym_name,
+       synonym_concept_code,
+       synonym_vocabulary_id,
+       language_concept_id
+FROM dev_naaccr.naaccr_update_concept_synonym_stage ncss
+;
 
---3. Working with replacement mappings
+
+--4. Working with replacement mappings
 DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.CheckReplacementMappings();
 END $_$;
 
---4. Add mapping from deprecated to fresh concepts (necessary for the next step)
+--5. Add mapping from deprecated to fresh concepts (necessary for the next step)
 DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.AddFreshMAPSTO();
+	PERFORM VOCABULARY_PACK.AddFreshMapsToValue();
 END $_$;
 
---5. Deprecate 'Maps to' mappings to deprecated and upgraded concepts
+--6. Deprecate 'Maps to' mappings to deprecated and upgraded concepts
 DO $_$
 BEGIN
 	PERFORM VOCABULARY_PACK.DeprecateWrongMAPSTO();
 END $_$;
 
+--7. Deprecate 'Maps to' mappings to deprecated and upgraded concepts
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.DeprecateWrongMAPSTOv();
+END $_$;
+
+--8. Delete ambiguous 'Maps to' mappings
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.DeleteAmbiguousMAPSTO();
+END $_$;
 -- At the end, the three tables concept_stage, concept_relationship_stage AND concept_synonym_stage should be ready to be fed into the generic_update.sql script

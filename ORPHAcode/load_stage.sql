1c0796006ca649a4591c467c98c51bbf65591095
m-khitrun <maria_khitrun@epam.com>
2026-07-16 12:31:07 +0300

    ORPHAcode load_stage added, readme added

--- ORPHAcode/load_stage.sql
+++ ORPHAcode/load_stage.sql
@@ -1,5 +1,39 @@
--- 1. Populate concept_stage table
-/*INSERT INTO concept_stage (
+/**************************************************************************
+* Copyright 2016 Observational Health Data Sciences AND Informatics (OHDSI)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* Authors: Masha Khitrun
+* Date: 2026
+**************************************************************************/
+
+--1. Update latest_update field to new date
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.SetLatestUpdate(
+	pVocabularyName			=> 'ORPHAcode',
+	pVocabularyDate			=> (SELECT vocabulary_date FROM sources.mrsmap LIMIT 1),
+	pVocabularyVersion		=> (SELECT EXTRACT (YEAR FROM vocabulary_date)||' Release' FROM sources.mrsmap LIMIT 1),
+	pVocabularyDevSchema	=> 'dev_orphanet'
+);
+END $_$;
+
+TRUNCATE TABLE concept_stage;
+TRUNCATE TABLE concept_synonym_stage;
+TRUNCATE TABLE concept_relationship_stage;
+
+-- 2. Populate concept_stage table
+INSERT INTO concept_stage (
 	concept_name,
     domain_id,
 	vocabulary_id,
@@ -9,18 +43,18 @@
 	valid_start_date,
 	valid_end_date,
 	invalid_reason
-	)*/
+	)
 SELECT DISTINCT vocabulary_pack.CutConceptName(str) AS concept_name,
                 'Condition' AS domain_id,
-                'Orphanet' AS vocabulary_id,
-                '??' AS concept_class_id,
+                'ORPHAcode' AS vocabulary_id,
+                'Disorder' AS concept_class_id,
                 NULL AS standard_concept,
                 m.code as concept_code,
-             /*   (
+                (
                 SELECT latest_update
                 FROM vocabulary
-                WHERE vocabulary_id = 'Orphanet'
-                ) AS valid_start_date,*/
+                WHERE vocabulary_id = 'ORPHAcode'
+                ) AS valid_start_date,
                 TO_DATE('20991231', 'YYYYMMDD') AS valid_end_date,
 	            NULL AS invalid_reason
 FROM sources.mrconso m
@@ -31,30 +65,51 @@ and tty = 'PT';
 
 
 -- 2. Populate concept_synonym table:
-/*INSERT INTO concept_synonym_stage (
+INSERT INTO concept_synonym_stage (
 	synonym_concept_code,
 	synonym_vocabulary_id,
 	synonym_name,
 	language_concept_id
-	)*/
+	)
 SELECT DISTINCT m.code,
-	            'Orphanet',
+	            'ORPHAcode',
 	            vocabulary_pack.CutConceptSynonymName(m.str),
 	            4180186
-from sources.mrconso m
+FROM sources.mrconso m
 WHERE sab = 'ORPHANET'
 and tty = 'SY';
 
+INSERT INTO concept_synonym_manual (
+    synonym_name,
+    synonym_concept_code,
+    synonym_vocabulary_id,
+    language_concept_id
+)
+SELECT DISTINCT synonym_name,
+       concept_code_1,
+       vocabulary_id_1,
+       language_concept_id
+FROM cc_submission sub
+WHERE NOT EXISTS(
+    SELECT 1
+    FROM concept_synonym_stage css
+    WHERE css.synonym_concept_code = sub.synonym_name
+    AND css.synonym_name = sub.synonym_name
+    AND css.synonym_vocabulary_id = sub.vocabulary_id_1
+    AND css.language_concept_id = sub.language_concept_id
+)
+AND synonym_name IS NOT NULL;
+
 -- semantic types (may be added to synonyms or used as classificators)
 -- not sure about concept_class. See sty = 'Pharmacologic substance': select * from sources.mrconso where cui = 'C0022230'
-SELECT distinct s.*
+/*SELECT distinct s.*
 from sources.mrsty s
 join sources.mrconso c using(cui)
 where c.sab = 'ORPHANET'
-and c.tty = 'PT';
+and c.tty = 'PT';*/
 
 -- 3. Create hierarchical relationships:
-/*INSERT INTO concept_relationship_stage (
+INSERT INTO concept_relationship_stage (
 	concept_code_1,
 	concept_code_2,
 	relationship_id,
@@ -63,15 +118,15 @@ and c.tty = 'PT';
 	valid_start_date,
 	valid_end_date,
 	invalid_reason
-	)*/
+	)
 SELECT DISTINCT c1.code AS concept_code_1,
 	c2.code AS concept_code_2,
 	'Is a' AS relationship_id,
-	'Orphanet' AS vocabulary_id_1,
-	'Orphanet' AS vocabulary_id_2,
+	'ORPHAcode' AS vocabulary_id_1,
+	'ORPHAcode' AS vocabulary_id_2,
 	(SELECT latest_update
 	 FROM vocabulary
-	 WHERE vocabulary_id = 'Orphanet') AS valid_start_date,
+	 WHERE vocabulary_id = 'ORPHAcode') AS valid_start_date,
 	TO_DATE('20991231', 'YYYYMMDD') AS valid_end_date,
 	NULL AS invalid_reason
 from sources.mrrel r
@@ -81,9 +136,13 @@ where r.sab = 'ORPHANET'
 and rela = 'isa'
 and c1.sab = 'ORPHANET'
 and c2.sab = 'ORPHANET'
+and c1.tty = 'PT'
+and c2.tty = 'PT'
+and c1.code != c2.code
+and c2.ts = 'P'
 ;
 
--- 4. Add mappings to SNOMED:
+-- 4. Add mappings to SNOMED: --!!currently postponed!!
 /*INSERT INTO concept_relationship_stage (
 	concept_code_1,
 	concept_code_2,
@@ -93,7 +152,7 @@ and c2.sab = 'ORPHANET'
 	valid_start_date,
 	valid_end_date,
 	invalid_reason
-	)*/
+	)
 
 WITH umls_map AS
     (SELECT DISTINCT c.code AS concept_code_1,
@@ -117,11 +176,11 @@ SELECT DISTINCT concept_code_1,
                /* source_name,
                 target_name,*/
                 'Maps to' AS relationship_id,
-                'Orphanet' AS vocabulary_id_1,
+                'ORPHAcode' AS vocabulary_id_1,
 	            'SNOMED' AS vocabulary_id_2,
                /* (SELECT latest_update
                  FROM vocabulary
-                 WHERE vocabulary_id = 'Orphanet') AS valid_start_date,*/
+                 WHERE vocabulary_id = 'ORPHAcode') AS valid_start_date,*/
 	            TO_DATE('20991231', 'YYYYMMDD') AS valid_end_date,
 	            NULL AS invalid_reason
 FROM umls_map
@@ -148,7 +207,7 @@ WITH umls_map AS
     AND cc.lat = 'ENG')
 SELECT DISTINCT source_code_description,
         concept_code_1,
-        'Orphanet' as vocabulary_id,
+        'ORPHAcode' as vocabulary_id,
 		'Maps to' as relationship_id,
         'UMLS' as source,
         cc.concept_id,
@@ -187,14 +246,14 @@ and cc.sab = 'SNOMEDCT_US'
 with tab as (
 SELECT DISTINCT vocabulary_pack.CutConceptName(str) AS concept_name,
                 'Condition' AS domain_id,
-                'Orphanet' AS vocabulary_id,
+                'ORPHAcode' AS vocabulary_id,
                 '??' AS concept_class_id,
                 NULL AS standard_concept,
                 m.code as concept_code,
              /*   (
                 SELECT latest_update
                 FROM vocabulary
-                WHERE vocabulary_id = 'Orphanet'
+                WHERE vocabulary_id = 'ORPHAcode'
                 ) AS valid_start_date,*/
                 TO_DATE('20991231', 'YYYYMMDD') AS valid_end_date,
 	            NULL AS invalid_reason
@@ -234,4 +293,50 @@ group by t.concept_name, t.concept_code, cr.relationship_id, cc.concept_id, cc.c
 )
 
 select *
-from tab2 where rating_in_section = 1;
+from tab2 where rating_in_section = 1;*/
+
+--16. Add everything from the Manual tables
+--Working with manual concepts
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.ProcessManualConcepts();
+END $_$;
+
+--Working with manual synonyms
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.ProcessManualSynonyms();
+END $_$;
+
+--Working with manual mappings
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.ProcessManualRelationships();
+END $_$;
+
+--Working with replacement mappings
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.CheckReplacementMappings();
+END $_$;
+
+--Add mapping from deprecated to fresh concepts
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.AddFreshMAPSTO();
+	PERFORM VOCABULARY_PACK.AddFreshMapsToValue();
+END $_$;
+
+--Deprecate 'Maps to' mappings to deprecated and upgraded concepts
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.DeprecateWrongMAPSTO();
+END $_$;
+
+--Delete ambiguous 'Maps to' mappings
+DO $_$
+BEGIN
+	PERFORM VOCABULARY_PACK.DeleteAmbiguousMAPSTO();
+END $_$;
+
+-- At the end, the concept_stage, concept_relationship_stage and concept_synonym_stage tables are ready to be fed into the generic_update script

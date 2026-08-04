1d0f0c3fb259f5d843ef53aa1109e114092a6532
m-khitrun <maria_khitrun@epam.com>
2026-08-04 11:52:42 +0300

    manual changes added

---
+++ working/manual_changes/2026/2026-08-04.sql
@@ -0,0 +1,19 @@
+-- Add Pregnancy to the Episode vocabulary:
+DO $_$
+BEGIN
+  PERFORM vocabulary_pack.AddNewConcept(
+    pConcept_name     =>'Pregnancy',
+    pDomain_id        =>'Episode',
+    pVocabulary_id    =>'Episode',
+    pConcept_class_id =>'Episode of Care',
+    pStandard_concept =>'S'
+);
+END $_$;
+
+-- Erase duplicative ICDO3 concept with non-trimmed concept_code:
+UPDATE concept
+SET concept_code = gen_random_uuid(),
+    concept_name = 'Invalid ICDO3 concept, do not use'
+WHERE concept_id = 36402927;
+
+
